218ee2c2c3627aa52c810e7a6ef2da7000a613b9
Yuri Vysotskiy <profy.net@gmail.com>
2013-06-07 22:23:40 +0300

    unindented some data files

--- share/installer_chat_tables_datas.php
+++ share/installer_chat_tables_datas.php
@@ -1,12 +1,12 @@
 <?php
-	// YF_CHAT datas
-	$data = my_array_merge((array)$data, array(
-		"chat_rooms"	=> array(
-			99999 => array(
-				"id"	=> 1,
-				"name"	=> "main",
-				"active"=> 1,
-			),
+// YF_CHAT datas
+$data = my_array_merge((array)$data, array(
+	"chat_rooms"	=> array(
+		99999 => array(
+			"id"	=> 1,
+			"name"	=> "main",
+			"active"=> 1,
 		),
-	));
+	),
+));
 ?>
