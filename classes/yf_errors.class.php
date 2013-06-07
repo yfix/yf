7429db207887f40ec086e978867ab4b5bb50376e
Yuri Vysotskiy <profy.net@gmail.com>
2013-06-07 23:19:43 +0300

    set_exception_handler details improved: added trace and other details

--- classes/yf_errors.class.php
+++ classes/yf_errors.class.php
@@ -91,7 +91,7 @@ class yf_errors {
 		set_error_handler(array($this, 'ERROR_HANDLER'), $this->NO_NOTICES ? E_ALL ^ E_NOTICE : E_ALL);
 		register_shutdown_function(array($this, 'error_handler_destructor'));
 		
-		set_exception_handler(array($this,  'EXCEPTION_HANDLER' ));
+		set_exception_handler(array($this,  'exception_handler' ));
 	}
 
 	/**
@@ -118,8 +118,52 @@ class yf_errors {
 		}
 	} 
 
-	function EXCEPTION_HANDLER($exception) {
-		echo "Uncaught exception: " , $exception->getMessage(), "\n";
+	function exception_handler($exception) {
+		// these are our templates
+		$traceline = "#%s %s(%s): %s(%s)";
+		$msg = "PHP Fatal error:  Uncaught exception '%s' with message '%s' in %s:%s\nStack trace:\n%s\n  thrown in %s on line %s";
+
+		// alter your trace as you please, here
+		$trace = $exception->getTrace();
+		foreach ($trace as $key => $stackPoint) {
+			// I'm converting arguments to their type
+			// (prevents passwords from ever getting logged as anything other than 'string')
+			$trace[$key]['args'] = array_map('gettype', $trace[$key]['args']);
+		}
+
+		// build your tracelines
+		$result = array();
+		foreach ($trace as $key => $stackPoint) {
+			$result[] = sprintf(
+				$traceline,
+				$key,
+				$stackPoint['file'],
+				$stackPoint['line'],
+				$stackPoint['function'],
+				implode(', ', $stackPoint['args'])
+			);
+		}
+		// trace always ends with {main}
+		$result[] = '#' . ++$key . ' {main}';
+
+		// write tracelines into main template
+		$msg = sprintf(
+			$msg,
+			get_class($exception),
+			$exception->getMessage(),
+			$exception->getFile(),
+			$exception->getLine(),
+			implode("\n", $result),
+			$exception->getFile(),
+			$exception->getLine()
+		);
+
+		// log or echo as you please
+		error_log($msg);
+
+		if (DEBUG_MODE) {
+			echo "<pre>".$msg."</pre>";
+		}
 	}
 
 	/**
