e40f0d443d340de8f106f2f5d4690d5295869061
Yuri Vysotskiy <profy.net@gmail.com>
2013-06-11 17:29:30 +0300

    added install v2 initial html

---
+++ .dev/__INSTALL2/install.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>YF Installation</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
+</head>
+<body>
+	<div class="navbar">
+		<div class="navbar-inner">
+			<a class="brand" href="https://github.com/yfix/yf">YF Framework</a>
+			<ul class="nav">
+				<li class=""><a href="https://github.com/yfix/yf">Home</a></li>
+				<li class=""><a href="./customize.html">Customize</a></li>
+			</ul>
+		</div>
+	</div>
+	<header>
+		<div class="container">
+			<p class="lead">Fundamental HTML elements styled and enhanced with extensible classes.</p>
+		</div>
+	</header>
+	<div class="container">
+		<form class="form-horizontal">
+			<div class="control-group">
+				<label class="control-label" for="input_yf_path">Filesystem path to YF</label>
+				<div class="controls"><input type="text" id="input_yf_path" placeholder="Filesystem path to YF"></div>
+			</div>
+			<div class="control-group">
+				<label class="control-label" for="inputPassword">Password</label>
+				<div class="controls"><input type="password" id="inputPassword" placeholder="Password"></div>
+			</div>
+			<div class="control-group">
+				<div class="controls">
+					<label class="checkbox"><input type="checkbox"> Remember me</label>
+					<button type="submit" class="btn">Sign in</button>
+				</div>
+			</div>
+		</form>
+	</div>
+	<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
+</body>
+</html>
