d589a7795bf285e8985541d0cb937c4477c86eae
alexvillin <alexvillin@yandex.ua>
2013-07-18 13:23:20 +0300

    more admin design

--- priority2/admin_modules/yf_log_emails_viewer.class.php
+++ priority2/admin_modules/yf_log_emails_viewer.class.php
@@ -315,6 +315,9 @@ class yf_log_emails_viewer {
 	//-----------------------------------------------------------------------------
 	// Session - based filter
 	function _show_filter () {
+		if($_GET["action"] != "show"){
+			return false;
+		}
 		$replace = array(
 			"save_action"	=> "./?object=".$_GET["object"]."&action=save_filter"._add_get(),
 			"clear_url"		=> "./?object=".$_GET["object"]."&action=clear_filter"._add_get(),
