e646af7f96770e6176ec8f686beca0836062d2e2
Yuri Vysotskiy <profy.net@gmail.com>
2013-09-17 13:28:31 +0300

    dev: script for small images up

--- .dev/__SCRIPTS/create_missing_small_images.php
+++ .dev/__SCRIPTS/create_missing_small_images.php
@@ -4,6 +4,7 @@
 # wget https://launchpad.net/~ondrej/+archive/php5/+files/php5-imagick_3.1.0%7Erc2-1%7Eprecise%2B1_i386.deb
 # dpkg -i php5-imagick_3.1.0~rc2-1~precise+1_i386.deb 
 
+$force_resize = true;
 $max_x = 100;
 $max_y = 100;
 $method = 'imagick_ext';
@@ -13,7 +14,7 @@ if (!class_exists('Imagick')) {
 
 function _resize($f = '', $s = '') {
 	global $max_x, $max_y, $method;
-	if (!$f || (is_array($f) && !$s)) {
+	if (!$f || (!is_array($f) && !$s)) {
 		return false;
 	}
 	if (!is_array($f)) {
@@ -23,7 +24,8 @@ function _resize($f = '', $s = '') {
 		$ts = microtime(true);
 		if ($method == 'imagick_ext') {
 			$im = new Imagick($_f);
-			$im->adaptiveResizeImage($max_x, $max_y);
+#			$im->adaptiveResizeImage($max_x, $max_y, $bestfit = true);
+			$im->thumbnailImage($max_x, $max_y, $bestfit = true);
 			$im->writeImage($_s);
 			$cmd = $_f.' -> '.$_s;
 		} elseif ($method == 'convert') {
@@ -38,10 +40,18 @@ foreach(glob('./*/*/') as $dir) {
 	$to_resize = array();
 	foreach(glob($dir.'product_*_full.jpg') as $img_full) {
 		$img_small = str_replace('_full.jpg', '_small.jpg', $img_full);
-		if (file_exists($img_small) || !filesize($img_full)) {
+		if (!filesize($img_full)) {
+			continue;
+		}
+		if (file_exists($img_small) && !$force_resize) {
 			continue;
 		}
 		$to_resize[$img_full] = $img_small;
 	}
 	_resize($to_resize);
 }
+
+# HINT: to remove all small images:
+# find . -type f -name '*small*' | xargs rm -v
+# _OR_ (slightly faster):
+# find . -type f -name '*small*' -exec rm {} \;
