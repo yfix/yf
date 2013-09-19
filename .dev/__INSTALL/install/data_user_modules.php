5bf0b36921fb5a3afba13753dc7660fa82448822
Yuri Vysotskiy <profy.net@gmail.com>
2013-09-19 23:42:01 +0300

    install up

--- .dev/__INSTALL/install/data_user_modules.php
+++ .dev/__INSTALL/install/data_user_modules.php
@@ -1,240 +1,52 @@
 <?php
 
-$GLOBALS['INSTALL']["data_user_modules"] = array(
-	array(
-		"id"	=> 1,
-		"name"	=> "home_page",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 2,
-		"name"	=> "login_form",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 3,
-		"name"	=> "static_pages",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 7,
-		"name"	=> "account",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 8,
-		"name"	=> "activity",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 9,
-		"name"	=> "articles",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 10,
-		"name"	=> "blog",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 11,
-		"name"	=> "chat",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 12,
-		"name"	=> "comments",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 13,
-		"name"	=> "dynamic",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 14,
-		"name"	=> "faq",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 15,
-		"name"	=> "forum",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 16,
-		"name"	=> "friends",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 17,
-		"name"	=> "gallery",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 18,
-		"name"	=> "geo_content",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 19,
-		"name"	=> "get_pswd",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 20,
-		"name"	=> "help",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 21,
-		"name"	=> "interests",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 22,
-		"name"	=> "language",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 23,
-		"name"	=> "mail",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 24,
-		"name"	=> "photo_rating",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 25,
-		"name"	=> "poll",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 26,
-		"name"	=> "rate",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 27,
-		"name"	=> "register",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 28,
-		"name"	=> "reputation",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 29,
-		"name"	=> "site_map",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 30,
-		"name"	=> "site_nav_bar",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 31,
-		"name"	=> "stats",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 32,
-		"name"	=> "task_loader",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 33,
-		"name"	=> "test",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 34,
-		"name"	=> "user_info",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 35,
-		"name"	=> "user_profile",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 39,
-		"name"	=> "banner_rotator",
-		"active"=> "0"
-	),
-	array(
-		"id"	=> 40,
-		"name"	=> "calendar",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 41,
-		"name"	=> "community",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 42,
-		"name"	=> "email",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 43,
-		"name"	=> "layout_settings",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 44,
-		"name"	=> "news",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 45,
-		"name"	=> "openid",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 46,
-		"name"	=> "right_block",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 47,
-		"name"	=> "search",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 48,
-		"name"	=> "tags",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 49,
-		"name"	=> "users",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 50,
-		"name"	=> "users_search",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 52,
-		"name"	=> "widgets",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 53,
-		"name"	=> "rss",
-		"active"=> "1"
-	),
-	array(
-		"id"	=> 54,
-		"name"	=> "shop",
-		"active"=> "1"
-	),
+$GLOBALS['INSTALL']['data_user_modules'] = array(
+	array('id' => 1, 'active'=> '1','name'	=> 'home_page'),
+	array('id' => 2, 'active'=> '1','name'	=> 'login_form'),
+	array('id' => 3, 'active'=> '1','name'	=> 'static_pages'),
+	array('id' => 7, 'active'=> '1','name'	=> 'account'),
+	array('id' => 8, 'active'=> '1','name'	=> 'activity'),
+	array('id' => 9, 'active'=> '1','name'	=> 'articles'),
+	array('id' => 10, 'active'=> '1','name'	=> 'blog'),
+	array('id' => 11, 'active'=> '1','name'	=> 'chat'),
+	array('id' => 12, 'active'=> '1','name'	=> 'comments'),
+	array('id' => 13, 'active'=> '1','name'	=> 'dynamic'),
+	array('id' => 14, 'active'=> '1','name'	=> 'faq'),
+	array('id' => 15, 'active'=> '1','name'	=> 'forum'),
+	array('id' => 16, 'active'=> '1','name'	=> 'friends'),
+	array('id' => 17, 'active'=> '1','name'	=> 'gallery'),
+	array('id' => 18, 'active'=> '1','name'	=> 'geo_content'),
+	array('id' => 19, 'active'=> '1','name'	=> 'get_pswd'),
+	array('id' => 20, 'active'=> '1','name'	=> 'help'),
+	array('id' => 21, 'active'=> '1','name'	=> 'interests'),
+	array('id' => 22, 'active'=> '1','name'	=> 'language'),
+	array('id' => 23, 'active'=> '1','name'	=> 'mail'),
+	array('id' => 24, 'active'=> '1','name'	=> 'photo_rating'),
+	array('id' => 25, 'active'=> '1','name'	=> 'poll'),
+	array('id' => 26, 'active'=> '1','name'	=> 'rate'),
+	array('id' => 27, 'active'=> '1','name'	=> 'register'),
+	array('id' => 28, 'active'=> '1','name'	=> 'reputation'),
+	array('id' => 29, 'active'=> '1','name'	=> 'site_map'),
+	array('id' => 30, 'active'=> '1','name'	=> 'site_nav_bar'),
+	array('id' => 31, 'active'=> '1','name'	=> 'stats'),
+	array('id' => 32, 'active'=> '1','name'	=> 'task_loader'),
+	array('id' => 33, 'active'=> '1','name'	=> 'test'),
+	array('id' => 34, 'active'=> '1','name'	=> 'user_info'),
+	array('id' => 35, 'active'=> '1','name'	=> 'user_profile'),
+	array('id' => 39, 'active'=> '1','name'	=> 'banner_rotator'),
+	array('id' => 40, 'active'=> '1','name'	=> 'calendar'),
+	array('id' => 41, 'active'=> '1','name'	=> 'community'),
+	array('id' => 42, 'active'=> '1','name'	=> 'email'),
+	array('id' => 43, 'active'=> '1','name'	=> 'layout_settings'),
+	array('id' => 44, 'active'=> '1','name'	=> 'news'),
+	array('id' => 45, 'active'=> '1','name'	=> 'openid'),
+	array('id' => 46, 'active'=> '1','name'	=> 'right_block'),
+	array('id' => 47, 'active'=> '1','name'	=> 'search'),
+	array('id' => 48, 'active'=> '1','name'	=> 'tags'),
+	array('id' => 49, 'active'=> '1','name'	=> 'users'),
+	array('id' => 50, 'active'=> '1','name'	=> 'users_search'),
+	array('id' => 52, 'active'=> '1','name'	=> 'widgets'),
+	array('id' => 53, 'active'=> '1','name'	=> 'rss'),
+	array('id' => 54, 'active'=> '1','name'	=> 'shop'),
 );
 ?>
