ae38ed84c5e0899951a4a0b9fdf5bfc5a1f4cadf
Yuri Vysotskiy <profy.net@gmail.com>
2013-09-20 23:43:28 +0300

    admin: user groups up

--- admin_modules/yf_user_groups.class.php
+++ admin_modules/yf_user_groups.class.php
@@ -11,74 +11,41 @@ class yf_user_groups {
 
 	/**
 	*/
-	function _init () {
-		$this->_boxes = array(
-			"active"		=> 'radio_box("active",			$this->_statuses,		$selected, false, 2, "", false)',
-		);
-		$this->_statuses = array(
-			"0" => "<span class='negative'>NO</span>", 
-			"1" => "<span class='positive'>YES</span>",
-		);
-	}
-
-	/**
-	* Show user groups
-	*/
 	function show() {
-		$blocks = main()->get_data("blocks_names");
+		$blocks = main()->get_data('blocks_names');
 		foreach ((array)$blocks as $_id => $_info) {
-			if ($_info["type"] == "user" && $_info["name"] == "center_area") {
+			if ($_info['type'] == 'user' && $_info['name'] == 'center_area') {
 				$block_center_id = $_id;
 				break;
 			}
 		}
-		$Q = db()->query("SELECT * FROM ".db('menus')." WHERE type='user' AND active='1' LIMIT 1");
-		while ($A = db()->fetch_assoc($Q)) {
-			$menu_id = $A["id"];
-		}
-		return common()->table2("SELECT * FROM ".db('user_groups')." ORDER BY id ASC")
-			->text("name")
-			->text("go_after_login")
+		$menu_id = db()->get_one('SELECT id FROM '.db('menus').' WHERE type="user" AND active="1" LIMIT 1');
+		return table('SELECT * FROM '.db('user_groups').' ORDER BY id ASC')
+			->text('name')
+			->text('go_after_login')
 			->btn_edit()
 			->btn_delete()
 			->btn_active()
 			->footer_add()
-			->footer_link("Blocks", "./?object=blocks&action=show_rules&id=".$block_center_id)
-			->footer_link("Menu", "./?object=menus_editor&action=show_items&id=".$menu_id);
+			->footer_link('Blocks', './?object=blocks&action=show_rules&id='.$block_center_id)
+			->footer_link('Menu', './?object=menus_editor&action=show_items&id='.$menu_id);
 	}
 
 	/**
 	*/
 	function add() {
-		if (!empty($_POST)) {
-			if (empty($_POST["name"])) {
-				_re("Name is empty", "name");
-			}
-			if (!common()->_error_exists()) {
-				db()->INSERT("user_groups", array(
-					"name"			=> _es($_POST["name"]),
-					"active"		=> intval((bool)$_POST["active"]),
-					"go_after_login"=> _es($_POST["go_after_login"]),
-				));
+		$a = $_POST;
+		$a['redirect_link'] = './?object='.$_GET['object'];
+		return form($a, array('autocomplete' => 'off'))
+			->validate(array(
+				'name' => 'trim|required|alpha_numeric|is_unique[admin_groups.name]'
+			))
+			->db_insert_if_ok('user_groups', array('name','go_after_login','active'), array(), array('on_after_update' => function() {
+				cache()->refresh(array('user_groups', 'user_groups_details'));
 				common()->admin_wall_add(array('user group added: '.$_POST['name'].'', db()->insert_id()));
-				if (main()->USE_SYSTEM_CACHE) {
-					cache()->refresh("user_groups");
-					cache()->refresh("user_groups_details");
-				}
-				return js_redirect("./?object=".$_GET["object"]);
-			}
-		}
-		$replace = array(
-			"form_action"		=> "./?object=".$_GET["object"]."&action=".$_GET["action"],
-			"name"				=> _prepare_html($group_info["name"]),
-			"go_after_login"	=> _prepare_html($group_info["go_after_login"]),
-			"active"			=> $group_info["active"],
-			"active_box"		=> $this->_box("active", $group_info["active"]),
-			"back_link"			=> "./?object=".$_GET["object"],
-		);
-		return common()->form2($replace)
-			->text("name","Group name")
-			->text("go_after_login","Url after login")
+			}))
+			->text('name','Group name')
+			->text('go_after_login','Url after login')
 			->active_box()
 			->save_and_back();
 	}
@@ -86,103 +53,67 @@ class yf_user_groups {
 	/**
 	*/
 	function edit() {
-		$_GET['id'] = intval($_GET['id']);
-		if (empty($_GET['id'])) {
-			return _e("No id");
+		$id = intval($_GET['id']);
+		if (!$id) {
+			return _e('No id');
 		}
-		$group_info = db()->query_fetch("SELECT * FROM ".db('user_groups')." WHERE id=".intval($_GET["id"]));
-		if (empty($group_info)) {
-			return _e("No such group");
-		}
-		if (!empty($_POST)) {
-			if (empty($_POST["name"])) {
-				_re("Name is empty", "name");
-			}
-			if (!common()->_error_exists()) {
-				db()->UPDATE("user_groups", array(
-					"name" 			=> _es($_POST["name"]),
-					"go_after_login"=> _es($_POST["go_after_login"]),
-				), "id=".intval($_GET['id']));
-				common()->admin_wall_add(array('user group updated: '.$_POST['name'].'', $_GET['id']));
-				if (main()->USE_SYSTEM_CACHE) {
-					cache()->refresh("user_groups");
-					cache()->refresh("user_groups_details");
-				}
-				return js_redirect("./?object=".$_GET["object"]);
-			}
-		}
-		$replace = array(
-			"form_action"		=> "./?object=".$_GET["object"]."&action=".$_GET["action"]."&id=".$_GET["id"],
-			"name"				=> _prepare_html($group_info["name"]),
-			"go_after_login"	=> _prepare_html($group_info["go_after_login"]),
-			"group_id"			=> intval($group_info["id"]),
-			"active"			=> $group_info["active"],
-			"active_box"		=> $this->_box("active", $group_info["active"]),
-			"back_link"			=> "./?object=".$_GET["object"],
-		);
-		return common()->form2($replace)
-			->text("name","Group name")
-			->text("go_after_login","Url after login")
-			->active_box()
+		$a = db()->query_fetch('SELECT * FROM '.db('user_groups').' WHERE id='.intval($_GET['id']));
+		$a['redirect_link'] = './?object='.$_GET['object'];
+		return form($a, array('autocomplete' => 'off'))
+			->validate(array(
+				'name' => 'trim|required|alpha_numeric|is_unique[admin_groups.name]'
+			))
+			->db_update_if_ok('user_groups', array('name','go_after_login'), 'id='.$id, array('on_after_update' => function() {
+				cache()->refresh(array('user_groups', 'user_groups_details'));
+				common()->admin_wall_add(array('user group edited: '.$_POST['name'].'', $id));
+			}))
+			->text('name','Group name')
+			->text('go_after_login','Url after login')
 			->save_and_back();
 	}
+
 
 	/**
 	*/
 	function delete() {
 		$_GET['id'] = intval($_GET['id']);
 		if (!empty($_GET['id'])) {
-			db()->query("DELETE FROM ".db('user_groups')." WHERE id=".intval($_GET['id'])." LIMIT 1");
+			db()->query('DELETE FROM '.db('user_groups').' WHERE id='.intval($_GET['id']).' LIMIT 1');
 			common()->admin_wall_add(array('user group deleted: '.$_GET['id'].'', $_GET['id']));
 		}
-		if (main()->USE_SYSTEM_CACHE) {
-			cache()->refresh("user_groups");
-			cache()->refresh("user_groups_details");
-		}
-		if ($_POST["ajax_mode"]) {
+		cache()->refresh(array('user_groups', 'user_groups_details'));
+		if ($_POST['ajax_mode']) {
 			main()->NO_GRAPHICS = true;
-			echo $_GET["id"];
+			echo $_GET['id'];
 		} else {
-			return js_redirect("./?object=".$_GET["object"]);
+			return js_redirect('./?object='.$_GET['object']);
 		}
 	}
 
 	/**
 	*/
-	function activity() {
+	function active() {
 		$_GET['id'] = intval($_GET['id']);
 		if (!empty($_GET['id'])) {
-			$group_info = db()->query_fetch("SELECT * FROM ".db('user_groups')." WHERE id=".intval($_GET["id"]));
+			$group_info = db()->query_fetch('SELECT * FROM '.db('user_groups').' WHERE id='.intval($_GET['id']));
 		}
 		if (!empty($group_info)) {
-			db()->UPDATE("user_groups", array(
-				"active"	=> intval(!$group_info["active"]),
-			), "id=".intval($_GET['id']));
+			db()->UPDATE('user_groups', array(
+				'active'	=> intval(!$group_info['active']),
+			), 'id='.intval($_GET['id']));
 			common()->admin_wall_add(array('user group: '.$group_info['name'].' '.($group_info['active'] ? 'inactivated' : 'activated'), $group_info['id']));
 		}
-		if (main()->USE_SYSTEM_CACHE) {
-			cache()->refresh("user_groups");
-			cache()->refresh("user_groups_details");
-		}
-		if ($_POST["ajax_mode"]) {
+		cache()->refresh(array('user_groups', 'user_groups_details'));
+		if ($_POST['ajax_mode']) {
 			main()->NO_GRAPHICS = true;
-			echo ($group_info["active"] ? 0 : 1);
+			echo ($group_info['active'] ? 0 : 1);
 		} else {
-			return js_redirect("./?object=".$_GET["object"]);
+			return js_redirect('./?object='.$_GET['object']);
 		}
 	}
 
 	/**
-	* Process custom box
 	*/
-	function _box ($name = "", $selected = "") {
-		if (empty($name) || empty($this->_boxes[$name])) {
-			return false;
-		} else {
-			return eval("return common()->".$this->_boxes[$name].";");
-		}
-	}
-
 	function _hook_widget__user_groups ($params = array()) {
 // TODO
 	}
