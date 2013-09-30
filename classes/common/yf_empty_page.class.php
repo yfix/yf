31ed711896c915126afb8da02d7879184269fcd0
Yuri Vysotskiy <profy.net@gmail.com>
2013-09-30 17:46:38 +0300

    redirect code cleanup

--- classes/common/yf_empty_page.class.php
+++ classes/common/yf_empty_page.class.php
@@ -12,31 +12,24 @@ class yf_empty_page {
 	/**
 	* Show empty page (useful for popup windows, etc)
 	*/
-	function _show ($text = "", $params = array()) {
-		$output = "";
+	function _show ($text = '', $params = array()) {
+		$output = '';
 		main()->NO_GRAPHICS = true;
-		// Prepare CSS file name
-		$CSS_FILE = !empty($params["css_file"]) ? $params["css_file"] : "style.css";
-		// Process template
+		$CSS_FILE = !empty($params['css_file']) ? $params['css_file'] : 'style.css';
 		$replace = array(
-			"css"			=> "<link rel='stylesheet' type='text/css' href='".WEB_PATH.tpl()->TPL_PATH.$CSS_FILE."'>",
-			"text"			=> $text,
-			"title"			=> $params["title"],
-			"close_button"	=> (int)((bool)$params["close_button"]),
-			"full_width"	=> (int)((bool)$params["full_width"]),
+			'css'			=> '<link rel="stylesheet" type="text/css" href="'.WEB_PATH. tpl()->TPL_PATH. $CSS_FILE.'">',
+			'text'			=> $text,
+			'title'			=> $params['title'],
+			'close_button'	=> (int)((bool)$params['close_button']),
+			'full_width'	=> (int)((bool)$params['full_width']),
 		);
-		// Throw output to user
-		$output .= tpl()->parse("system/empty_page", $replace);
-		// Display debug info
-		if (DEBUG_MODE && common()->EMPTY_PAGE_DEBUG_INFO) {
+		$output .= tpl()->parse('system/empty_page', $replace);
+		if (DEBUG_MODE && common()->EMPTY_PAGE_DEBUG_INFO && !$params['no_debug']) {
 			$output .= common()->_show_execution_time();
 			$output .= common()->show_debug_info();
 		}
-		// Do filter content with registered callbacks
 		$output = tpl()->_apply_output_filters($output);
-		// Send main headers
 		main()->_send_main_headers(strlen($output));
-		// Send content
 		echo $output;
 	}
 }
