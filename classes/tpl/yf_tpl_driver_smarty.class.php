4b7c579a717b2e5c52d67a44934041b8fcbddfb9
Yuri Vysotskiy <profy.net@gmail.com>
2013-10-09 21:55:32 +0300

    smarty driver started working in very basic mode

--- classes/tpl/yf_tpl_driver_smarty.class.php
+++ classes/tpl/yf_tpl_driver_smarty.class.php
@@ -8,7 +8,13 @@ class yf_tpl_driver_smarty {
 	* Constructor
 	*/
 	function _init () {
-// TODO
+		require_once YF_PATH. 'libs/smarty/libs/Smarty.class.php';
+		$smarty = new Smarty();
+		$smarty->setTemplateDir(YF_PATH. tpl()->TPL_PATH);
+#		$smarty->setCompileDir('/web/www.example.com/guestbook/templates_c/');
+#		$smarty->setConfigDir('/web/www.example.com/guestbook/configs/');
+#		$smarty->setCacheDir('/web/www.example.com/guestbook/cache/');
+		$this->smarty = $smarty;
 	}
 
 	/**
@@ -22,6 +28,11 @@ class yf_tpl_driver_smarty {
 	/**
 	*/
 	function parse($name, $replace = array(), $params = array()) {
-// TODO
+		foreach ((array)$replace as $k => $v) {
+			$this->smarty->assign($k, $v);
+		}
+		if ($params['string']) {
+			return $this->smarty->fetch('string:'.$params['string']);
+		}
 	}
 }
