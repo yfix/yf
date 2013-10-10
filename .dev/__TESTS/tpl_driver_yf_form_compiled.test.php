ff72845c72bb31cbe0891719fdf4b4377752367d
Yuri Vysotskiy <profy.net@gmail.com>
2013-10-10 17:39:38 +0300

    unit tests cleanup

--- .dev/__TESTS/tpl_driver_yf_form_compiled.test.php
+++ .dev/__TESTS/tpl_driver_yf_form_compiled.test.php
@@ -5,5 +5,4 @@ $GLOBALS['PROJECT_CONF']['tpl']['COMPILE_TEMPLATES'] = true;
 require_once dirname(__FILE__).'/tpl_driver_yf_form.test.php';
 class tpl_driver_yf_form_compiled_test extends tpl_driver_yf_form_test {
 	public static function tearDownAfterClass() { _class('dir')->delete_dir('./stpls_compiled/', $delete_start_dir = true); }
-#	function tearDown() { _class('dir')->delete_dir('./stpls_compiled/', $delete_start_dir = true); }
 }
