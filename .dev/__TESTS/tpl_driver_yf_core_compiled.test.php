610fc502a8ffd31e9f199f38c796ceef52fa04e1
Yuri Vysotskiy <profy.net@gmail.com>
2013-10-10 16:02:59 +0300

    unit tests up

--- .dev/__TESTS/tpl_driver_yf_core_compiled.test.php
+++ .dev/__TESTS/tpl_driver_yf_core_compiled.test.php
@@ -4,5 +4,6 @@ $GLOBALS['PROJECT_CONF']['tpl']['COMPILE_TEMPLATES'] = true;
 
 require_once dirname(__FILE__).'/tpl_driver_yf_core.test.php';
 class tpl_driver_yf_core_compiled_test extends tpl_driver_yf_core_test {
-	function tearDown() { _class('dir')->delete_dir('./stpls_compiled/', $delete_start_dir = true); }
+	public static function tearDownAfterClass() { _class('dir')->delete_dir('./stpls_compiled/', $delete_start_dir = true); }
+#	function tearDown() { _class('dir')->delete_dir('./stpls_compiled/', $delete_start_dir = true); }
 }
