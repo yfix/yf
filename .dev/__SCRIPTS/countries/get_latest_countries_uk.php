027b2b9a7f6cdedc08cc3af41a0742288e7555dc
Yuri Vysotskiy <profy.net@gmail.com>
2013-10-18 14:32:53 +0300

    scripts cleanup

--- .dev/__SCRIPTS/countries/get_latest_countries_uk.php
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/php
-<?php
-
-$suffix = '_uk';
-$url = 'https://uk.wikipedia.org/wiki/ISO_3166-1';
-$result_file = dirname(__FILE__).'/countries_uk.php';
-
-require dirname(__FILE__).'/get_latest_countries.php';
