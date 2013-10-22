b1994f5cf7d8324be4c041c4bb54473687121c3b
Yuri Vysotskiy <profy.net@gmail.com>
2013-10-22 11:17:14 +0300

    tests up

--- .dev/__TESTS/form2/form2_new_controls.class.php
+++ .dev/__TESTS/form2/form2_new_controls.class.php
@@ -2,77 +2,6 @@
 
 class form2_new_controls {
 	function show() {
-/*
-		return '
-<div class="bfh-selectbox">
-  <input type="hidden" name="selectbox3" value="">
-  <a class="bfh-selectbox-toggle" role="button" data-toggle="bfh-selectbox" href="#">
-    <span class="bfh-selectbox-option bfh-selectbox-medium" data-option="12">Option 12</span>
-    <b class="caret"></b>
-  </a>
-  <div class="bfh-selectbox-options">
-    <input type="text" class="bfh-selectbox-filter">
-    <div role="listbox">
-      <ul role="option">
-        <li><a tabindex="-1" href="#" data-option="1">Option 1</a></li>
-        <li><a tabindex="-1" href="#" data-option="2">Option 2</a></li>
-        <li><a tabindex="-1" href="#" data-option="3">Option 3</a></li>
-        <li><a tabindex="-1" href="#" data-option="4">Option 4</a></li>
-        <li><a tabindex="-1" href="#" data-option="5">Option 5</a></li>
-        <li><a tabindex="-1" href="#" data-option="6">Option 6</a></li>
-        <li><a tabindex="-1" href="#" data-option="7">Option 7</a></li>
-        <li><a tabindex="-1" href="#" data-option="8">Option 8</a></li>
-        <li><a tabindex="-1" href="#" data-option="9">Option 9</a></li>
-        <li><a tabindex="-1" href="#" data-option="10">Option 10</a></li>
-        <li><a tabindex="-1" href="#" data-option="11">Option 11</a></li>
-        <li><a tabindex="-1" href="#" data-option="12">Option 12</a></li>
-        <li><a tabindex="-1" href="#" data-option="13">Option 13</a></li>
-        <li><a tabindex="-1" href="#" data-option="14">Option 14</a></li>
-        <li><a tabindex="-1" href="#" data-option="15">Option 15</a></li>
-      </ul>
-    </div>
-  </div>
-</div>
-		';
-*/
-/*
-		return '
-<div>
-	<span class="bfh-countries" data-country="US" data-flags="true"></span>
-</div>
-
-<div>
-	<i class="bfh-flag-AF"></i>Afghanistan
-</div>
-
-<div class="bfh-selectbox" data-flags="true">
-	<input type="hidden" value="US">
-	<a class="bfh-selectbox-toggle" role="button" data-toggle="bfh-selectbox" href="#">
-		<span class="bfh-selectbox-option bfh-selectbox-medium" data-option=""><i class="icon bfh-flag-US"></i> United States</span>
-		<b class="caret"></b>
-	</a>
-	<div class="bfh-selectbox-options">
-		<input type="text" class="bfh-selectbox-filter">
-		<div role="listbox">
-			<ul role="option">
-				<li><a tabindex="-1" href="#" data-option=""></a></li>
-				<li><a tabindex="-1" href="#" data-option="AF"><i class="icon bfh-flag-AF"></i>Afghanistan</a></li>
-				<li><a tabindex="-1" href="#" data-option="US"><i class="icon bfh-flag-US"></i>United States</a></li>
-			</ul>
-		</div>
-	</div>
-</div>
-		';
-*/
-/*
-		$data = array();
-		$a = db()->get_all('SELECT * FROM '.db('countries').' ORDER BY name ASC');
-		foreach ((array)$a as $v) {
-			$data[$v['code']] = '<i class="bfh-flag-'.$v['code'].'"></i> '. trim($v['name']).' ['.strtoupper($v['code']).']';
-		}
-		return _class('html_controls')->list_box('country', $data, 'US', array());
-*/
-
 		return form($r)
 			->icon_select_box(array('selected' => 'icon-anchor'))
 			->currency_box(array('selected' => 'RUB'))
@@ -91,26 +20,6 @@ class form2_new_controls {
 #			->date_box()
 #			->datetime_box()
 #			->birth_box()
-		;
-
-#		$params = array('no_form' => 1);//, array('css_framework' => 'empty','class' => 'form-inline')
-#		return form($r, $params)
-/*
-			->text('title')
-			->select_box('want', array('val1','val2'))
-			->row_start(array('desc' => 'For a period of'))
-				->number('duration_day', 'day')
-				->number('duration_week', 'week')
-				->number('duration_month', 'month')
-				->number('duration_year', 'year')
-			->row_end()
-			->row_start(array('desc' => 'Interest rate'))
-				->number('percent', array('class' => 'input-small'))
-				->button('per', array('disabled' => 1))
-				->select_box('split', array('val1','val2'))
-			->row_end()
-			->textarea('desc')
-*/
 #			->div_box('testdiv', array('val1','val2'))
 /*
 			->navbar_start()->currency_box()->navbar_end()
@@ -119,7 +28,6 @@ class form2_new_controls {
 			->navbar_start()->country_box()->navbar_end()
 			->navbar_start()->region_box()->navbar_end()
 */
-#			->submit();
-		return $body;
+		;
 	}
 }
---
+++ .dev/__TESTS/form2/form2_validate_sample.class.php
@@ -0,0 +1,52 @@
+<?php
+
+class form2_stacked_sample {
+	function show() {
+		$replace = array(
+			'title'			=> 'title',
+			'amount'		=> '50',
+			'type'			=> common()->select_box('type',array(1,2)),
+			'split_period'	=> common()->select_box('split',array(1,2)),
+			'duration'		=> array(
+				'day'	=> 10,
+				'week'	=> 2,
+				'month'	=> 3,
+				'year'	=> 0,
+			),
+		);
+		$validate_rules = array(
+			'title'         => array('trim|required|xss_clean'),
+			'type'          => array('trim|min_length[4]|max_length[4]|required|xss_clean'),
+			'amount'        => array('trim|required|min_length[1]|max_length[10]|xss_clean|numeric'),
+			'percent'       => array('trim|required|min_length[1]|max_length[4]|xss_clean|numeric'),
+			'split_period'  => array('trim|min_length[1]|max_length[1]|xss_clean|'),
+			'descr'         => array('trim|xss_clean'),
+			'duration'      => 'required_any[duration_*]',
+		);
+		$a = $_POST;		
+		$form_html .= form($a)
+			->validate($validate_rules)
+		//	->db_insert_if_ok('credits', array('group','email','password','first_name','last_name','middle_name'), array('add_date' => time()), array('on_success_text' => 'Your account was created successfully!'))
+			->text('title')
+			->select_box('type', array(1,2), array('desc' => 'I want'))
+            ->money('amount')
+            ->row_start(array('desc' => 'For a period of', 'name' => 'duration'))
+                ->number('duration_day', 'day', array('class' => 'input-small'))
+                ->number('duration_week', 'week', array('class' => 'input-small'))
+                ->number('duration_month', 'month', array('class' => 'input-small'))
+                ->number('duration_year', 'year', array('class' => 'input-small'))
+            ->row_end()
+            ->row_start(array('desc' => 'Interest rate'))
+                ->number('percent', array('class' => 'input-small'))
+                ->button('per', array('disabled' => 1))
+                ->select_box('split_period', array('val1','val2'))
+            ->row_end()
+            ->textarea('desc')
+            ->submit()
+        ;
+        $body .= _e();
+        $body .= $form_html;
+        return  $body ;
+
+	}
+}
