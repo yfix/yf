67554ebe0f0809a893b6221bf4e96b8cce43e0ed
Yuri Vysotskiy <profy.net@gmail.com>
2013-10-22 23:36:57 +0300

    unit tests up

--- .dev/__TESTS/class_form_validate.Test.php
+++ .dev/__TESTS/class_form_validate.Test.php
@@ -2,30 +2,6 @@
 
 require dirname(__FILE__).'/yf_unit_tests_setup.php';
 
-/*
-		$rules_raw = array(
-			'login'		=> array( 'trim|required|min_length[2]|max_length[12]|is_unique[user.login]|xss_clean', function($in){ return module('register')->_login_not_exists($in); } ),
-			'email'		=> array( 'trim|required|valid_email|is_unique[user.email]', function($in){ return module('register')->_email_not_exists($in); } ),
-			'emailconf'	=> 'trim|required|valid_email|matches[email]',
-			'password'	=> 'trim|required', //|md5
-			'pswdconf'	=> 'trim|required|matches[password]', // |md5
-			'captcha'	=> 'trim|captcha',
-		);
-
-	* Examples of validate rules setting:
-	* 	'name1' => 'trim|required',
-	* 	'name2' => array('trim', 'required'),
-	* 	'name3' => array('trim|required', 'other_rule|other_rule2|other_rule3'),
-	* 	'name4' => array('trim|required', function() { return true; } ),
-	* 	'name5' => array('trim', 'required', function() { return true; } ),
-	* 	'__before__' => 'trim',
-	* 	'__after__' => 'some_method2|some_method3',
-
-
-$a = _class('form2')->_validate_rules_cleanup($rules_raw);
-var_export($a);
-
-*/
 class class_form_validate_test extends PHPUnit_Framework_TestCase {
 	public function test_cleanup_10() {
 		$rules_raw = array(
--- .dev/__TESTS/class_utf8.Test.php
+++ .dev/__TESTS/class_utf8.Test.php
@@ -4,25 +4,22 @@ require dirname(__FILE__).'/yf_unit_tests_setup.php';
 
 class class_utf8_test extends PHPUnit_Framework_TestCase {
 	public function test_main() {
+		$str = 'Mutual Friends (пользователь должен быть у Вас в друзьях а Вы у него)';
+		$this->assertEquals(111, strlen($str));
+		$this->assertEquals(69, _strlen($str));
+		$this->assertEquals(26, strlen(_substr($str, 0, 21)));
+		$this->assertEquals(21, _strlen(_substr($str, 0, 21)));
+#		$this->assertNotEquals(strtoupper($str), _strtoupper($str));
+#		$this->assertNotEquals(strtolower($str), _strtolower($str));
 /*
-		$this->assertEquals('', _bbcode('[img]http://www.google.com/intl/en_ALL/images/logo.gif[/img]'));
-
-		$_string = "Mutual Friends (пользователь должен быть у Вас в друзьях а Вы у него)";
-		strlen($_string)
-		_strlen($_string)
-		substr($_string, 0, 21)
-		_substr($_string, 0, 21)
-		strtoupper($_string)
-		_strtoupper($_string)
-		_strtolower($_string)
 		_truncate($_string, 21)
 		_truncate($_string, 21, true)
 		_truncate($_string, 21, '', true)
 
 #		$testcase = array(
-#			"tHe QUIcK bRoWn" => "QUI",
-#			"frànçAIS" => "çAI",
-#			"über-åwesome" => "-åw",
+#			'tHe QUIcK bRoWn' => 'QUI',
+#			'frànçAIS' => 'çAI',
+#			'über-åwesome' => '-åw',
 #		);
 #		foreach ((array)$testcase as $input => $output) {
 #			$body .= "<br />_substr(\"".$input."\", 4, 3) == \""._substr($input, 4, 3)."\", must be: \"".$output."\"\n";
