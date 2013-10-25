807799141ed19fcda777725e6bf33fd6b53ea7c4
Yuri Vysotskiy <profy.net@gmail.com>
2013-10-25 14:11:24 +0300

    home_page: implemented module_safe with caching for whoel resultset

--- modules/yf_home_page.class.php
+++ modules/yf_home_page.class.php
@@ -42,19 +42,16 @@ class yf_home_page extends yf_module {
 		$cache_name = 'home_page_default';
 		$data = cache_get($cache_name);
 		if (!$data) {
-// TODO
-/*
 			$data = array(
-				'newest_users'			=> module('users')->_for_home_page($this->NUM_NEWEST_USERS),
-				'newest_news'			=> module('news')->_for_home_page(array('num_items' => $this->NUM_NEWEST_NEWS)),
-				'newest_forum_posts'	=> module('forum')->_for_home_page($this->NUM_NEWEST_FORUM_POSTS, $this->NEWEST_FORUM_TEXT_LEN),
-				'newest_blog_posts'		=> module('blog')->_for_home_page($this->NUM_NEWEST_BLOG_POSTS, $this->NEWEST_BLOG_TEXT_LEN),
-				'newest_gallery_photo'	=> module('gallery')->_for_home_page($this->NUM_NEWEST_GALLERY_PHOTO),
-				'newest_article_post'	=> module('articles')->_for_home_page($this->NUM_NEWEST_ARTICLE_POST),
-				'newest_comments'		=> module('comments')->_for_home_page($this->NUM_NEWEST_COMMENTS),
+				'newest_users'			=> module_safe('users')->_for_home_page($this->NUM_NEWEST_USERS),
+				'newest_news'			=> module_safe('news')->_for_home_page(array('num_items' => $this->NUM_NEWEST_NEWS)),
+				'newest_forum_posts'	=> module_safe('forum')->_for_home_page($this->NUM_NEWEST_FORUM_POSTS, $this->NEWEST_FORUM_TEXT_LEN),
+				'newest_blog_posts'		=> module_safe('blog')->_for_home_page($this->NUM_NEWEST_BLOG_POSTS, $this->NEWEST_BLOG_TEXT_LEN),
+				'newest_gallery_photo'	=> module_safe('gallery')->_for_home_page($this->NUM_NEWEST_GALLERY_PHOTO),
+				'newest_article_post'	=> module_safe('articles')->_for_home_page($this->NUM_NEWEST_ARTICLE_POST),
+				'newest_comments'		=> module_safe('comments')->_for_home_page($this->NUM_NEWEST_COMMENTS),
 			);
-*/
-#			cache_set($cache_name, $data);
+			cache_set($cache_name, $data);
 		}
 		$replace = &$data;
 		return tpl()->parse($_GET['object'].'/main', $replace);
