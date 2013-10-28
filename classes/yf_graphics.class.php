0de338903c5d172b62303a0483f6c5d93b2c96d4
Yuri Vysotskiy <profy.net@gmail.com>
2013-10-28 14:43:45 +0200

    moved several modules into .dev/__UNSTABLE/ to not confuse with its current status

--- classes/yf_graphics.class.php
+++ classes/yf_graphics.class.php
@@ -297,18 +297,49 @@ class yf_graphics {
 	* Welcome message method
 	*/
 	function show_welcome () {
-		return _class('graphics_welcome', $this->SUB_MODULES_PATH)->_show_welcome();
+		$admin_id	= (int)$_SESSION['admin_id'];
+		$admin_group= (int)$_SESSION['admin_group'];
+		$user_id	= (int)main()->USER_ID;
+		$user_group	= (int)main()->ADMIN_ID;
+		$login_time = MAIN_TYPE_ADMIN ? $_SESSION['admin_login_time'] : $_SESSION['user_login_time'];
+		// For authorized admins only
+		if (MAIN_TYPE_ADMIN && $admin_id && $admin_group) {
+			$admin_info		= db()->query_fetch('SELECT * FROM '.db('admin').' WHERE id='.$admin_id);
+			$admin_groups	= main()->get_data('admin_groups');
+
+			$body .= tpl()->parse('system/admin_welcome', array(
+				'id'		=> intval($admin_id),
+				'name'		=> _prepare_html($admin_info['first_name'].' '.$admin_info['last_name']),
+				'group'		=> _prepare_html(t($admin_groups[$admin_group])),
+				'time'		=> _format_date($login_time),
+				'edit_link'	=> './?object=admin_account',
+			));
+		// For authorized users only
+		} elseif (MAIN_TYPE_USER && $user_id && $user_group) {
+			$user_info 		= user($user_id);
+			$user_groups	= main()->get_data('user_groups');
+
+			$body .= tpl()->parse('system/user_welcome', array(
+				'id'	=> intval($user_info['id']),
+				'name'	=> _prepare_html(_display_name($user_info)),
+				'group'	=> _prepare_html(t($user_groups[$user_group])),
+				'time'	=> _format_date($login_time),
+			));
+		}
+		return $body;
 	}
 
 	/**
 	* Welcome message for the admin section
 	*/
-	function _show_welcome2 () {
-		return _class('graphics_welcome', $this->SUB_MODULES_PATH)->_show_welcome2();
+	function show_welcome2 () {
+		if (MAIN_TYPE_ADMIN) {
+			$body = t('You logged in as %user at %date', array('%date' => date('H:i:s', $_SESSION['admin_login_time']), '%user' => t('admin')));
+		}
+		return $body;
 	}
 
 	/**
-	* Show SE Keywords
 	*/
 	function _show_se_keywords ($input = '') {
 		if (!$this->USE_SE_KEYWORDS) {
@@ -318,7 +349,6 @@ class yf_graphics {
 	}
 
 	/**
-	* Set SE Keywords
 	*/
 	function _set_se_keywords () {
 		if (!$this->USE_SE_KEYWORDS) {
