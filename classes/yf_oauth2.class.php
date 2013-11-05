833b5d8e46532d9dee59d3dffcfd4e3a7ede471f
Yuri Vysotskiy <profy.net@gmail.com>
2013-11-05 18:16:25 +0200

    oauth2 up

--- classes/yf_oauth2.class.php
+++ classes/yf_oauth2.class.php
@@ -254,24 +254,20 @@ class yf_oauth2 {
 	/**
 	*/
 	function initialize($provider) {
-		require('/home/www/oauth-api/.dev/config.php');
-		require('/home/www/oauth-api/http/http.php');
-		require('/home/www/oauth-api/oauth/oauth_client.php');
+		require(PROJECT_PATH. '.dev/config.php');
+		require(YF_PATH. 'libs/oauth-api/http/http.php');
+		require(YF_PATH. 'libs/oauth-api/oauth/oauth_client.php');
 
 		$client = new oauth_client_class();
 		$client->debug = true;
 		$client->debug_http = true;
-		$client->server = 'Bitbucket';
-#		$client->redirect_uri = 'http://'.$_SERVER['HTTP_HOST'].dirname(strtok($_SERVER['REQUEST_URI'],'?')).'/login_with_bitbucket.php';
-		$client->redirect_uri = WEB_PATH.'?object='.$_GET['object'].'&action='.$_GET['action'].'&id='.$_GET['id'];
-		$client->client_id = $config['bitbucket']['client_id'] ?: ''; $application_line = __LINE__;
+		$client->server = $provider;
+		$client->redirect_uri = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'].'?object='.$_GET['object'].'&action='.$_GET['action'].'&id='.$_GET['id'];
+		$client->client_id = $config[$provider]['client_id'] ?: ''; $application_line = __LINE__;
 		$client->client_secret = $config['bitbucket']['client_secret'] ?: '';
-		if(strlen($client->client_id) == 0 || strlen($client->client_secret) == 0)
-			die('Please go to Bitbucket page to Manage Account '.
-				'https://bitbucket.org/account/ , click on Integrated Applications, '.
-				'then Add Consumer, and in the line '.$application_line.
-				' set the client_id with Key and client_secret with Secret. '.
-				'The URL must be '.$client->redirect_uri);
+		if (strlen($client->client_id) == 0 || strlen($client->client_secret) == 0) {
+			die('Please set the client_id with Key and client_secret with Secret. The URL must be '.$client->redirect_uri);
+		}
 
 		$settings = $this->_providers[$provider] + $this->_providers['__default__'];
 		foreach ($settings as $k => $v) {
@@ -290,7 +286,7 @@ class yf_oauth2 {
 #			exit;
 #		}
 		if($success) {
-			return '<pre>'.print_r($user, 1).'</pre>';
+			return '<h1>Success</h1><pre>'.print_r($user, 1).'</pre>';
 		}
 #		return $this;
 	}
