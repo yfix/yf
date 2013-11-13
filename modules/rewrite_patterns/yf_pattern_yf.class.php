255c8be8b934e11b6a3664419c6d31a31113a00d
Yuri Vysotskiy <profy.net@gmail.com>
2013-11-13 14:35:40 +0200

    main: enabled rewrite again, not adding .html extension to urls

--- modules/rewrite_patterns/yf_pattern_yf.class.php
+++ modules/rewrite_patterns/yf_pattern_yf.class.php
@@ -4,57 +4,60 @@
 * Default YF rewrite pattern
 */
 class yf_pattern_yf {
-	function _get($A) {
-		if ($A['task'] == 'login' || $A['task'] == 'logout') {
-			$u = $A['task'];
-			unset($A['task']);
-		} else{ 
+	function _get($a) {
+		if ($a['task'] == 'login' || $a['task'] == 'logout') {
+			$u = $a['task'];
+			unset($a['task']);
+		} else {
 			$u = array();
-			if (!empty($A['object'])) {
-				$u[] = "{$A['object']}";
-				if (empty($A['action'])) {
-					$A['action'] = 'show';
+			if (!empty($a['object'])) {
+				$u[] = $a['object'];
+				if (empty($a['action'])) {
+					$a['action'] = 'show';
 				}
-				$u[] = "{$A['action']}";
-				if (!empty($A['id'])) {
-					$u[] = "{$A['id']}";
+				if ($a['action'] != 'show') {
+					$u[] = $a['action'];
+				}
+				if (!empty($a['id'])) {
+					$u[] = $a['id'];
 				}
 			}
-			$u = implode("/",$u);
+			$u = implode('/',$u);
 		}
-		$arr = $A;
+		$arr = $a;
 		$arr_out = array();
 		unset($arr['object']);
 		unset($arr['action']);
 		unset($arr['host']);
 		unset($arr['id']);
-		foreach((array)$arr as $k => $v) {
-			$arr_out[] = $k."=".$v;
+		foreach ((array)$arr as $k => $v) {
+			$arr_out[] = $k.'='.$v;
 		}
 		if (!empty($u)) {
-			$u .= ".html";
+#			$u .= '.html';
 		}
 		if (!empty($arr_out)) {
-			$u .= "?".implode("&",$arr_out);
+			$u .= '?'.implode('&',$arr_out);
 		}
-		return module('rewrite')->_correct_protocol("http://{$A[host]}/{$u}");
+#		return module('rewrite')->_correct_protocol('http://'.$a['host'].'/'.$u);
+		return module('rewrite')->_correct_protocol(WEB_PATH. $u);
 	}
 
 	/**
 	*/
 	function _parse ($host, $url, $query) {
-		$s = "";
+		$s = '';
 		if ($url[0] == 'login' || $url[0] == 'logout') {
-			$s = "task={$url[0]}";
+			$s = 'task='.$url[0];
 		} elseif (!empty($url[0]) && !empty($url[1])) {
-			$s= "object={$url[0]}&action={$url[1]}";
+			$s = 'object='.$url[0].'&action='.$url[1];
 		} elseif (!empty($url[0])) {
-			$s= "object={$url[0]}&action=show";			
+			$s = 'object='.$url[0].'&action=show';
 		} else {
-			$s = "object=home_page&action=show";
+			$s = 'object=home_page&action=show';
 		}
 		if (!empty($url[2])) {
-			$s .= "&id={$url[2]}";
+			$s .= '&id='.$url[2];
 		}
 		if ($s != '') {
 			parse_str($s, $arr);
@@ -64,5 +67,4 @@ class yf_pattern_yf {
 		}
 		return $arr;
 	}
-
 }
--- modules/yf_rewrite.class.php
+++ modules/yf_rewrite.class.php
@@ -3,21 +3,21 @@
 class yf_rewrite {
 	
 	/** @var string @conf_skip Links pattern */
-	var	$_links_pattern			= "/(action|location|href|src)[\s]{0,1}=[\s]{0,1}[\"\']?(\.\/\?[^\"\'\>\s]+|\.\/)[\"\']?/ims";
+	var	$_links_pattern			= '/(action|location|href|src)[\s]{0,1}=[\s]{0,1}["\']?(\.\/\?[^"\'\>\s]+|\.\/)["\']?/ims';
 	/** @var string @conf_skip Pattern for iframe links */
-	var	$_iframe_pattern		= "/(action|location|href)[\s]{0,1}=[\s]{0,1}[\"\']+\.\/\?([^\"\'>\s]*)[\"\']+/ims";
+	var	$_iframe_pattern		= '/(action|location|href)[\s]{0,1}=[\s]{0,1}["\']+\.\/\?([^"\'>\s]*)["\']+/ims';
 	
-    /**
-    * YF module constructor
-    */
-    function _init () {
-        $this->REWRITE_PATTERNS = array('yf' => _class("pattern_yf", "modules/rewrite_patterns/"));
-    }
+	/**
+	* YF module constructor
+	*/
+	function _init () {
+		$this->REWRITE_PATTERNS = array('yf' => _class('pattern_yf', 'modules/rewrite_patterns/'));
+	}
 
 	/**
-	* Replace links for mod_rewrite
+	* Replace links for url rewrite
 	*/
-	function _rewrite_replace_links ($body = "", $standalone = false, $force_rewrite = false, $for_site_id = false) {
+	function _rewrite_replace_links ($body = '', $standalone = false, $force_rewrite = false, $for_site_id = false) {
 		// Skip rewriting for the admin section
 		if (MAIN_TYPE_ADMIN && !$force_rewrite) {
 			return $body;
@@ -33,18 +33,25 @@ class yf_rewrite {
 			foreach ($links as $v) {
 				$url = parse_url($v);
 				parse_str($url['query'],$arr);
-				$replace = $this->_force_get_url($arr,$_SERVER["HTTP_HOST"]);
+				$replace = $this->_force_get_url($arr,$_SERVER['HTTP_HOST']);
 				$r_array[$v] = $replace;
 			}
-			// Fix for bug with similar shorter links
-			uksort($r_array, array(&$this, "_sort_by_length"));
+			// Fix for bug with similar shorter links, sort by length DESC
+			uksort($r_array, function ($a, $b) {
+				$sa = strlen($a);
+				$sb = strlen($b);
+				if ($sa == $sb) {
+					return 0;
+				}
+				return ($sa < $sb) ? +1 : -1;
+			});
 			// DO NOT USE strtr() here!!!
 			$body = str_replace(array_keys($r_array), array_values($r_array), $body);
 			// Show debug info if needed
 			if (DEBUG_MODE && !$this->FORCE_NO_DEBUG) {
-				if (empty($GLOBALS["REWRITE_DEBUG"])) $GLOBALS["REWRITE_DEBUG"] = array("SOURCE"=> array(),	"REWRITED"	=> array());
-				$GLOBALS["REWRITE_DEBUG"]["SOURCE"]		= array_merge($GLOBALS["REWRITE_DEBUG"]["SOURCE"],		array_keys($r_array));
-				$GLOBALS["REWRITE_DEBUG"]["REWRITED"]	= array_merge($GLOBALS["REWRITE_DEBUG"]["REWRITED"],	array_values($r_array));
+				if (empty($GLOBALS['REWRITE_DEBUG'])) $GLOBALS['REWRITE_DEBUG'] = array('SOURCE'=> array(),	'REWRITED'	=> array());
+				$GLOBALS['REWRITE_DEBUG']['SOURCE']		= array_merge($GLOBALS['REWRITE_DEBUG']['SOURCE'],		array_keys($r_array));
+				$GLOBALS['REWRITE_DEBUG']['REWRITED']	= array_merge($GLOBALS['REWRITE_DEBUG']['REWRITED'],	array_values($r_array));
 			}
 		}
 		if (DEBUG_MODE && !$this->FORCE_NO_DEBUG) {
@@ -55,77 +62,67 @@ class yf_rewrite {
 		}
 		return $body;
 	}
-	
-	// Fix for bug with similar shorter links
-	function _sort_by_length ($a, $b) {
-		$sa = strlen($a);
-		$sb = strlen($b);
-		if ($sa == $sb) {
-			return 0;
-		}
-		return ($sa < $sb) ? +1 : -1;
-	}
 
-    function _force_get_url ($params = array(), $host = "", $url_str = "", $gen_cache = true) {
+	/**
+	*/
+	function _force_get_url ($params = array(), $host = '', $url_str = '', $gen_cache = true) {
 		$time_start = microtime(true);
-		if(!is_array($params) && empty($url_str)){
+		if (!is_array($params) && empty($url_str)){
 			return false;
 		}
-		if(isset($_GET["debug"]) || isset($_GET["no_cache"]) || isset($_GET["no_core_cache"])){
+		if (isset($_GET['debug']) || isset($_GET['no_cache']) || isset($_GET['no_core_cache'])){
 			$params['debug'] = $_GET['debug'];
-			$params['no_cache'] = isset($_GET["no_cache"]) ? "y" : "";
-			$params['no_core_cache'] = isset($_GET["no_core_cache"]) ? "y" : "";
+			$params['no_cache'] = isset($_GET['no_cache']) ? 'y' : '';
+			$params['no_core_cache'] = isset($_GET['no_core_cache']) ? 'y' : '';
 		}
 
-		if(empty($url_str)){
-			if(isset($params["action"]) && empty($params["action"])){
-				$params["action"] = "show";
+		if (empty($url_str)){
+			if (isset($params['action']) && empty($params['action'])){
+				$params['action'] = 'show';
 			}
 		}
-		foreach((array)$params as $k => $v){
-			if(empty($v) && ($v !== '0')){
+		foreach ((array)$params as $k => $v){
+			if (empty($v) && ($v !== '0')){
 				unset($params[$k]);
 				continue;
 			}
 		}
 		// patterns support here
-		$params['host'] = !empty($host) ? $host : $_SERVER["HTTP_HOST"];
-		if($GLOBALS['PROJECT_CONF']['tpl']['REWRITE_MODE'] == 1){
+		$params['host'] = !empty($host) ? $host : $_SERVER['HTTP_HOST'];
+		if ($GLOBALS['PROJECT_CONF']['tpl']['REWRITE_MODE'] == 1){
 			$link = $this->REWRITE_PATTERNS['yf']->_get($params);
-		}else{
-			foreach((array)$params as $k=>$v) {
-				if($k == 'host') continue;
-				$arr_out[] = $k."=".$v;
+		} else {
+			foreach ((array)$params as $k=>$v) {
+				if ($k == 'host') continue;
+				$arr_out[] = $k.'='.$v;
 			}
-			if (!empty($arr_out)) $u .= "?".implode("&",$arr_out);
-			$link = $this->_correct_protocol("http://{$params[host]}/{$u}");
+			if (!empty($arr_out)) $u .= '?'.implode('&',$arr_out);
+			$link = $this->_correct_protocol('http://{$params[host]}/{$u}');
 		}
 		return $link;
-    }
-
-    /**
-     *
-    **/
-    function _correct_protocol($url){
-        if(empty($url)){
-            Return false;
-        }
-        if(empty(main()->HTTPS_ENABLED_FOR)){
-            Return $url;
-        }
-        // Return links to the http protocol
-        if (substr($url, 0, 8) == "https://" && !main()->USE_ONLY_HTTPS) {
-            $url = str_replace("https://", "http://", $url);
-        } else {
-            $url = str_replace("http://", "https://", $url);
-        }
-        Return $url;
-    }
+	}
 
 	/**
-	* Get all ProEngine links
 	*/
-	function _get_unique_links ($text = "", $for_iframe = false) {
+	function _correct_protocol($url){
+		if (empty($url)){
+			return false;
+		}
+		if (empty(main()->HTTPS_ENABLED_FOR)){
+			return $url;
+		}
+		// Return links to the http protocol
+		if (substr($url, 0, 8) == 'https://' && !main()->USE_ONLY_HTTPS) {
+			$url = str_replace('https://', 'http://', $url);
+		} else {
+			$url = str_replace('http://', 'https://', $url);
+		}
+		return $url;
+	}
+
+	/**
+	*/
+	function _get_unique_links ($text = '', $for_iframe = false) {
 		$unique = array();
 		preg_match_all($for_iframe ? $this->_iframe_pattern : $this->_links_pattern, $text, $matches);
 		foreach ((array)$matches['2'] as $k => $v) {
@@ -135,6 +132,4 @@ class yf_rewrite {
 		}
 		return $unique;
 	}
-	
 }
-
