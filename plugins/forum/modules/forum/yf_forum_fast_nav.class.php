e1debd3ae1278113a64050f929c42c49b4491f5d
Yuri Vysotskiy <profy.net@gmail.com>
2013-11-14 11:50:32 +0200

    plugin forum: code cleanup and bugfixes

--- plugins/forum/modules/forum/yf_forum_fast_nav.class.php
+++ plugins/forum/modules/forum/yf_forum_fast_nav.class.php
@@ -10,9 +10,9 @@
 class yf_forum_fast_nav {
 
 	/** @var string */
-	public $_text_forum_divider	= "&nbsp;&nbsp;&#0124;-- ";
+	public $_text_forum_divider	= '&nbsp;&nbsp;&#0124;-- ';
 	/** @var string */
-	public $_text_level_filler		= "&nbsp;&nbsp;&nbsp;&nbsp;";
+	public $_text_level_filler		= '&nbsp;&nbsp;&nbsp;&nbsp;';
 	
 	/**
 	* Board Fast Navigation Box
@@ -20,40 +20,41 @@ class yf_forum_fast_nav {
 	function _board_fast_nav_box() {
 		// Create site jump array
 		$site_jump_array = array(
-			"sj_home"	=> "Forum Home",
-			"sj_search"	=> "Search",
-			"sj_help"	=> "Help",
+			'sj_home'	=> 'Forum Home',
+			'sj_search'	=> 'Search',
+			'sj_help'	=> 'Help',
 		);
 		// Prepare array for processing (to avoid slow manipulations)
 		foreach ((array)module('forum')->_forums_array as $_info) {
 			// Skip non-active forums
-			if ($_info["status"] != "a") {
+			if ($_info['status'] != 'a') {
 				continue;
 			}
 			$cat_info = module('forum')->_forum_cats_array[$_info['category']];
 			// Skip non-active categories
-			if ($cat_info["status"] != "a") {
+			if ($cat_info['status'] != 'a') {
 				break;
 			}
-			$this->_forum_parents[$_info["parent"]][$_info["id"]] = $_info["category"];
-			$this->_forum_cat_names[$_info['category']] = $cat_info["name"];
+			$this->_forum_parents[$_info['parent']][$_info['id']] = $_info['category'];
+			$this->_forum_cat_names[$_info['category']] = $cat_info['name'];
 		}
 		// Create forum jump array
 		$forum_jump_array = $this->_prepare_parents_for_select();
 		// Try to set selected index
-		if ($_GET["action"] == "show" && empty($_GET["id"]))		$selected = "sj_home";
-		elseif ($_GET["action"] == "show" && !empty($_GET["id"]))	$selected = "cat_".$_GET["id"];
-		elseif ($_GET["action"] == "search")						$selected = "sj_search";
-		elseif ($_GET["action"] == "help")							$selected = "sj_help";
-		elseif ($_GET["action"] == "view_forum")					$selected = $_GET["id"];
-		elseif ($_GET["action"] == "view_topic")					$selected = module('forum')->_topic_info["forum"];
-		else														$selected = "sj_home";
+		if ($_GET['action'] == 'show' && empty($_GET['id']))		$selected = 'sj_home';
+		elseif ($_GET['action'] == 'show' && !empty($_GET['id']))	$selected = 'cat_'.$_GET['id'];
+		elseif ($_GET['action'] == 'search')						$selected = 'sj_search';
+		elseif ($_GET['action'] == 'help')							$selected = 'sj_help';
+		elseif ($_GET['action'] == 'view_forum')					$selected = $_GET['id'];
+		elseif ($_GET['action'] == 'view_topic')					$selected = module('forum')->_topic_info['forum'];
+		else														$selected = 'sj_home';
 		// Process main template
 		$replace = array(
-			"form_action"	=> "./?object=".'forum'."&action=site_jump"._add_get(array("page")),
-			"fast_nav_box"	=> common()->select_box("fast_nav", array("Site Jump" => $site_jump_array, "Forum Jump" => $forum_jump_array), $selected, false, 2, " onchange=\"if(this.options[this.selectedIndex].value != -1){ document.jumpmenu.submit() }\"", false),
+			'form_action'	=> './?object='.'forum'.'&action=site_jump'._add_get(array('page')),
+			'fast_nav_box'	=> common()->select_box('fast_nav', array('Site Jump' => $site_jump_array, 'Forum Jump' => $forum_jump_array), $selected, false, 2, 
+				" onchange=\"if(this.options[this.selectedIndex].value != -1){ document.jumpmenu.submit() }\"", false),
 		);
-		return tpl()->parse('forum'."/board_fast_nav", $replace);
+		return tpl()->parse('forum'.'/board_fast_nav', $replace);
 	}
 	
 	/**
@@ -67,12 +68,12 @@ class yf_forum_fast_nav {
 			if ($_forum_id == $skip_id) {
 				continue;
 			}
-			// Add category (with prefix: "c_")
-			if (!isset($forums["c_".$_cat_id])) {
-				$forums["cat_".$_cat_id] = $this->_forum_cat_names[$_cat_id];
+			// Add category (with prefix: 'c_')
+			if (!isset($forums['c_'.$_cat_id])) {
+				$forums['cat_'.$_cat_id] = $this->_forum_cat_names[$_cat_id];
 			}
 			// Add current forum
-			$forums[$_forum_id] = str_repeat($this->_text_level_filler, $level). $this->_text_forum_divider. module('forum')->_forums_array[$_forum_id]["name"];
+			$forums[$_forum_id] = str_repeat($this->_text_level_filler, $level). $this->_text_forum_divider. module('forum')->_forums_array[$_forum_id]['name'];
 			// Try to find sub-forums
 			foreach ((array)$this->$f($skip_id, $_forum_id, $level + 1) as $_sub_id => $_sub_name) {
 				$forums[$_sub_id] = $_sub_name;
--- plugins/forum/modules/forum/yf_forum_integration.class.php
+++ plugins/forum/modules/forum/yf_forum_integration.class.php
@@ -13,61 +13,61 @@ class yf_forum_integration {
 	* Home page integration part
 	*/
 	function _for_home_page($NUM_NEWEST_FORUM_POSTS = 4, $NEWEST_FORUM_TEXT_LEN = 100, $params = array()){
-		$item_stpl_name = $params["for_widgets"] ? "widgets_last_post" : "for_home_page_item";
-		$Q = db()->query("SELECT * FROM ".db('forum_posts')." WHERE status='a' ORDER BY created DESC LIMIT ".intval($NUM_NEWEST_FORUM_POSTS));
+		$item_stpl_name = $params['for_widgets'] ? 'widgets_last_post' : 'for_home_page_item';
+		$Q = db()->query('SELECT * FROM '.db('forum_posts').' WHERE status="a" ORDER BY created DESC LIMIT '.intval($NUM_NEWEST_FORUM_POSTS));
 		while ($A = db()->fetch_assoc($Q)) {
-			$forum_posts[$A["id"]] = $A;	
-			$forum_topics_id[$A["topic"]] = $A["topic"];
+			$forum_posts[$A['id']] = $A;	
+			$forum_topics_id[$A['topic']] = $A['topic'];
 		}
 		if (!empty($forum_topics_id)){
-			$Q = db()->query("SELECT * FROM ".db('forum_topics')." WHERE id IN(".implode(",",array_keys($forum_topics_id)).")");
+			$Q = db()->query('SELECT * FROM '.db('forum_topics').' WHERE id IN('.implode(',',array_keys($forum_topics_id)).')');
 			while ($A = db()->fetch_assoc($Q)) {					
-				$forum_topics[$A["id"]] = $A["name"];				
+				$forum_topics[$A['id']] = $A['name'];				
 			}
 		}
 		foreach ((array)$forum_posts as $A) {						
-			$text = module('forum')->_cut_bb_codes(_prepare_html($A["text"]));
+			$text = module('forum')->_cut_bb_codes(_prepare_html($A['text']));
 
 			if(strlen($text) > $NEWEST_FORUM_TEXT_LEN){											
 				$text = _truncate($text, $NEWEST_FORUM_TEXT_LEN, true, true);
 			}
 			$replace2 = array(
-				"user_id" 		=> intval($A["user_id"]),
-				"user_name" 	=> $A["user_name"],
-				"topic"			=> $forum_topics[$A["topic"]],
-				"text"			=> $text,
-				"user_link"		=> "./?object=user_profile&action=show&id=".$A["user_id"],
-				"topic_link"	=> "./?object=forum&action=view_topic&id=".$A["topic"],
-				"created"		=> _format_date($A["created"], "long"),
+				'user_id' 		=> intval($A['user_id']),
+				'user_name' 	=> $A['user_name'],
+				'topic'			=> $forum_topics[$A['topic']],
+				'text'			=> $text,
+				'user_link'		=> './?object=user_profile&action=show&id='.$A['user_id'],
+				'topic_link'	=> './?object=forum&action=view_topic&id='.$A['topic'],
+				'created'		=> _format_date($A['created'], 'long'),
 			);			
-			$items .= tpl()->parse('forum'."/".$item_stpl_name, $replace2);		
+			$items .= tpl()->parse('forum'.'/'.$item_stpl_name, $replace2);		
 		}
 		if(empty($items)) {
 			return;
 		}
 		$replace = array(
-			"items"	=> $items,
+			'items'	=> $items,
 		);
-		return tpl()->parse('forum'."/for_home_page_main", $replace);
+		return tpl()->parse('forum'.'/for_home_page_main', $replace);
 	}
 
 	/**
 	* User profile integration part
 	*/
 	function _for_user_profile($user_id, $MAX_SHOW_FORUM_POSTS){
-		$sql = "SELECT id,topic,subject,user_id,user_name,created FROM ".db('forum_posts')." WHERE status='a' AND user_id=".intval($user_id);
-		list($add_sql, $pages, $this->_num_forum_posts) = common()->divide_pages($sql, "", null, $MAX_SHOW_FORUM_POSTS);
+		$sql = 'SELECT id,topic,subject,user_id,user_name,created FROM '.db('forum_posts').' WHERE status="a" AND user_id='.intval($user_id);
+		list($add_sql, $pages, $this->_num_forum_posts) = common()->divide_pages($sql, '', null, $MAX_SHOW_FORUM_POSTS);
 		$Q = db()->query($sql. $add_sql);
 		while ($A = db()->fetch_assoc($Q)) {
 			$replace2 = array(
-				"num"			=> ++$i,
-				"title"			=> _prepare_html($A["subject"]),
-				"author"		=> _prepare_html($A["user_name"]),
-				"reply_link"	=> "./?object=forum&action=view_topic&id=".$A["topic"],
-				"created"		=> _format_date($A["created"]),
-				"profile_url"	=> "./?object=".'forum'."&action=show&id=".$A["user_id"],
+				'num'			=> ++$i,
+				'title'			=> _prepare_html($A['subject']),
+				'author'		=> _prepare_html($A['user_name']),
+				'reply_link'	=> './?object=forum&action=view_topic&id='.$A['topic'],
+				'created'		=> _format_date($A['created']),
+				'profile_url'	=> './?object='.'forum'.'&action=show&id='.$A['user_id'],
 			);
-			$items .= tpl()->parse('forum'."/for_profile_forum_item", $replace2);
+			$items .= tpl()->parse('forum'.'/for_profile_forum_item', $replace2);
 		}
 			$value[0] = $items;
 			$value[1] = $pages;
@@ -79,29 +79,29 @@ class yf_forum_integration {
 	*/
 	function _rss_general(){
 	
-		$Q = db()->query("SELECT * FROM ".db('forum_posts')." WHERE status='a' ORDER BY created DESC LIMIT ".intval(module('forum')->NUM_RSS));
+		$Q = db()->query('SELECT * FROM '.db('forum_posts').' WHERE status="a" ORDER BY created DESC LIMIT '.intval(module('forum')->NUM_RSS));
 		while ($A = db()->fetch_assoc($Q)) {
-			$forum_posts[$A["id"]] = $A;	
-			$forum_topics_id[$A["topic"]] = $A["topic"];
+			$forum_posts[$A['id']] = $A;	
+			$forum_topics_id[$A['topic']] = $A['topic'];
 		}
 		if (!empty($forum_topics_id)){
-			$Q = db()->query("SELECT * FROM ".db('forum_topics')." WHERE id IN(".implode(",",array_keys($forum_topics_id)).")");
+			$Q = db()->query('SELECT * FROM '.db('forum_topics').' WHERE id IN('.implode(',',array_keys($forum_topics_id)).')');
 			while ($A = db()->fetch_assoc($Q)) {					
-				$forum_topics[$A["id"]] = $A["name"];				
+				$forum_topics[$A['id']] = $A['name'];				
 			}
 		}
 		
 		if(!empty($forum_posts)){
 			foreach ((array)$forum_posts as $A) {						
-				$text = module('forum')->_cut_bb_codes(nl2br(_prepare_html($A["text"])));
+				$text = module('forum')->_cut_bb_codes(nl2br(_prepare_html($A['text'])));
 
 				$data[] = array(
-					"title"			=> _prepare_html(t("Forum")." - ".$forum_topics[$A["topic"]]),
-					"link"			=> process_url("./?object=forum&action=view_topic&id=".$A["topic"]),
-					"description"	=> $text,
-					"date"			=> $A["created"],
-					"author"		=> $A["user_name"],
-					"source"		=> "",
+					'title'			=> _prepare_html(t('Forum').' - '.$forum_topics[$A['topic']]),
+					'link'			=> process_url('./?object=forum&action=view_topic&id='.$A['topic']),
+					'description'	=> $text,
+					'date'			=> $A['created'],
+					'author'		=> $A['user_name'],
+					'source'		=> '',
 				);
 
 			}
@@ -118,45 +118,45 @@ class yf_forum_integration {
 		}
 		// Main page		
 		$OBJ->_store_item(array(
-			"url"	=> "./?object=forum",
+			'url'	=> './?object=forum',
 		));
 		// Get forums from db and divide each by pages
-		$sql = "SELECT id, name FROM ".db('forum_forums')." ORDER BY id";
+		$sql = 'SELECT id, name FROM '.db('forum_forums').' ORDER BY id';
 		$Q = db()->query($sql);
 		while ($A = db()->fetch_assoc($Q)) {
-			$sql = "SELECT COUNT(id) AS num FROM ".db('forum_topics')." WHERE forum='".$A["id"]."' AND approved=1 AND pinned=0";
+			$sql = 'SELECT COUNT(id) AS num FROM '.db('forum_topics').' WHERE forum='.intval($A['id']).' AND approved=1 AND pinned=0';
 			$B = db()->query_fetch($sql);
-			$total_pages = ceil(intval($B["num"]) / intval(module('forum')->SETTINGS["NUM_TOPICS_ON_PAGE"]));
+			$total_pages = ceil(intval($B['num']) / intval(module('forum')->SETTINGS['NUM_TOPICS_ON_PAGE']));
 			// Process pages
 			if ($total_pages > 1) {
 				for ($i = 1; $i <= $total_pages; $i++) {
 					$OBJ->_store_item(array(
-						"url"	=> "./?object=forum&action=view_forum&id=".str_replace(" ", "_", strtolower($A["name"]))."&page=".$i,
+						'url'	=> './?object=forum&action=view_forum&id='.str_replace(' ', '_', strtolower($A['name'])).'&page='.$i,
 					));
 				}	
 			} else {
 				$OBJ->_store_item(array(
-					"url"	=> "./?object=forum&action=view_forum&id=".str_replace(" ", "_", strtolower($A["name"])),
+					'url'	=> './?object=forum&action=view_forum&id='.str_replace(' ', '_', strtolower($A['name'])),
 				));
 			}
 		}
 		// Get topics from db and divide each by pages
-		$sql = "SELECT id FROM ".db('forum_topics')." ORDER BY id";
+		$sql = 'SELECT id FROM '.db('forum_topics').' ORDER BY id';
 		$Q = db()->query($sql);
 		while ($A = db()->fetch_assoc($Q)) {
-			$sql = "SELECT COUNT(id) AS num FROM ".db('forum_posts')." WHERE topic='".$A["id"]."' AND status='a'";
+			$sql = 'SELECT COUNT(id) AS num FROM '.db('forum_posts').' WHERE topic='.intval($A['id']).' AND status="a"';
 			$B = db()->query_fetch($sql);
-			$total_pages = ceil(intval($B["num"]) / intval(module('forum')->SETTINGS["NUM_POSTS_ON_PAGE"]));
+			$total_pages = ceil(intval($B['num']) / intval(module('forum')->SETTINGS['NUM_POSTS_ON_PAGE']));
 			// Process pages
 			if ($total_pages > 1) {
 				for ($i = 1; $i <= $total_pages; $i++) {
 					$OBJ->_store_item(array(
-						"url"	=> "./?object=forum&action=view_topic&id=".$A["id"]."&page=".$i,
+						'url'	=> './?object=forum&action=view_topic&id='.$A['id'].'&page='.$i,
 					));
 				}	
 			} else {
 				$OBJ->_store_item(array(
-					"url"	=> "./?object=forum&action=view_topic&id=".$A["id"]."&page=".$i,
+					'url'	=> './?object=forum&action=view_topic&id='.$A['id'].'&page='.$i,
 				));
 			}
 		}
@@ -167,26 +167,26 @@ class yf_forum_integration {
 	* Hook for navigation bar
 	*/
 	function _nav_bar_items ($params = array()) {
-		$NAV_BAR_OBJ = &$params["nav_bar_obj"];
+		$NAV_BAR_OBJ = &$params['nav_bar_obj'];
 		if (!is_object($NAV_BAR_OBJ)) {
 			return false;
 		}
 		// Save old items
-		$old_items = $params["items"];
+		$old_items = $params['items'];
 		// Create new items
 		$items = array();
-		$items[]	= $NAV_BAR_OBJ->_nav_item("Home", "./");
-		$items[]	= $NAV_BAR_OBJ->_nav_item("Forum", "./?object=".'forum');
+		$items[]	= $NAV_BAR_OBJ->_nav_item('Home', './');
+		$items[]	= $NAV_BAR_OBJ->_nav_item('Forum', './?object='.'forum');
 		// Get pregenerated forum items
-		$OBJ = module('forum')->_load_sub_module("forum_main_tpl");
+		$OBJ = module('forum')->_load_sub_module('forum_main_tpl');
 		$_FORUM_ITEMS = $OBJ->_show_navigation(true);
 		// Prepare links
-		foreach ((array)$_FORUM_ITEMS["items_links"] as $_item) {
-			$items[]	= $NAV_BAR_OBJ->_nav_item($_item["name"], $_item["link"]);
+		foreach ((array)$_FORUM_ITEMS['items_links'] as $_item) {
+			$items[]	= $NAV_BAR_OBJ->_nav_item($_item['name'], $_item['link']);
 		}
 		// Prepare texts
-		foreach ((array)$_FORUM_ITEMS["items_texts"] as $_item) {
-			$items[]	= $NAV_BAR_OBJ->_nav_item($_item["name"]);
+		foreach ((array)$_FORUM_ITEMS['items_texts'] as $_item) {
+			$items[]	= $NAV_BAR_OBJ->_nav_item($_item['name']);
 		}
 		return $items;
 	}
