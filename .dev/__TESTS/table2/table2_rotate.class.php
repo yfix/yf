99b0c3d8ee663119596f509fe683b2ab15e228cd
Yuri Vysotskiy <profy.net@gmail.com>
2013-11-14 17:51:39 +0200

    table2: rotated modee now fully working

--- .dev/__TESTS/table2/table2_rotate.class.php
+++ .dev/__TESTS/table2/table2_rotate.class.php
@@ -4,7 +4,7 @@ class table2_rotate {
 	function show() {
 		$values = array('', 'k1' => 'v1', 'k2' => 'v2');
 
-		return table('SELECT * FROM '.db('shop_products'), array('rotate' => 1))
+		return table('SELECT * FROM '.db('shop_products'), array('rotate_table' => 1))
 			->image('id', 'uploads/shop/products/{subdir2}/{subdir3}/product_%d_1_thumb.jpg', array('width' => '50px'))
 			->text('name')
 			->link('cat_id', './?object=category_editor&action=show_items&&id=%d', _class('cats')->_get_items_names('shop_cats'))
