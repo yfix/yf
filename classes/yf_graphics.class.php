5999eafac9290500013feed7e93ee535bc7e503b
Yuri Vysotskiy <profy.net@gmail.com>
2013-11-18 17:12:28 +0200

    changed prev_info url, now info is inside get_id

--- classes/yf_graphics.class.php
+++ classes/yf_graphics.class.php
@@ -315,7 +315,7 @@ class yf_graphics {
 				'edit_link'	=> './?object=admin_account',
 			));
 			if (main()->ADMIN_GROUP != 1 && $_SESSION['admin_prev_info']) {
-				$body .= '<li><a href="./?task=login&action=prev_info"><i class="icon icon-arrow-up"></i> '.t('Login back').'</a></li>';
+				$body .= '<li><a href="./?task=login&id=prev_info"><i class="icon icon-arrow-up"></i> '.t('Login back').'</a></li>';
 			}
 		// For authorized users only
 		} elseif (MAIN_TYPE_USER && $user_id && $user_group) {
