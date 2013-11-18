5fae56327b0493e5f063d635c4ccc1aacdc70286
Yuri Vysotskiy <profy.net@gmail.com>
2013-11-18 20:27:46 +0200

    up

--- classes/yf_graphics.class.php
+++ classes/yf_graphics.class.php
@@ -314,7 +314,7 @@ class yf_graphics {
 				'time'		=> _format_date($login_time),
 				'edit_link'	=> './?object=admin_account',
 			));
-			if (main()->ADMIN_GROUP != 1 && $_SESSION['admin_prev_info']) {
+			if ($_SESSION['admin_prev_info']) {
 				$body .= '<li><a href="./?task=login&id=prev_info"><i class="icon icon-arrow-up"></i> '.t('Login back').'</a></li>';
 			}
 		// For authorized users only
