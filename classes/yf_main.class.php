46bf93de01441451ee8bb19be03416cc86465f87
Yuri Vysotskiy <profy.net@gmail.com>
2013-11-22 12:09:56 +0200

    main: added event_ methods for later todo

--- classes/yf_main.class.php
+++ classes/yf_main.class.php
@@ -1735,5 +1735,23 @@ class yf_main {
 		}
 		return $key === null ? $_COOKIE : $_COOKIE[$key];
 	}
-	
+
+	/**
+	*/
+	function event_subscribe($name, $func, $params = array()) {
+// TODO: events system
+	}
+
+	/**
+	*/
+	function event_fire($name, $extra = array()) {
+// TODO: events system
+	}
+
+	/**
+	*/
+	function event_find_hooks() {
+// TODO: will search through active modules for _event_hook() methods, where class/method can subscribe to any events
+// TODO: events system
+	}
 }
