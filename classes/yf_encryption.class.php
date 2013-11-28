1811adc287b707b479c7b65ed39aed8408fe3ac1
Yuri Vysotskiy <profy.net@gmail.com>
2013-11-28 16:44:44 +0200

    added libs/phpcrypt submodule

--- classes/yf_encryption.class.php
+++ classes/yf_encryption.class.php
@@ -31,6 +31,70 @@ class yf_encryption {
 		13	=>	'RIJNDAEL_256',
 		14	=>	'SAFERPLUS',
 	);
+/*
+MCRYPT_3DES
+MCRYPT_ARCFOUR_IV (libmcrypt > 2.4.x only)
+MCRYPT_ARCFOUR (libmcrypt > 2.4.x only)
+MCRYPT_BLOWFISH
+MCRYPT_CAST_128
+MCRYPT_CAST_256
+MCRYPT_CRYPT
+MCRYPT_DES
+MCRYPT_DES_COMPAT (libmcrypt 2.2.x only)
+MCRYPT_ENIGMA (libmcrypt > 2.4.x only, alias for MCRYPT_CRYPT)
+MCRYPT_GOST
+MCRYPT_IDEA (non-free)
+MCRYPT_LOKI97 (libmcrypt > 2.4.x only)
+MCRYPT_MARS (libmcrypt > 2.4.x only, non-free)
+MCRYPT_PANAMA (libmcrypt > 2.4.x only)
+MCRYPT_RIJNDAEL_128 (libmcrypt > 2.4.x only)
+MCRYPT_RIJNDAEL_192 (libmcrypt > 2.4.x only)
+MCRYPT_RIJNDAEL_256 (libmcrypt > 2.4.x only)
+MCRYPT_RC2
+MCRYPT_RC4 (libmcrypt 2.2.x only)
+MCRYPT_RC6 (libmcrypt > 2.4.x only)
+MCRYPT_RC6_128 (libmcrypt 2.2.x only)
+MCRYPT_RC6_192 (libmcrypt 2.2.x only)
+MCRYPT_RC6_256 (libmcrypt 2.2.x only)
+MCRYPT_SAFER64
+MCRYPT_SAFER128
+MCRYPT_SAFERPLUS (libmcrypt > 2.4.x only)
+MCRYPT_SERPENT(libmcrypt > 2.4.x only)
+MCRYPT_SERPENT_128 (libmcrypt 2.2.x only)
+MCRYPT_SERPENT_192 (libmcrypt 2.2.x only)
+MCRYPT_SERPENT_256 (libmcrypt 2.2.x only)
+MCRYPT_SKIPJACK (libmcrypt > 2.4.x only)
+MCRYPT_TEAN (libmcrypt 2.2.x only)
+MCRYPT_THREEWAY
+MCRYPT_TRIPLEDES (libmcrypt > 2.4.x only)
+MCRYPT_TWOFISH (for older mcrypt 2.x versions, or mcrypt > 2.4.x )
+MCRYPT_TWOFISH128 (TWOFISHxxx are available in newer 2.x versions, but not in the 2.4.x versions)
+MCRYPT_TWOFISH192
+MCRYPT_TWOFISH256
+MCRYPT_WAKE (libmcrypt > 2.4.x only)
+MCRYPT_XTEA (libmcrypt > 2.4.x only)
+*/
+/*
+3DES
+3Way
+AES128
+AES192
+AES256
+ARC4
+Blowfish
+CAST128
+CAST256
+DES
+Enigma
+RC2
+Rijndael128
+Rijndael192
+Rijndael256
+Rijndael
+SimpleXOR
+Skipjack
+Vigenere
+*/
 	/** @var string Secret key */
 	public $_secret_key	= 'secret 134578';
 	/** @var mixed @conf_skip Mcrypt cipher constant value */
@@ -55,6 +119,24 @@ class yf_encryption {
 		if (isset($crypto_use_cipher)) {
 			$this->USE_CIPHER = $crypto_use_cipher;
 		}
+/*
+include(dirname(__FILE__)."/../phpCrypt.php");
+use PHP_Crypt\PHP_Crypt as PHP_Crypt;
+
+$text = "This is my secret message.";
+$key = "^mY@TEst~Key_0123456789abcefghij"; // the key will be truncated if it's too long
+
+$crypt = new PHP_Crypt($key, PHP_Crypt::CIPHER_AES_256, PHP_Crypt::MODE_CTR);
+
+$iv = $crypt->createIV();
+$encrypt = $crypt->encrypt($text);
+
+$crypt->IV($iv);
+$decrypt = $crypt->decrypt($encrypt);
+
+print "CIPHER: ".$crypt->cipherName()."\n";
+print "MODE: ".$crypt->modeName()."\n";
+*/
 	}
 
 	/**
