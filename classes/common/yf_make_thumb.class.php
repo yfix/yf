fa3fa5e4c25a43c39909538deea616310c235d19
root <alexvillin@gmail.com>
2013-12-03 13:23:32 +0200

    make_thumb fix

--- classes/common/yf_make_thumb.class.php
+++ classes/common/yf_make_thumb.class.php
@@ -373,7 +373,7 @@ class yf_make_thumb {
 
         $watermarkwidth = imagesx($watermark);
         $watermarkheight = imagesy($watermark);
-		if($watermarkwidth > $watermarkheight){
+		if($watermarkwidth > $watermarkheight && $width_orig > $height_orig){
 	        $thumb_watermark_w = intval($width_orig / 1.2);
 	        $thumb_watermark_h = intval(($thumb_watermark_w / $watermarkwidth) * $watermarkheight);
 		}else{
