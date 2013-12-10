585b2cbf08b4bde62b38f0aedec1af743df02d97
Yuri Vysotskiy <profy.net@gmail.com>
2013-12-10 14:05:15 +0200

    db_installer cleanup

--- classes/db/yf_installer_db_mysql.class.php
+++ classes/db/yf_installer_db_mysql.class.php
@@ -6,7 +6,7 @@ class yf_installer_db_mysql extends yf_installer_db {
 	/** @var int */
 	public $NUM_RETRIES = 3;
 	/** @var int */
-	public $RETRY_DELAY = 3;
+	public $RETRY_DELAY = 1;
 	/** @var string */
 	public $DEFAULT_CHARSET = 'utf8';
 	/** @var array */
@@ -46,13 +46,28 @@ class yf_installer_db_mysql extends yf_installer_db {
 	}
 
 	/**
+	* Execute original query again safely
+	*/
+	function _db_query_safe($sql, $db) {
+		for ($i = 0; $i <= $this->NUM_RETRIES; $i++) {
+			$result = $db->db->query($sql);
+			if (!empty($result)) {
+				break;
+			} else {
+				sleep($this->RETRY_DELAY);
+			}
+		}
+		return $result;
+	}
+
+	/**
 	* Trying to repair given table structure (and possibly data)
 	*/
 	function _auto_repair_table($sql, $db_error, $db) {
 		$sql = trim($sql);
-/*
 		// #1191 Can't find FULLTEXT index matching the column list
-		if ($this->RESTORE_FULLTEXT_INDEX && in_array($db_error['code'], array(1191))) {
+		if (in_array($db_error['code'], array(1191)) && $this->RESTORE_FULLTEXT_INDEX) {
+
 			foreach ((array)conf('fulltext_needed_for') as $_fulltext_field) {
 				list($f_table, $f_field) = explode('.', $_fulltext_field);
 				if (empty($f_table) || false === strpos($sql, $f_table) || empty($f_field)) {
@@ -64,42 +79,24 @@ class yf_installer_db_mysql extends yf_installer_db {
 						continue;
 					}
 					if ($v['Index_type'] == 'FULLTEXT') {
-						// Continue the outer loop
 						continue 2;
 					}
 				}
 				$db->query('ALTER TABLE '.$f_table.' ADD FULLTEXT KEY '.$f_field.' ('.$f_field.')');
 			}
-			// Execute original query again
-			$result = $db->query($sql);
-			return $result;
-		}
-*/
+			return $this->_db_query_safe($sql, $db);
+
 		// Errors related to server high load (currently we will handle only SELECTs)
 		// #2013 means 'Lost connection to MySQL server during query'
 		// #1205 means 'Lock wait timeout expired. Transaction was rolled back' (InnoDB)
 		// #1213 means 'Transaction deadlock. You should rerun the transaction.' (InnoDB)
-		if (in_array($db_error['code'], array(2013,1205,1213)) && substr($sql, 0, strlen('SELECT ')) == 'SELECT ') {
-			$result = false;
-			// Try 5 times with delay
-			for ($i = 0; $i <= $this->NUM_RETRIES; $i++) {
-				$result = $db->db->query($sql);
-				// Stop after success
-				if (!empty($result)) {
-					break;
-				// Wait some time and try again
-				} else {
-					sleep($this->RETRY_DELAY);
-				}
-			}
-			return $result;
-		}
-		// Try to refresh tables names cache (error #1146 means 'Table %s doesn't exist')
-		// (error #1054 means 'Unknown column %s')
-		if (!in_array($db_error['code'], array(1146, 1054))) {
-			return false;
-		}
-		if ($db_error['code'] == 1146) {
+		} elseif (in_array($db_error['code'], array(2013,1205,1213)) && substr($sql, 0, strlen('SELECT ')) == 'SELECT ') {
+
+			return $this->_db_query_safe($sql, $db);
+
+		// #1146 means "Table %s doesn't exist"
+		} elseif ($db_error['code'] == 1146) {
+
 			// Try to get table name from error message
 			preg_match("#Table [\'][a-z_0-9]+\.([a-z_0-9]+)[\'] doesn\'t exist#ims", $db_error['message'], $m);
 			$item_to_repair = trim($m[1]);
@@ -117,7 +114,11 @@ class yf_installer_db_mysql extends yf_installer_db {
 					return false;
 				}
 			}
+			return $this->_db_query_safe($sql, $db);
+
+		// #1054 means "Unknown column %s"
 		} elseif ($db_error['code'] == 1054) {
+
 			// Try to get column name from error message
 			preg_match("#Unknown column [\']([a-z_0-9]+)[\'] in#ims", $db_error['message'], $m);
 			$item_to_repair = $m[1];
@@ -144,23 +145,11 @@ class yf_installer_db_mysql extends yf_installer_db {
 					return false;
 				}
 			}
-		}
-		$result = false;
-		// Try to repair query
-		if ($db_error['code'] == 1146) {
-#			$this->_sql_retries[$sql]++;
-#			if ($this->_sql_retries[$sql] <= $this->NUM_RETRIES) {
-# WTF? recursion level 100 reached
-#echo $sql.'<br><br><br>';
-				$result = $db->db->query($sql);
-#echo intval($result).'<br>';
-#			}
-		} elseif ($db_error['code'] == 1054) {
 			if (!empty($installer_result)) {
-#				$result = $db->db->query($sql);
+				return $this->_db_query_safe($sql, $db);
 			}
 		}
-		return $result;
+		return true;
 	}
 
 	/**
@@ -204,29 +193,19 @@ class yf_installer_db_mysql extends yf_installer_db {
 		if (!empty($_tmp)) {
 			$_table_options_string = ' '.implode(', ', $_tmp);
 		}
-		// Try to create table
-		$sql = 'CREATE TABLE '
-			.($this->USE_SQL_IF_NOT_EXISTS ? 'IF NOT EXISTS' : '')
-			.' '.$db->enclose_field_name($full_table_name)
-			.' ('."\r\n".
-			$table_struct
-			.')'.$_table_options_string;
-		// Try to execute query
-		$result = $db->query($sql);
-		return $result;
+		$sql = 'CREATE TABLE '.($this->USE_SQL_IF_NOT_EXISTS ? 'IF NOT EXISTS' : '').' '.$db->enclose_field_name($full_table_name).' ('.PHP_EOL. 
+			$table_struct.')'.$_table_options_string;
+		return $db->query($sql);
 	}
 
 	/**
 	* Do alter table structure
 	*/
 	function _do_alter_table ($table_name = '', $column_name = '', $table_struct = array(), $db) {
-		// Shorthand for the column structure
 		$column_struct = $table_struct[$column_name];
-		// Fix for the 'int' default value
 		if ($column_struct['type'] != 'int' && $column_struct['default'] == '') {
 			unset($column_struct['default']);
 		}
-		// Generate 'ALTER TABLE' query
 		$sql = 'ALTER TABLE '.$db->DB_PREFIX.$table_name."\r\n".
 			"\t".'ADD '._es($column_name).' '.strtoupper($column_struct['type']).
 			(!empty($column_struct['length'])	? '('.$column_struct['length'].')' : '').
@@ -235,7 +214,6 @@ class yf_installer_db_mysql extends yf_installer_db {
 			(isset($column_struct['default'])	? ' DEFAULT \''.$column_struct['default'].'\'' : '').
 			(!empty($column_struct['auto_inc'])	? ' AUTO_INCREMENT' : '').
 			';';
-		// Do execute generated query
 		return $db->query($sql);
 	}
 }
