c8ea0a698c65a4b7ceca54b162d34e02b6630786
Yuri Vysotskiy <profy.net@gmail.com>
2013-12-13 13:23:39 +0200

    oauth added new roadmap drivers

---
+++ classes/oauth/yf_oauth_driver_paypal.__TODO__.class.php
@@ -0,0 +1,14 @@
+<?php
+
+load('oauth_driver', 'framework', 'classes/oauth/');
+class yf_oauth_driver_paypal extends yf_oauth_driver {
+
+#	protected $url_authorize = '';
+#	protected $url_access_token = '';
+#	protected $url_user = '';
+#	protected $scope = '';
+#	protected $get_access_token_method = 'POST';
+
+// TODO
+
+}
