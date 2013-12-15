0334af49250c29ccaf1e82e06d679539bb86d83b
Yuri Vysotskiy <profy.net@gmail.com>
2013-12-15 20:24:56 +0200

    oauth up

--- classes/oauth/yf_oauth_driver_amazon.class.php
+++ classes/oauth/yf_oauth_driver_amazon.class.php
@@ -6,7 +6,7 @@ class yf_oauth_driver_amazon extends yf_oauth_driver2 {
 	protected $url_authorize = 'https://www.amazon.com/ap/oa';
 	protected $url_access_token = 'https://www.amazon.com/ap/oatoken';
 	protected $url_user = 'https://api.amazon.com/';
-	protected $scope = '';
+	protected $scope = 'profile';
 	protected $get_access_token_method = 'POST';
 
 }
--- classes/oauth/yf_oauth_driver_flickr.class.php
+++ classes/oauth/yf_oauth_driver_flickr.class.php
@@ -3,11 +3,10 @@
 load('oauth_driver1', 'framework', 'classes/oauth/');
 class yf_oauth_driver_flickr extends yf_oauth_driver1 {
 
-	protected $url_authorize = 'http://www.flickr.com/services/oauth/authorize';
+	protected $url_authenticate = 'http://www.flickr.com/services/oauth/authorize';
 	protected $url_request_token = 'http://www.flickr.com/services/oauth/request_token';
 	protected $url_access_token = 'http://www.flickr.com/services/oauth/access_token';
 	protected $url_user = 'http://api.flickr.com/services/rest'; // ?nojsoncallback=1  &method=flickr.test.login
-	protected $scope = '';
 	protected $get_access_token_method = 'POST';
 
 }
