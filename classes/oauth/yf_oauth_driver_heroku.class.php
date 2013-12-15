a546fbbfd2a6d675e2328362efe7bc9523c0fb18
Yuri Vysotskiy <profy.net@gmail.com>
2013-12-15 15:42:47 +0200

    oauth heroku up

--- classes/oauth/yf_oauth_driver_heroku.class.php
+++ classes/oauth/yf_oauth_driver_heroku.class.php
@@ -1,14 +1,16 @@
 <?php
 
-load('oauth_driver', 'framework', 'classes/oauth/');
-class yf_oauth_driver_youtube extends yf_oauth_driver {
+load('oauth_driver2', 'framework', 'classes/oauth/');
+class yf_oauth_driver_heroku extends yf_oauth_driver2 {
 
-#	protected $url_authorize = '';
-#	protected $url_access_token = '';
-#	protected $url_user = '';
-#	protected $scope = '';
-#	protected $get_access_token_method = 'POST';
-
-// TODO
+	protected $url_authorize = 'https://id.heroku.com/oauth/authorize';
+	protected $url_access_token = 'https://id.heroku.com/oauth/token';
+	protected $url_user = 'https://api.heroku.com/account';
+	protected $scope = 'identity';
+	protected $get_access_token_method = 'POST';
+	protected $url_params_access_token = array(
+		'grant_type'	=> 'authorization_code',
+	);
+	protected $get_user_info_user_bearer = true;
 
 }
