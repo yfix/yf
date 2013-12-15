77c77749ca9b51beb2d0c986a03437feed943d61
Yuri Vysotskiy <profy.net@gmail.com>
2013-12-15 20:01:58 +0200

    oauth bitly done

--- classes/oauth/yf_oauth_driver_bitly.class.php
+++ classes/oauth/yf_oauth_driver_bitly.class.php
@@ -4,7 +4,7 @@ load('oauth_driver2', 'framework', 'classes/oauth/');
 class yf_oauth_driver_bitly extends yf_oauth_driver2 {
 
 	protected $url_authorize = 'https://bitly.com/oauth/authorize';
-	protected $url_access_token = 'https://bitly.com/oauth/access_token';
+	protected $url_access_token = 'https://api-ssl.bitly.com/oauth/access_token';
 	protected $url_user = 'https://api-ssl.bitly.com/v3/user/info';
 	protected $scope = '';
 	protected $get_access_token_method = 'POST';
