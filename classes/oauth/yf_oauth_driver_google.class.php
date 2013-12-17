ea8e592138d7ad2cd8ba1a4d402123cad549837f
root <root@y.df.org.ua>
2013-12-17 13:56:46 +0100

    oauth: google normalized info done

--- classes/oauth/yf_oauth_driver_google.class.php
+++ classes/oauth/yf_oauth_driver_google.class.php
@@ -6,22 +6,24 @@ class yf_oauth_driver_google extends yf_oauth_driver2 {
 	protected $url_authorize = 'https://accounts.google.com/o/oauth2/auth';
 	protected $url_access_token = 'https://accounts.google.com/o/oauth2/token';
 	protected $url_user = 'https://www.googleapis.com/oauth2/v1/userinfo';
-	protected $scope = 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email';
+#	protected $url_user = 'https://www.googleapis.com/plus/v1/people/me';
+	protected $scope = 'email profile https://www.googleapis.com/auth/plus.login';
 	protected $get_access_token_method = 'POST';
+	protected $url_params_access_token = array(
+		'grant_type'	=> 'authorization_code',
+	);
 
 	/**
 	*/
 	function _get_user_info_for_auth($raw = array()) {
-/*
 		$user_info = array(
 			'user_id'		=> $raw['id'],
-			'login'			=> $raw['login'],
-			'name'			=> $raw['id'],
-			'email'			=> current($raw['emails']),
-			'avatar_url'	=> $raw['avatar_url'],
-			'profile_url'	=> $raw['url'],
+			'login'			=> $raw['email'],
+			'name'			=> $raw['name'],
+			'email'			=> $raw['email'],
+			'avatar_url'	=> '',
+			'profile_url'	=> $raw['link'],
 		);
-*/
 		return $user_info;
 	}
 }
