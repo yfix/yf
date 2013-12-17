6c5ea99e86416b222a73ec5e4a285198d70d066c
Yuri Vysotskiy <profy.net@gmail.com>
2013-12-17 10:41:04 +0200

    oauth up

--- classes/oauth/yf_oauth_driver_bitbucket.class.php
+++ classes/oauth/yf_oauth_driver_bitbucket.class.php
@@ -6,7 +6,6 @@ class yf_oauth_driver_bitbucket extends yf_oauth_driver1 {
 	protected $url_request_token = 'https://bitbucket.org/api/1.0/oauth/request_token';
 	protected $url_authenticate = 'https://bitbucket.org/api/1.0/oauth/authenticate';
 	protected $url_access_token = 'https://bitbucket.org/api/1.0/oauth/access_token';
-#	protected $url_user = 'https://api.bitbucket.org/2.0/users/{user_id}';
 	protected $url_user = 'https://bitbucket.org/api/1.0/user';
 	protected $get_access_token_method = 'POST';
 }
--- classes/oauth/yf_oauth_driver_flickr.class.php
+++ classes/oauth/yf_oauth_driver_flickr.class.php
@@ -6,7 +6,7 @@ class yf_oauth_driver_flickr extends yf_oauth_driver1 {
 	protected $url_authenticate = 'http://www.flickr.com/services/oauth/authorize';
 	protected $url_request_token = 'http://www.flickr.com/services/oauth/request_token';
 	protected $url_access_token = 'http://www.flickr.com/services/oauth/access_token';
-	protected $url_user = 'http://api.flickr.com/services/rest'; // ?nojsoncallback=1  &method=flickr.test.login
+	protected $url_user = 'http://api.flickr.com/services/rest?nojsoncallback=1&method=flickr.test.login&format=json';
 	protected $get_access_token_method = 'POST';
 	protected $url_params_authenticate = array(
 		'perms'	=> 'write',
