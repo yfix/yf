df64d8ef9d0e4928c72b50a33e46566f3f159d04
Yuri Vysotskiy <profy.net@gmail.com>
2013-12-17 14:13:40 +0200

    oauth yandex: normalized info

--- classes/oauth/yf_oauth_driver_yandex.class.php
+++ classes/oauth/yf_oauth_driver_yandex.class.php
@@ -15,16 +15,16 @@ class yf_oauth_driver_yandex extends yf_oauth_driver2 {
 	/**
 	*/
 	function _get_user_info_for_auth($raw = array()) {
-/*
 		$user_info = array(
 			'user_id'		=> $raw['id'],
-			'login'			=> $raw['login'],
-			'name'			=> $raw['id'],
-			'email'			=> current($raw['emails']),
-			'avatar_url'	=> $raw['avatar_url'],
-			'profile_url'	=> $raw['url'],
+			'login'			=> $raw['display_name'],
+			'name'			=> $raw['real_name'],
+			'email'			=> $raw['default_email'],
+#			'avatar_url'	=> $raw['avatar_url'],
+#			'profile_url'	=> $raw['url'],
+			'birthday'		=> $raw['birthday'],
+			'gender'		=> $raw['sex'],
 		);
-*/
 		return $user_info;
 	}
 
