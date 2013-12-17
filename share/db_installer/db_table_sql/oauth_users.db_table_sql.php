428f172a59a4e2d7de5b1abcf5d05d5504fb0343
Yuri Vysotskiy <profy.net@gmail.com>
2013-12-17 16:35:55 +0200

    oauth_users db sql info up

--- share/db_installer/db_table_sql/oauth_users.db_table_sql.php
+++ share/db_installer/db_table_sql/oauth_users.db_table_sql.php
@@ -1,12 +1,13 @@
 <?php
 $data = '
 	`id` int(11) NOT NULL auto_increment,
-	`user_id` int(11) NOT NULL default \'\',
+	`user_id` int(11) NOT NULL default \'0\',
 	`provider` varchar(64) NOT NULL default \'\',
+	`provider_uid` varchar(64) NOT NULL default \'\',
 	`login` varchar(128) NOT NULL default \'\',
 	`email` varchar(128) NOT NULL default \'\',
-	`add_date` int(11) NOT NULL default \'\',
-	`last_date` int(11) NOT NULL default \'\',
+	`add_date` int(11) NOT NULL default \'0\',
+	`last_date` int(11) NOT NULL default \'0\',
 	PRIMARY KEY	(`id`),
 	UNIQUE KEY `user_id_provider` (`user_id`,`provider`)
 ';
