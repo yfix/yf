0fcf83e90cf65d94b73dc2680a67de41ba1b19c4
Arsen Melkumiants <arsen.mem@gmail.com>
2014-01-03 18:34:05 +0200

    update image script

--- admin_modules/manage_shop/yf_manage_shop_pics_browser.class.php
+++ admin_modules/manage_shop/yf_manage_shop_pics_browser.class.php
@@ -3,6 +3,14 @@
 class yf_manage_shop_pics_browser {
 
 	function pics_browser() {
+		if (isset($_GET['active']) && $_GET['active'] == 1) {
+			$active = ' AND p.active = \'1\' ';
+		} elseif (isset($_GET['active']) && $_GET['active'] == 0) {
+			$active = ' AND p.active = \'\' ';
+		} else {
+			$active = '';
+		}
+
 		if (!empty($_POST)) {
 			foreach($_POST['delete'] as $k=>$v) {
 				list ($id,$product_id) = explode("_",$k);
@@ -10,7 +18,7 @@ class yf_manage_shop_pics_browser {
 			}
 		}
 		$cats_list = _class( '_shop_categories', 'modules/shop/' )->recursive_get_child_ids(62521);
-		$sql = "SELECT `i`.`product_id`,`i`.`id` FROM `".db('shop_products')."` AS `p`, `".db('shop_product_images')."` AS `i` WHERE `p`.`id`=`i`.`product_id` AND `p`.`cat_id` IN ('".implode("','",$cats_list)."')";
+		$sql = "SELECT `i`.`product_id`,`i`.`id` FROM `".db('shop_products')."` AS `p`, `".db('shop_product_images')."` AS `i` WHERE `p`.`id`=`i`.`product_id` AND `p`.`cat_id` IN ('".implode("','",$cats_list)."')".$active;
 		
 		list( $add_sql, $pages, $total_records, $page_current, $pages_total, $pages_limited ) = common()->divide_pages($sql);
 		
