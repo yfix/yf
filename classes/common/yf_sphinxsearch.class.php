0b97c05edb2c80db760fb5e30e77a39544db4391
Yuri Vysotskiy <profy.net@gmail.com>
2014-01-10 13:12:09 +0200

    sphinxsearch client class up

--- classes/common/yf_sphinxsearch.class.php
+++ classes/common/yf_sphinxsearch.class.php
@@ -5,6 +5,48 @@
 */
 class yf_sphinxsearch {
 
+	/** In seconds */
+	private $CONNECT_TIMEOUT = 2;
+	/** Just counter, set to 0 to disable reconnect tries */
+	private $RECONNECT_TRIES = 2;
+	/** In milli-seconds (1 second == 1000000) */
+	private $RECONNECT_SLEEP = 1000000;
+	/** In milli-seconds (1 second == 1000000) */
+	private $QUERY_RETRY_SLEEP = 1000000;
+	/**
+	* Lis of mysql client library errors, returned from sphinxsearch server, when we can retry query
+	*	2003 - Can't connect to MySQL server on
+	*	2006 - MySQL server has gone away
+	*	2013 - Lost connection to MySQL server during query
+	*	2020 - Got packet bigger than 'max_allowed_packet' bytes
+	*/
+	private $QUERY_RETRY_ERROR_CODES = array(2003,2006,2013,2020);
+	/** [Resource] or null */
+	private $sphinx_connection = null;
+	/** Host:port like this: 127.0.0.1:9306 */
+	private $HOST = '127.0.0.1:9306';
+	/***/
+	private $DEF_PORT = '9306';
+	/***/
+	private $EMPTY_RESULTS_LOG_PATH = '';
+	/***/
+	private $CACHE_TTL = 300;
+
+	/**
+	*/
+	function _init () {
+		// SPHINX_HOST can contain port or not, both variants supported
+		if (defined('SPHINX_HOST')) {
+			$this->HOST = SPHINX_HOST;
+			if (false === strpos($this->HOST, ':')) {
+				$this->HOST .= ':'.(defined('SPHINX_HOST') ? SPHINX_PORT : $this->DEF_PORT);
+			}
+		}
+		if (!$this->EMPTY_RESULTS_LOG_PATH && common()->SPHINX_EMPTY_LOG_PATH) {
+			$this->EMPTY_RESULTS_LOG_PATH = common()->SPHINX_EMPTY_LOG_PATH;
+		}
+	}
+
 	/**
 	* Sphinx QL query wrapper
 	*/
@@ -14,151 +56,280 @@ class yf_sphinxsearch {
 		}
 		if (DEBUG_MODE) {
 			$trace = main()->trace_string();
+			$time =  microtime(true);
 		}
-		$time =  microtime(true);
-
-		ini_set('mysql.connect_timeout', 2);
-
+		$data = null;
 		$CACHE_NAME = 'SPHINX_'.md5($sql);
-		$data = cache_get($CACHE_NAME);
-		if ($data) {
-			list($data, $meta, $warnings, $query_error) = $data;
+		$cached = cache_get($CACHE_NAME);
+		if ($cached) {
+			list($data, $meta, $warnings, $query_error, $describe, $time_wo_cache) = $cached;
 			if (DEBUG_MODE) {
-				$debug_index = count(debug('sphinx'));
-				debug('sphinx::'.$debug_index, array(
-					'query'	=> $sql,
-					'time'	=> microtime(true) - $time,
-					'trace'	=> $trace,
-					'count'	=> count($data),
-					'meta'	=> $meta,
-					'warnings'	=> $warnings,
-					'error'	=> $query_error,
-					'cached'=> 1,
-					'results' => null,
+				$this->_set_query_debug(array(
+					'query'			=> $sql,
+					'time'			=> $time,
+					'trace'			=> $trace,
+					'meta'			=> $meta,
+					'warnings'		=> $warnings,
+					'describe'		=> $describe,
+					'error'			=> $query_error,
+					'results'		=> $data,
+					'cached'		=> 1,
+					'time_wo_cache'	=> $time_wo_cache,
 				));
 			}
 			$GLOBALS['_SPHINX_META'] = $meta;
 			return $data;
 		}
-
-	 	$host = SPHINX_HOST.':'.SPHINX_PORT;
-
-		if (!isset($this->sphinx_connect)) {
-			$time =  microtime(true);
-			$this->sphinx_connect = mysql_connect($host, DB_USER, DB_PSWD, true);
-
-			// Try to reconnect
-			if(!$this->sphinx_connect){
-				usleep(1000000);	// wait for 1 second
-				$this->sphinx_connect = mysql_connect($host, DB_USER, DB_PSWD, true);
-			}
-			if(!$this->sphinx_connect){
-				$query_error = mysql_error($this->sphinx_connect);
-				$q_error_num = mysql_errno($this->sphinx_connect);
-				conf('http_headers::X-Details', conf('http_headers::X-Details').';SE=('.$q_error_num.') '.$query_error.';');
-				trigger_error('No connection to sphinx', E_USER_WARNING);
-			}
-			if (DEBUG_MODE) {
-				$debug_index = count(debug('sphinx'));
-				debug('sphinx::'.$debug_index, array(
-					'query'	=> 'sphinx connect',
-					'time'	=> microtime(true) - $time,
-					'count'	=> '',
-					'trace'	=> $trace,
-					'meta'	=> '',
-					'error'	=> $query_error,
-				));
-			}
+		$results		= array();
+		$query_error	= '';
+		$q_error_num	= '';
+		if (!isset($this->sphinx_connection)) {
+			$this->_connect();
 		}
-		$meta		= array();
-		$warnings	= array();
-		$results	= array();
-		$query_error= '';
-		$q_error_num= '';
-		if ($this->sphinx_connect) {
-			$Q = mysql_query($sql, $this->sphinx_connect);
-			if (!$Q) {
-				$query_error = mysql_error($this->sphinx_connect);
-				$q_error_num = mysql_errno($this->sphinx_connect);
+		if ($this->sphinx_connection) {
+			$q = mysql_query($sql, $this->sphinx_connection);
+			if (!$q) {
+				$query_error = mysql_error($this->sphinx_connection);
+				$q_error_num = mysql_errno($this->sphinx_connection);
 				// Try to execute query again in case of these errors returned:
-				// 2003 - Can't connect to MySQL server on
-				// 2006 - MySQL server has gone away
-				// 2013 - Lost connection to MySQL server during query
-				// 2020 - Got packet bigger than 'max_allowed_packet' bytes
-				if (in_array($q_error_num, array(2003,2006,2013,2020))) {
-					usleep(1000000);	// wait for 1 second
-					$Q = mysql_query($sql, $this->sphinx_connect);
-					if (!$Q) {
-						$query_error = mysql_error($this->sphinx_connect);
-						$q_error_num = mysql_errno($this->sphinx_connect);
+				if (in_array($q_error_num, $this->QUERY_RETRY_ERROR_CODES)) {
+					usleep($this->QUERY_RETRY_SLEEP);
+					$q = mysql_query($sql, $this->sphinx_connection);
+					if (!$q) {
+						$query_error = mysql_error($this->sphinx_connection);
+						$q_error_num = mysql_errno($this->sphinx_connection);
 					}
 				}
 				if ($query_error) {
-					trigger_error('Sphinx error: '.$sql, E_USER_WARNING);
+					trigger_error('Sphinx error: '.$query_error.'; for query: '.$sql, E_USER_WARNING);
 					conf('http_headers::X-Details', conf('http_headers::X-Details').';SE=('.$q_error_num.') '.$query_error.';');
 				}
-			} else {
-				while ($A = mysql_fetch_assoc($Q)) {
-					$results[] = $A;
+			}
+			if ($q) {
+				while ($a = mysql_fetch_assoc($q)) {
+					$results[] = $a;
 				}
-				// log empty results
-				if (count($results) == 0 && $this->SPHINX_EMPTY_LOG_PATH != '') {
-					$out = array(
-						date('YmdH'),
-						conf('CUR_DOMAIN_SHORT'),
-						common()->_db_escape($_SERVER['HTTP_REFERER']),
-						common()->_db_escape($_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']),
-						common()->_db_escape($_SERVER['HTTP_USER_AGENT']),			
-						common()->_db_escape($sql),
-					);
-					file_put_contents(common()->SPHINX_EMPTY_LOG_PATH, implode('#|#',$out).PHP_EOL, FILE_APPEND);
+				if (count($results) == 0) {
+					$this->_save_empty_results_log($sql);
 				}
 			}
-			if (DEBUG_MODE || $need_meta) {
-				// Get query internal details
-				$Q = mysql_query('SHOW META', $this->sphinx_connect);
-				if (!is_bool($Q)) {
-					while ($A = mysql_fetch_row($Q)) {
-						$meta[$A[0]] = $A[1];
-					}
-				}
-				$GLOBALS['_SPHINX_META'] = $meta;
-
-				// Get query warnings
-				$Q = mysql_query('SHOW WARNINGS', $this->sphinx_connect);
-				if (!is_bool($Q)) {
-					while ($A = mysql_fetch_row($Q)) {
-						$warnings[$A[0]] = $A[1];
-					}
-				}
-				$GLOBALS['_SPHINX_WARNINGS'] = $warnings;
+			$meta			= array();
+			$warnings		= array();
+			$describe		= array();
+			if ($need_meta || DEBUG_MODE) {
+				$meta = $this->_get_latest_meta();
+				$warnings = $this->_get_latest_warnings();
+				$describe = $this->_get_latest_describe($sql);
 			}
+			$time_wo_cache = 0;
 			if (DEBUG_MODE) {
-				$debug_index = count(debug('sphinx'));
-				debug('sphinx::'.$debug_index, array(
+				$this->_set_query_debug(array(
 					'query'	=> $sql,
-					'time'	=> microtime(true) - $time,
-					'count'	=> intval(count($results)),
+					'time'	=> $time,
 					'trace'	=> $trace,
 					'meta'	=> $meta,
 					'error'	=> $query_error,
 					'warnings' => $warnings,
+					'describe' => $describe,
 					'results' => $results,
 				));
+				$time_wo_cache = microtime(true) - $time;
 			}
 		}
-		if (empty($query_error) && $this->sphinx_connect) {
-			cache_set($CACHE_NAME, array($results, $meta, $warnings, $query_error), 300);
+		if (empty($query_error) && $this->sphinx_connection) {
+			cache_set($CACHE_NAME, array($results, $meta, $warnings, $query_error, $describe, $time_wo_cache), $this->CACHE_TTL);
 		}
 		return $results;
 	}
 	
 	/**
-	* Sphinx-related
 	*/
-	function escape_string ( $string ){
+	function _connect() {
+		if (isset($this->sphinx_connection)) {
+			return $this->sphinx_connection;
+		}
+		if ($this->_connects_tried > $this->RECONNECT_TRIES) {
+			return false;
+		}
+		if (DEBUG_MODE) {
+			$time =  microtime(true);
+		}
+		// For sphinxsearch we need small connect timeout, also save default for later restore
+		if ($this->CONNECT_TIMEOUT) {
+			$orig_connect_timeout = ini_get('mysql.connect_timeout');
+			if ($orig_connect_timeout != $this->CONNECT_TIMEOUT) {
+				ini_set('mysql.connect_timeout', $this->CONNECT_TIMEOUT);
+			}
+		}
+		$this->sphinx_connection = mysql_connect($this->HOST, '', '', $new_link = true);
+		$this->_connects_tried++;
+		// Try to reconnect
+		if (!$this->sphinx_connection && $this->RECONNECT_TRIES) {
+			for ($i = 0; $i < $this->RECONNECT_TRIES; $i++) {
+				usleep($this->RECONNECT_SLEEP);
+				$this->sphinx_connection = mysql_connect($this->HOST, '', '', $new_link = true);
+				$this->_connects_tried++;
+			}
+		}
+		if (!$this->sphinx_connection) {
+			$query_error = mysql_error($this->sphinx_connection);
+			$q_error_num = mysql_errno($this->sphinx_connection);
+			conf('http_headers::X-Details', conf('http_headers::X-Details').';SE=('.$q_error_num.') '.$query_error.';');
+			trigger_error('No connection to sphinx', E_USER_WARNING);
+		}
+		if (DEBUG_MODE) {
+			$this->_set_query_debug(array(
+				'query'	=> 'sphinx connect',
+				'time'	=> $time,
+				'error'	=> $query_error,
+			));
+		}
+		// Revert default mysql connect timeout
+		if ($this->CONNECT_TIMEOUT && $orig_connect_timeout != $this->CONNECT_TIMEOUT) {
+			ini_set('mysql.connect_timeout', $orig_connect_timeout);
+		}
+		return $this->sphinx_connection;
+	}
+	
+	/**
+	*/
+	function escape_string ($string) {
 		$from = array ( "\\", '(',')','|','-','!','@','~','"','&', '/', '^', '$', '=' );
 		$to   = array ( "\\\\", '\(','\)','\|','\-','\!','\@','\~','\"', '\&', '\/', '\^', '\$', '\=' );
 		return str_replace ( $from, $to, $string );
+	}
+
+	/**
+	* Get latest query internal details
+	*/
+	function _get_latest_meta() {
+		if (!$this->sphinx_connection) {
+			$this->_connect();
+		}
+		if (!$this->sphinx_connection) {
+			return false;
+		}
+		$q = mysql_query('SHOW META', $this->sphinx_connection);
+		if (!is_bool($q)) {
+			while ($a = mysql_fetch_row($q)) {
+				$meta[$a[0]] = $a[1];
+			}
+		}
+		$GLOBALS['_SPHINX_META'] = $meta;
+		return $meta;
+	}
+
+	/**
+	* Get latest query warnings
+	*/
+	function _get_latest_warnings() {
+		if (!$this->sphinx_connection) {
+			$this->_connect();
+		}
+		if (!$this->sphinx_connection) {
+			return false;
+		}
+		$q = mysql_query('SHOW WARNINGS', $this->sphinx_connection);
+		if (!is_bool($q)) {
+			while ($a = mysql_fetch_row($q)) {
+				$warnings[$a[0]] = $a[1];
+			}
+		}
+		$GLOBALS['_SPHINX_WARNINGS'] = $warnings;
+		return $warnings;
+	}
+	
+	/**
+	*/
+	function _get_latest_describe($sql = '') {
+		if (!$this->sphinx_connection) {
+			$this->_connect();
+		}
+		if (!$this->sphinx_connection) {
+			return false;
+		}
+		$describe = array();
+		if (preg_match('/SELECT[\s\t]+.+[\s\t]+FROM[\s\t]+([a-z0-9\_]+)[\s\t]+WHERE[\s\t]+/ims', $sql, $m)) {
+			$describe_sql = 'DESCRIBE '.$m[1];
+			$q = mysql_query('DESCRIBE '.$m[1], $this->sphinx_connection);
+			if (!is_bool($q)) {
+				while ($a = mysql_fetch_row($q)) {
+					$describe[$a[0]] = $a[1];
+				}
+			}
+		}
+		return $describe;
+	}
+	
+	/**
+	*/
+	function _get_server_status() {
+		if (!$this->sphinx_connection) {
+			$this->_connect();
+		}
+		if (!$this->sphinx_connection) {
+			return false;
+		}
+		$status = array();
+		$q = mysql_query('SHOW STATUS', $this->sphinx_connection);
+		if (!is_bool($q)) {
+			while ($a = mysql_fetch_row($q)) {
+				$status[$a[0]] = $a[1];
+			}
+		}
+		return $status;
+	}
+	
+	/**
+	*/
+	function _get_server_version() {
+		if (!$this->sphinx_connection) {
+			$this->_connect();
+		}
+		if (!$this->sphinx_connection) {
+			return false;
+		}
+		return mysql_get_server_info($this->sphinx_connection);
+	}
+	
+	/**
+	*/
+	function _get_host() {
+		return $this->HOST;
+	}
+
+	/**
+	*/
+	function _set_query_debug($a) {
+		debug('sphinx[]', array(
+			'query'		=> $a['query'],
+			'results'	=> $a['results'],
+			'count'		=> is_array($a['results']) ? intval(count($a['results'])) : '',
+			'meta'		=> $a['meta'],
+			'error'		=> $a['error'],
+			'warnings'	=> $a['warnings'],
+			'describe'	=> $a['describe'],
+			'trace'		=> $a['trace'] ?: main()->trace_string(),
+			'cached'	=> (int)$a['cached'],
+			'time'		=> round(microtime(true) - $a['time'], 5),
+			'time_wo_cache'	=> $a['time_wo_cache'] ? round($a['time_wo_cache'], 5) : '',
+		));
+	}
+
+	/**
+	*/
+	function _save_empty_results_log($sql) {
+		if (!$this->EMPTY_RESULTS_LOG_PATH) {
+			return false;
+		}
+		$out = implode('#|#', array(
+			date('YmdH'),
+			conf('CUR_DOMAIN_SHORT'),
+			common()->_db_escape($_SERVER['HTTP_REFERER']),
+			common()->_db_escape($_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']),
+			common()->_db_escape($_SERVER['HTTP_USER_AGENT']),			
+			common()->_db_escape($sql),
+		)). PHP_EOL;
+		return file_put_contents($this->EMPTY_RESULTS_LOG_PATH, $out, FILE_APPEND);
 	}
 }
