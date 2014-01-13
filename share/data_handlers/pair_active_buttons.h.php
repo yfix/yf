89c32b7888891958b0bb25ccb91be2e7430b34ff
Yuri Vysotskiy <profy.net@gmail.com>
2014-01-13 12:24:26 +0200

    added data: pair_active_buttons

---
+++ share/data_handlers/pair_active_buttons.h.php
@@ -0,0 +1,6 @@
+<?php
+
+$data = array(
+	'0' => '<button class="btn btn-mini btn-warning"><i class="icon-ban-circle"></i> '.t('Disabled').'</button>',
+	'1' => '<button class="btn btn-mini btn-success"><i class="icon-ok"></i> '.t('Active').'</button>',
+);
