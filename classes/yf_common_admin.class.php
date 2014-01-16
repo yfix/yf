b990eedfa27186406533c713eded3529e4bdda2a
Yuri Vysotskiy <profy.net@gmail.com>
2014-01-16 16:28:29 +0200

    common_admin: links checking started working in basic mode

--- classes/yf_common_admin.class.php
+++ classes/yf_common_admin.class.php
@@ -8,39 +8,63 @@ class yf_common_admin {
 	/**
 	*/
 	function _init() {
+		$this->USER_ID		= main()->USER_ID;
+		$this->USER_GROUP	= main()->USER_GROUP;
 		$this->ADMIN_ID		= main()->ADMIN_ID;
 		$this->ADMIN_GROUP	= main()->ADMIN_GROUP;
-		$this->CUR_SITE		= (int)conf('SITE_ID');
-		$this->CUR_SERVER	= (int)conf('SERVER_ID');
-		$this->CUR_LANG		= conf('language');
+#		$this->CUR_SITE		= (int)conf('SITE_ID');
+#		$this->CUR_SERVER	= (int)conf('SERVER_ID');
+#		$this->CUR_LANG		= conf('language');
+		$this->CENTER_BLOCK_ID = _class('core_blocks')->_get_center_block_id();
+	}
+
+	/**
+	*/
+	function _get_center_block_rules() {
+		$rules = &$this->CENTER_BLOCK_RULES;
+		if (isset($rules)) {
+			return $rules;
+		}
+		$rules = array();
+		foreach ((array)_class('core_blocks')->_blocks_rules as $rid => $rinfo) {
+			if ($rinfo != $this->CENTER_BLOCK_ID) {
+				continue;
+			}
+			$rules[$rid] = $rinfo;
+		}
+		$this->CENTER_BLOCK_RULES = $rules;
+		return $rules;
 	}
 
 	/**
 	*/
 	function _admin_link_is_allowed($link = '') {
+		// Currently this works only for admin section
+		if (MAIN_TYPE == 'user') {
+			return false;
+		}
 		// Guests can see nothing
-		if (!strlen($link) || !main()->ADMIN_ID) {
+		if (!strlen($link) || !main()->ADMIN_ID || MAIN_TYPE == 'user') {
 			return false;
 		}
 		// Super-admin can see any links
 		if (main()->ADMIN_GROUP === 1) {
 			return true;
 		}
-/*
-		if (empty($menu_id) || empty($this->_menu_items[$menu_id])) {
-			return false;
+		$u = array();
+		parse_str(parse_url($link, PHP_URL_QUERY), $u);
+		$u = (array)$u;
+		if (isset($u['task']) && in_array($u['task'], array('login','logout'))) {
+			return true;
 		}
-		if (MAIN_TYPE_ADMIN) {
-			$CUR_USER_GROUP = (int)main()->ADMIN_GROUP;
-		} elseif (MAIN_TYPE_USER) {
-			$CUR_USER_GROUP = (int)main()->USER_GROUP;
-			if (empty($CUR_USER_GROUP)) {
-				$CUR_USER_GROUP = 1;
-			}
-		}
-		$CUR_SITE		= (int)conf('SITE_ID');
-		$CUR_SERVER		= (int)conf('SERVER_ID');
+		return (int)_class('core_blocks')->_check_block_rights($this->CENTER_BLOCK_ID, $u['object'], $u['action']);
+#print_r($u);
+// Temporary allow until checking code is done
+#		return true;
 
+#		$is_allowed = _class('core_blocks')->_check_block_rights($this->CENTER_BLOCK_ID, $object, $action);
+
+/*
 		$items_ids		= array();
 		$items_array	= array();
 		foreach ((array)$this->_menu_items[$menu_id] as $item_info) {
