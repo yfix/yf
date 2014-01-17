fff6c4bc6ce748cbcf2354d53f41ddc75aaa2b9e
Yuri Vysotskiy <profy.net@gmail.com>
2014-01-17 14:19:45 +0200

    manage_shop: product_edit big refactoring done, now working ok

--- admin_modules/manage_shop/yf_manage_shop_product_edit.class.php
+++ admin_modules/manage_shop/yf_manage_shop_product_edit.class.php
@@ -1,5 +1,6 @@
 <?php
-class yf_manage_shop_product_edit{
+
+class yf_manage_shop_product_edit {
 
 	function product_edit () {
 		$_GET['id'] = intval($_GET['id']);
@@ -9,51 +10,40 @@ class yf_manage_shop_product_edit{
 		if (module('manage_shop')->SUPPLIER_ID) {
 			$sql = 'SELECT p.* FROM '.db('shop_products').' AS p
 					INNER JOIN '.db('shop_admin_to_supplier').' AS m ON m.supplier_id = p.supplier_id 
-					WHERE 
-						p.id='.intval($_GET['id']).'
+					WHERE p.id='.intval($_GET['id']).'
 						AND m.admin_id='.intval(main()->ADMIN_ID).'';
 		} else {
 			$sql = 'SELECT * FROM '.db('shop_products').' WHERE id='.$_GET['id'];
 		}
-		$product_info = db()->query_fetch($sql);
+		$product_info = db()->get($sql);
 		if (empty($product_info['id'])) {
 			return _e('Product not found');
 		}
 		if (main()->is_post()) {
+// TODO: use validation from form2()
 			if (!$_POST['name']) {
-				_re('Product name must be filled');
-			}
-			if ($_POST['ext_url']) {
-				if (substr($_POST['ext_url'], 0, 7) !== 'http://') {
-					$_POST['ext_url'] = 'http://'.$_POST['ext_url'];
-				}
+				_re('Product name must be filled', 'name');
 			}
 			if (!common()->_error_exists()) {
 				$sql_array = array(
-					'name'				=> $_POST['name'],
-					'model'				=> $_POST['model'],
-					'articul'			=> $_POST['articul'],
-					'cat_id'			=> $_POST['cat_id'],
 					'url'				=> $_POST['url'] ?: common()->_propose_url_from_name($_POST['name']),
-					'description'		=> $_POST['desc'],
-					'meta_keywords'		=> $_POST['meta_keywords'],
-					'meta_desc'			=> $_POST['meta_desc'],
-					'external_url'		=> $_POST['ext_url'],
 					'price'				=> number_format($_POST['price'], 2, '.', ''),
 					'price_promo'		=> number_format($_POST['price_promo'], 2, '.', ''),
 					'price_partner'		=> number_format($_POST['price_partner'], 2, '.', ''),
 					'price_raw'			=> number_format($_POST['price_raw'], 2, '.', ''),
-					'old_price'			=> number_format($_POST['old_price'], 2, '.', ''),
-					'manufacturer_id'	=> intval($_POST['manufacturer']),
-					'supplier_id'		=> intval($_POST['supplier']),
-					'quantity'			=> intval($_POST['quantity']),
-					'featured'			=> intval((bool)$_POST['featured']),
 					'active'			=> intval((bool)$_POST['active']),
 					'update_date'		=> time(),
 				);
+				foreach (array('name','description','model','articul','cat_id','meta_keywords','meta_desc','featured','external_url','sku','stock_status_id','old_price','manufacturer_id','supplier_id','quantity') as $k) {
+					if (isset($_POST[$k])) {
+						$sql_array[$k] = $_POST[$k];
+					}
+				}
 				if (!empty($_FILES)) {
-					module('manage_shop')->_product_image_upload($_GET['id']);
-					module('manage_shop')->_product_images_add_revision($_GET['id']);
+					$last_img_insert_id = module('manage_shop')->_product_image_upload($_GET['id']);
+					if ($last_img_insert_id) {
+						module('manage_shop')->_product_images_add_revision($_GET['id']);
+					}
 				} 
 				db()->update_safe(db('shop_products'), $sql_array, 'id='.$_GET['id']);
 				
@@ -115,117 +105,63 @@ class yf_manage_shop_product_edit{
 			}
 			return js_redirect('./?object='.main()->_get('object').'&action=product_edit&id='.$_GET['id']);
 		}
-		
-		$images = common()->shop_get_images($product_info['id']);
-		$base_url = WEB_PATH;
+
 		$media_host = defined('MEDIA_HOST') ? MEDIA_HOST : false;
+		$base_url = WEB_PATH;
 		if (!empty($media_host)) {
 			$base_url = '//' . $media_host . '/';
 		}
 		$images_items = array();
-		foreach ((array)$images as $A) {
-			$product_image_delete_url = './?object='.main()->_get('object').'&action=product_image_delete&id='.$product_info['id'].'&key='.$A['id'];
-			$replace2 = array(
-				'img_path'   => $base_url . $A['big'],
-				'thumb_path' => $base_url . $A['thumb'],
-				'del_url'    => $product_image_delete_url,
-				'image_key'  => $A['id'],
-			);
-			$images_items[] = tpl()->parse('manage_shop/image_items', $replace2);
+		foreach ((array)common()->shop_get_images($product_info['id']) as $a) {
+			$images_items[] = tpl()->parse('manage_shop/image_items', array(
+				'img_path'   => $base_url . $a['big'],
+				'thumb_path' => $base_url . $a['thumb'],
+				'del_url'    => './?object='.main()->_get('object').'&action=product_image_delete&id='.$product_info['id'].'&key='.$a['id'],
+				'image_key'  => $a['id'],
+			));
 		}
-		// 1-st type of assigning attributes
-		$fields = module('manage_shop')->_attributes_html($_GET['id']);
-		// 2-nd type of assigning attributes (select boxes)
-		// For case when we need just select custom attributes only one value of each
-		$all_atts	= module('manage_shop')->_get_attributes();
-		$saved_attrs	= module('manage_shop')->_get_products_attributes($_GET['id']);
-		foreach ((array)$all_atts as $_attr_id => $_attr_info) {
-			$_name_in_form = 'single_attr['.$_attr_id.']';
-			$_selected = '';
-			// Try to get selected value
-			$_cur_item_prefix = $_attr_id.'_';
-			foreach ((array)$saved_attrs as $_item_id => $_item_info) {
-				if (substr($_item_id, 0, strlen($_cur_item_prefix)) == $_cur_item_prefix) {
-					$_selected = substr($_item_id, strlen($_cur_item_prefix));
-					break;
-				}
-			}
-			$single_atts[$_attr_info['title']] = array(
-				'title'			=> _prepare_html($_attr_info['title']),
-				'name_in_form'	=> _prepare_html($_name_in_form),
-				'box'			=> common()->select_box($_name_in_form, $_attr_info['value_list'], $_selected, false, 2, '', false),
-			);
-		}
-		$sql1 = 'SELECT category_id FROM '.db('shop_product_to_category').' WHERE product_id = '. $_GET['id'];
-		$products = db()->query($sql1);
-		while ($A = db()->fetch_assoc($products)) {
-			$cat_id[$A['category_id']] .= $A['category_id'];
+		$products_to_category = array();
+		foreach ((array)db()->get_all('SELECT category_id FROM '.db('shop_product_to_category').' WHERE product_id='.intval($_GET['id'])) as $a) {
+			$products_to_category[$a['category_id']] = $a['category_id'];
 		}	
-		$replace = array(
-			'name'               => $product_info['name'],
-			'model'              => $product_info['model'],
-			'desc'               => $product_info['description'],
-			'meta_keywords'      => $product_info['meta_keywords'],
-			'meta_desc'          => $product_info['meta_desc'],
-			'price'              => $product_info['price'],
-			'price_promo'        => $product_info['price_promo'],
-			'price_partner'      => $product_info['price_partner'],
-			'price_raw'          => $product_info['price_raw'],
-			'old_price'          => $product_info['old_price'],
-			'quantity'           => $product_info['quantity'],
-			'url'                => $product_info['url'],
-			'articul'            => $product_info['articul'],
-			'active'             => $product_info['active'],
-			'add_date'           => $product_info['add_date'],
-			'update_date'        => $product_info['update_date'],
-			'productparams'      => module('manage_shop','admin_modules')->_productparams_container($_GET['id']),
-			'dynamic_fields'     => $fields,
-			'single_atts'        => $single_atts,
-			'ext_url'            => $product_info['external_url'],
-			'manufacturer_box'   => common()->select_box('manufacturer', module('manage_shop')->_man_for_select, $product_info['manufacturer_id'], false, 2),
-			'supplier_box'       => common()->select_box('supplier', module('manage_shop')->_suppliers_for_select, $product_info['supplier_id'], false, 2),
-			'category_box'       => common()->multi_select('category', module('manage_shop')->_cats_for_select, $cat_id, false, 2, ' size=5 ', false),
-			'cat_id_box'         => common()->select_box('cat_id', module('manage_shop')->_cats_for_select, $product_info['cat_id'], false, 2),
-			'featured_box'       => module('manage_shop')->_box('featured', $product_info['featured']),
-			'featured'           => $product_info['featured'],
+		$replace = $product_info + array(
 			'form_action'        => './?object='.main()->_get('object').'&action=product_edit&id='.$product_info['id'],
 			'back_url'           => './?object='.main()->_get('object').'&action=products',
-			'image'              => $images_items ? implode(PHP_EOL, $images_items) : '',
-			'categories_url'     => './?object=category_editor&action=show_items&id=shop_cats',
-			'manufacturers_url'  => './?object='.main()->_get('object').'&action=manufacturers',
-			'suppliers_url'      => './?object='.main()->_get('object').'&action=suppliers',
-			'manage_attrs_url'   => './?object='.main()->_get('object').'&action=attributes',
-			'group_prices'       => !empty($group_prices) ? $group_prices : '',
-			'link_get_product'   => process_url('./?object='.main()->_get('object').'&action=show_product_by_category&cat_id='),
-			'product_related'    => module('manage_shop')->related_products($product_info['id']),
-			'set_main_image_url' => './?object='.$_GET['object'].'&action=set_main_image&id='.$product_info['id'],
-			'search_url'         => './?object='.main()->_get('object').'&action=product_image_search&id='.$product_info['id'],
-			'product_url_user'	 => url('/shop/product/'.$product_info['id']),
 		);
-#		return tpl()->parse('manage_shop/product_edit', $replace);
 		return form($replace, array(
+// TODO: use validation
 				'for_upload' => 1,
 				'currency' => module('manage_shop')->CURRENCY,
 				'hide_empty' => 1
 			))
-			->info_link('product_url_user')
+			->link('product_url_user', url('/shop/product/'.$product_info['id']), array('target' => '_blank'))
 			->text('name')
 			->text('articul')
 			->text('url')
-			->box('cat_id_box', 'Main category', array('edit_link' => 'categories_url'))
-			->box('category_box', 'Secondary categories', array('edit_link' => 'categories_url'))
-			->box('manufacturer_box', 'Manufacturer', array('edit_link' => 'manufacturers_url'))
-			->box('supplier_box', 'Supplier', array('edit_link' => 'suppliers_url'))
-			->textarea('desc', 'Description')
+			->select_box('cat_id', module('manage_shop')->_cats_for_select, array('desc' => 'Main category', 'edit_link' => './?object=category_editor&action=show_items&id=shop_cats'))
+// TODO: replace with similar JS container as for params and images
+#			->multi_select('category', module('manage_shop')->_cats_for_select, array('desc' => 'Secondary categories', 'edit_link' => './?object=category_editor&action=show_items&id=shop_cats', 'selected' => $products_to_category))
+			->select_box('manufacturer_id', module('manage_shop')->_man_for_select, array('desc' => 'Manufacturer', 'edit_link' => './?object='.main()->_get('object').'&action=manufacturers'))
+			->select_box('supplier_id', module('manage_shop')->_suppliers_for_select, array('desc' => 'Supplier', 'edit_link' => './?object='.main()->_get('object').'&action=suppliers'))
+			->textarea('description')
 			->money('price')
 			->money('price_promo')
 			->money('price_partner')
 			->money('price_raw')
 			->number('quantity')
-			->container(($images_items ? implode(PHP_EOL, $images_items) : ''). '<a class="btn btn-mini" onclick="addImage();"><span>{t(Add Image)}</span></a>'. '<div id="images"></div>', array('desc' => 'Images'))
-			->link('Set main image', $replace['set_main_image_url'], array('class_add' => 'ajax_edit', 'display_func' => function() use ($images_items) { return (is_array($images_items) && count($images_items) > 1); }))
-			->link('Search images', $replace['search_url'], array('class_add' => 'btn-success'))
-			->container(module('manage_shop')->_productparams_container($_GET['id']), array('desc' => 'Product params'))
+			->container(
+				($images_items ? implode(PHP_EOL, $images_items) : ''). 
+				'<a class="btn btn-mini" onclick="addImage();"><span>'.t('Add Image').'</span></a> <div id="images"></div>'
+				, array('desc' => 'Images')
+			)
+			->link('Set main image', './?object='.$_GET['object'].'&action=set_main_image&id='.$product_info['id'], array(
+				'class_add' => 'ajax_edit',
+				'display_func' => function() use ($images_items) { return (is_array($images_items) && count($images_items) > 1); }
+			))
+			->link('Search images', './?object='.main()->_get('object').'&action=product_image_search&id='.$product_info['id'], array('class_add' => 'btn-success'))
+			->container(module('manage_shop')->_productparams_container($_GET['id']), array('desc' => 'Product params'/*, 'edit_link' => './?object='.main()->_get('object').'&action=attributes'*/))
+// TODO: replace with similar JS container as for params and images
+#			->container(module('manage_shop')->related_products($product_info['id']), array('desc' => 'Related products'))
 			->active_box('active')
 			->save_and_back()
 			.tpl()->parse('manage_shop/product_edit_js');
