d8fa5792586fe3826dc8caf59f3558f6b9785d92
Yuri Vysotskiy <profy.net@gmail.com>
2014-01-22 13:43:35 +0200

    manage shop: added side hook submodule

---
+++ admin_modules/manage_shop/yf_manage_shop_hook_side_column.class.php
@@ -0,0 +1,35 @@
+<?php
+
+class yf_manage_shop_hook_side_column {
+
+	/***/
+	function _hook_side_column () {
+		if ($_GET['action'] == 'product_edit') {
+			return $this->_product_revisions();
+		} elseif ($_GET['action'] == 'view_order') {
+			return $this->_order_revisions();
+		}
+		return '';
+	}
+
+	/***/
+	function _product_revisions () {
+		$product_id = intval($_GET['id']);
+		$product_info = module('manage_shop')->_product_get_info($product_id);
+		if (!$product_info) {
+			return false;
+		}
+		$sql = 'SELECT * FROM '.db('shop_product_revisions').' WHERE item_id='.intval($product_id).' ORDER BY add_date DESC';
+		return table($sql, array('caption' => 'Product revisions'))
+			->date('add_date', array('format' => 'full', 'nowrap' => 1))
+			->admin('user_id', array('desc' => 'admin'))
+			->text('action')
+			->btn_view('', './?object=manage_shop&action=product_revisions&id=%d')
+		;
+	}
+
+	/***/
+	function _order_revisions () {
+#		return 'Hello';
+	}
+}
