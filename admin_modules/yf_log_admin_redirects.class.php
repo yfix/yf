d96ede12ca24a1fa65a07e11e11d048e7bdf27c9
Yuri Vysotskiy <profy.net@gmail.com>
2014-01-23 16:24:26 +0200

    admin: added log admin and user redirects

---
+++ admin_modules/yf_log_admin_redirects.class.php
@@ -0,0 +1,93 @@
+<?php
+
+class yf_log_admin_redirects {
+
+	/**
+	*/
+	function show () {
+		$filter_name = $_GET['object'].'__'.$_GET['action'];
+		$default_filter = array(
+			'order_by' => 'date',
+			'order_direction' => 'desc',
+		);
+		$sql = 'SELECT * FROM '.db('log_redirects').' WHERE is_admin='.strval(!$this->FOR_USER ? 1 : 0);
+		return table($sql, array(
+				'filter' => (array)$_SESSION[$filter_name] + $default_filter,
+				'filter_params' => array(
+					'url_from'		=> 'like',
+					'url_to'		=> 'like',
+					'ip'			=> 'like',
+					'user_agent'	=> 'like',
+					'referer'		=> 'like',
+				),
+			))
+			->admin('user_id')
+			->link('ip', './?object='.$_GET['object'].'&action=show_for_ip&id=%d')
+			->date('date', array('format' => 'full', 'nowrap' => 1))
+			->text('user_agent')
+			->text('referer')
+			->text('url_from')
+			->text('url_to')
+			->text('exec_time')
+		;
+	}
+
+	/**
+	*/
+	function show_for_admin() {
+		$_GET['page'] = 'clear';
+		$_GET['filter'] = 'admin_id:'.intval($_GET['id']);
+		return $this->filter_save();
+	}
+
+	/**
+	*/
+	function show_for_ip() {
+		$_GET['page'] = 'clear';
+		$_GET['filter'] = 'ip:'.preg_replace('~[^0-9\.]+~ims', '', $_GET['id']);
+		return $this->filter_save();
+	}
+
+	/**
+	*/
+	function filter_save() {
+		return _class('admin_methods')->filter_save();
+	}
+
+	/**
+	*/
+	function _show_filter() {
+		if (!in_array($_GET['action'], array('show'))) {
+			return false;
+		}
+		$filter_name = $_GET['object'].'__'.$_GET['action'];
+		$r = array(
+			'form_action'	=> './?object='.$_GET['object'].'&action=filter_save&id='.$filter_name,
+			'clear_url'		=> './?object='.$_GET['object'].'&action=filter_save&id='.$filter_name.'&page=clear',
+		);
+		$order_fields = array();
+		foreach (explode('|', 'user_id|user_group|date|ip|user_agent|referer|url_from|url_to') as $f) {
+			$order_fields[$f] = $f;
+		}
+		return form($r, array(
+				'selected'	=> $_SESSION[$filter_name],
+			))
+			->number('user_id')
+			->text('ip')
+			->text('user_agent')
+			->text('referer')
+			->text('url_from')
+			->text('url_to')
+			->select_box('order_by', $order_fields, array('show_text' => 1))
+			->radio_box('order_direction', array('asc'=>'Ascending','desc'=>'Descending'))
+			->save_and_clear();
+		;
+	}
+
+	/**
+	*/
+	function _hook_widget__redirects_log ($params = array()) {
+// TODO
+	}
+
+}
---
+++ admin_modules/yf_log_user_redirects.class.php
@@ -0,0 +1,6 @@
+<?php
+
+load('log_admin_redirects', 'framework');
+class yf_log_user_redirects extends yf_log_admin_redirects {
+	public $FOR_USER = 1;
+}
