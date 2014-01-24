4bbd2e9cfeea564443e9b265ea172d78f7f5e7f7
Yuri Vysotskiy <profy.net@gmail.com>
2014-01-24 13:04:19 +0200

    data handlers up

--- share/data_handlers.php
+++ share/data_handlers.php
@@ -1,13 +1,18 @@
 <?php
-if (!defined('H_PATH')) {
-	define('H_PATH', YF_PATH.'share/data_handlers/');
-}
+
+$handlers_dirs = array(
+	'framework'	=> YF_PATH.'share/data_handlers/',
+	'project'	=> PROJECT_PATH.'share/data_handlers/',
+);
 $handlers = array();
-$strlen_dir = strlen(H_PATH);
-$strlen_suffix = strlen('.h.php');
-foreach(glob(H_PATH.'*.h.php') as $f) {
-	$name = substr($f, $strlen_dir, -$strlen_suffix);
-	$handlers[$name] = 'include("'.$f.'");';
+$h_suffix = '.h.php';
+$strlen_suffix = strlen($h_suffix);
+foreach ($handlers_dirs as $handlers_dir) {
+	$strlen_dir = strlen($handlers_dir);
+	foreach(glob($handlers_dir.'*'.$h_suffix) as $f) {
+		$name = substr($f, $strlen_dir, -$strlen_suffix);
+		$handlers[$name] = 'include("'.$f.'");';
+	}
 }
 $handlers_aliases = array(
 	'category_sets'	=> 'cats_blocks',
