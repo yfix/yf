c658fa39b78d891fd8ec42943c1800c5362b2191
Yuri Vysotskiy <profy.net@gmail.com>
2014-01-24 15:19:17 +0200

    db_revisions db structure up

--- share/db_installer/db_table_sql/sys_db_revisions.db_table_sql.php
+++ share/db_installer/db_table_sql/sys_db_revisions.db_table_sql.php
@@ -1,17 +1,18 @@
 <?php
 $data = '
 	`id` int(10) unsigned NOT NULL auto_increment,
+	`query_method` varchar(128) NOT NULL default \'\',
+	`query_table` varchar(128) NOT NULL default \'\',
 	`date` datetime NOT NULL,
 	`data_old` longtext NOT NULL default \'\',
 	`data_new` longtext NOT NULL default \'\',
 	`data_diff` longtext NOT NULL default \'\',
-	`object_name` varchar(64) NOT NULL default \'\',
-	`object_id` varchar(64) NOT NULL default \'\',
 	`user_id` int(10) unsigned NOT NULL default \'0\',
+	`user_group` int(10) unsigned NOT NULL default \'0\',
 	`site_id` tinyint(3) unsigned NOT NULL default \'0\',
 	`server_id` tinyint(3) unsigned NOT NULL default \'0\',
 	`ip` char(15) NOT NULL,
-	`query_type` varchar(32) NOT NULL default \'\',
-	`comment` varchar(255) NOT NULL default \'\',
+	`url` text NOT NULL default \'\',
+	`extra` longtext NOT NULL default \'\',
 	PRIMARY KEY	(`id`)
 ';
