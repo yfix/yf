cd301b3bada5e9351d6c9fa4dc6cf7f060c131b1
Yuri Vysotskiy <profy.net@gmail.com>
2014-02-09 00:33:30 +0200

    installer sql converted into php data

--- .dev/__SCRIPTS/installer_data/installer_sql_to_php.php
+++ .dev/__SCRIPTS/installer_data/installer_sql_to_php.php
@@ -1,9 +1,10 @@
 #!/usr/bin/php
 <?php
 
+define('DB_TYPE', 'mysql5');
 define('DB_HOST', 'localhost');
 #define('DB_NAME', 'tmp_'.substr(md5(microtime().'bt6reedc5zw3'), 8));
-define('DB_NAME', 'tmp_123');
+define('DB_NAME', 'tmp_sql_to_php');
 define('DB_USER', 'root');
 define('DB_PSWD', '123456');
 define('DB_PREFIX', 'tmp_');
@@ -27,24 +28,42 @@ function get_table_create_sql($table) {
 	if (!$data) {
 		return false;
 	}
-	return 'CREATE TABLE IF NOT EXISTS '.DB_PREFIX. $table.' ('.$data.')';
+	return 'CREATE TABLE IF NOT EXISTS '.DB_PREFIX. $table.' ('.$data.') ENGINE=InnoDB DEFAULT CHARSET=utf8;';
 }
 
+mkdir('./data/', 0755, true);
 foreach (glob(dirname(dirname(dirname(__FILE__))).'/__INSTALL/install/sql/*.sql') as $f) {
 	$fname = basename($f);
 	$table = substr($fname, 0, -strlen('.sql'));
 	$tname = DB_PREFIX. $table;
 	if ($fname[0] == '_') {
-// TODO
-		continue;
+		$lang = substr($fname, -strlen('.sql') - 2, -strlen('.sql'));
+		$dir = './data_'.$lang.'/';
+		mkdir($dir, 0755, true);
+		foreach (explode(';'.PHP_EOL, file_get_contents($f)) as $sql) {
+			if (preg_match('/%%prefix%%([a-z0-9_]+)/ims', $sql, $m)) {
+				$t = trim($m[1]);
+				$create_sql = get_table_create_sql($t);
+				if (!$create_sql) {
+					continue;
+				}
+				db()->query($create_sql);
+				db()->query('TRUNCATE TABLE '.DB_PREFIX. $t);
+				db()->query(str_replace('%%prefix%%', DB_PREFIX, $sql));
+				$data = db()->get_all('SELECT * FROM '.DB_PREFIX. $t);
+				if (!empty($data)) {
+					file_put_contents($dir. $t.'.data.php', '<?'.'php'.PHP_EOL.'$data = '.var_export($data, 1).';');
+				}
+			}
+		}
 	} else {
-		echo get_table_create_sql($table);
 		db()->query(get_table_create_sql($table));
-		$sql = str_replace('%%prefix%%', DB_PREFIX, file_get_contents($f));
-		db()->query($sql)
- or print db()->error();
+		db()->query('TRUNCATE TABLE '.DB_PREFIX. $table);
+		db()->query(str_replace('%%prefix%%', DB_PREFIX, file_get_contents($f)));
 		$data = db()->get_all('SELECT * FROM '.$tname);
-var_export($data);
+		if (!empty($data)) {
+			file_put_contents('./data/'.$table.'.data.php', '<?'.'php'.PHP_EOL.'$data = '.var_export($data, 1).';');
+		}
 	}
 }
 
