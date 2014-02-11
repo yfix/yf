aadb63f8c1fead5886df2fa662e5a09907b13b2f
Yuri Vysotskiy <profy.net@gmail.com>
2014-02-11 17:59:21 +0200

    db query builder unit tests up

--- .dev/__TESTS/class_db_query_builder.Test.php
+++ .dev/__TESTS/class_db_query_builder.Test.php
@@ -10,6 +10,7 @@ class class_db_query_builder_test extends PHPUnit_Framework_TestCase {
 		$this->assertEquals( 'SELECT *', self::qb()->select()->_sql['select'] );
 		$this->assertEquals( 'SELECT *', self::qb()->select('*')->_sql['select'] );
 		$this->assertEquals( 'SELECT *', self::qb()->select(' *')->_sql['select'] );
+		$this->assertEquals( 'SELECT *', self::qb()->select('* ')->_sql['select'] );
 		$this->assertEquals( 'SELECT *', self::qb()->select('   *   ')->_sql['select'] );
 		$this->assertFalse( self::qb()->select()->render() );
 		$this->assertFalse( self::qb()->select()->__toString() );
@@ -34,5 +35,23 @@ class class_db_query_builder_test extends PHPUnit_Framework_TestCase {
 		$this->assertFalse( self::qb()->select(false)->render() );
 		$this->assertFalse( self::qb()->select(0)->render() );
 		$this->assertFalse( self::qb()->select('0')->render() );
+	}
+// TODO
+#	public function test_05() {
+#		$this->assertEquals( 'SELECT `id id`', self::qb()->select('id id')->_sql['select'] );
+#		$this->assertEquals( 'SELECT u.id, s.id, t.pid', self::qb()->select('u.id', 's.id', 't.pid')->_sql['select'] );
+#		$this->assertEquals( 'SELECT u.id, a.id, b.id', self::qb()->select(array('u.id', 'a.id', 'b.id'))->_sql['select'] );
+#		$this->assertEquals( 'SELECT u.id AS user_id, a.id AS article_id, b.id AS blog_id', self::qb()->select(array('u.id' => 'user_id', 'a.id' => 'article_id', 'b.id' => 'blog_id'))->_sql['select'] );
+#		$this->assertEquals( 'SELECT u.id AS user_id', self::qb()->select(array('u.id' => 'user_id'))->_sql['select'] );
+#		$this->assertEquals( 'SELECT COUNT(*) AS num', self::qb()->select(array('COUNT(*)' => 'num'))->_sql['select'] );
+#		$this->assertEquals( 'SELECT COUNT(id) AS num', self::qb()->select(array('COUNT(id)' => 'num'))->_sql['select'] );
+#		$this->assertEquals( 'SELECT COUNT(u.id) AS num', self::qb()->select(array('COUNT(u.id)' => 'num'))->_sql['select'] );
+#		$this->assertEquals( 'SELECT DISTINCT u.id', self::qb()->select('DISTINCT u.id')->_sql['select'] );
+#		$this->assertEquals( 'SELECT DISTINCT u.id AS num', self::qb()->select(array('DISTINCT u.id' => 'num'))->_sql['select'] );
+#		$this->assertEquals( 'SELECT DISTINCT u.id AS num, a.id AS article_id', self::qb()->select( function(){return 'DISTINCT u.id AS num';}, function(){return 'a.id AS article_id';} )->_sql['select'] );
+#	}
+	public function test_10() {
+		$this->assertFalse( self::qb()->select()->from()->render() );
+		$this->assertEquals( 'SELECT * FROM '.DB_PREFIX.'user', self::qb()->select()->from('user')->render() );
 	}
 }
