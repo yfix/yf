58c1684e45501defa6cfccfabbd977a4104fbe25
Yuri Vysotskiy <profy.net@gmail.com>
2014-02-13 13:21:38 +0200

    installer data cleanup

--- share/db_installer/data/sys_locale_langs.data.php
+++ share/db_installer/data/sys_locale_langs.data.php
@@ -1,11 +1,30 @@
 <?php
 $data = array (
-  99999 => 
+  1 => 
   array (
+    'id' => '1',
     'locale' => 'en',
     'name' => 'English',
     'charset' => 'utf-8',
-    'active' => 1,
-    'is_default' => 1,
+    'active' => '1',
+    'is_default' => '1',
+  ),
+  2 => 
+  array (
+    'id' => '2',
+    'locale' => 'ru',
+    'name' => 'Russian',
+    'charset' => 'utf-8',
+    'active' => '1',
+    'is_default' => '0',
+  ),
+  3 => 
+  array (
+    'id' => '3',
+    'locale' => 'uk',
+    'name' => 'Ukrainian',
+    'charset' => 'utf-8',
+    'active' => '1',
+    'is_default' => '0',
   ),
 );
