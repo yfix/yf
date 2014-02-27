10d406d3ac62783d5adf55551a4452cc4a144aa6
Yuri Vysotskiy <profy.net@gmail.com>
2014-02-27 14:58:05 +0200

    added todo unit tests for require_js and require_css

---
+++ .dev/__TESTS/func_require_js.Test.php
@@ -0,0 +1,32 @@
+<?php
+
+require dirname(__FILE__).'/yf_unit_tests_setup.php';
+
+class func_require_js_test extends PHPUnit_Framework_TestCase {
+	public function test_01() {
+#		$this->assertEquals( require_js('//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.3.2/ckeditor.js'), _class('core_js')-> );
+// TODO
+/*
+	function _content_is_url($content) {
+// TODO: //domain.com/script.js
+// TODO: http://domain.com/script.js
+// TODO: https://domain.com/script.js
+// TODO: domain.com/script.js
+// TODO: /script.js
+// TODO: script.js
+// TODO: explode by newlines into several items 
+	}
+
+	function _content_is_file($content) {
+// TODO: file_exists and not starts with (http:// | https:// | //)
+// TODO: file allowed to begin with PROJECT_PATH, SITE_PATH or YF_PATH
+	}
+
+	function _content_is_inline($content) {
+// TODO: can contain one line, but not like url
+// TODO: can be without <script> inside
+// TODO: trim <script[^>]+>(?P<content>.+?)</script>
+	}
+*/
+	}
+}
