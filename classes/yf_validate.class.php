bcc71c2a8dc1019d78a87a34c266359a2b4ab02d
Yuri Vysotskiy <profy.net@gmail.com>
2014-03-13 17:54:13 +0200

    validate bugfix for unique_without

--- classes/yf_validate.class.php
+++ classes/yf_validate.class.php
@@ -483,7 +483,7 @@ class yf_validate {
 			list($check_table, $check_field, $check_id) = explode('.', $param);
 		}
 		if ($check_table && $check_field && $check_id && $in) {
-			$exists = db()->get_one('SELECT `'.db()->es($check_field).'` FROM '.db($check_table).' WHERE `'.db()->es($check_field).'`="'.db()->es($in).'" AND `'.db()->es($check_field).'` != "'.db()->es($check_id).'"');
+			$exists = db()->get_one('SELECT `'.db()->es($check_field).'` FROM '.db($check_table).' WHERE `'.db()->es($check_field).'`="'.db()->es($in).'" AND `'.db()->es($id_field).'` != "'.db()->es($check_id).'"');
 			if ($exists == $in) {
 				return false;
 			}
