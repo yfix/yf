d8e8c5228dd33612d15b6f0549ed9a3cdbe2fdb9
Yuri Vysotskiy <profy.net@gmail.com>
2014-03-18 15:10:23 +0200

    unit tests for db class: covered _fix_table_name()

--- .dev/__TESTS/class_db.Test.php
+++ .dev/__TESTS/class_db.Test.php
@@ -4,6 +4,32 @@ require dirname(__FILE__).'/yf_unit_tests_setup.php';
 require dirname(__FILE__).'/db_setup.php';
 
 class class_db_test extends PHPUnit_Framework_TestCase {
+	private function db() {
+		return _class('db');
+	}
+	public function test_db_prefix() {
+		$this->assertEquals(DB_PREFIX, self::db()->DB_PREFIX);
+	}
+	public function test_fix_table_name() {
+		$this->assertEquals('', self::db()->_fix_table_name(''));
+		$this->assertEquals(self::db()->DB_PREFIX.'sys_admin', self::db()->_fix_table_name('admin'));
+		$this->assertEquals(self::db()->DB_PREFIX.'sys_admin_modules', self::db()->_fix_table_name('admin_modules'));
+
+		$this->assertEquals(self::db()->DB_PREFIX.'sys_admin', self::db()->_fix_table_name(self::db()->DB_PREFIX.'admin'));
+		$this->assertEquals(self::db()->DB_PREFIX.'sys_admin_modules', self::db()->_fix_table_name(self::db()->DB_PREFIX.'admin_modules'));
+
+		$this->assertEquals(self::db()->DB_PREFIX.'sys_admin', self::db()->_fix_table_name('sys_admin'));
+		$this->assertEquals(self::db()->DB_PREFIX.'sys_admin_modules', self::db()->_fix_table_name('sys_admin_modules'));
+
+		$this->assertEquals(self::db()->DB_PREFIX.'sys_admin', self::db()->_fix_table_name(self::db()->DB_PREFIX.'sys_admin'));
+		$this->assertEquals(self::db()->DB_PREFIX.'sys_admin_modules', self::db()->_fix_table_name(self::db()->DB_PREFIX.'sys_admin_modules'));
+
+		$this->assertEquals(self::db()->DB_PREFIX.'admin_not_existing_table', self::db()->_fix_table_name('admin_not_existing_table'));
+		$this->assertEquals(self::db()->DB_PREFIX.'dashboards', self::db()->_fix_table_name('dashboards'));
+
+		$this->assertEquals(self::db()->DB_PREFIX.'admin_not_existing_table', self::db()->_fix_table_name(self::db()->DB_PREFIX.'admin_not_existing_table'));
+		$this->assertEquals(self::db()->DB_PREFIX.'dashboards', self::db()->_fix_table_name(self::db()->DB_PREFIX.'dashboards'));
+	}
 	public function test_insert_01() {
 		$out = array();
 
