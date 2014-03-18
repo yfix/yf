d507034dc715677c52ce2c27674fbb7d132dbbe3
root <alexvillin@gmail.com>
2014-03-18 17:55:32 +0200

    shop_revisions.sql.php

---
+++ share/db_installer/sql/shop_revisions.sql.php
@@ -0,0 +1,12 @@
+<?php
+$data = '
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL DEFAULT \'0\',
+  `add_date` int(11) NOT NULL DEFAULT \'0\',
+  `action` varchar(127) NOT NULL DEFAULT '',
+  `item_id` int(11) NOT NULL DEFAULT \'0\',
+  `ip` char(15) NOT NULL,
+  `data` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `item_id` (`item_id`)
+';
