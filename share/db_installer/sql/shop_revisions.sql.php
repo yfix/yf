d7392de70efce7cfd5dcf2b43b8a2ec6b80c8c92
root <alexvillin@gmail.com>
2014-03-19 11:41:05 +0200

    fix revisions sql

--- share/db_installer/sql/shop_revisions.sql.php
+++ share/db_installer/sql/shop_revisions.sql.php
@@ -3,7 +3,7 @@ $data = '
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL DEFAULT \'0\',
   `add_date` int(11) NOT NULL DEFAULT \'0\',
-  `action` varchar(127) NOT NULL DEFAULT '',
+  `action` varchar(127) NOT NULL DEFAULT \'\',
   `item_id` int(11) NOT NULL DEFAULT \'0\',
   `ip` char(15) NOT NULL,
   `data` text NOT NULL,
