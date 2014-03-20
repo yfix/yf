ddcb1ff84d1bf215a30032b0a7ec4d9537282f0a
root <alexvillin@gmail.com>
2014-03-20 12:25:58 +0200

    fix

--- share/db_installer/sql/shop_revisions.sql.php
+++ share/db_installer/sql/shop_revisions.sql.php
@@ -6,6 +6,7 @@ $data = '
   `action` varchar(127) NOT NULL DEFAULT \'\',
   `item_id` int(11) NOT NULL DEFAULT \'0\',
   `ip` char(15) NOT NULL,
+  `table` varchar(127) NOT NULL DEFAULT \'\',
   `data` text NOT NULL,
   PRIMARY KEY (`id`),
   KEY `item_id` (`item_id`)
