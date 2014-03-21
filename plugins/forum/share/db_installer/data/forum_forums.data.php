c55ebdfd359dbd817ebd34594aa8ef0ec164e762
Yuri Vysotskiy <profy.net@gmail.com>
2014-03-21 14:22:01 +0200

    code reorganization: moved into plugins some data handlers and db installer

---
+++ plugins/forum/share/db_installer/data/forum_forums.data.php
@@ -0,0 +1,11 @@
+<?php
+$data = array (
+  99999 => 
+  array (
+    'id' => 1,
+    'category' => 1,
+    'name' => 'Test forum',
+    'desc' => 'First auto-created test forum',
+    'created' => 1386276665,
+  ),
+);
