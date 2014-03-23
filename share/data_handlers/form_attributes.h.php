2e246ae541fd933b6522806c51e201adc7134acb
Yuri Vysotskiy <profy.net@gmail.com>
2014-03-23 18:46:36 +0200

    form attributes data handler

---
+++ share/data_handlers/form_attributes.h.php
@@ -0,0 +1,6 @@
+<?php
+
+$q = db()->query('SELECT * FROM '.db('form_attributes').' WHERE active="1"');
+while ($a = db()->fetch_assoc($q)) {
+	$data[$a['form_id']][$a['field']][$a['attr']] = $a['value'];
+}
