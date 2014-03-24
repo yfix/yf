f75540dfc26ed1defd0a3d650c3c6c2486cd616f
Yuri Vysotskiy <profy.net@gmail.com>
2014-03-24 12:37:16 +0200

    added bug 03

--- .dev/__TESTS/tpl_driver_yf_bugs.Test.php
+++ .dev/__TESTS/tpl_driver_yf_bugs.Test.php
@@ -9,4 +9,18 @@ class tpl_driver_yf_bugs_test extends tpl_abstract {
 	public function test_bug_02() {
 		$this->assertEquals(' {} ', self::_tpl( ' {} ', array('' => '') ));
 	}
+	public function test_bug_03() {
+		$a = array('quantity' => 10, 'active' => 1);
+		$this->assertEquals(' ok ', self::_tpl( '{if("quantity" gt 0)} ok {/if}', $a ));
+		$this->assertEquals(' ok ', self::_tpl( '{if("active" ne 0)} ok {/if}', $a ));
+		$this->assertEquals(' ok ', self::_tpl( '{if("quantity" gt "0" and "active" ne "0")} ok {/if}', $a ));
+		$this->assertEquals(' ok ', self::_tpl( '{if(quantity gt 0 and active ne 0)} ok {/if}', $a ));
+		$this->assertEquals(' ok ', self::_tpl( '{if("quantity" gt 0 and active ne 0)} ok {/if}', $a ));
+#		$this->assertEquals(' ok ', self::_tpl( '{if("quantity" gt 0 and "active" ne 0)} ok {/if}', $a ));
+
+#		$a = array('quantity' => 10, 'active' => 0);
+#		$this->assertEquals('', self::_tpl( '{if("quantity" gt 0 and "active" ne 0)} ok {/if}', $a ));
+#		$a = array('quantity' => 0, 'active' => 0);
+#		$this->assertEquals('', self::_tpl( '{if("quantity" gt 0 and "active" ne 0)} ok {/if}', $a ));
+	}
 }
