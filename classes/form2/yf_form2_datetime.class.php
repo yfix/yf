9152263bc86b4b8385cd94e3258c3a1eebf2fb2e
Yuri Vysotskiy <profy.net@gmail.com>
2014-03-26 17:40:06 +0200

    form datetime code cleanup

--- classes/form2/yf_form2_datetime.class.php
+++ classes/form2/yf_form2_datetime.class.php
@@ -27,30 +27,34 @@ class yf_form2_datetime {
 					$extra['value'] = $_this->_params['selected'][$extra['name']];
 				}
 			}else{
-				$extra['value'] = date("d-m-Y", $extra['value']);
+				$extra['value'] = date('d-m-Y', $extra['value']);
 			}
 
 			$format = array();
-			if ($extra['no_date']!=1) $format[] = "MM/dd/yyyy";
-			if ($extra['no_time']!=1) $format[] = "HH:mm:ss";
-			_class('core_js')->add("//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.min.js", true);
-			_class('core_js')->add("//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/locales/bootstrap-datepicker.ru.min.js", true);
-			_class('core_css')->add("//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker.min.css", true);
-			$body = "
-<div id=\"{$extra['name']}\" class=\"input-append date date_bd\">
-    <input data-format=\"".implode(" ",$format)."\" name=\"{$extra['name']}\" value=\"{$extra['value']}\" type=\"text\" class=\"input-medium\" placeholder=\"{$extra['placeholder']}\"></input>
-    <span class=\"add-on\">
-		<i class=\"fa fa-calendar\"></i>
+			if ($extra['no_date']!=1) $format[] = 'MM/dd/yyyy';
+			if ($extra['no_time']!=1) $format[] = 'HH:mm:ss';
+			_class('core_js')->add('//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.min.js', true);
+			_class('core_js')->add('//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/locales/bootstrap-datepicker.ru.min.js', true);
+			_class('core_css')->add('//cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/css/datepicker.min.css', true);
+			$body = '
+<div id="'.$extra['name'].'" class="input-append date date_bd">
+    <input data-format="'.implode(' ',$format).'" name="'.$extra['name'].'" value="'.$extra['value'].'" type="text" class="input-medium" placeholder="'.$extra['placeholder'].'"></input>
+    <span class="add-on">
+		<i class="icon icon-calendar fa fa-calendar"></i>
     </span>		
 </div>
-";
-			_class('core_js')->add("<script type=\"text/javascript\">
-  $(function() {
-    $('#{$extra['name']}').datepicker({
-      language: 'ru', format: 'dd-mm-yyyy',".($extra['no_time']==1 ? "pickTime: false," : "")."".($extra['no_date']==1 ? "pickDate: false," : "")."
-    });
-  });
-</script>", false);
+';
+			_class('core_js')->add('<script type="text/javascript">
+$(function() {
+	$("#'.$extra['name'].'").datepicker({
+		language: "ru", 
+		format: "dd-mm-yyyy",'
+		.($extra['no_time'] == 1 ? 'pickTime: false,' : '')
+		.($extra['no_date'] == 1 ? 'pickDate: false,' : '')
+		.'
+	});
+});
+</script>', false);
 			return $_this->_row_html($body, $extra, $r);
 		};
 		if ($__this->_chained_mode) {
