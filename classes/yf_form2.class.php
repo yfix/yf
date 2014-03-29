684d1cf3db96eda929af7250b6c052d807d9f096
Yuri Vysotskiy <profy.net@gmail.com>
2014-03-30 00:29:47 +0200

    form2 removed warning

--- classes/yf_form2.class.php
+++ classes/yf_form2.class.php
@@ -215,7 +215,7 @@ class yf_form2 {
 			$extra = array();
 		}
 		$extra_override = array();
-		$form_id = $this->_replace['__form_id__'] ?: $this->_form_id;
+		$form_id = isset($this->_replace['__form_id__']) ? $this->_replace['__form_id__'] : $this->_form_id;
 		if ($form_id) {
 			$extra_override = $this->_get_extra_override($form_id);
 		}
