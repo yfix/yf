acc7b3e3363f24267f062e13aadc05eca1c63e3a
Yuri Vysotskiy <profy.net@gmail.com>
2014-03-30 00:36:57 +0200

    form navbar sample up

--- .dev/__TESTS/form2/form2_navbar.class.php
+++ .dev/__TESTS/form2/form2_navbar.class.php
@@ -5,6 +5,7 @@ class form2_navbar {
 		$body[] = form('', array('no_form' => 1))->country_box(array('selected' => 'US', 'renderer' => 'div_box', 'stacked' => 1));
 		$body[] = form('', array('no_form' => 1))->language_box(array('selected' => 'ru', 'renderer' => 'div_box', 'stacked' => 1));
 		$body[] = form('', array('no_form' => 1))->currency_box(array('selected' => 'UAH', 'renderer' => 'div_box', 'stacked' => 1));
+		$body[] = form('', array('no_form' => 1))->timezone_box(array('selected' => 'UTC', 'renderer' => 'div_box', 'stacked' => 1));
 		return implode($body);
 	}
 }
