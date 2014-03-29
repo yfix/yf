8d4226629e1963555d3c1fbcdd906708a9803dc5
Yuri Vysotskiy <profy.net@gmail.com>
2014-03-29 22:40:54 +0200

    form2 sample new controls up

--- .dev/__TESTS/form2/form2_new_controls.class.php
+++ .dev/__TESTS/form2/form2_new_controls.class.php
@@ -10,17 +10,29 @@ class form2_new_controls {
 			->currency_box(array('selected' => 'RUB'))
 			->language_box(array('selected' => 'uk'))
 			->timezone_box(array('selected' => 'UTC'))
-			->country_box(array('selected' => 'US'))
 
-			->check_box( 'restricted_view', '', array( 'desc' => 'Ограничить просмотр (категорий +21)', 'no_label' => true ) )
+			->country_box(array('selected' => 'US'))
+			->country_box(array('selected' => 'US', 'renderer' => 'div_box'))
+			->country_box(array('selected' => 'US', 'renderer' => 'select_box'))
+			->country_box(array('selected' => 'US', 'renderer' => 'multi_select_box'))
+			->country_box(array('selected' => 'US', 'renderer' => 'multi_check_box'))
+			->country_box(array('selected' => 'US', 'renderer' => 'radio_box'))
+			->country_box(array('selected' => 'US', 'renderer' => 'radio_box', 'row_tpl' => '%name %icon'))
+
 			->check_box( 'restricted_view', 'Ограничить просмотр (категорий +21)' )
+			->check_box( 'restricted_view', '', array( 'desc' => 'Ограничить просмотр (категорий +21)', 'no_label' => true ) )
 
 			->region_box() // TODO
 			->image()
+
+			->datetime_select('add_date')
+			->datetime_select('add_date__and')
+
 			->time_box()
 			->date_box()
 			->datetime_box()
 			->birth_box()
+
 			->user_method_box(array('desc' => 'user method'))
 			->admin_method_box(array('desc' => 'admin method'))
 			->user_template_box(array('desc' => 'user template'))
