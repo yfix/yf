fb591330118fd88a5efeace6b28831efe0e8ed77
Yuri Vysotskiy <profy.net@gmail.com>
2014-03-30 19:03:37 +0300

    html test up

--- .dev/__TESTS/html/test_html.class.php
+++ .dev/__TESTS/html/test_html.class.php
@@ -10,9 +10,29 @@ class test_html {
 				Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. 
 				Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably havent heard of them accusamus labore sustainable VHS.',
 		);
+		$body .= '<h1>dd table</h1>';
 		$body .= _class('html')->dd_table($data, array());
+		$body .= '<h1>accordion</h1>';
 		$body .= _class('html')->accordion($data, array('selected' => 'second'));
+		$body .= '<h1>tabs</h1>';
 		$body .= _class('html')->tabs($data, array('selected' => 'third'));
+		$body .= '<h1>modal</h1>';
+		$body .= _class('html')->modal(array(
+			'inline'		=> 1,
+			'show_close'	=> 1,
+			'header'		=> 'Modal header',
+			'body'			=> '<p>Some body</p>',
+			'footer'		=> form_item()->save(),
+		));
+		$body .= '<h1>carousel</h1>';
+		$body .= _class('html')->carousel(array(
+		));
+		$body .= '<h1>navbar</h1>';
+		$body .= _class('html')->navbar(array(
+		));
+		$body .= '<h1>breadcrumbs</h1>';
+		$body .= _class('html')->breadcrumbs(array(
+		));
 		return $body;
 	}
 }
