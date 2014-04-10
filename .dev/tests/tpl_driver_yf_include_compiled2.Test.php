6a8c25022ff984a6840455b9c21ed0e9cee0e9d8
Yuri Vysotskiy <profy.net@gmail.com>
2014-04-10 11:45:29 +0300

    unit tests: tpl compiled include now passing tests

--- .dev/tests/tpl_driver_yf_include_compiled2.Test.php
+++ .dev/tests/tpl_driver_yf_include_compiled2.Test.php
@@ -10,18 +10,12 @@ class tpl_driver_yf_include_compiled2_test extends tpl_abstract {
 	}
 	public function test_111() {
 		self::_tpl( 'Inherited var: {key1}', array(), 'unittest_include2' );
-// TODO: we need to avoid this double execution
-		// Needed 2 times to correctly compile templates
 		$result = self::_tpl( '{include("unittest_include2")}', array('key1' => 'val1') );
-#		$result = self::_tpl( '{include("unittest_include2")}', array('key1' => 'val1') );
 		$this->assertEquals('Inherited var: val1', $result);
 	}
 	public function test_112() {
 		self::_tpl( 'Inherited var: {key1}, passed var: {var2}', array(), 'unittest_include3' );
-// TODO: we need to avoid this double execution
-		// Needed 2 times to correctly compile templates
 		$result = self::_tpl( '{include("unittest_include3",var2=42)}', array('key1' => 'val1') );
-#		$result = self::_tpl( '{include("unittest_include3",var2=42)}', array('key1' => 'val1') );
 		$this->assertEquals('Inherited var: val1, passed var: 42', $result);
 	}
 	public function test_113() {
