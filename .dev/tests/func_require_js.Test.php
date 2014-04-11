87bf36f043f7cc154e399d89a80165a095338573
Yuri Vysotskiy <profy.net@gmail.com>
2014-04-11 19:46:40 +0300

    unit tests up

--- .dev/tests/func_require_js.Test.php
+++ .dev/tests/func_require_js.Test.php
@@ -36,4 +36,12 @@ class func_require_js_test extends PHPUnit_Framework_TestCase {
 		require_js('//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.3.2/ckeditor.js');
 		$this->assertEquals('<script src="//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.3.2/ckeditor.js" type="text/javascript"></script>', _class('core_js')->show());
 	}
+	public function test_shortcut_function() {
+		js('//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.3.2/ckeditor.js');
+		$this->assertEquals('<script src="//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.3.2/ckeditor.js" type="text/javascript"></script>', _class('core_js')->show());
+	}
+	public function test_params() {
+		require_js('//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.3.2/ckeditor.js', array('class' => 'yf_core'));
+		$this->assertEquals('<script src="//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.3.2/ckeditor.js" type="text/javascript" class="yf_core"></script>', _class('core_js')->show());
+	}
 }
