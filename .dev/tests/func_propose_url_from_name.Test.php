6de063c0faba7edd8d2d8c645e9dce021da637fd
Yuri Vysotskiy <profy.net@gmail.com>
2014-04-14 23:39:01 +0300

    cleanup

--- .dev/tests/func_propose_url_from_name.Test.php
+++ .dev/tests/func_propose_url_from_name.Test.php
@@ -7,6 +7,5 @@ class func_propose_url_from_name_test extends PHPUnit_Framework_TestCase {
 		$this->assertEquals('test', common()->_propose_url_from_name('Тест'));
 		$this->assertEquals('filtr_junior_vauen_pach_10_sht', common()->_propose_url_from_name('ФИЛЬТР JUNIOR VAUEN (пач/10 шт)'));
 		$this->assertEquals('folga_dlya_kalyana_al_fakher_pach35', common()->_propose_url_from_name('ФОЛЬГА ДЛЯ  КАЛЬЯНА AL FAKHER Пач35'));
-// TODO: lot of tests
 	}
 }
