e6944ba5edc6f21c63d64654706672771caf6356
Yuri Vysotskiy <profy.net@gmail.com>
2014-04-14 16:45:44 +0300

    unit tests setup updated error reporting

--- .dev/tests/yf_unit_tests_setup.php
+++ .dev/tests/yf_unit_tests_setup.php
@@ -3,5 +3,7 @@
 if (!defined('YF_PATH')) {
 	define('YF_PATH', dirname(dirname(dirname(__FILE__))).'/');
 	require YF_PATH.'classes/yf_main.class.php';
-	new yf_main('user', 1, 0);
+	new yf_main('user', $no_db_connect = 1, $auto_init_all = 0);
+	ini_set('display_errors', 'on');
+	error_reporting(E_ALL ^ E_NOTICE);
 }
