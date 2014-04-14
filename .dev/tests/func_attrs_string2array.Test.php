31c18cf3eac7284eef318127615d0c08adf2df5b
Yuri Vysotskiy <profy.net@gmail.com>
2014-04-14 23:09:27 +0300

    unit test up

---
+++ .dev/tests/func_attrs_string2array.Test.php
@@ -0,0 +1,29 @@
+<?php
+
+require dirname(__FILE__).'/yf_unit_tests_setup.php';
+
+class func_attrs_string2array_test extends PHPUnit_Framework_TestCase {
+	public function test_negative() {
+		$this->assertEquals(array(), _attrs_string2array());
+		$this->assertEquals(array(), _attrs_string2array(null));
+		$this->assertEquals(array(), _attrs_string2array(''));
+#		$this->assertEquals(array(), _attrs_string2array(array()));
+#		$this->assertEquals(array(), _attrs_string2array(new stdClass()));
+#		$this->assertEquals(array(), _attrs_string2array(function(){}));
+	}
+	public function test_simple() {
+		$a = array('k1' => 'v1');
+		$this->assertEquals($a, _attrs_string2array('k1=v1'));
+		$this->assertEquals($a, _attrs_string2array(' k1=v1'));
+		$this->assertEquals($a, _attrs_string2array('k1=v1 '));
+		$this->assertEquals($a, _attrs_string2array(' k1=v1 '));
+		$this->assertEquals($a, _attrs_string2array(' k1 = v1 '));
+		$this->assertEquals($a, _attrs_string2array(' k1 = v1; '));
+		$this->assertEquals($a, _attrs_string2array('   ;,k1   =   v1,;   '));
+	}
+	public function test_two() {
+		$a = array('k1' => 'v1', 'k2' => 'v2');
+		$this->assertEquals($a, _attrs_string2array('k1=v1;k2=v2'));
+		$this->assertEquals($a, _attrs_string2array('k1=v1;k2=v2'));
+	}
+}
