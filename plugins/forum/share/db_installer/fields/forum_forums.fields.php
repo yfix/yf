af699168f33baa338eafb731ca18112a89718bbe
Yuri Vysotskiy <profy.net@gmail.com>
2014-04-14 15:43:40 +0300

    db installer: auto-generated fields from sql

---
+++ plugins/forum/share/db_installer/fields/forum_forums.fields.php
@@ -0,0 +1,170 @@
+<?php
+$data = array (
+	'fields' => 
+	array (
+		'id' => 
+		array (
+			'type' => 'int',
+			'length' => '10',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '0',
+			'auto_inc' => 1,
+		),
+		'parent' => 
+		array (
+			'type' => 'int',
+			'length' => '10',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '0',
+			'auto_inc' => 0,
+		),
+		'category' => 
+		array (
+			'type' => 'int',
+			'length' => '10',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '0',
+			'auto_inc' => 0,
+		),
+		'name' => 
+		array (
+			'type' => 'varchar',
+			'length' => '255',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '',
+			'auto_inc' => 0,
+		),
+		'desc' => 
+		array (
+			'type' => 'varchar',
+			'length' => '255',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '',
+			'auto_inc' => 0,
+		),
+		'created' => 
+		array (
+			'type' => 'int',
+			'length' => '10',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '0',
+			'auto_inc' => 0,
+		),
+		'status' => 
+		array (
+			'type' => 'char',
+			'length' => '1',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => 'a',
+			'auto_inc' => 0,
+		),
+		'icon' => 
+		array (
+			'type' => 'varchar',
+			'length' => '255',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '',
+			'auto_inc' => 0,
+		),
+		'order' => 
+		array (
+			'type' => 'int',
+			'length' => '10',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '0',
+			'auto_inc' => 0,
+		),
+		'num_views' => 
+		array (
+			'type' => 'int',
+			'length' => '10',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '0',
+			'auto_inc' => 0,
+		),
+		'num_topics' => 
+		array (
+			'type' => 'int',
+			'length' => '10',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '0',
+			'auto_inc' => 0,
+		),
+		'num_posts' => 
+		array (
+			'type' => 'int',
+			'length' => '10',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '0',
+			'auto_inc' => 0,
+		),
+		'last_post_id' => 
+		array (
+			'type' => 'int',
+			'length' => '10',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '0',
+			'auto_inc' => 0,
+		),
+		'last_post_date' => 
+		array (
+			'type' => 'int',
+			'length' => '10',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '0',
+			'auto_inc' => 0,
+		),
+		'language' => 
+		array (
+			'type' => 'varchar',
+			'length' => '12',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '0',
+			'auto_inc' => 0,
+		),
+		'options' => 
+		array (
+			'type' => 'char',
+			'length' => '10',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '',
+			'auto_inc' => 0,
+		),
+		'user_groups' => 
+		array (
+			'type' => 'varchar',
+			'length' => '255',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '',
+			'auto_inc' => 0,
+		),
+	),
+	'keys' => 
+	array (
+		'id' => 
+		array (
+			'fields' => 
+			array (
+				0 => 'id',
+			),
+			'type' => 'primary',
+		),
+	),
+);
