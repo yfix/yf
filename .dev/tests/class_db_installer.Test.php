68e4a778948a666786886128ff03236148e4ccfc
Yuri Vysotskiy <profy.net@gmail.com>
2014-04-14 17:57:20 +0300

    up

--- .dev/tests/class_db_installer.Test.php
+++ .dev/tests/class_db_installer.Test.php
@@ -129,6 +129,21 @@ $sql = '
 			KEY `user_id` (`user_id`)
 			/** ENGINE=MEMORY **/
 		';
+
+		$this->assertEquals( $this->data_good, _class('installer_db_mysql', 'classes/db/')->_db_table_struct_into_array($sql) );
+		$sql = '
+			`id` varchar(32) NOT NULL default \'\',
+			`user_id` int(10) unsigned NOT NULL,
+			`user_group` int(10) unsigned NOT NULL,
+			`time` int(10) unsigned NOT NULL default \'0\',
+			`type` enum(\'user\',\'admin\') NOT NULL,
+			`ip` varchar(16) NOT NULL,
+			`user_agent` varchar(255) NOT NULL,
+			`query_string` varchar(255) NOT NULL,
+			`site_id` tinyint(3) unsigned NOT NULL,
+			PRIMARY KEY	(`id`),
+			KEY `user_id` (`user_id`)
+		';
 		$this->assertEquals( $this->data_good, _class('installer_db_mysql', 'classes/db/')->_db_table_struct_into_array($sql) );
 /*
 		$sql = '`id` varchar(32) NOT NULL default \'\', `user_id` int(10) unsigned NOT NULL, `user_group` int(10) unsigned NOT NULL, `time` int(10) unsigned NOT NULL default \'0\',
