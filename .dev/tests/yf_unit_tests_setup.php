c4738f680c58771e1b3901185a794edc604e95ca
Yuri Vysotskiy <profy.net@gmail.com>
2014-04-14 17:20:17 +0300

    unit tests: changed default error reporting level

--- .dev/tests/yf_unit_tests_setup.php
+++ .dev/tests/yf_unit_tests_setup.php
@@ -6,5 +6,5 @@ if (!defined('YF_PATH')) {
 	new yf_main('user', $no_db_connect = 1, $auto_init_all = 0);
 	date_default_timezone_set('Europe/Kiev');
 	ini_set('display_errors', 'on');
-	error_reporting(E_ALL ^ E_NOTICE);
+	error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_STRICT);
 }
