201827605a03727f1bf9b3d5eef4a7dce63081e3
Yuri Vysotskiy <profy.net@gmail.com>
2014-04-14 17:18:07 +0300

    unit tests: set default timezone

--- .dev/tests/yf_unit_tests_setup.php
+++ .dev/tests/yf_unit_tests_setup.php
@@ -4,6 +4,7 @@ if (!defined('YF_PATH')) {
 	define('YF_PATH', dirname(dirname(dirname(__FILE__))).'/');
 	require YF_PATH.'classes/yf_main.class.php';
 	new yf_main('user', $no_db_connect = 1, $auto_init_all = 0);
+	date_default_timezone_set('Europe/Kiev');
 	ini_set('display_errors', 'on');
 	error_reporting(E_ALL ^ E_NOTICE);
 }
