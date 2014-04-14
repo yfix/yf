7dd0e6ff5e7e3a0f4b7ee2f8229ad8c37be35f8a
Yuri Vysotskiy <profy.net@gmail.com>
2014-04-14 15:43:53 +0300

    db installer: auto-generated fields from sql

---
+++ plugins/gallery/share/db_installer/fields/gallery_rate_views.fields.php
@@ -0,0 +1,51 @@
+<?php
+$data = array (
+	'fields' => 
+	array (
+		'sid' => 
+		array (
+			'type' => 'char',
+			'length' => '32',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '',
+			'auto_inc' => 0,
+		),
+		'user_id' => 
+		array (
+			'type' => 'int',
+			'length' => '10',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '0',
+			'auto_inc' => 0,
+		),
+		'photo_id' => 
+		array (
+			'type' => 'int',
+			'length' => '10',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '0',
+			'auto_inc' => 0,
+		),
+		'date' => 
+		array (
+			'type' => 'int',
+			'length' => '10',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '0',
+			'auto_inc' => 0,
+		),
+		'/**' => 
+		array (
+			'type' => 'engine',
+			'length' => '=MEMORY',
+			'attrib' => NULL,
+			'not_null' => 0,
+			'default' => '**/',
+			'auto_inc' => 0,
+		),
+	),
+);
---
+++ plugins/gallery/share/db_installer/fields/gallery_rate_votes.fields.php
@@ -0,0 +1,107 @@
+<?php
+$data = array (
+	'fields' => 
+	array (
+		'id' => 
+		array (
+			'type' => 'int',
+			'length' => '10',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '0',
+			'auto_inc' => 1,
+		),
+		'photo_id' => 
+		array (
+			'type' => 'int',
+			'length' => '10',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '0',
+			'auto_inc' => 0,
+		),
+		'user_id' => 
+		array (
+			'type' => 'int',
+			'length' => '10',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '0',
+			'auto_inc' => 0,
+		),
+		'target_user_id' => 
+		array (
+			'type' => 'int',
+			'length' => '10',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '0',
+			'auto_inc' => 0,
+		),
+		'add_date' => 
+		array (
+			'type' => 'int',
+			'length' => '10',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '0',
+			'auto_inc' => 0,
+		),
+		'voted' => 
+		array (
+			'type' => 'int',
+			'length' => '11',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '0',
+			'auto_inc' => 0,
+		),
+		'counted' => 
+		array (
+			'type' => 'int',
+			'length' => '11',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '0',
+			'auto_inc' => 0,
+		),
+		'ip' => 
+		array (
+			'type' => 'varchar',
+			'length' => '16',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '',
+			'auto_inc' => 0,
+		),
+		'comment' => 
+		array (
+			'type' => 'varchar',
+			'length' => '255',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '',
+			'auto_inc' => 0,
+		),
+		'active' => 
+		array (
+			'type' => 'enum',
+			'length' => '\'1\',\'0\'',
+			'attrib' => NULL,
+			'not_null' => 1,
+			'default' => '',
+			'auto_inc' => 0,
+		),
+	),
+	'keys' => 
+	array (
+		'id' => 
+		array (
+			'fields' => 
+			array (
+				0 => 'id',
+			),
+			'type' => 'primary',
+		),
+	),
+);
