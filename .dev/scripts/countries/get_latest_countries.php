9adf4082b5db47cd4d4a4cb858068bbdf8890291
Yuri Vysotskiy <profy.net@gmail.com>
2014-04-15 23:46:37 +0300

    countries up

--- .dev/scripts/countries/get_latest_countries.php
+++ .dev/scripts/countries/get_latest_countries.php
@@ -1,31 +1,7 @@
 #!/usr/bin/php
 <?php
 
-if (!function_exists('html_table_to_array')) {
-function html_table_to_array($html) {
-	if (!preg_match_all('~<tr[^>]*>(.*?)</tr>~ims', $html, $m)) {
-		return array();
-	}
-	$tmp_tbl = array();
-	foreach ($m[1] as $v) {
-		if (!preg_match_all('~<td[^>]*>(.*?)</td>~ims', $v, $m2)) {
-			continue;
-		}
-		$val = $m2[1];
-		// Get contents of within the tags, cannot be done with strip_tags
-		foreach ($val as &$v1) {
-			if (preg_match('~<[^>]+>([^<]+?)</[^>]+>~ims', $v1, $mm)) {
-				$v1 = $mm[1];
-			}
-#			$v1 = trim(strip_tags($v1));
-			$v1 = trim(preg_replace('~&[#]?[0-9a-z]+;~ims', '', $v1));
-			$v1 = trim(preg_replace('~\!.+~ims', '', $v1));
-		}
-		$tmp_tbl[] = $val;
-	}
-	return $tmp_tbl;
-}
-}
+require_once dirname(dirname(__FILE__)).'/scripts_utils.php';
 
 // TODO: try json api from wikipedia
 #$u = 'http://en.wikipedia.org/w/api.php?format=json&action=query&titles=ISO_3166-1&prop=langlinks';
@@ -48,7 +24,6 @@ $mtpl = $mtpl ?: array(
 if (!function_exists('data_get_latest_countries')) {
 function data_get_latest_countries() {
 	global $url, $result_file, $suffix, $mtpl;
-print_r($mtpl);
 
 	$f2 = dirname(__FILE__).'/'.basename($url).'.table'.$suffix.'.html';
 	if (!file_exists($f2)) {
