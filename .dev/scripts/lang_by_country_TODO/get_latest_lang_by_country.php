6f3148ab7880843b6b087eee6d3e9ba4fe817de6
Yuri Vysotskiy <profy.net@gmail.com>
2014-04-16 17:08:46 +0300

    script lang by country tmp data

--- .dev/scripts/lang_by_country_TODO/get_latest_lang_by_country.php
+++ .dev/scripts/lang_by_country_TODO/get_latest_lang_by_country.php
@@ -5,23 +5,32 @@ require_once dirname(dirname(__FILE__)).'/scripts_utils.php';
 
 // TODO
 
-$url = $url ?: 'https://en.wikipedia.org/wiki/ISO_3166-1';
+$url = $url ?: 'https://www.cia.gov/library/publications/the-world-factbook/fields/2098.html';
 $result_file = $result_file ?: dirname(__FILE__).'/countries.php';
 $suffix = $suffix ?: '';
 
 if (!function_exists('data_get_latest_lang_by_country')) {
 function data_get_latest_lang_by_country() {
 	global $url, $result_file, $suffix;
-/*
+
 	$f2 = dirname(__FILE__).'/'.basename($url).'.table'.$suffix.'.html';
 	if (!file_exists($f2)) {
 		$html1 = file_get_contents($url);
-		$regex1 = '~<table[^>]*wikitable[^>]*>(.*?)</table>~ims';
+		$regex1 = '~<article class="description-box">(.+?)</article>~ims';
 		preg_match($regex1, $html1, $m1);
-		file_put_contents($f2, $m1[1]);
+		$html1 = $m1[1];
+		$html1 = preg_replace('~<script[^>]*?>.*?</script>~ims', '', $html1);
+		$html1 = preg_replace('~<style[^>]*?>.*?</style>~ims', '', $html1);
+		$html1 = preg_replace('~<select[^>]*?>.*?</select>~ims', '', $html1);
+		preg_match_all('~<table[^>]*id="[a-z]{2}"[^>]*>.+?</table>~ims', $html1, $m1_2);
+		// Strip empty lines inside matches
+		$html1 = implode(PHP_EOL.PHP_EOL, preg_replace("/(^[\r\n]*|[\r\n]+)[\s\t]*[\r\n]+/", "\n", $m1_2[0]));
+		// Strip multiple whitespaces/tabs
+		$html1 = preg_replace('~[ \t]{2,}~im', ' ', $html1);
+		file_put_contents($f2, $html1);
 	}
 	$html2 = file_get_contents($f2);
-
+/*
 	$tmp_tbl = html_table_to_array($html2);
 	$data = array();
 	foreach ($tmp_tbl as $v) {
