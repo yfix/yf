8dead9e228765c9549b9156f99b5ca2bb0ddeed9
Yuri Vysotskiy <profy.net@gmail.com>
2014-04-22 00:19:04 +0300

    unit test for i18n up

--- .dev/tests/class_i18n.Test.php
+++ .dev/tests/class_i18n.Test.php
@@ -3,17 +3,13 @@
 require dirname(__FILE__).'/yf_unit_tests_setup.php';
 
 class class_i18n_test extends PHPUnit_Framework_TestCase {
-#	public static $driver_bak = array();
 	public static function setUpBeforeClass() {
 		define('DEFAULT_LANG', 'en');
+		$_GET['no_core_cache'] = true;
+		_class('i18n')->USE_TRANSLATE_CACHE = false;
 		_class('i18n')->TR_VARS['en']['unit_test_var1'] = 'unit_test_value1';
-#		self::$driver_bak = tpl()->DRIVER_NAME;
-#		tpl()->DRIVER_NAME = 'smarty';
-#		parent::setUpBeforeClass();
 	}
 	public static function tearDownAfterClass() {
-#		tpl()->DRIVER_NAME = self::$driver_bak;
-#		parent::tearDownAfterClass();
 	}
 	public function test_10() {
 		$this->assertEquals('unit_test_value1', t('unit_test_var1'));
@@ -21,40 +17,42 @@ class class_i18n_test extends PHPUnit_Framework_TestCase {
 		$this->assertEquals('unit_test_value1', t('unit_test_var1 '));
 		$this->assertEquals('unit_test_value1', t(' unit_test_var1 '));
 		$this->assertEquals('unit_test_value1', t('   unit_test_var1   '));
-/*
-		t("Test var")."<br /><br />";
-		t("::forum::Test var")."<br /><br />";
-		t("::forum__new_post::Test var")."<br /><br />";
-		t("::gallery::Test var")."<br /><br />";
-		t("::bl_ablabla::Test var")."<br /><br />";
-		t("Read %numreads times", array("%numreads" => "0"))."<br /><br />";
-		t("Read %numreads times", array("%numreads" => "1"))."<br /><br />";
-		t("Read %numreads times", array("%numreads" => "2"))."<br /><br />";
-		t("Read %numreads times", array("%numreads" => "11"))."<br /><br />";
-		t("Read %numreads times", array("%numreads" => "20"))."<br /><br />";
-		t("Read %numreads times", array("%numreads" => "10001"))."<br /><br />";
-
-		t("While searching %num folders found", array("%num" => "0"))
-		t("While searching %num folders found", array("%num" => "1"))
-		t("While searching %num folders found", array("%num" => "2"))
-		t("While searching %num folders found", array("%num" => "3"))
-		t("While searching %num folders found", array("%num" => "4"))
-		t("While searching %num folders found", array("%num" => "5"))
-		t("While searching %num folders found", array("%num" => "9"))
-		t("While searching %num folders found", array("%num" => "10"))
-		t("While searching %num folders found", array("%num" => "11"))
-		t("While searching %num folders found", array("%num" => "12"))
-		t("While searching %num folders found", array("%num" => "20"))
-		t("While searching %num folders found", array("%num" => "100"))
-		t("While searching %num folders found", array("%num" => "101"))
-		t("While searching %num folders found", array("%num" => "111"))
-		t("While searching %num folders found", array("%num" => "10003"))
-
-#	* {t(While searching %num folders found,%num=1001)}
-#	* В процессе поиска {Найдено %num папок|0:Папок не найдено|1:Найдена %num папка|2,3,4:Найдено %num папки|11-14:Найдено %num папок|Найдено %num папок}
-#	function _process_sub_patterns($text = '', $args = array()) {
-
+	}
+	public function test_20() {
+		$str = 'В процессе поиска {Найдено %num папок|0:Папок не найдено|1:Найдена %num папка|2,3,4:Найдено %num папки|11-14:Найдено %num папок|Найдено %num папок}';
+		$this->assertEquals('В процессе поиска Папок не найдено', _class('i18n')->_process_sub_patterns($str, array('%num' => '0')) );
+		$this->assertEquals('В процессе поиска Найдена 1 папка', _class('i18n')->_process_sub_patterns($str, array('%num' => '1')) );
+		$this->assertEquals('В процессе поиска Найдено 2 папки', _class('i18n')->_process_sub_patterns($str, array('%num' => '2')) );
+		$this->assertEquals('В процессе поиска Найдено 3 папки', _class('i18n')->_process_sub_patterns($str, array('%num' => '3')) );
+		$this->assertEquals('В процессе поиска Найдено 4 папки', _class('i18n')->_process_sub_patterns($str, array('%num' => '4')) );
+		$this->assertEquals('В процессе поиска Найдено 5 папок', _class('i18n')->_process_sub_patterns($str, array('%num' => '5')) );
+		$this->assertEquals('В процессе поиска Найдено 6 папок', _class('i18n')->_process_sub_patterns($str, array('%num' => '6')) );
+		$this->assertEquals('В процессе поиска Найдено 7 папок', _class('i18n')->_process_sub_patterns($str, array('%num' => '7')) );
+		$this->assertEquals('В процессе поиска Найдено 8 папок', _class('i18n')->_process_sub_patterns($str, array('%num' => '8')) );
+		$this->assertEquals('В процессе поиска Найдено 9 папок', _class('i18n')->_process_sub_patterns($str, array('%num' => '9')) );
+		$this->assertEquals('В процессе поиска Найдено 10 папок', _class('i18n')->_process_sub_patterns($str, array('%num' => '10')) );
+		$this->assertEquals('В процессе поиска Найдено 11 папок', _class('i18n')->_process_sub_patterns($str, array('%num' => '11')) );
+		$this->assertEquals('В процессе поиска Найдено 12 папок', _class('i18n')->_process_sub_patterns($str, array('%num' => '12')) );
+		$this->assertEquals('В процессе поиска Найдено 13 папок', _class('i18n')->_process_sub_patterns($str, array('%num' => '13')) );
+		$this->assertEquals('В процессе поиска Найдено 14 папок', _class('i18n')->_process_sub_patterns($str, array('%num' => '14')) );
+		$this->assertEquals('В процессе поиска Найдено 15 папок', _class('i18n')->_process_sub_patterns($str, array('%num' => '15')) );
+		$this->assertEquals('В процессе поиска Найдено 100 папок', _class('i18n')->_process_sub_patterns($str, array('%num' => '100')) );
+		$this->assertEquals('В процессе поиска Найдено 1222 папки', _class('i18n')->_process_sub_patterns($str, array('%num' => '1222')) );
+	}
+	public function test_30() {
+		$str = 'В процессе поиска {Найдено %num папок|0:Папок не найдено|1:Найдена %num папка|2,3,4:Найдено %num папки|11-14:Найдено %num папок|Найдено %num папок}';
+		_class('i18n')->TR_VARS['ru']['While searching %num folders found'] = $str;
+#		$this->assertEquals('', t('While searching %num folders found', array('%num' => '0'), 'ru') );
+#		$this->assertEquals('', t('While searching %num folders found', array('%num' => '1'), 'ru') );
+	}
+	public function test_40() {
 //		$this->assertEquals('', t('[img]http://www.google.com/intl/en_ALL/images/logo.gif[/img]'));
+/*
+		t("Test var");
+		t("::forum::Test var");
+		t("::forum__new_post::Test var");
+		t("::gallery::Test var");
+		t("::bl_ablabla::Test var");
 */
 	}
 }
