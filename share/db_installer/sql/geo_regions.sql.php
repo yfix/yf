86dffa9ec3151cd8c9effe111309dddc156170ca
Yuri Vysotskiy <profy.net@gmail.com>
2014-04-21 13:21:19 +0300

    sql create table geo_regions

--- share/db_installer/sql/geo_regions.sql.php
+++ share/db_installer/sql/geo_regions.sql.php
@@ -1,8 +1,11 @@
 <?php
 $data = '
+	`id` int(10) unsigned NOT NULL,
 	`country` char(2) NOT NULL,
 	`code` char(2) NOT NULL,
 	`name` varchar(255) NOT NULL,
+	`capital_id` int(10) unsigned NOT NULL,
+	PRIMARY KEY (`id`),
 	KEY `country` (`country`),
 	KEY `code` (`code`)
 ';
