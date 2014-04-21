8dc6834ea4b50a7a7856ad538e68cb42cdfdfbcb
Yuri Vysotskiy <profy.net@gmail.com>
2014-04-21 13:22:30 +0300

    sql data geo_continents

---
+++ share/db_installer/data/geo_continents.data.php
@@ -0,0 +1,45 @@
+<?php
+$data = array (
+  0 => 
+  array (
+    'code' => 'AF',
+    'name' => 'Africa',
+    'geoname_id' => '6255146',
+  ),
+  1 => 
+  array (
+    'code' => 'AN',
+    'name' => 'Antarctica',
+    'geoname_id' => '6255152',
+  ),
+  2 => 
+  array (
+    'code' => 'AS',
+    'name' => 'Asia',
+    'geoname_id' => '6255147',
+  ),
+  3 => 
+  array (
+    'code' => 'EU',
+    'name' => 'Europe',
+    'geoname_id' => '6255148',
+  ),
+  4 => 
+  array (
+    'code' => 'NA',
+    'name' => 'North America',
+    'geoname_id' => '6255149',
+  ),
+  5 => 
+  array (
+    'code' => 'OC',
+    'name' => 'Oceania',
+    'geoname_id' => '6255151',
+  ),
+  6 => 
+  array (
+    'code' => 'SA',
+    'name' => 'South America',
+    'geoname_id' => '6255150',
+  ),
+);
