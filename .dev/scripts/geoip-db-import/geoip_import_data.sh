c540c5e45732d2cc2071091c51241c9aad9332e1
Yuri Vysotskiy <profy.net@gmail.com>
2014-04-21 23:08:56 +0300

    geoip scripts up

--- .dev/scripts/geoip-db-import/geoip_import_data.sh
+++ .dev/scripts/geoip-db-import/geoip_import_data.sh
@@ -3,7 +3,7 @@
 . ./geoip_mysql_config.sh
 
 echo "Importing geoip data v1"
-$mysql $db_name < ./sql/geoip_import_data.sql
+$mysql --local-infile=1 $db_name < ./sql/geoip_import_data.sql
 
 echo "Importing geoip data v2"
-$mysql $db_name < ./sql/geoip_import_data2.sql
+$mysql --local-infile=1 $db_name < ./sql/geoip_import_data2.sql
