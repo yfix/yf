fa0df82a2538e1c707cd24539635c6c941844c81
Yuri Vysotskiy <profy.net@gmail.com>
2014-04-21 13:21:45 +0300

    sql create table countries upgraded

--- share/db_installer/sql/countries.sql.php
+++ share/db_installer/sql/countries.sql.php
@@ -1,10 +1,18 @@
 <?php
 $data = '
-	`c` char(2) NOT NULL default \'\',
-	 `n` varchar(64) NOT NULL default \'\',
-	 `f` enum(\'0\',\'1\') NOT NULL default \'0\',
-	`cont` char(1) NOT NULL default \'\',
-	`call_code` char(4) NOT NULL default \'\',
-	PRIMARY KEY  (`c`)
-	/** ENGINE=InnoDB DEFAULT CHARSET=utf8 **/
+	`code` char(2) NOT NULL DEFAULT \'\',
+	`code3` char(3) NOT NULL DEFAULT \'\',
+	`num` char(3) NOT NULL DEFAULT \'\',
+	`name` varchar(64) NOT NULL DEFAULT \'\',
+	`cont` char(2) NOT NULL DEFAULT \'\',
+	`tld` char(2) NOT NULL DEFAULT \'\',
+	`currency` char(3) NOT NULL DEFAULT \'\',
+	`area` int(10) unsigned NOT NULL DEFAULT \'0\',
+	`population` int(10) unsigned NOT NULL DEFAULT \'0\',
+	`phone_prefix` char(10) NOT NULL DEFAULT \'\',
+	`languages` varchar(256) NOT NULL DEFAULT \'\',
+	`geoname_id` int(10) NOT NULL DEFAULT \'0\',
+	`capital_id` int(10) NOT NULL DEFAULT \'0\',
+	`active` enum(\'0\',\'1\') NOT NULL DEFAULT \'0\',
+	PRIMARY KEY (`code`)
 ';
