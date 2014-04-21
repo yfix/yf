880c2c47ca997c10aba2efc3adcae858edc9eebd
Yuri Vysotskiy <profy.net@gmail.com>
2014-04-21 23:39:15 +0300

    db utils up

--- classes/db/yf_db_utils_mysql.class.php
+++ classes/db/yf_db_utils_mysql.class.php
@@ -93,37 +93,43 @@ class yf_db_utils_mysql extends yf_db_utils_driver {
 	/**
 	*/
 	function list_databases($extra = array()) {
-#		$sql = 'SHOW DATABASES';
-// TODO
+		$sql = 'SHOW DATABASES';
+		return $extra['sql'] ? $sql : $this->db->get_2d($sql);
 	}
 
 	/**
 	*/
 	function create_database($name, $extra = array()) {
-#		$sql = 'CREATE DATABASE '.$this->db->_es($name).'';
-// TODO
+		$sql = 'CREATE DATABASE '.($extra['if_not_exists'] ? 'IF NOT EXISTS ' : '').''.$this->db->_es($name).'';
+		return $extra['sql'] ? $sql : $this->db->query($sql);
 	}
 
 	/**
 	*/
 	function drop_database($name, $extra = array()) {
-#		$sql = 'DROP DATABASE '.$this->db->_es($name).'';
-// TODO
+		$sql = 'DROP DATABASE '.($extra['if_exists'] ? 'IF EXISTS ' : '').''.$this->db->_es($name).'';
+		return $extra['sql'] ? $sql : $this->db->query($sql);
 	}
 
 	/**
 	*/
 	function alter_database($name, $extra = array()) {
-#		$sql = 'ALTER DATABASE '.$this->db->_es($name).'';
-// TODO
+		foreach ((array)$extra as $k => $v) {
+			$params[$k] = $k.' = '.$v;
+		}
+		$sql = 'ALTER DATABASE '.$this->db->_es($name).' '.implode(' ', $params);
+		return $extra['sql'] ? $sql : $this->db->query($sql);
 	}
 
 	/**
 	*/
-	function rename_database($name, $new_name) {
-#		$sql = 'ALTER DATABASE '.$this->db->_es($name).'';
-#Script, in bash: for table in mysql -u root -s -N -e "show tables from goedi_drupal"; do mysql -u root -s -N -e "rename table goedi_drupal.$table to iota_drupal.$table"; done;
-// TODO
+	function rename_database($name, $new_name, $extra = array()) {
+		$sql[] = $this->create_database($new_name, $extra);
+		foreach ((array)$this->list_tables($name) as $t) {
+			$sql[] = $this->rename_table($name.'.'.$t, $new_name.'.'.$t, $extra);
+		}
+		$sql[] = $this->drop_database($name, $extra);
+		return $extra['sql'] ? implode(PHP_EOL, $sql) : true;
 	}
 
 	/**
@@ -161,7 +167,7 @@ class yf_db_utils_mysql extends yf_db_utils_driver {
 			$error = 'data is empty';
 			return false;
 		}
-		$sql = 'CREATE TABLE IF NOT EXISTS `'.$this->db->_fix_table_name($name).'` ('. PHP_EOL. $data. PHP_EOL. ') ENGINE=InnoDB DEFAULT CHARSET=utf8;'. PHP_EOL;
+		$sql = 'CREATE TABLE IF NOT EXISTS '.$this->db->_fix_table_name($name).' ('. PHP_EOL. $data. PHP_EOL. ') ENGINE=InnoDB DEFAULT CHARSET=utf8;'. PHP_EOL;
 		return $extra['sql'] ? $sql : $this->db->query($sql);
 	}
 
@@ -172,65 +178,79 @@ class yf_db_utils_mysql extends yf_db_utils_driver {
 			$error = 'name is empty';
 			return false;
 		}
-		$sql = 'DROP TABLE IF EXISTS `'.$this->db->_fix_table_name($name).'`;'.PHP_EOL;
+		$sql = 'DROP TABLE IF EXISTS '.$this->db->_fix_table_name($name).';'.PHP_EOL;
 		return $extra['sql'] ? $sql : $this->db->query($sql);
 	}
 
 	/**
 	*/
 	function alter_table($name, $extra = array()) {
-#		$sql = 'ALTER TABLE '.$this->db->_es($name).'';
-// TODO
+		foreach ((array)$extra as $k => $v) {
+			$params[$k] = $k.' = '.$v;
+		}
+		$sql = 'ALTER TABLE '.$this->db->_es($name).' '.implode(' ', $params);
+		return $extra['sql'] ? $sql : $this->db->query($sql);
 	}
 
 	/**
 	*/
-	function rename_table($name, $new_name) {
-#		$sql = 'RENAME TABLE '.$this->db->_es($name).' TO '.$this->db->_es($new_name);
-// TODO
+	function rename_table($name, $new_name, $extra = array()) {
+		$sql = 'RENAME TABLE '.$this->db->_es($name).' TO '.$this->db->_es($new_name);
+		return $extra['sql'] ? $sql : $this->db->query($sql);
 	}
 
 	/**
 	*/
 	function truncate_table($name, $extra = array()) {
-// TODO
+		$sql = 'TRUNCATE TABLE '.$this->db->_es($name).'';
+		return $extra['sql'] ? $sql : $this->db->query($sql);
 	}
 
 	/**
 	*/
 	function check_table($name, $extra = array()) {
-// TODO
+		$sql = 'TRUNCATE TABLE '.$this->db->_es($name).'';
+		return $extra['sql'] ? $sql : $this->db->query($sql);
 	}
 
 	/**
 	*/
 	function optimize_table($name, $extra = array()) {
-// TODO
+		$sql = 'OPTIMIZE TABLE '.$this->db->_es($name).'';
+		return $extra['sql'] ? $sql : $this->db->query($sql);
 	}
 
 	/**
 	*/
 	function repair_table($name, $extra = array()) {
-// TODO
+		$sql = 'REPAIR TABLE '.$this->db->_es($name);
+		return $extra['sql'] ? $sql : $this->db->query($sql);
 	}
 
 	/**
 	*/
 	function list_indexes($table, $extra = array()) {
+/*
+		SELECT table_name AS `Table`,
+       index_name AS `Index`,
+       GROUP_CONCAT(column_name ORDER BY seq_in_index) AS `Columns`
+FROM information_schema.statistics
+WHERE table_schema = 'sakila'
+GROUP BY 1,2;
+*/
 // TODO
 	}
 
 	/**
 	*/
-	function add_index($table, $fields, $name) {
+	function add_index($table, $fields, $extra = array()) {
 # CREATE INDEX id_index ON lookup (id)
-#		$sql = 'ALTER TABLE '.$this->db->_es($name).'';
 // TODO
 	}
 
 	/**
 	*/
-	function drop_index($table, $fields, $name) {
+	function drop_index($table, $name) {
 # DROP INDEX index_name ON tbl_name
 // TODO
 	}
@@ -243,13 +263,13 @@ class yf_db_utils_mysql extends yf_db_utils_driver {
 
 	/**
 	*/
-	function add_foreign_key($table, $fields, $name) {
+	function add_foreign_key($table, $fields, $extra = array()) {
 // TODO
 	}
 
 	/**
 	*/
-	function drop_foreign_key($table, $fields, $name) {
+	function drop_foreign_key($table, $fields, $extra = array()) {
 // TODO
 	}
 
@@ -261,25 +281,25 @@ class yf_db_utils_mysql extends yf_db_utils_driver {
 
 	/**
 	*/
-	function add_column($table, $name, $data) {
+	function add_column($table, $name, $data, $extra = array()) {
 // TODO
 	}
 
 	/**
 	*/
-	function rename_column($table, $name, $data) {
+	function rename_column($table, $name, $data, $extra = array()) {
 // TODO
 	}
 
 	/**
 	*/
-	function alter_column($table, $name, $data) {
+	function alter_column($table, $name, $data, $extra = array()) {
 // TODO
 	}
 
 	/**
 	*/
-	function drop_column($table, $name, $data) {
+	function drop_column($table, $name, $data, $extra = array()) {
 // TODO
 	}
 
@@ -291,14 +311,14 @@ class yf_db_utils_mysql extends yf_db_utils_driver {
 
 	/**
 	*/
-	function create_view($name, $data) {
+	function create_view($name, $data, $extra = array()) {
 # CREATE VIEW test.v AS SELECT * FROM t;
 // TODO
 	}
 
 	/**
 	*/
-	function drop_view($name, $data) {
+	function drop_view($name, $extra = array()) {
 # DROP VIEW view_name
 // TODO
 	}
@@ -311,7 +331,7 @@ class yf_db_utils_mysql extends yf_db_utils_driver {
 
 	/**
 	*/
-	function create_procedure($name, $data) {
+	function create_procedure($name, $data, $extra = array()) {
 # CREATE PROCEDURE simpleproc (OUT param1 INT)
 # BEGIN
 #    SELECT COUNT(*) INTO param1 FROM t;
@@ -321,7 +341,7 @@ class yf_db_utils_mysql extends yf_db_utils_driver {
 
 	/**
 	*/
-	function drop_procedure($name, $data) {
+	function drop_procedure($name, $data, $extra = array()) {
 # DROP PROCEDURE name
 // TODO
 	}
@@ -334,7 +354,7 @@ class yf_db_utils_mysql extends yf_db_utils_driver {
 
 	/**
 	*/
-	function create_function($name, $data) {
+	function create_function($name, $data, $extra = array()) {
 # CREATE FUNCTION hello (s CHAR(20))
 # RETURNS CHAR(50) DETERMINISTIC
 # RETURN CONCAT('Hello, ',s,'!');
@@ -343,7 +363,7 @@ class yf_db_utils_mysql extends yf_db_utils_driver {
 
 	/**
 	*/
-	function drop_function($name, $data) {
+	function drop_function($name, $data, $extra = array()) {
 # DROP FUNCTION name
 // TODO
 	}
@@ -355,14 +375,14 @@ class yf_db_utils_mysql extends yf_db_utils_driver {
 
 	/**
 	*/
-	function create_trigger($name, $data) {
+	function create_trigger($name, $data, $extra = array()) {
 # CREATE    [DEFINER = { user | CURRENT_USER }]    TRIGGER trigger_name    trigger_time trigger_event     ON tbl_name FOR EACH ROW    trigger_body
 // TODO
 	}
 
 	/**
 	*/
-	function drop_trigger($name, $data) {
+	function drop_trigger($name, $data, $extra = array()) {
 # DROP TRIGGER [IF EXISTS] [schema_name.]trigger_name
 // TODO
 	}
@@ -375,13 +395,13 @@ class yf_db_utils_mysql extends yf_db_utils_driver {
 
 	/**
 	*/
-	function create_event($name, $data) {
+	function create_event($name, $data, $extra = array()) {
 // TODO
 	}
 
 	/**
 	*/
-	function drop_event($name, $data) {
+	function drop_event($name, $data, $extra = array()) {
 // TODO
 	}
 
@@ -393,13 +413,13 @@ class yf_db_utils_mysql extends yf_db_utils_driver {
 
 	/**
 	*/
-	function create_user($name, $data) {
+	function create_user($name, $data, $extra = array()) {
 // TODO
 	}
 
 	/**
 	*/
-	function drop_user($name, $data) {
+	function drop_user($name, $data, $extra = array()) {
 // TODO
 	}
 
