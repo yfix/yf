0a42bc32b71e20a04b58fd0f802ba5259b15ab6d
Yuri Vysotskiy <profy.net@gmail.com>
2014-04-21 18:46:03 +0300

    sql structure for cities updated

--- share/db_installer/sql/geo_cities.sql.php
+++ share/db_installer/sql/geo_cities.sql.php
@@ -2,12 +2,12 @@
 $data = '
 	`id` int(10) unsigned NOT NULL,
 	`country` char(2) NOT NULL,
-	`region` char(3) NOT NULL,
+	`region_id` int(10) unsigned NOT NULL,
 	`name` varchar(32) NOT NULL,
 	`lat` float NOT NULL,
 	`lon` float NOT NULL,
 	`population` int(8) NOT NULL default \'0\',
 	PRIMARY KEY	(`id`),
 	KEY (`country`),
-	KEY (`region`)
+	KEY (`region_id`)
 ';
