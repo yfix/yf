5c38574176a4b9004c7da9a595b876686ed8a85a
Yuri Vysotskiy <profy.net@gmail.com>
2014-04-22 16:04:40 +0300

    db test up

--- .dev/scripts/geonames_db_test/geonames_db_test.php
+++ .dev/scripts/geonames_db_test/geonames_db_test.php
@@ -3,6 +3,12 @@
 
 require_once dirname(__FILE__).'/geonames_db_init.php';
 
+echo '== columns inside geo_geoname =='.PHP_EOL;
+print_r( db()->utils()->list_columns('geo_geoname') );
+
+echo '== indexes inside geo_geoname =='.PHP_EOL;
+print_r( db()->utils()->list_indexes('geo_geoname') );
+
 echo '== tables =='.PHP_EOL;
 print_r( db()->utils()->list_tables() );
 
@@ -14,9 +20,3 @@ print_r( db()->utils()->list_procedures() );
 
 echo '== triggers =='.PHP_EOL;
 print_r( db()->utils()->list_triggers() );
-
-#echo '== columns inside geo_geoname =='.PHP_EOL;
-#print_r( db()->utils()->list_columns('geo_geoname') );
-
-#echo '== indexes inside geo_geoname =='.PHP_EOL;
-#print_r( db()->utils()->list_indexes('geo_geoname') );
