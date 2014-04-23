4d4301351d2d5f47f7b30de2616f3f32a0759d68
Yuri Vysotskiy <profy.net@gmail.com>
2014-04-23 17:30:43 +0300

    scritps up

--- .dev/scripts/github/clone_github_forks.php
+++ .dev/scripts/github/clone_github_forks.php
@@ -9,6 +9,11 @@ include __DIR__.'/data/'.$user.'_repos.php';
 $d = '/home/www/github_forks/';
 !file_exists($d) && mkdir($d, 1);
 
+function get_repo_info($user, $name) {
+	include __DIR__.'/data/'.$user.'/'.$name.'.php';
+	return $data;
+}
+
 # https://help.github.com/articles/syncing-a-fork
 foreach ($data as $k => $a) {
 	if (!$a['fork']) {
@@ -19,8 +24,28 @@ foreach ($data as $k => $a) {
 	if (file_exists($target.'/.git/config')) {
 		passthru('(cd '.$target.' && git pull -r)');
 	} else {
-		$clone_url = 'https://github.com/yfix/'.$a['name'].'.git';
+		$clone_url = 'git@github.com:yfix/'.$a['name'].'.git';
 		passthru('git clone '.$clone_url.' '.$target);
 	}
-	passthru('git remote -v');
+
+	$repo_info = get_repo_info($user, $a['name']);
+	$source_repo = $repo_info['source']['full_name'];
+	$upstream = 'https://github.com/'.$source_repo.'.git';
+
+	echo $source_repo. PHP_EOL;
+
+	$out = null;
+	exec('(cd '.$target.' && git remote -v)', $out);
+	$out = implode(PHP_EOL, $out);
+
+	echo PHP_EOL. $out. PHP_EOL;
+
+	if (false === strpos($out, $upstream)) {
+		passthru('(cd '.$target.' && git remote add upstream '.$upstream.')');
+	}
+	passthru('(cd '.$target.' && git fetch upstream)');
+	passthru('(cd '.$target.' && git checkout master)');
+	passthru('(cd '.$target.' && git reset --hard upstream/master)');
+	passthru('(cd '.$target.' && git merge upstream/master)');
+	passthru('(cd '.$target.' && git push --all)');
 }
