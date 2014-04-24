08cf96d4107481c0d2c7d49b3f85cc8845258e39
Yuri Vysotskiy <profy.net@gmail.com>
2014-04-24 16:41:22 +0300

    script countries into db up

--- .dev/scripts/countries/countries_into_db.php
+++ .dev/scripts/countries/countries_into_db.php
@@ -1,14 +1,31 @@
 #!/usr/bin/php
 <?php
 
+$lang = 'ru';
+if (strlen($argv[2]) == 2) {
+	$lang = $argv[2];
+}
+
 require_once dirname(dirname(__FILE__)).'/scripts_init.php';
 
-require dirname(__FILE__).'/countries.php';
+function load_data($lang) {
+	require dirname(__FILE__).'/countries'.($lang && $lang != 'en' ? '_'.$lang : '').'.php';
+	return $data;
+}
+$data = load_data($lang);
 if (!$data) {
 	exit('Error: $data is missing');
 }
-
-$table = DB_PREFIX. 'countries';
+foreach($data as &$v) {
+	$v['name_eng'] = $v['name'];
+}
+if ($lang != 'en') {
+	$data_en = load_data('en');
+	foreach($data as $k => &$v) {
+		$v['name_eng'] = $data_en[$k]['name'];
+	}
+}
+$table = DB_PREFIX. 'geo_countries';
 if ( ! db()->utils()->table_exists($table) || $force) {
 	db()->utils()->drop_table($table);
 	db()->utils()->create_table($table);
