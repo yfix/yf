70f56f4561cb00b6331500cda1b003550d1ac994
Yuri Vysotskiy <profy.net@gmail.com>
2014-04-25 12:44:29 +0300

    bench up

--- .dev/scripts/bench/dir_recursive_bench.php
+++ .dev/scripts/bench/dir_recursive_bench.php
@@ -4,21 +4,6 @@
 $argv[1] = '/home/www/test2/';
 require dirname(dirname(__FILE__)).'/scripts_init.php';
 
-##############
-
-echo PHP_EOL.'== 1) _class("dir")->scan()'.PHP_EOL.PHP_EOL;
-
-$start_mem = memory_get_usage();
-$start_time = microtime(true);
-$files = _class('dir')->scan(YF_PATH, 1, '-f ~gallery.*.php~ims');
-print_r($files);
-
-echo 'time: '.round(microtime(true) - $start_time, 3).', mem: '.(memory_get_usage() - $start_mem).', peakmem: '.memory_get_peak_usage(). PHP_EOL;
-
-###############
-
-echo PHP_EOL.'== 2) DirectoryIterator'.PHP_EOL.PHP_EOL;
-
 function rsearch($folder, $pattern) {
     $dir = new RecursiveDirectoryIterator($folder);
     $ite = new RecursiveIteratorIterator($dir);
@@ -29,18 +14,6 @@ function rsearch($folder, $pattern) {
     }
     return $fileList;
 }
-
-$start_mem = memory_get_usage();
-$start_time = microtime(true);
-$files = rsearch(YF_PATH, '~gallery.*.php~ims');
-print_r($files);
-
-echo 'time: '.round(microtime(true) - $start_time, 3).', mem: '.(memory_get_usage() - $start_mem).', peakmem: '.memory_get_peak_usage(). PHP_EOL;
-
-###############
-
-echo PHP_EOL.'== 3) rglob()'.PHP_EOL.PHP_EOL;
-
 function rglob($pattern, $flags = 0) {
     $files = glob($pattern, $flags); 
     foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
@@ -48,18 +21,6 @@ function rglob($pattern, $flags = 0) {
     }
     return $files;
 }
-
-$start_mem = memory_get_usage();
-$start_time = microtime(true);
-$files = rglob(YF_PATH.'*gallery*.php');
-print_r($files);
-
-echo 'time: '.round(microtime(true) - $start_time, 3).', mem: '.(memory_get_usage() - $start_mem).', peakmem: '.memory_get_peak_usage(). PHP_EOL;
-
-###############
-
-echo PHP_EOL.'== 4) rglob2()'.PHP_EOL.PHP_EOL;
-
 function rglob2($pattern, $flags = 0) {
     $files = iterator_to_array(new GlobIterator($pattern, $flags));
     foreach (new GlobIterator(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
@@ -68,20 +29,41 @@ function rglob2($pattern, $flags = 0) {
     return $files;
 }
 
-$start_mem = memory_get_usage();
-$start_time = microtime(true);
-$files = rglob2(YF_PATH.'*gallery*.php');
-print_r($files);
+class bench {
+	function __toString() {
+		$out = array();
+		foreach (get_class_methods($this) as $name) {
+			if ($name[0] == '_') {
+				continue;
+			}
+			$start_mem = memory_get_usage();
+			$start_time = microtime(true);
+			list($desc, $_files) = $this->$name();
+			$files[$name] = $_files;
+			$out[$name] = ++$i.') '.$desc.' | time: '.round(microtime(true) - $start_time, 3).' | mem: '.(memory_get_usage() - $start_mem).' | peakmem: '.memory_get_peak_usage();
+		}
+		return print_r($files). PHP_EOL. implode(PHP_EOL, $out). PHP_EOL;
+	}
+	function dir_scan() {
+		$files = _class('dir')->scan(YF_PATH, 1, '-f ~gallery.*.php~ims');
+		return array('_class("dir")->scan()', $files);
+	}
+	function directory_iterator() {
+		$files = rsearch(YF_PATH, '~gallery.*.php~ims');
+		return array('DirectoryIterator', $files);
+	}
+	function rglob() {
+		$files = rglob(YF_PATH.'*gallery*.php');
+		return array('rglob()', $files);
+	}
+	function rglob2() {
+		$files = rglob2(YF_PATH.'*gallery*.php');
+		return array('rglob2()', $files);
+	}
+	function exec_find() {
+		$files = explode("\n", trim(shell_exec('find '.YF_PATH.' -name '.'*gallery*.php')));
+		return array('exec(find ...)', $files);
+	}
+}
 
-echo 'time: '.round(microtime(true) - $start_time, 3).', mem: '.(memory_get_usage() - $start_mem).', peakmem: '.memory_get_peak_usage(). PHP_EOL;
-
-###############
-
-echo PHP_EOL.'== 5) exec + find'.PHP_EOL.PHP_EOL;
-
-$start_mem = memory_get_usage();
-$start_time = microtime(true);
-$files = explode("\n", trim(shell_exec('find '.YF_PATH.' -name '.'*gallery*.php')));
-print_r($files);
-
-echo 'time: '.round(microtime(true) - $start_time, 3).', mem: '.(memory_get_usage() - $start_mem).', peakmem: '.memory_get_peak_usage(). PHP_EOL;
+print new bench();
