630ba83b7809dc3e68a055152783237db7fc28e7
Sergey Mushta <futukrishna@gmail.com>
2014-04-28 15:58:21 +0300

    fix: cleanup product-related tables when adding new to avoid collisions with broken data

--- admin_modules/manage_shop/yf_manage_shop_product_add.class.php
+++ admin_modules/manage_shop/yf_manage_shop_product_add.class.php
@@ -8,6 +8,12 @@ class yf_manage_shop_product_add {
 			->validate(array('name' => 'trim|required'))
 			->db_insert_if_ok('shop_products', array('name'), array('active' => 0, 'supplier_id' => $supplier_id), array('on_after_update' => function($data, $table, $fields, $type, &$extra) {
 				$id = db()->insert_id();
+				//initial cleanup
+				db()->query("DELETE FROM `".db('shop_aliases')."` WHERE `dst_item_id`='{$id}' AND `type`='products'");
+				db()->query("DELETE FROM `".db('shop_product_images')."` WHERE `product_id`='{$id}'");
+				db()->query("DELETE FROM `".db('shop_products_productparams')."` WHERE `product_id`='{$id}'");				
+				db()->query("DELETE FROM `".db('shop_product_to_category')."` WHERE `product_id`='{$id}'"); 
+				
 				$extra['redirect_link'] = './?object='.main()->_get('object').'&action=product_edit&id='.$id;
 				common()->admin_wall_add(array(t('shop product added: %name', array('%name' => $_POST['name'])), $id));
 				module('manage_shop')->_product_cache_purge($id);
