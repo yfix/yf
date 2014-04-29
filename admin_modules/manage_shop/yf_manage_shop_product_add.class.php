7f2c48a3d96d26ac203c9d353ddae0fb6aa5bcf2
Yuri Vysotskiy <profy.net@gmail.com>
2014-04-29 14:36:29 +0300

    admin shop: product add now set add_date

--- admin_modules/manage_shop/yf_manage_shop_product_add.class.php
+++ admin_modules/manage_shop/yf_manage_shop_product_add.class.php
@@ -6,7 +6,7 @@ class yf_manage_shop_product_add {
 		$r = array('back_link' => './?object='.main()->_get('object').'&action=products');
 		return form2($r + (array)$_POST)
 			->validate(array('name' => 'trim|required'))
-			->db_insert_if_ok('shop_products', array('name'), array('active' => 0, 'supplier_id' => $supplier_id), array('on_after_update' => function($data, $table, $fields, $type, &$extra) {
+			->db_insert_if_ok('shop_products', array('name'), array('active' => 0, 'supplier_id' => $supplier_id, 'add_date' => time()), array('on_after_update' => function($data, $table, $fields, $type, &$extra) {
 				$id = db()->insert_id();
 				$extra['redirect_link'] = './?object='.main()->_get('object').'&action=product_edit&id='.$id;
 				common()->admin_wall_add(array(t('shop product added: %name', array('%name' => $_POST['name'])), $id));
