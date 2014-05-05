efe592a2c34ac64b5532f10fcfd1591bf3ef16fd
Yuri Vysotskiy <profy.net@gmail.com>
2014-05-05 15:54:11 +0300

    unit tests for cache improved

--- .dev/tests/class_cache.Test.php
+++ .dev/tests/class_cache.Test.php
@@ -5,18 +5,41 @@ require dirname(__FILE__).'/yf_unit_tests_setup.php';
 class class_cache_test extends PHPUnit_Framework_TestCase {
 	public static $_cache = array();
 	public static function setUpBeforeClass() {
+		main()->modules['cache'] = null;
 		self::$_cache = clone _class('cache');
-		self::$_cache->_init(array('driver' => 'tmp'));
+		self::$_cache->_init(array('driver' => self::_get_driver_name()));
 		self::$_cache->NO_CACHE = false;
 		self::$_cache->FORCE_REBUILD_CACHE = false;
+	}
+	public static function tearDownAfterClass() {
+		$driver = self::_get_driver_name();
+		if ($driver == 'files') {
+			_class('dir')->delete_dir('./core_cache/', $delete_start_dir = true);
+		}
+	}
+	public function _get_driver_name() {
+		$called = strtolower(get_called_class());
+		if (false !== strpos($called, '_files')) {
+			return 'files';
+		} elseif (false !== strpos($called, '_memcache')) {
+			return 'memcache';
+		} else {
+			return 'tmp';
+		}
 	}
 	public static function _cache() {
 		return self::$_cache;
 	}
 	public function test_driver() {
-		$this->assertEquals('tmp', self::_cache()->DRIVER);
+		$this->assertEquals(self::_get_driver_name(), self::_cache()->DRIVER);
+	}
+	public function test_is_ready() {
+		$this->assertTrue(self::_cache()->_driver_ok);
+		$this->assertNotEmpty(self::_cache()->_driver);
+		$this->assertTrue(self::_cache()->_driver->is_ready());
 	}
 	public function test_get() {
+		self::_cache()->flush();
 		$this->assertFalse(@self::_cache()->get());
 		$this->assertNull(self::_cache()->get('k1'));
 
@@ -27,16 +50,19 @@ class class_cache_test extends PHPUnit_Framework_TestCase {
 		$this->assertFalse(self::_cache()->get('k1'));
 	}
 	public function test_set() {
-		$this->assertTrue(self::_cache()->set('k2', 'some_data'));
+		self::_cache()->flush();
+		$this->assertNotEmpty(self::_cache()->set('k2', 'some_data'));
 		$this->assertEquals('some_data', self::_cache()->get('k2'));
 	}
 	public function test_del() {
+		self::_cache()->flush();
 		self::_cache()->set('k3', 'val3');
 		$this->assertEquals('val3', self::_cache()->get('k3'));
 		$this->assertTrue(self::_cache()->del('k3'));
 		$this->assertNull(self::_cache()->get('k3'));
 	}
 	public function test_flush() {
+		self::_cache()->flush();
 		self::_cache()->set('k4', 'val4');
 		$this->assertTrue(self::_cache()->flush());
 		$this->assertNull(self::_cache()->get('k4'));
@@ -59,13 +85,13 @@ class class_cache_test extends PHPUnit_Framework_TestCase {
 	public function test_multi_set() {
 		$this->assertTrue(self::_cache()->flush());
 		$this->assertEquals(array(), self::_cache()->multi_get(array('k1', 'k2')));
-		$this->assertTrue(self::_cache()->multi_set(array('k1' => 'v1', 'k2' => 'v2')));
+		$this->assertEquals(array('k1' => true, 'k2' => true), self::_cache()->multi_set(array('k1' => 'v1', 'k2' => 'v2')));
 		$this->assertEquals(array('k1' => 'v1', 'k2' => 'v2'), self::_cache()->multi_get(array('k1', 'k2')));
 	}
 	public function test_multi_del() {
 		$this->assertTrue(self::_cache()->flush());
 		$this->assertEquals(array(), self::_cache()->multi_get(array('k1', 'k2')));
-		$this->assertTrue(self::_cache()->multi_set(array('k1' => 'v1', 'k2' => 'v2')));
+		$this->assertEquals(array('k1' => true, 'k2' => true), self::_cache()->multi_set(array('k1' => 'v1', 'k2' => 'v2')));
 		$this->assertTrue(self::_cache()->set('k3', 'v3'));
 		self::_cache()->multi_del(array('k1', 'k2'));
 		$this->assertEquals(array(), self::_cache()->multi_get(array('k1', 'k2')));
