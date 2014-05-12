64365962ca6e4893ccc496b68e003da21e855baf
Yuri Vysotskiy <profy.net@gmail.com>
2014-05-12 13:51:42 +0300

    main: moved out events methods

--- classes/yf_main.class.php
+++ classes/yf_main.class.php
@@ -1742,23 +1742,4 @@ class yf_main {
 	function is_redirect() {
 		return (bool)$this->_IS_REDIRECTING;
 	}
-
-	/**
-	*/
-	function event_subscribe($name, $func, $params = array()) {
-// TODO: events system
-	}
-
-	/**
-	*/
-	function event_fire($name, $extra = array()) {
-// TODO: events system
-	}
-
-	/**
-	*/
-	function event_find_hooks() {
-// TODO: will search through active modules for _event_hook() methods, where class/method can subscribe to any events
-// TODO: events system
-	}
 }
