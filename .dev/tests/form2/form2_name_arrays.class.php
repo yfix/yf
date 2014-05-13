3f0d4c1b532dd985c220312c10ccb72894e8c5f5
Yuri Vysotskiy <profy.net@gmail.com>
2014-05-14 00:48:14 +0300

    test up

--- .dev/tests/form2/form2_name_arrays.class.php
+++ .dev/tests/form2/form2_name_arrays.class.php
@@ -2,11 +2,20 @@
 
 class form2_name_arrays {
 	function show() {
-		return form()
+		$a = array(
+			'name'	=> array(
+				'key1'	=> 'v1',
+			),
+		);
+		return form($a)
+			->on_post(function(){
+var_dump($_POST);
+			})
 			->text('name[]')
 			->text('name[]')
 			->text('name[key1]')
 			->text('name[key2]')
+			->save()
 		;
 	}
 }
