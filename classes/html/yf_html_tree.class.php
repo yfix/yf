5b491cc68e8f66988ce3367710d014190c740702
Yuri Vysotskiy <profy.net@gmail.com>
2014-05-20 16:59:54 +0300

    html tree updated

--- classes/html/yf_html_tree.class.php
+++ classes/html/yf_html_tree.class.php
@@ -25,10 +25,10 @@ class yf_html_tree {
 			'add_link'		=> $extra['add_link'] ?: './?object='.$_GET['object'].'&action=add_item&id='.$_GET['id'],
 			'back_link'		=> $extra['back_link'] ?: './?object='.$_GET['object'].'&action=show_items&id='.$_GET['id'],
 		);
-#		$btn_save	= '<button type="submit" class="btn btn-primary btn-mini btn-xs"><i class="icon-save"></i> '.t('Save').'</button>';
-#		$btn_back	= $r['back_link'] ? '<a href="'.$r['back_link'].'" class="btn btn-mini btn-xs"><i class="icon-backward"></i> '.t('Go Back').'</a>' : '';
-#		$btn_add	= $r['add_link'] ? '<a href="'.$r['add_link'].'" class="btn btn-mini btn-xs ajax_add"><i class="icon-plus-sign"></i> '.t('Add').'</a>' : '';
-		$btn_expand = '<a href="javascript:void(0);" class="btn btn-mini btn-xs draggable-menu-expand-all"><i class="icon-expand-alt fa-expand"></i> '.t('Expand').'</a>';
+#		$btn_save	= '<button type="submit" class="btn btn-primary btn-mini btn-xs"><i class="icon-large icon-save"></i> '.t('Save').'</button>';
+#		$btn_back	= $r['back_link'] ? '<a href="'.$r['back_link'].'" class="btn btn-mini btn-xs"><i class="icon-large icon-backward"></i> '.t('Go Back').'</a>' : '';
+#		$btn_add	= $r['add_link'] ? '<a href="'.$r['add_link'].'" class="btn btn-mini btn-xs ajax_add"><i class="icon-large icon-plus-sign"></i> '.t('Add').'</a>' : '';
+		$btn_expand = '<a href="javascript:void(0);" class="btn btn-mini btn-xs draggable-menu-expand-all"><i class="icon-large icon-expand-alt fa-expand"></i> '.t('Expand').'</a>';
 		return '<form action="'.$r['form_action'].'" method="post" class="draggable_form span4">
 				<div class="controls">'
 					. $btn_save
@@ -179,10 +179,7 @@ class yf_html_tree {
 		}
 		return false;
 	});
-});
 
-//init functions
-$(function() {
 	$("dl, .dropzone", ".draggable_menu").droppable({
 		accept: ".draggable_menu li",
 		tolerance: "pointer",
