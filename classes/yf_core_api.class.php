d44fed81aa82760d6928b59d27ee16e519e03dde
Yuri Vysotskiy <profy.net@gmail.com>
2014-05-22 17:24:46 +0300

    core api todo added

---
+++ classes/yf_core_api.class.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+* Core API
+*/
+class yf_core_api {
+// TODO
+	function get_classes() {
+// TODO
+	}
+	function get_methods() {
+// TODO
+	}
+	function get_templates() {
+// TODO
+	}
+	function get_langs() {
+// TODO
+	}
+	function get_sites() {
+// TODO
+	}
+	function get_servers() {
+// TODO
+	}
+	function get_user_roles() {
+// TODO
+	}
+	function get_user_groups() {
+// TODO
+	}
+	function get_admin_roles() {
+// TODO
+	}
+	function get_admin_groups() {
+// TODO
+	}
+	function get_hooks() {
+// TODO
+	}
+	function get_hook_types() {
+// TODO
+	}
+	function get_callbacks() {
+// TODO
+	}
+	function get_callback_types() {
+// TODO
+	}
+	function get_events() {
+// TODO
+	}
+	function get_functions() {
+// TODO
+	}
+	function get_libs() {
+// TODO
+	}
+}
