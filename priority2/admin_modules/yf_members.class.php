ab8433a13002d4f8b107e6ef4e7de75135f04e48
Yuri Vysotskiy <profy.net@gmail.com>
2014-05-27 23:39:44 +0300

    paths cleanup

--- priority2/admin_modules/yf_members.class.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-# Internal symlink
-load('manage_users','framework','admin_modules/');
-$_GET['object'] = 'manage_users';
-class yf_members extends yf_manage_users { }
--- priority2/admin_modules/yf_shop_contentm_panel.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
-* Special panel for the supplier
-*/
-load('manage_shop','framework');
-class yf_shop_contentm_panel extends yf_manage_shop {
-}
