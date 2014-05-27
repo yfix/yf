0175cd4c613deffe198ae9213abbcbb9e50adabb
Yuri Vysotskiy <profy.net@gmail.com>
2014-05-27 14:53:50 +0300

    core api up

--- classes/yf_core_api.class.php
+++ classes/yf_core_api.class.php
@@ -5,6 +5,12 @@
 */
 class yf_core_api {
 
+	var $section_paths = array(
+		'core'	=> 'classes/',
+		'user'	=> 'modules/',
+		'admin'	=> 'admin_modules/',
+	);
+
 	/**
 	* This method will search and call all found hook methods from active modules
 	* @example: call_hooks('settings', $params)
@@ -117,13 +123,10 @@ class yf_core_api {
 
 	/**
 	*/
-	function get_class_instance($name, $section) {
-		if ($section == 'core') {
-			$path = 'classes/';
-		} elseif ($section == 'user') {
-			$path = 'modules/';
-		} elseif ($section == 'admin') {
-			$path = 'admin_modules/';
+	function get_class_instance($name, $section, $force_path) {
+		$path = $this->section_paths[$section];
+		if ($force_path) {
+			$path = $force_path;
 		}
 		return _class($name, $path);
 	}
@@ -176,42 +179,10 @@ class yf_core_api {
 
 	/**
 	*/
-	function get_submodules($section = 'all') {
-		$folders = array(
-			'core'	=> 'classes/',
-			'user'	=> 'modules/',
-			'admin'	=> 'admin_modules/',
-		);
-		$data = array();
-		foreach ($folders as $_section => $folder) {
-			if ($section != 'all' && $section != $_section) {
-				continue;
-			}
-			$_data = array();
-			$paths = array();
-			$this->_get_classes_by_params(array('folder' => $folder.'*/'), $paths);
-			foreach ((array)$paths as $name => $_paths) {
-				if (!is_array($_paths)) {
-					continue;
-				}
-				$path = current($_paths);
-				$subdir = basename(dirname($path));
-				$_data[$subdir][$name] = $name;
-			}
-			if (is_array($_data)) {
-				ksort($_data);
-			}
-			$data[$_section] = $_data;
-		}
-		return $data;
-	}
-
-	/**
-	*/
 	function _get_classes_by_params($extra = array(), &$paths = array()) {
 		$prefix = isset($extra['prefix']) ? $extra['prefix'] : YF_PREFIX;
 		$suffix = isset($extra['suffix']) ? $extra['suffix'] : YF_CLS_EXT;
-		$folder = isset($extra['folder']) ? $extra['folder'] : 'classes/';
+		$folder = isset($extra['folder']) ? $extra['folder'] : $this->section_paths['core'];
 		$globs = array(
 			'project'			=> PROJECT_PATH. $folder.'*'.$suffix,
 			'project_plugins'	=> PROJECT_PATH. 'plugins/*/'.$folder.'*'.$suffix,
@@ -237,6 +208,51 @@ class yf_core_api {
 			ksort($classes);
 		}
 		return $classes;
+	}
+
+	/**
+	*/
+	function get_submodules($section = 'all') {
+		$data = array();
+		foreach ($this->section_paths as $_section => $folder) {
+			if ($section != 'all' && $section != $_section) {
+				continue;
+			}
+			$_data = array();
+			$paths = array();
+			$this->_get_classes_by_params(array('folder' => $folder.'*/'), $paths);
+			foreach ((array)$paths as $name => $_paths) {
+				if (!is_array($_paths)) {
+					continue;
+				}
+				$path = current($_paths);
+				$subdir = basename(dirname($path));
+				$_data[$subdir][$name] = $name;
+			}
+			if (is_array($_data)) {
+				ksort($_data);
+			}
+			$data[$_section] = $_data;
+		}
+		return $data;
+	}
+
+	/**
+	*/
+	function get_submodules_methods($section = 'all') {
+		$methods = array();
+		foreach ((array)$this->get_submodules($section) as $_section => $modules) {
+			foreach ((array)$modules as $module) {
+				$obj = $this->get_class_instance($module, $_section, $this->section_paths[$_section].$module.'/');
+				foreach ((array)get_class_methods($obj) as $method) {
+					$methods[$module][$method] = $method;
+				}
+			}
+		}
+		foreach ((array)$methods as $module => $_methods) {
+			ksort($methods[$module]);
+		}
+		return $methods;
 	}
 
 	/**
