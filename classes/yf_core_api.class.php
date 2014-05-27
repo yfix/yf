e9049a7c4829d19cebdc6efcfd5278bbbbdf2cb8
Yuri Vysotskiy <profy.net@gmail.com>
2014-05-28 00:51:17 +0300

    core api updated

--- classes/yf_core_api.class.php
+++ classes/yf_core_api.class.php
@@ -403,7 +403,7 @@ class yf_core_api {
 	*/
 	function get_server_info() {
 		$sites = $this->get_servers();
-		return $sites[main()->SERVER_ID];
+		return $servers[main()->SERVER_ID];
 	}
 
 	/**
@@ -603,11 +603,11 @@ class yf_core_api {
 	/**
 	*/
 	function add_syntax_highlighter() {
-		require_js('//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.0/highlight.min.js');
-		require_js('//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.0/languages/php.min.js');
-		require_js('<script>hljs.initHighlightingOnLoad();</script>');
-		require_css('//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.0/styles/railscasts.min.css');
-		require_css('section.page-contents pre, pre.prettyprint {
+		js('//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.0/highlight.min.js');
+		js('//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.0/languages/php.min.js');
+		js('<script>hljs.initHighlightingOnLoad();</script>');
+		css('//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.0/styles/railscasts.min.css');
+		css('section.page-contents pre, pre.prettyprint {
 			background-color: transparent;
 			border: 0;
 			font-family: inherit;
