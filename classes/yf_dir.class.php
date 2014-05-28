5e1bf85a731d1baff37e12d347194bc63b1d3b00
Yuri Vysotskiy <profy.net@gmail.com>
2014-05-28 19:08:53 +0300

    added main extending support

--- classes/yf_dir.class.php
+++ classes/yf_dir.class.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
-* Directory handling class
+* Filesystem utils
 * 
 * @package		YF
 * @author		YFix Team <yfix.dev@gmail.com>
@@ -17,7 +17,17 @@ class yf_dir {
 	/**
 	* Catch missing method call
 	*/
-	function __call($name, $arguments) {
+	function __call($name, $args) {
+		$self = 'dir';
+		$func = null;
+		if (isset( $this->_extend[$name] )) {
+			$func = $this->_extend[$name];
+		} elseif (isset( main()->_extend[$self][$name] )) {
+			$func = main()->_extend[$self][$name];
+		}
+		if ($func) {
+			return $func($args[0], $args[1], $args[2], $args[3], $this);
+		}
 		trigger_error(__CLASS__.': No method '.$name, E_USER_WARNING);
 		return false;
 	}
