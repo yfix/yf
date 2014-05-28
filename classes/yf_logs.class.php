7feb98b6aef26c5ba5876951ef1c03223b4ffbe4
Yuri Vysotskiy <profy.net@gmail.com>
2014-05-28 19:10:00 +0300

    added main extending support

--- classes/yf_logs.class.php
+++ classes/yf_logs.class.php
@@ -48,7 +48,17 @@ class yf_logs {
 	/**
 	* Catch missing method call
 	*/
-	function __call($name, $arguments) {
+	function __call($name, $args) {
+		$self = 'logs';
+		$func = null;
+		if (isset( $this->_extend[$name] )) {
+			$func = $this->_extend[$name];
+		} elseif (isset( main()->_extend[$self][$name] )) {
+			$func = main()->_extend[$self][$name];
+		}
+		if ($func) {
+			return $func($args[0], $args[1], $args[2], $args[3], $this);
+		}
 		trigger_error(__CLASS__.': No method '.$name, E_USER_WARNING);
 		return false;
 	}
