7555582009c6700e2a50e3c07772ca15541b0dd5
Yuri Vysotskiy <profy.net@gmail.com>
2014-06-06 13:48:55 +0300

    divide pages: cleanup and changed default render type to new simpler one 'slide'

--- classes/common/yf_divide_pages.class.php
+++ classes/common/yf_divide_pages.class.php
@@ -20,7 +20,7 @@ class yf_divide_pages {
 	/** @var */
 	public $DEFAULT_PER_PAGE	= 20;
 	/** @var */
-	public $DEFAULT_RENDER_TYPE	= 'blocks';
+	public $DEFAULT_RENDER_TYPE	= 'slide'; // blocks|slide
 	/** @var */
 	public $DEFAULT_TPLS_PATH	= 'system/divide_pages/';
 
@@ -53,7 +53,6 @@ class yf_divide_pages {
 			$render_type = $this->DEFAULT_RENDER_TYPE;
 		}
 		$requested_page = isset($extra['requested_page']) ? $extra['requested_page'] : $_GET['page'];
-		// Number of records to show on one page
 		$records_on_page = abs(intval($extra['records_on_page'] ?: $records_on_page));
 		$per_page = $records_on_page ?: (MAIN_TYPE_ADMIN ? conf('admin_per_page') : conf('user_per_page'));
 		if (!$per_page) {
@@ -74,7 +73,7 @@ class yf_divide_pages {
 			$old_total_pages = $total_pages;
 			$total_pages = $this->PAGES_LIMIT;
 		}
-		// Filter pages numbers that are do not exists
+		// Filter not existing pages numbers
 		if (!$requested_page || $requested_page < 1) {
 			$cur_page = 1;
 		} elseif ($requested_page > $total_pages) {
@@ -82,7 +81,6 @@ class yf_divide_pages {
 		} else {
 			$cur_page = $requested_page;
 		}
-
 		$render_func = 'render_type_'.$render_type;
 		$rendered = $this->$render_func(array(
 			'total_records'		=> $total_records,
@@ -93,7 +91,6 @@ class yf_divide_pages {
 			'url_path'			=> $url_path. ($add_get_vars ? _add_get(array('page')) : ''),
 			'tpls_path'			=> $tpls_path,
 		));
-		// Generate SQL string to limit output
 		$result = array(
 			'limit_sql'		=> ' LIMIT '.intval($rendered['first']).', '.intval($per_page),
 			'pages_html'	=> trim($rendered['html']),
@@ -177,9 +174,7 @@ class yf_divide_pages {
 		} else {
 			$total_blocks = $pages_per_block ? ceil($total_pages / $pages_per_block) : 0;
 			$cur_block = $pages_per_block ? ceil($cur_page / $pages_per_block) : 0;
-			// Start page in block number
 			$start_page = ($cur_block - 1) * $pages_per_block + 1;
-			// End page in block number
 			$end_page = $start_page + $pages_per_block;
 			if ($end_page > $total_pages) {
 				$end_page = $total_pages + 1;
@@ -254,6 +249,90 @@ class yf_divide_pages {
 			'html'			=> $html,
 			'total_pages'	=> $total_pages,
 			'limited_pages'	=> $limited_pages,
+			'first'			=> $first,
+		);
+	}
+
+	/**
+	* Render pager type "slide"
+	*/
+	function render_type_slide ($params = array()) {
+		$total_records	= $params['total_records'];
+		$per_page		= $params['per_page'];
+		$requested_page	= $params['requested_page'];
+		$url_path		= $params['url_path'];
+		$tpls_path		= $params['tpls_path'];
+		$total_pages	= $params['total_pages'];
+		$cur_page		= $params['cur_page'];
+		$pages_per_block= $this->PAGES_PER_BLOCK;
+
+		$items = array();
+		if ($total_records < $per_page) {
+			$first = 0;
+		} else {
+			$half = ceil($pages_per_block / 2);
+			$start_page = ($cur_page - $half) > 0 ? ($cur_page - $half) : 1;
+			$end_page = $start_page + $pages_per_block;
+			if ($end_page > $total_pages) {
+				$end_page = $total_pages + 1;
+			}
+			// Show link to first page
+			if ($cur_page > 1) {
+				$items['page_first'] = tpl()->parse($tpls_path.'page_first', array(
+					'link'	=> $url_path. '&page=1',
+				));
+			}
+			// Show link to previous page
+			if ($cur_page > 1) {
+				$items['page_prev'] = tpl()->parse($tpls_path.'page_prev', array(
+					'link'	=> $url_path. '&page='.intval($cur_page - 1),
+				));
+			}
+			// Process current block of pages
+			for ($k = $start_page; $k < $end_page; $k++) {
+				$items['pages'] .= tpl()->parse($tpls_path. ($cur_page == $k ? 'page_current' : 'page_other'), array(
+					'link'		=> $url_path. '&page='.$k,
+					'page_num'	=> $k,
+				));
+			}
+			// Show link to next page
+			if ($cur_page < $total_pages) {
+				$items['page_next'] = tpl()->parse($tpls_path.'page_next', array(
+					'link'	=> $url_path. '&page='.($cur_page + 1),
+				));
+			}
+			// Show link to last page
+			if ($total_pages > $pages_per_block && $cur_page < $total_pages) {
+				$items['page_last'] = tpl()->parse($tpls_path.'page_last', array(
+					'link'	=> $url_path. '&page='.$total_pages,
+				));
+			}
+			// Set first value for the database query
+			$first = ($cur_page - 1) * $per_page;
+			// First record could be only positive
+			if ($first < 0) {
+				$first = 0;
+			}
+		}
+		$html = tpl()->parse($tpls_path.'main', array(
+			'total_pages'	=> intval($total_pages),
+			'total_records'	=> intval($total_records),
+			'record_first'	=> intval($first + 1),
+			'record_last'	=> intval($cur_page * $per_page),
+			'page_first'	=> $items['page_first'],
+			'block_prev'	=> '',
+			'page_prev'		=> $items['page_prev'],
+			'pages'			=> $items['pages'],
+			'page_next'		=> $items['page_next'],
+			'block_next'	=> '',
+			'page_last'		=> $items['page_last'],
+			'current_page'	=> $cur_page,
+		));
+		return array(
+			'html'			=> $html,
+			'total_pages'	=> $total_pages,
+			'limited_pages'	=> $limited_pages,
+			'first'			=> $first,
 		);
 	}
 }
