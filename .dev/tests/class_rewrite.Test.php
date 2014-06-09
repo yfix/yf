72545a5ad7102683ac11592b5071ba9209a176b6
Yuri Vysotskiy <profy.net@gmail.com>
2014-06-09 15:26:54 +0300

    rewrite tests up

--- .dev/tests/class_rewrite.Test.php
+++ .dev/tests/class_rewrite.Test.php
@@ -17,34 +17,35 @@ class class_rewrite_test extends PHPUnit_Framework_TestCase {
 		self::$_bak_settings['REWRITE_MODE'] = $GLOBALS['PROJECT_CONF']['tpl']['REWRITE_MODE'];
 		$GLOBALS['PROJECT_CONF']['tpl']['REWRITE_MODE'] = true;
 	}
-
 	public static function tearDownAfterClass() {
 		$GLOBALS['PROJECT_CONF']['tpl']['REWRITE_MODE'] = self::$_bak_settings['REWRITE_MODE'];
 	}
+	public function test_rewrite_enabled() {
+		$GLOBALS['PROJECT_CONF']['tpl']['REWRITE_MODE'] = true;
 
-	public function test_required_1() {
 #		$this->assertEquals(false, module('rewrite')->_force_get_url() );
 #		$this->assertEquals(false, module('rewrite')->_force_get_url('') );
 #		$this->assertEquals(false, module('rewrite')->_force_get_url('', '') );
 #		$this->assertEquals(false, module('rewrite')->_force_get_url('', self::$host) );
 
 		$this->assertEquals('http://'.self::$host.'/test', _force_get_url(array('object' => 'test'), self::$host) );
-		$this->assertEquals('http://'.self::$host.'/test', _force_get_url(array('object' => 'test', 'host' => self::$host)) );
-		$this->assertEquals('http://'.self::$host.'/test', _force_get_url(array('object' => 'test', 'action' => 'show', 'host' => self::$host)) );
-		$this->assertEquals('http://'.self::$host.'/test/my', _force_get_url(array('object' => 'test', 'action' => 'my', 'host' => self::$host)) );
-		$this->assertEquals('http://'.self::$host.'/test/my/123', _force_get_url(array('object' => 'test', 'action' => 'my', 'id' => '123', 'host' => self::$host)) );
-		$this->assertEquals('http://'.self::$host.'/test/123', _force_get_url(array('object' => 'test', 'action' => 'show', 'id' => '123', 'host' => self::$host)) );
-		$this->assertEquals('http://'.self::$host.'/test/my/123/456', _force_get_url(array('object' => 'test', 'action' => 'my', 'id' => '123', 'page' => '456', 'host' => self::$host)) );
-		$this->assertEquals('http://'.self::$host.'/test/0/678', _force_get_url(array('object' => 'test', 'page' => '678', 'host' => self::$host)) );
+		$this->assertEquals('http://'.self::$host.'/test', url(array('object' => 'test'), self::$host) );
+		$this->assertEquals('http://'.self::$host.'/test', url(array('object' => 'test', 'host' => self::$host)) );
+		$this->assertEquals('http://'.self::$host.'/test', url(array('object' => 'test', 'action' => 'show', 'host' => self::$host)) );
+		$this->assertEquals('http://'.self::$host.'/test/my', url(array('object' => 'test', 'action' => 'my', 'host' => self::$host)) );
+		$this->assertEquals('http://'.self::$host.'/test/my/123', url(array('object' => 'test', 'action' => 'my', 'id' => '123', 'host' => self::$host)) );
+		$this->assertEquals('http://'.self::$host.'/test/123', url(array('object' => 'test', 'action' => 'show', 'id' => '123', 'host' => self::$host)) );
+		$this->assertEquals('http://'.self::$host.'/test/my/123/456', url(array('object' => 'test', 'action' => 'my', 'id' => '123', 'page' => '456', 'host' => self::$host)) );
+		$this->assertEquals('http://'.self::$host.'/test/0/678', url(array('object' => 'test', 'page' => '678', 'host' => self::$host)) );
 
-		$this->assertEquals('http://'.self::$host.'/login', _force_get_url(array('task' => 'login', 'host' => self::$host)) );
-		$this->assertEquals('http://'.self::$host.'/logout', _force_get_url(array('task' => 'logout', 'host' => self::$host)) );
-		$this->assertEquals('http://'.self::$host.'/login/asdf', _force_get_url(array('task' => 'login', 'id' => 'asdf', 'host' => self::$host)) );
+		$this->assertEquals('http://'.self::$host.'/login', url(array('task' => 'login', 'host' => self::$host)) );
+		$this->assertEquals('http://'.self::$host.'/logout', url(array('task' => 'logout', 'host' => self::$host)) );
+		$this->assertEquals('http://'.self::$host.'/login/asdf', url(array('task' => 'login', 'id' => 'asdf', 'host' => self::$host)) );
 
-#		$this->assertEquals('http://'.self::$host.'/', _force_get_url(array('task' => 'fdsfdsfds'), self::$host) );
+#		$this->assertEquals('http://'.self::$host.'/', url(array('task' => 'fdsfdsfds'), self::$host) );
 
-		// Short form of writing _force_get_url
-		$this->assertEquals('http://'.self::$host.'/test/my/123/456', _force_get_url('/test/my/123/456', array('host' => self::$host)) );
+		// Short form of writing url
+		$this->assertEquals('http://'.self::$host.'/test/my/123/456', url('/test/my/123/456', array('host' => self::$host)) );
 
 		// Alias
 		$this->assertEquals('http://'.self::$host.'/test', url('/test', array('host' => self::$host)) );
@@ -68,9 +69,34 @@ class class_rewrite_test extends PHPUnit_Framework_TestCase {
 		$this->assertEquals('http://'.self::$host.'/test', url(self::$host.'/test/////////////') );
 		$this->assertEquals('http://'.self::$host.'/test', url(self::$host.'/test/////////////something') );
 	}
-	public function test_rewrite_simple() {
+	public function test_rewrite_enabled_simple() {
+		$GLOBALS['PROJECT_CONF']['tpl']['REWRITE_MODE'] = true;
+// TODO
+#		$this->assertEquals('/test', process_url('./?object=test'));
 		$this->assertEquals('./test', process_url('./?object=test'));
-		$GLOBALS['PROJECT_CONF']['tpl']['REWRITE_MODE'] = self::$_bak_settings['REWRITE_MODE'];
-		$this->assertEquals('./?object=test', process_url('./?object=test'));
+	}
+	public function test_rewrite_disabled_simple() {
+		$GLOBALS['PROJECT_CONF']['tpl']['REWRITE_MODE'] = false;
+#		$_SERVER['HTTP_HOST'] = self::$host;
+		$this->assertEquals('./?object=test', process_url('./?object=test', $force_rewrite = false));
+	}
+	public function test_rewrite_disabled() {
+#		$GLOBALS['PROJECT_CONF']['tpl']['REWRITE_MODE'] = false;
+#		$_GET['object'] = 'testobj';
+#		$this->assertEquals('./?object=testobj&action=testme&id=4', url('/@object/testme/4') );
+	}
+	public function test_rewrite_url_user_enabled() {
+		$GLOBALS['PROJECT_CONF']['tpl']['REWRITE_MODE'] = true;
+// TODO
+#		$this->assertEquals('./?object=testobj&action=testme&id=4', url('/@object/testme/4') );
+	}
+	public function test_rewrite_url_user_disabled() {
+		$GLOBALS['PROJECT_CONF']['tpl']['REWRITE_MODE'] = false;
+// TODO
+	}
+	public function test_rewrite_url_admin() {
+		$GLOBALS['PROJECT_CONF']['tpl']['REWRITE_MODE'] = false;
+// TODO
+#		$this->assertEquals('./?object=testobj&action=testme&id=4', url('/@object/testme/4') );
 	}
 }
