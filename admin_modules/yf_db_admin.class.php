708c4ecc51f5e6fc361bb09ddbaaff4aa39d770f
Yuri Vysotskiy <profy.net@gmail.com>
2014-06-10 15:15:24 +0300

    db admin updated

--- admin_modules/yf_db_admin.class.php
+++ admin_modules/yf_db_admin.class.php
@@ -58,26 +58,59 @@ class yf_db_admin {
 			return _e('Wrong name');
 		}
 		$db = $this->_db_custom_connection($db_name);
-		foreach ((array)$db->utils()->list_tables() as $name) {
-			$tables[$name] = array(
-				'name'	=> $name,
-			);
-		}
+
 		return _class('html')->tabs(array(
-			'tables' => table($tables, $this->table_params)
+			'tables' => table(
+				function() use ($db) {
+					foreach ((array)$db->utils()->list_tables() as $name) {
+						$data[$name] = array('name'	=> $name);
+					}; return $data;
+				}, $this->table_params)
 				->link('name', url_admin('/@object/table_show/%d/'), array(), array('class' => ' '))
 				->btn_edit('', url_admin('/@object/table_edit/%d/'))
 				->btn_delete('', url_admin('/@object/table_delete/%d/'))
 				->header_add('Add database', url_admin('/@object/table_add/'))
 			,
-			'views' => table($views, $this->table_params)
+			'views' => table(
+				function() use ($db) {
+					foreach ((array)$db->utils()->list_views() as $name) {
+						$data[$name] = array('name'	=> $name);
+					}; return $data;
+				}, $this->table_params)
 				->link('name', url_admin('/@object/view_show/%d/'), array(), array('class' => ' '))
 			,
-			'triggers' => table($views, $this->table_params)
+			'triggers' => table(
+				function() use ($db) {
+					foreach ((array)$db->utils()->list_triggers() as $name) {
+						$data[$name] = array('name'	=> $name);
+					}; return $data;
+				}, $this->table_params)
 				->link('name', url_admin('/@object/trigger_show/%d/'), array(), array('class' => ' '))
 			,
-			'events' => table($views, $this->table_params)
+			'procedures' => table(
+				function() use ($db) {
+#					foreach ((array)$db->utils()->list_procedures() as $name) {
+#						$data[$name] = array('name'	=> $name);
+#					}; return $data;
+				}, $this->table_params)
+				->link('name', url_admin('/@object/procedure_show/%d/'), array(), array('class' => ' '))
+			,
+			'functions' => table(
+				function() use ($db) {
+#					foreach ((array)$db->utils()->list_functions() as $name) {
+#						$data[$name] = array('name'	=> $name);
+#					}; return $data;
+				}, $this->table_params)
+				->link('name', url_admin('/@object/function_show/%d/'), array(), array('class' => ' '))
+			,
+			'events' => table(
+				function() use ($db) {
+					foreach ((array)$db->utils()->list_events() as $name) {
+						$data[$name] = array('name'	=> $name);
+					}; return $data;
+				}, $this->table_params)
 				->link('name', url_admin('/@object/event_show/%d/'), array(), array('class' => ' '))
+			,
 		));
 	}
 
