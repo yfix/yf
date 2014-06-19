ff5fb8777e34e3c5414dc64d4a0504fb898065c3
LArV <larv.job@gmail.com>
2014-06-19 12:54:57 +0300

    fix _filter_sql_prepare

--- classes/yf_table2.class.php
+++ classes/yf_table2.class.php
@@ -653,11 +653,11 @@ class yf_table2 {
 
 	/**
 	*/
-	function _filter_sql_prepare($filter_data = array(), $filter_params = array(), $__sql = '') {
+	function _filter_sql_prepare($filter_data = array(), $filter_params = array(), $__sql = '', $_this = null) {
 		if (!$filter_data) {
 			return '';
 		}
-		return _class('table2_filter', 'classes/table2/')->_filter_sql_prepare($filter_data, $filter_params, $__sql);
+		return _class('table2_filter', 'classes/table2/')->_filter_sql_prepare($filter_data, $filter_params, $__sql, $_this);
 	}
 
 	/**
