6baff6a1d5b5844545dd9900db2303d7910dc820
Yuri Vysotskiy <profy.net@gmail.com>
2014-07-01 23:23:02 +0300

    fix

--- classes/db/yf_db_orm_driver.class.php
+++ classes/db/yf_db_orm_driver.class.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
-* YF db utils driver abstract class
+* YF db ORM driver abstract class
 */
-abstract class yf_db_utils_driver {
+abstract class yf_db_orm_driver {
 // TODO: add abstract methods here
 }
