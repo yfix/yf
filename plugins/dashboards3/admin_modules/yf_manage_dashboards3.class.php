1f4cf3a30fd33a023583b252a2c65415d55b0f2b
Yaroslav Yakovenko <yyv.yyv@gmail.com>
2014-07-02 13:36:40 +0300

    start dashboards3

---
+++ plugins/dashboards3/admin_modules/yf_manage_dashboards3.class.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+* Dashboards3 management
+* 
+* @package		YF
+* @author		YFix Team <yfix.dev@gmail.com>
+* @version		1.0
+*/
+class yf_manage_dashboards3 {
+
+	/**
+	*/
+	function _init () {
+		conf('css_framework', 'bs3');
+	}
+
+	/**
+	*/
+	function show() {
+	}
+
+	/**
+	*/
+	function save() {
+	
+	}
+
+	/**
+	*/
+	function delete () {
+	
+	}
+
+
+	/**
+	*/
+	function clone_item() {
+	
+	}
+
+	/**
+	*/
+	function active () {
+	
+	}	
+
+	/**
+	*/
+	function lock () {
+	
+	}
+
+	/**
+	*/
+	function add () {
+	
+	}
+
+	/**
+	*/
+	function edit() {
+	}
+
+
+	/**
+	*/
+	function view () {
+	}
+
+}
