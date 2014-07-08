3157ef8db4723fb167f92d9b6d927bf5ace0e438
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-07-08 15:07:23 +0300

    db admin js postload cleanup

--- admin_modules/yf_db_admin.class.php
+++ admin_modules/yf_db_admin.class.php
@@ -181,49 +181,59 @@ class yf_db_admin {
 		}
 		$db = $this->_db_custom_connection($db_name);
 
+		$attr_postload_url = 'postload-url';
+		$tr_id_key = 'name';
+
 		$jquery = '
-			$("table[data-ajax-table-url]").each(function(){
-				var table = $(this)
-					, url = table.data("ajax-table-url")
-					, tr_id_key = "name"
-					, spinner = table.before("<i class=\"icon icon-spinner icon-2x icon-spin fa fa-spinner fa-2x fa-spin\" id=\"table_ajax_spinner\" style=\"position:fixed;\"></i>").prev()
+			var attr_postload_url = "'.$attr_postload_url.'"
+				, tr_id_key = "'.$tr_id_key.'"
+			;
+			var process_ajax_data_func = function(data, table) {
+				var th_id_prefix = "th_"
+					, th_id_cut_len = th_id_prefix.length
+					, tr_id_prefix = "tr_"
+					, tr_id_cut_len = tr_id_prefix.length
 					, th_positions = { }
 				;
 				table.find("th").each(function(i) {
 					var th_id = $(this).attr("id");
-					if (th_id && th_id.substring(0, 3) == "th_") {
-						th_positions[th_id.substring(3)] = i;
+					if (th_id && th_id.substring(0, th_id_cut_len) == th_id_prefix) {
+						th_positions[th_id.substring(th_id_cut_len)] = i;
 					}
 				})
-				var _process_ajax_data = function(data) {
-					var tr_id_key_len = tr_id_key.length + 1
-					table.find("tr").each(function(i) {
-						var tr = $(this);
-						var tr_id = tr.find(":checkbox").attr("name");
-						if (tr_id) {
-							// name[test_user] => user
-							tr_id = tr_id.substring(tr_id_key_len, tr_id.length - 1);
+				table.find("tr").each(function(i) {
+					var tr = $(this)
+						, tr_id = tr.attr("id")
+					;
+					if (tr_id && tr_id.length && tr_id.substring(0, tr_id_cut_len) == tr_id_prefix) {
+						tr_id = tr_id.substring(tr_id_cut_len)
+					}
+					if (!tr_id) {
+						return;
+					}
+					for (key1 in data) {
+						var td = tr.find("td").eq(th_positions[key1])
+							, ajax_arr = data[key1][tr_id]
+							, td_a = td.find("a")
+						;
+						if (td_a) {
+							td_a.text(ajax_arr)
+						} else {
+							td.text(ajax_arr)
 						}
-						if (!tr_id) {
-							return;
-						}
-						for (key1 in data) {
-							var td = tr.find("td").eq(th_positions[key1])
-							var ajax_arr = data[key1][tr_id];
-							var td_a = td.find("a");
-							if (td_a) {
-								td_a.text(ajax_arr)
-							} else {
-								td.text(ajax_arr)
-							}
-						}
-					})
-					spinner.remove();
-				}
-				$.post(url, function(data) {
-					_process_ajax_data(data);
+					}
 				})
-			})
+			};
+			$("table[data-" + attr_postload_url + "]").each(function(){
+				var table = $(this)
+					, url = table.data(attr_postload_url)
+					, spinner = table.before("<i class=\"icon icon-spinner icon-2x icon-spin fa fa-spinner fa-2x fa-spin\" id=\"table_ajax_spinner\" style=\"position:fixed;\"></i>").prev()
+				;
+				$.post(url, function(data) {
+					process_ajax_data_func(data, table);
+					spinner.remove();
+				})
+			});
 		';
 		_class('core_events')->listen('show_js.append', function() use ($jquery) {
 			return '<script type="text/javascript">'.PHP_EOL.'$(function(){'.PHP_EOL.$jquery.PHP_EOL.'})'.PHP_EOL.'</script>'.PHP_EOL;
@@ -246,7 +256,11 @@ class yf_db_admin {
 					}; return $data;
 				}, $this->table_params + array(
 					'feedback' => &$totals['tables'],
-					'table_attr' => 'data-ajax-table-url="'._prepare_html(url_admin('/@object/database_show_ajax/'.$db_name.'/')).'"'
+					'table_attr' => 'data-postload-url="'._prepare_html(url_admin('/@object/database_show_ajax/'.$db_name.'/')).'"',
+#					'data-postload-url' => 'data-postload-url="'._prepare_html(url_admin('/@object/database_show_ajax/'.$db_name.'/')).'"',
+					'tr' => function($row, $_id) {
+						return ' id="tr_'.$_id.'"';
+					},
 				))
 				->check_box('name', array('th_desc' => '#'))
 				->link('name', url_admin('/@object/table_show/'.$db_name.'.%d/'))
