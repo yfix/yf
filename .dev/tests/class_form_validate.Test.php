d590338be0b0c7f7e63bfd04bcbb5faa0400a8e1
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-07-16 14:50:09 +0300

    form validate complex tests added

--- .dev/tests/class_form_validate.Test.php
+++ .dev/tests/class_form_validate.Test.php
@@ -4,15 +4,75 @@ require dirname(__FILE__).'/yf_unit_tests_setup.php';
 
 class class_form_validate_test extends PHPUnit_Framework_TestCase {
 	function test_complex() {
-/*
-		form($a)->text('name');
 		$this->assertEquals('', common()->_get_error_messages());
 
 		$old = $_SERVER['REQUEST_METHOD'];
 		$_SERVER['REQUEST_METHOD'] = 'POST';
-		form($a)->text('name', array('validate' => 'required'))->validate(array('name' => 'required'), array('name' => ''));
+
+		$_POST['name'] = '';
+
+		form()
+			->text('name', array('validate' => 'required'))
+			->validate()
+			->render();
+		$this->assertEquals(array('name' => 'The Name field is required.'), common()->_get_error_messages());
+
+		common()->_show_error_message($msg = '', $clear = true);
 		$this->assertEquals('', common()->_get_error_messages());
+
+		$_POST['name'] = '';
+
+		form()
+			->text('name')
+			->validate($rules = array('name' => 'required'))
+			->render();
+		$this->assertEquals(array('name' => 'The Name field is required.'), common()->_get_error_messages());
+
+		common()->_show_error_message($msg = '', $clear = true);
+		$this->assertEquals('', common()->_get_error_messages());
+
+		$_POST['name'] = '';
+
+		form($a)
+			->text('name')
+			->validate($rules = array('name' => 'trim'))
+			->render();
+		$this->assertEquals('', common()->_get_error_messages());
+
+		$_POST['name'] = 'something';
+
+		form($a)
+			->text('name')
+			->validate($rules = array('name' => 'required'), $post = array('name' => ''))
+			->render();
+		$this->assertEquals(array('name' => 'The Name field is required.'), common()->_get_error_messages());
+
+		common()->_show_error_message($msg = '', $clear = true);
+		$this->assertEquals('', common()->_get_error_messages());
+
+		$_POST['name1'] = 'val';
+		$_POST['name2'] = 'val';
+
+		form($a)
+			->text('name1')
+			->text('name2')
+			->validate($rules = array('name1' => 'trim', 'name2' => 'matches:name1'))
+			->render();
+		$this->assertEquals('', common()->_get_error_messages());
+
+		$_POST['name1'] = 'val';
+		$_POST['name2'] = 'other';
+
+		form($a)
+			->text('name1')
+			->text('name2')
+			->validate($rules = array('name1' => 'trim', 'name2' => 'matches:name1'))
+			->render();
+		$this->assertEquals(array('name2' => 'The Name2 field does not match the name1 field.'), common()->_get_error_messages());
+
+		common()->_show_error_message($msg = '', $clear = true);
+		$this->assertEquals('', common()->_get_error_messages());
+
 		$_SERVER['REQUEST_METHOD'] = $old;
-*/
 	}
 }
