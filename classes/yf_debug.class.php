a1e0e9b480aa785510ac233c77e38446421d32b0
Yaroslav Yakovenko <yyv.yyv@gmail.com>
2014-07-16 17:54:43 +0300

    add debug from dashboard

--- classes/yf_debug.class.php
+++ classes/yf_debug.class.php
@@ -21,6 +21,7 @@ class yf_debug {
 	public $SHOW_INCLUDED_FILES		= true;
 	public $SHOW_LOADED_MODULES		= true;
 	public $SHOW_MEMCACHED_INFO		= true;
+	public $SHOW_DASHBOARD_INFO		= true;
 	public $SHOW_EACCELERATOR_INFO	= true;
 	public $SHOW_XCACHE_INFO		= true;
 	public $SHOW_APC_INFO			= true;
@@ -1318,5 +1319,27 @@ class yf_debug {
 
 		$body .= 'var _i18n_for_page = '.json_encode($js_vars);
 		return '<script type="text/javascript">'.$body.'</script>';
+	}
+
+	/**
+		*
+		*     */
+
+	function _debug_dashboards () {
+		if (!$this->SHOW_DB_STATS) {
+			return '';
+		}
+
+		$items = $this->_get_debug_data('dashboard');
+
+		$_items = $items['widgets'];
+		$_items = $this->_time_count_changes($_items);
+
+		$data  = '<div class="span4 col-lg-4">dashboard name: <b>'.$items['name'].'</b><br /><br />';
+		$data .= 'Total time: <b>'.$items['total_time'].'</b><br /><br />';
+		$data .= $this->_show_auto_table($_items, array('hidden_map' => array()));
+		$data .= '</div>';
+
+		return $data;
 	}
 }
--- plugins/dashboards2/classes/yf_dashboards2.class.php
+++ plugins/dashboards2/classes/yf_dashboards2.class.php
@@ -19,6 +19,8 @@ class yf_dashboards2 {
 		12 => 'span1 col-md-1 column',
 	);
 
+	private $_debug_info = array();
+	private $_time_start;
 // TODO: add options for items:
 // min_height=0|(int)
 // max_height=0|(int)
@@ -36,6 +38,7 @@ class yf_dashboards2 {
 	*/
 	function display($params = array()) {
 
+		$this->_time_start = microtime(true);
 		if (is_string($params)) {
 			$name = $params;
 		}
@@ -49,7 +52,12 @@ class yf_dashboards2 {
 			return _e('Empty dashboard name');
 		}
 		$this->_name = $params['name'];
-		return $this->view($params);
+
+		$dashboard_data = $this->view($params);
+
+		$this->_debug_info['total_time'] =	round(microtime(true) - $this->_time_start, 5);
+		DEBUG_MODE && debug('dashboard', $this->_debug_info);		
+		return $dashboard_data;
 	}
 
 	/**
@@ -60,6 +68,7 @@ class yf_dashboards2 {
 			$params = array();
 		}
 		$ds_name = isset($params['name']) ? $params['name'] : ($this->_name ? $this->_name : $_GET['id']);
+		$this->_debug_info['name'] = $ds_name;
 		$ds = $this->_get_dashboard_data($ds_name);
  
 		if (!$ds['id']) {
@@ -70,6 +79,7 @@ class yf_dashboards2 {
 		if(isset($ds['data']['rows']) && is_array($ds['data']['rows'] )){
 			$grid = $this->_get_grid($ds['data']['rows']);
 		}
+	//	print_r($this->_debug_info);
 		return $grid;
 	}
 
@@ -102,7 +112,6 @@ class yf_dashboards2 {
 		$replace = array(
 			'rows'	=> $rows,
 		);
-		
 		return tpl()->parse(__CLASS__.'/view_main', $replace);
 	}
 
@@ -115,58 +124,68 @@ class yf_dashboards2 {
 		$_orig_action = $_GET['action'];
 		$is_cloneable_item = true;
 
-	
-			$module_name = '';
-			$method_name = '';
-			$content = '';
-			if ($is_cloneable_item) {
-				if ($widgets["type"] == 'php') {
-					if (strlen($info['code'])) {
-//						$content = eval('<?'.'php '.$info['code']);
-					} elseif ($info['method_name']) {
-//						list($module_name, $method_name) = explode('.', $info['method_name']);
-					}
-					list($module_name, $method_name) = explode('.', $widgets['val']);
-				} elseif ($widgets["type"] == 'block') {
-					$content = _class('core_blocks')->show_block(array('block_id' => $info['block_name']));
-				} elseif ($widgets["type"] == 'stpl') {
-					if (strlen($info['code'])) {
-						$content = tpl()->parse_string($info['code']);
-					} elseif ($info['stpl_name']) {
-						$content = tpl()->parse($info['stpl_name']);
-					}
+
+		$module_name = '';
+		$method_name = '';
+		$content = '';
+		if ($is_cloneable_item) {
+			if ($widgets["type"] == 'php') {
+				if (strlen($info['code'])) {
+					//						$content = eval('<?'.'php '.$info['code']);
+				} elseif ($info['method_name']) {
+					//						list($module_name, $method_name) = explode('.', $info['method_name']);
 				}
+				list($module_name, $method_name) = explode('.', $widgets['val']);
+			} elseif ($widgets["type"] == 'block') {
+				$content = _class('core_blocks')->show_block(array('block_id' => $info['block_name']));
+			} elseif ($widgets["type"] == 'stpl') {
+				if (strlen($info['code'])) {
+					$content = tpl()->parse_string($info['code']);
+				} elseif ($info['stpl_name']) {
+					$content = tpl()->parse($info['stpl_name']);
+				}
+			}
+		} else {
+			list($module_name, $method_name) = explode('::', $info['full_name']);
+		}
+
+		if ($module_name && $method_name) {
+			// This is needed to correctly execute widget (maybe not nicest method, I know...)
+			//$_GET['object'] = $module_name;
+			//$_GET['action'] = $method_name;
+			//
+
+			$_time_start = microtime(true);
+			$module_obj = module_safe($module_name);
+			if (is_object($module_obj) && method_exists($module_obj, $method_name)) {
+				$content = $module_obj->$method_name($saved_config);
+				$path = $this->get_storadge_and_path($module_name);	
+				//	var_dump($bb);				
+				$this->_debug_info['widgets'][] = array (
+					'object'       => $module_name,
+					'action'       => $method_name,
+					'storage'      => $path['storage'],
+					'loaded_path'  => $path ['loaded_path'],
+					'time'         => round(microtime(true) - $_time_start, 5),
+				);
 			} else {
-				list($module_name, $method_name) = explode('::', $info['full_name']);
+				trigger_error(__CLASS__.': called module.method from widget not exists: '.$module_name.'.'.$method_name.'', E_USER_WARNING);
 			}
-			
-			if ($module_name && $method_name) {
-				// This is needed to correctly execute widget (maybe not nicest method, I know...)
-				//$_GET['object'] = $module_name;
-				//$_GET['action'] = $method_name;
-				//
+			$_GET['object'] = $_orig_object;
+			$_GET['action'] = $_orig_action;
+		}
 
-				$module_obj = module_safe($module_name);
-				if (is_object($module_obj) && method_exists($module_obj, $method_name)) {
-					$content = $module_obj->$method_name($saved_config);
-				} else {
-					trigger_error(__CLASS__.': called module.method from widget not exists: '.$module_name.'.'.$method_name.'', E_USER_WARNING);
-				}
-				$_GET['object'] = $_orig_object;
-				$_GET['action'] = $_orig_action;
-			}
-
-			$items[$info['auto_id']] = tpl()->parse(__CLASS__.'/view_item', array(
+		$items[$info['auto_id']] = tpl()->parse(__CLASS__.'/view_item', array(
 			//	'id'			=> $info['auto_id'].'_'.$info['auto_id'],
-				'name'			=> _prepare_html($widgets["type"]),
-				'content'		=> $content,
-				'has_config'	=> 0,
-//				'css_class'		=> $saved_config['color'],
-				'hide_header'	=> 1,
-				'hide_border'	=> 1,
-			));
-	
-		
+			'name'			=> _prepare_html($widgets["type"]),
+			'content'		=> $content,
+			'has_config'	=> 0,
+			//				'css_class'		=> $saved_config['color'],
+			'hide_header'	=> 1,
+			'hide_border'	=> 1,
+		));
+
+
 
 		if (!$items) {
 			return '';
@@ -194,6 +213,151 @@ class yf_dashboards2 {
 		$this->_dashboard_data[$id] = $ds;
 		return $ds;
 	}
+/**
+	 */
+	function get_storadge_and_path ($class_name = '', $custom_path=''){
+	
+		if (empty($class_name) || $class_name == 'main') {
+			return false;
+		}
+		$cur_hook_prefix = MAIN_TYPE_ADMIN ? YF_ADMIN_CLS_PREFIX : YF_SITE_CLS_PREFIX;
+		$loaded_class_name	= false;
+		// Site loaded class have top priority
+		$site_class_name = $cur_hook_prefix. $class_name;
+		$class_file = $class_name. YF_CLS_EXT;
+		// Developer part of path is related to hostname to be able to make different code overrides for each
+		$dev_path = '.dev/'. main()->HOSTNAME.'/';
+		// additional path variables
+		$SITE_PATH = MAIN_TYPE_USER ? SITE_PATH : ADMIN_SITE_PATH;
+		if (MAIN_TYPE_USER) {
+			if (empty($custom_path)) {
+				$site_path			= USER_MODULES_DIR;
+				$site_path_dev		= $dev_path. USER_MODULES_DIR;
+				$project_path		= USER_MODULES_DIR;
+				$project_path_dev	= $dev_path. USER_MODULES_DIR;
+				$fwork_path			= USER_MODULES_DIR;
+				$fwork_path2		= 'priority2/'. USER_MODULES_DIR;
+			} elseif (false === strpos($custom_path, SITE_PATH) && false === strpos($custom_path, PROJECT_PATH)) {
+				$site_path			= $custom_path;
+				$site_path_dev		= $dev_path. $custom_path;
+				$project_path		= $custom_path;
+				$project_path_dev	= $dev_path. $custom_path;
+				$fwork_path			= $custom_path;
+				$fwork_path2		= 'priority2/'. $custom_path;
+			} else {
+				$site_path			= $custom_path;
+			}
+		} elseif (MAIN_TYPE_ADMIN) {
+			if (empty($custom_path)) {
+				$site_path			= ADMIN_MODULES_DIR;
+				$site_path_dev		= $dev_path. ADMIN_MODULES_DIR;
+				$project_path		= ADMIN_MODULES_DIR;
+				$project_path_dev	= $dev_path. ADMIN_MODULES_DIR;
+				$fwork_path			= ADMIN_MODULES_DIR;
+				$fwork_path2		= 'priority2/'. ADMIN_MODULES_DIR;
+				$project_path2		= USER_MODULES_DIR;
+			} elseif (false === strpos($custom_path, SITE_PATH) && false === strpos($custom_path, PROJECT_PATH) && false === strpos($custom_path, ADMIN_SITE_PATH)) {
+				$site_path			= $custom_path;
+				$site_path_dev		= $dev_path. $custom_path;
+				$project_path		= $custom_path;
+				$project_path_dev	= $dev_path. $custom_path;
+				$fwork_path			= $custom_path;
+				$fwork_path2		= 'priority2/'. $custom_path;
+			} else {
+				$site_path			= $custom_path;
+			}
+		}
+		if (!isset(main()->_plugins)) {
+			main()->_preload_plugins_list();
+		}
+		$yf_plugins = main()->_plugins;
+		$yf_plugins_classes = main()->_plugins_classes;
 
+		// Order of storages matters a lot!
+		$storages = array();
+		if (conf('DEV_MODE')) {
+			if ($site_path_dev && $site_path_dev != $project_path_dev) {
+				$storages['dev_site']	= array($SITE_PATH. $site_path_dev);
+			}
+			$storages['dev_project'] = array(PROJECT_PATH. $project_path_dev);
+		}
+		if (strlen($SITE_PATH. $site_path) && ($SITE_PATH. $site_path) != (PROJECT_PATH. $project_path)) {
+			$storages['site'] 		= array($SITE_PATH. $site_path);
+		}
+		$storages['site_hook']		= array($SITE_PATH. $site_path, $cur_hook_prefix);
+		$storages['project']		= array(PROJECT_PATH. $project_path);
+		$storages['framework']		= array(YF_PATH. $fwork_path, YF_PREFIX);
+		$storages['framework_p2']	= array(YF_PATH. $fwork_path2, YF_PREFIX);
+		if (MAIN_TYPE_ADMIN) {
+			$storages['admin_user_project']		= array(PROJECT_PATH. $project_path2);
+			$storages['admin_user_framework']	= array(YF_PATH. USER_MODULES_DIR, YF_PREFIX);
+		}
+		if (isset($yf_plugins[$class_name]) || isset($yf_plugins_classes[$class_name])) {
+			if (isset($yf_plugins[$class_name])) {
+				$plugin_name = $class_name;
+			} else {
+				$plugin_name = $yf_plugins_classes[$class_name];
+			}
+			$plugin_info = $yf_plugins[$plugin_name];
+			$plugin_subdir = 'plugins/'.$plugin_name.'/';
+
+			if ($site_path && $site_path != $project_path) {
+				$storages['plugins_site']	= array($SITE_PATH. $plugin_subdir. $site_path);
+			}
+			if (isset($plugin_info['project'])) {
+				$storages['plugins_project']	= array(PROJECT_PATH. $plugin_subdir. $project_path);
+				if (MAIN_TYPE_ADMIN) {
+					$storages['plugins_admin_user_project']	= array(PROJECT_PATH. $plugin_subdir. $project_path2);
+				}
+			} elseif (isset($plugin_info['framework'])) {
+				$storages['plugins_framework']	= array(YF_PATH. $plugin_subdir. $fwork_path, YF_PREFIX);
+				if (MAIN_TYPE_ADMIN) {
+					$storages['plugins_admin_user_framework'] = array(YF_PATH. $plugin_subdir. USER_MODULES_DIR, YF_PREFIX);
+				}
+			}
+		}
+		$storage = '';
+		$loaded_path = '';
+		foreach ((array)$storages as $_storage => $v) {
+			$_path		= strval($v[0]);
+			$_prefix	= strval($v[1]);
+			if (empty($_path)) {
+				continue;
+			}
+
+		/*	if(file_exists(	$_path. $_prefix. $class_file)){
+				$data = array (
+					'loaded_class_name'	=> $_prefix. $class_name,
+					'storage' => $_storage,
+					'loaded_path' => $_path. $_prefix. $class_file,
+				);
+			//	break;
+		}*/
+			if (class_exists($_prefix. $class_name)) {
+				$data = array (
+					'loaded_class_name'	=> $_prefix. $class_name,
+					'storage' => $_storage,
+					'loaded_path' => $_path. $_prefix. $class_file,
+				);
+				break;
+
+			}
+
+		}
+		// Try to load classes from db
+	/*	if (empty($loaded_class_name) && $this->ALLOW_SOURCE_FROM_DB && is_object($this->db)) {
+			$result_from_db = $this->db->query_fetch('SELECT * FROM '.db('code_source').' WHERE keyword="'._es($class_name).'"');
+			if (!empty($result_from_db)) {
+				eval($result_from_db['source']);
+			}
+			if (class_exists($class_name)) {
+				$loaded_class_name	= $class_name;
+				$storage = 'db';
+			}
+		
+	}*/
+		return $data;
+	}
+	
 	
 }
