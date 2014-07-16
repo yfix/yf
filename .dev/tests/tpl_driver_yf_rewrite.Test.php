45e62149e565ca7e30f1228e7532a910bb67f65c
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-07-16 17:15:22 +0300

    unit tests for tpl rewrite added

---
+++ .dev/tests/tpl_driver_yf_rewrite.Test.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once dirname(__FILE__).'/tpl__setup.php';
+
+class tpl_driver_yf_rewrite_test extends tpl_abstract {
+	public function test_url() {
+		$host = 'subdomain.test.dev';
+		$_SERVER['HTTP_HOST'] = $host;
+		$this->assertEquals('http://'.$host.'/?object=shop&action=basket', self::_tpl( '{url(object=shop;action=basket)}' ));
+		$this->assertEquals('http://'.$host.'/?object=shop&action=basket', self::_tpl( '{url(/shop/basket/)}' ));
+		$this->assertEquals('http://'.$host.'/?object=shop&action=autocomplete&search_word=%QUERY', self::_tpl( '{url(object=shop;action=autocomplete;search_word=%QUERY)}' ));
+		$this->assertEquals('http://'.$host.'/?object=shop&action=autocomplete&id=&search_word=%QUERY', self::_tpl( '{url(/shop/autocomplete/&search_word=%QUERY)}' ));
+	}
+}
