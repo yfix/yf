f1ca317e6c3095efa72591f29809a0f7a3f39006
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-07-18 14:28:50 +0300

    db query builder bugfix

--- classes/db/yf_db_query_builder_driver.class.php
+++ classes/db/yf_db_query_builder_driver.class.php
@@ -463,7 +463,7 @@ abstract class yf_db_query_builder_driver {
 				$right_generated = '('.$right_generated.')';
 			}
 		}
-		if (is_string($right) && !strlen($right) && !strlen($right_generated)) {
+		if ((empty($right) || !strlen($right)) && !strlen($right_generated)) {
 			return '';
 		}
 		return $this->_escape_key($left). ' '. strtoupper($op). ($right_generated ?: ' '.$this->_escape_val($right));
