d380e2fef9febb9a8d64cc520a21e430151ce9c0
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-07-21 14:22:16 +0300

    unit tests setup added storage dir

--- .dev/tests/yf_unit_tests_setup.php
+++ .dev/tests/yf_unit_tests_setup.php
@@ -1,7 +1,8 @@
 <?php
 
+define('STORAGE_PATH', __DIR__.'/tests_tmp_storage/');
 if (!defined('YF_PATH')) {
-	define('YF_PATH', dirname(dirname(dirname(__FILE__))).'/');
+	define('YF_PATH', dirname(dirname(__DIR__)).'/');
 	require YF_PATH.'classes/yf_main.class.php';
 	new yf_main('user', $no_db_connect = 1, $auto_init_all = 0);
 	date_default_timezone_set('Europe/Kiev');
