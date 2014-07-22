4d0e7e867fb4afb074cb72753f8c52ab05c679ed
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-07-22 17:46:37 +0300

    unit tests for tpl extend cleanup

--- .dev/tests/tpl_driver_yf_extend.Test.php
+++ .dev/tests/tpl_driver_yf_extend.Test.php
@@ -4,25 +4,18 @@ require_once dirname(__FILE__).'/tpl__setup.php';
 
 class tpl_driver_yf_extend_test extends tpl_abstract {
 	public function test_extend() {
-		$old = tpl()->_custom_patterns;
-		tpl()->_custom_patterns = array();
-		$this->assertEmpty(tpl()->_custom_patterns);
-/*
-		$this->assertEquals('{my_new_tpl_func(testme)}', self::_tpl('{my_new_tpl_func(testme)}'));
-		tpl()->add_pattern_callback('/\{my_new_tpl_func\(\s*["\']{0,1}([a-z0-9_:\.]+?)["\']{0,1}\s*\)\}/i', function($m, $r, $name, $_this) {
-			return '__'.$m[1].'__';
-		});
-		$this->assertEquals('__testme__', self::_tpl('{my_new_tpl_func(testme)}'));
-		tpl()->_custom_patterns = array();
-		$this->assertEmpty(tpl()->_custom_patterns);
-*/
-#		$this->assertEquals('{my_new_tpl_func(testme)}', self::_tpl('{my_new_tpl_func(testme)}'));
+		$old1 = tpl()->_custom_patterns_funcs;
+		$old2 = tpl()->_custom_patterns_indexes;
+		tpl()->_custom_patterns_funcs = array();
+		tpl()->_custom_patterns_index = array();
+		$this->assertEmpty(tpl()->_custom_patterns_funcs);
+		$this->assertEmpty(tpl()->_custom_patterns_index);
+
 		tpl()->add_function_callback('my_new_tpl_func', function($m, $r, $name, $_this) {
 			return '__'.$m['args'].'__';
 		});
 		$this->assertEquals('__testme__', self::_tpl('{my_new_tpl_func(testme)}'));
 
-#		$this->assertEquals('{my_new_tpl_section(k1=v1;k2=v2)} section_body {/my_new_tpl_section}', self::_tpl('{my_new_tpl_section(k1=v1;k2=v2)} section_body {/my_new_tpl_section}'));
 		tpl()->add_section_callback('my_new_tpl_section', function($m, $r, $name, $_this) {
 			return '__'.$m['args'].'__'.$m['body'].'__';
 		});
