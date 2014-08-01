e1e6a83010513f19ae9e822b7f1eac298908752d
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-08-01 13:15:42 +0300

    renamed db installer

--- .dev/scripts/db_installer/sql_into_fields.php
+++ .dev/scripts/db_installer/sql_into_fields.php
@@ -30,7 +30,7 @@ foreach ($globs_sql as $glob) {
 			continue;
 		}
 
-		$a = _class('installer_db_mysql', 'classes/db/')->_db_table_struct_into_array($data);
+		$a = _class('db_installer_mysql', 'classes/db/')->_db_table_struct_into_array($data);
 
 		if (!$a) {
 			echo '-- ERROR: empty fields'. PHP_EOL;
