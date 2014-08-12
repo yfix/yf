2ae567e0ef3c339653ac12fd532bc30b04e6142e
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-08-12 15:12:57 +0300

    unit tests for rss updated

--- .dev/tests/func_common_rss_page.Test.php
+++ .dev/tests/func_common_rss_page.Test.php
@@ -75,4 +75,69 @@ class func_common_rss_page extends PHPUnit_Framework_TestCase {
 		$actual = preg_replace('~<lastBuildDate>[^<]+</lastBuildDate>~ims', '<lastBuildDate></lastBuildDate>', $actual);
 		$this->assertSame(trim($expected), trim($actual));
 	}
+	public function test_with_data_full() {
+		$data = array(
+			array(
+				'date'	=> strtotime('2014-08-12 12:03:19 UTC'),
+				'link'	=> url('/unit_tests/testme/1/'),
+				'author' => 'user 1',
+				'title' => 'test 1',
+				'description' => 'my description 1',
+				'enclosure' => array(
+					'url' => 'http://lh3.ggpht.com/smoliarov/Rwygj8ucrbE/AAAAAAAABIA/UkNlwQ7eniw/_200708.jpg',
+					'length'=>'65036',
+					'type'=>'image/jpeg',
+				),
+			),
+			array(
+				'date'	=> strtotime('2014-08-12 12:03:20 UTC'),
+				'link'	=> url('/unit_tests/testme/2/'),
+				'author' => 'user 2',
+				'title' => 'test 2',
+				'description' => 'my description 2',
+				'enclosure' => array(
+					'url' => 'http://lh3.ggpht.com/smoliarov/Rwygj8ucrbE/AAAAAAAABIA/UkNlwQ7eniw/_200709.jpg',
+					'length'=>'65036',
+					'type'=>'image/jpeg',
+				),
+			),
+		);
+#		$params = array('return_feed_text' => 1);
+		ob_start();
+		common()->rss_page($data, $params);
+		$actual = ob_get_clean();
+
+		$expected = '<?xml version="1.0" encoding="utf-8"?>
+<rss version="2.0"
+ xmlns:dc="http://purl.org/dc/elements/1.1/"
+ xmlns:atom="http://www.w3.org/2005/Atom"
+>
+	<channel>
+		<title>Site feed title</title>
+		<description><![CDATA[Site feed description]]></description>
+		<link>./</link>
+		<lastBuildDate></lastBuildDate>
+		<item>
+			<title>test 1</title>
+			<link>./?object=unit_tests&amp;action=testme&amp;id=1</link>
+			<description><![CDATA[my description 1]]></description>
+			<dc:creator>user 1</dc:creator>
+			<pubDate>Tue, 12 Aug 2014 12:03:19</pubDate>
+			<guid isPermaLink="false">./?object=unit_tests&amp;action=testme&amp;id=1#66fc49122ca308e5fe6347c8b32ed803</guid>
+			<enclosure url="http://lh3.ggpht.com/smoliarov/Rwygj8ucrbE/AAAAAAAABIA/UkNlwQ7eniw/_200708.jpg" length="65036" type="image/jpeg"/>
+		</item>
+		<item>
+			<title>test 2</title>
+			<link>./?object=unit_tests&amp;action=testme&amp;id=2</link>
+			<description><![CDATA[my description 2]]></description>
+			<dc:creator>user 2</dc:creator>
+			<pubDate>Tue, 12 Aug 2014 12:03:20</pubDate>
+			<guid isPermaLink="false">./?object=unit_tests&amp;action=testme&amp;id=2#98b97f2808a3744908b3fe1956f1c37b</guid>
+			<enclosure url="http://lh3.ggpht.com/smoliarov/Rwygj8ucrbE/AAAAAAAABIA/UkNlwQ7eniw/_200709.jpg" length="65036" type="image/jpeg"/>
+		</item>
+	</channel>
+</rss>';
+		$actual = preg_replace('~<lastBuildDate>[^<]+</lastBuildDate>~ims', '<lastBuildDate></lastBuildDate>', $actual);
+		$this->assertSame(trim($expected), trim($actual));
+	}
 }
