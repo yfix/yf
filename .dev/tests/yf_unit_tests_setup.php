1278ab37857a7e718fc5e2c05fc9f27e1486b228
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-08-12 15:30:16 +0300

    unit tests removed output buffering

--- .dev/tests/yf_unit_tests_setup.php
+++ .dev/tests/yf_unit_tests_setup.php
@@ -2,7 +2,7 @@
 
 define('STORAGE_PATH', __DIR__.'/tests_tmp_storage/');
 if (!defined('YF_PATH')) {
-	ob_start();
+	define('YF_IN_UNIT_TESTS', true);
 	define('YF_PATH', dirname(dirname(__DIR__)).'/');
 	require YF_PATH.'classes/yf_main.class.php';
 	new yf_main('user', $no_db_connect = 1, $auto_init_all = 0);
