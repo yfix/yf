1be46e41eebe9bed955ec6f77f7de72088517e3d
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-08-12 15:30:31 +0300

    unit tests for rss data updated

--- .dev/tests/func_common_rss_page.Test.php
+++ .dev/tests/func_common_rss_page.Test.php
@@ -10,10 +10,8 @@ class func_common_rss_page extends PHPUnit_Framework_TestCase {
 	}
 	public function test_simple() {
 		$data = array();
-#		$params = array('return_feed_text' => 1);
-		ob_start();
-		common()->rss_page($data, $params);
-		$actual = ob_get_clean();
+		$params = array('return_feed_text' => 1, 'use_cached' => 0);
+		$actual = common()->rss_page($data, $params);
 
 		$expected = '<?xml version="1.0" encoding="utf-8"?>
 <rss version="2.0"
@@ -34,17 +32,15 @@ class func_common_rss_page extends PHPUnit_Framework_TestCase {
 		$data = array(
 			array(
 				'date'	=> strtotime('2014-08-12 12:03:19 UTC'),
-				'link'	=> url('/unit_tests/testme/1/'),
+				'link'	=> 'http://unit.dev/?object=unit_tests&action=testme&id=1',
 			),
 			array(
 				'date'	=> strtotime('2014-08-12 12:03:20 UTC'),
-				'link'	=> url('/unit_tests/testme/2/'),
+				'link'	=> 'http://unit.dev/?object=unit_tests&action=testme&id=2',
 			),
 		);
-#		$params = array('return_feed_text' => 1);
-		ob_start();
-		common()->rss_page($data, $params);
-		$actual = ob_get_clean();
+		$params = array('return_feed_text' => 1, 'use_cached' => 0);
+		$actual = common()->rss_page($data, $params);
 
 		$expected = '<?xml version="1.0" encoding="utf-8"?>
 <rss version="2.0"
@@ -58,17 +54,17 @@ class func_common_rss_page extends PHPUnit_Framework_TestCase {
 		<lastBuildDate></lastBuildDate>
 		<item>
 			<title></title>
-			<link>./?object=unit_tests&amp;action=testme&amp;id=1</link>
+			<link>http://unit.dev/?object=unit_tests&amp;action=testme&amp;id=1</link>
 			<description></description>
 			<pubDate>Tue, 12 Aug 2014 12:03:19</pubDate>
-			<guid isPermaLink="false">./?object=unit_tests&amp;action=testme&amp;id=1#1cbc7597c5e24bf5cc3f7e453ff34b03</guid>
+			<guid isPermaLink="false">http://unit.dev/?object=unit_tests&amp;action=testme&amp;id=1#1cbc7597c5e24bf5cc3f7e453ff34b03</guid>
 		</item>
 		<item>
 			<title></title>
-			<link>./?object=unit_tests&amp;action=testme&amp;id=2</link>
+			<link>http://unit.dev/?object=unit_tests&amp;action=testme&amp;id=2</link>
 			<description></description>
 			<pubDate>Tue, 12 Aug 2014 12:03:20</pubDate>
-			<guid isPermaLink="false">./?object=unit_tests&amp;action=testme&amp;id=2#168aa275eb4cf49613c1ab23f8e9b88d</guid>
+			<guid isPermaLink="false">http://unit.dev/?object=unit_tests&amp;action=testme&amp;id=2#168aa275eb4cf49613c1ab23f8e9b88d</guid>
 		</item>
 	</channel>
 </rss>';
@@ -79,7 +75,7 @@ class func_common_rss_page extends PHPUnit_Framework_TestCase {
 		$data = array(
 			array(
 				'date'	=> strtotime('2014-08-12 12:03:19 UTC'),
-				'link'	=> url('/unit_tests/testme/1/'),
+				'link'	=> 'http://unit.dev/?object=unit_tests&action=testme&id=1',
 				'author' => 'user 1',
 				'title' => 'test 1',
 				'description' => 'my description 1',
@@ -91,7 +87,7 @@ class func_common_rss_page extends PHPUnit_Framework_TestCase {
 			),
 			array(
 				'date'	=> strtotime('2014-08-12 12:03:20 UTC'),
-				'link'	=> url('/unit_tests/testme/2/'),
+				'link'	=> 'http://unit.dev/?object=unit_tests&action=testme&id=2',
 				'author' => 'user 2',
 				'title' => 'test 2',
 				'description' => 'my description 2',
@@ -102,10 +98,8 @@ class func_common_rss_page extends PHPUnit_Framework_TestCase {
 				),
 			),
 		);
-#		$params = array('return_feed_text' => 1);
-		ob_start();
-		common()->rss_page($data, $params);
-		$actual = ob_get_clean();
+		$params = array('return_feed_text' => 1, 'use_cached' => 0);
+		$actual = common()->rss_page($data, $params);
 
 		$expected = '<?xml version="1.0" encoding="utf-8"?>
 <rss version="2.0"
@@ -119,20 +113,20 @@ class func_common_rss_page extends PHPUnit_Framework_TestCase {
 		<lastBuildDate></lastBuildDate>
 		<item>
 			<title>test 1</title>
-			<link>./?object=unit_tests&amp;action=testme&amp;id=1</link>
+			<link>http://unit.dev/?object=unit_tests&amp;action=testme&amp;id=1</link>
 			<description><![CDATA[my description 1]]></description>
 			<dc:creator>user 1</dc:creator>
 			<pubDate>Tue, 12 Aug 2014 12:03:19</pubDate>
-			<guid isPermaLink="false">./?object=unit_tests&amp;action=testme&amp;id=1#66fc49122ca308e5fe6347c8b32ed803</guid>
+			<guid isPermaLink="false">http://unit.dev/?object=unit_tests&amp;action=testme&amp;id=1#66fc49122ca308e5fe6347c8b32ed803</guid>
 			<enclosure url="http://lh3.ggpht.com/smoliarov/Rwygj8ucrbE/AAAAAAAABIA/UkNlwQ7eniw/_200708.jpg" length="65036" type="image/jpeg"/>
 		</item>
 		<item>
 			<title>test 2</title>
-			<link>./?object=unit_tests&amp;action=testme&amp;id=2</link>
+			<link>http://unit.dev/?object=unit_tests&amp;action=testme&amp;id=2</link>
 			<description><![CDATA[my description 2]]></description>
 			<dc:creator>user 2</dc:creator>
 			<pubDate>Tue, 12 Aug 2014 12:03:20</pubDate>
-			<guid isPermaLink="false">./?object=unit_tests&amp;action=testme&amp;id=2#98b97f2808a3744908b3fe1956f1c37b</guid>
+			<guid isPermaLink="false">http://unit.dev/?object=unit_tests&amp;action=testme&amp;id=2#98b97f2808a3744908b3fe1956f1c37b</guid>
 			<enclosure url="http://lh3.ggpht.com/smoliarov/Rwygj8ucrbE/AAAAAAAABIA/UkNlwQ7eniw/_200709.jpg" length="65036" type="image/jpeg"/>
 		</item>
 	</channel>
