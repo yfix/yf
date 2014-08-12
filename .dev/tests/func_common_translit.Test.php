8de600d4e5de1fd0d2cfd25b88237489cf652661
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-08-12 13:30:41 +0300

    renamed

---
+++ .dev/tests/func_common_translit.Test.php
@@ -0,0 +1,13 @@
+<?php
+
+require dirname(__FILE__).'/yf_unit_tests_setup.php';
+
+class func_common_translit_test extends PHPUnit_Framework_TestCase {
+	public function test_simple() {
+	}
+	public function test_complex() {
+		$this->assertEquals('Test', common()->make_translit('Тест'));
+		$this->assertEquals('FILTR JUNIOR VAUEN (pach/10 sht)', common()->make_translit('ФИЛЬТР JUNIOR VAUEN (пач/10 шт)'));
+		$this->assertEquals('FOLGA DLYA  KALYANA AL FAKHER Pach35', common()->make_translit('ФОЛЬГА ДЛЯ  КАЛЬЯНА AL FAKHER Пач35'));
+	}
+}
