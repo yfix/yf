6e00a010be837d4739ebd85d3cfe9348a67622c9
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-08-13 17:19:06 +0300

    unit tests fixes for hhvm and cache memcache driver

--- .dev/tests/class_cache_memcache.test.php
+++ .dev/tests/class_cache_memcache.test.php
@@ -2,7 +2,19 @@
 
 require_once dirname(__FILE__).'/class_cache.Test.php';
 
+# * @requires extension memcache
+
 /**
- * @requires extension memcache
  */
-class class_cache_memcache_test extends class_cache_test { }
+class class_cache_memcache_test extends class_cache_test {
+	protected function setUp() {
+		if (defined('HHVM_VERSION')) {
+			$this->markTestSkipped('Right now we skip this test, when running inside HHVM.');
+			return ;
+    	}
+		if (!extension_loaded('memcache') && !extension_loaded('memcache') || defined('HHVM_VERSION')) {
+			$this->markTestSkipped('The memcache and memcached extensions are not available.');
+			return ;
+    	}
+	}
+}
