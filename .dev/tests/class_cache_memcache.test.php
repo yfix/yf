a7bad3c1bd091af23647966a75cdd3e17b8b2667
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-08-15 17:40:27 +0300

    unit tests for cache memcache up

--- .dev/tests/class_cache_memcache.test.php
+++ .dev/tests/class_cache_memcache.test.php
@@ -2,19 +2,21 @@
 
 require_once dirname(__FILE__).'/class_cache.Test.php';
 
-# * @requires extension memcache
 
 /**
+ * @requires extension memcache
  */
 class class_cache_memcache_test extends class_cache_test {
+/*
 	protected function setUp() {
-		if (defined('HHVM_VERSION')) {
-			$this->markTestSkipped('Right now we skip this test, when running inside HHVM.');
-			return ;
-    	}
-		if (!extension_loaded('memcache') && !extension_loaded('memcache') || defined('HHVM_VERSION')) {
+#		if (defined('HHVM_VERSION')) {
+#			$this->markTestSkipped('Right now we skip this test, when running inside HHVM.');
+#			return ;
+#    	}
+		if (!extension_loaded('memcache') && !extension_loaded('memcached')) { // || defined('HHVM_VERSION')
 			$this->markTestSkipped('The memcache and memcached extensions are not available.');
 			return ;
     	}
 	}
+*/
 }
