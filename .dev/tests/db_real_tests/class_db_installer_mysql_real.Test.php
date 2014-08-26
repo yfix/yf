f0c3f83f04a48e4211b4f3ca5fe3861d0ba983e4
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-08-26 17:59:39 +0300

    added more db unit real tests

---
+++ .dev/tests/db_real_tests/class_db_installer_mysql_real.Test.php
@@ -0,0 +1,13 @@
+<?php
+
+require dirname(__DIR__).'/yf_unit_tests_setup.php';
+require dirname(__DIR__).'/db_setup.php';
+
+/**
+ * @requires extension mysql
+ */
+class class_db_installer_mysql_real_test extends PHPUnit_Framework_TestCase {
+	public function test_todo() {
+// TODO
+	}
+}
