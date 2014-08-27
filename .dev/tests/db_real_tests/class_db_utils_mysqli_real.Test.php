e806975316029d01635152a40872751ebdf43feb
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-08-27 11:13:50 +0300

    db utils tests added mysqli version

---
+++ .dev/tests/db_real_tests/class_db_utils_mysqli_real.Test.php
@@ -0,0 +1,10 @@
+<?php
+
+require __DIR__.'/class_db_utils_mysql_real.Test.php';
+
+/**
+ * @requires extension mysqli
+ */
+class class_db_utils_mysqli_real_test extends class_db_utils_mysql_real_test {
+	public static $DB_DRIVER = 'mysqli';
+}
