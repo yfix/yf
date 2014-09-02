8da72241869dbef753e2981aa97207d9d111e249
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-09-02 13:22:21 +0300

    db unit tests bugfix

--- .dev/tests/db/db_offline_abstract.php
+++ .dev/tests/db/db_offline_abstract.php
@@ -4,15 +4,23 @@ require_once dirname(__DIR__).'/yf_unit_tests_setup.php';
 require_once dirname(__DIR__).'/db_setup.php';
 
 abstract class db_offline_abstract extends PHPUnit_Framework_TestCase {
+	public static $db = null;
 	public static $_er = array();
 	public static function setUpBeforeClass() {
+		self::$db = _class('db');
 		self::$_er = error_reporting();
 		error_reporting(0);
 	}
 	public static function tearDownAfterClass() {
 		error_reporting(self::$_er);
 	}
-	private function db() {
-		return _class('db');
+	protected static function db() {
+		return self::$db;
+	}
+	protected static function utils() {
+		return self::$db->utils();
+	}
+	protected static function qb() {
+		return self::$db->query_builder();
 	}
 }
