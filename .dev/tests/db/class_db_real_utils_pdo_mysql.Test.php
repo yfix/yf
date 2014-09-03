9fa1daf168d91e9d1b21dbca29254b7a58d33303
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-09-03 16:50:06 +0300

    cleanup

--- .dev/tests/db/class_db_real_utils_pdo_mysql.Test.php
+++ .dev/tests/db/class_db_real_utils_pdo_mysql.Test.php
@@ -19,35 +19,12 @@ class class_db_real_utils_pdo_mysql_test extends class_db_real_utils_mysql_test 
 	public static function tearDownAfterClass() {
 		self::$DB_DRIVER = self::$_bak['DB_DRIVER'];
 	}
-	public function _need_skip_test($name) {
-		if (substr($name, 0, 5) !== 'test_') {
-			return false;
-		}
-		$short = substr($name, 5);
-		return in_array($short, array(
-/*
-			'database_info',
-			'alter_database',
-			'check_table',
-			'procedure_exists',
-			'procedure_info',
-			'drop_procedure',
-			'create_procedure',
-			'function_exists',
-			'function_info',
-			'drop_function',
-			'create_function',
-			'list_triggers',
-			'trigger_exists',
-			'trigger_info',
-			'drop_trigger',
-			'create_trigger',
-			'list_events',
-			'event_exists',
-			'event_info',
-			'drop_event',
-			'create_event',
-*/
-		));
-	}
+#	public function _need_skip_test($name) {
+#		if (substr($name, 0, 5) !== 'test_') {
+#			return false;
+#		}
+#		$short = substr($name, 5);
+#		return in_array($short, array(
+#		));
+#	}
 }
