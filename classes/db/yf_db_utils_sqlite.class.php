1d915b0b4c6665cf5f2b988488c676bb0192955f
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-09-03 17:53:41 +0300

    db utils sqlite up

--- classes/db/yf_db_utils_sqlite.class.php
+++ classes/db/yf_db_utils_sqlite.class.php
@@ -4,5 +4,79 @@
 */
 load('db_utils_driver', 'framework', 'classes/db/');
 class yf_db_utils_sqlite extends yf_db_utils_driver {
-	// TODO
+
+	/**
+	*/
+	function list_collations($extra = array()) {
+		return true;
+	}
+
+	/**
+	*/
+	function list_charsets($extra = array()) {
+		return true;
+	}
+
+	/**
+	* @not_supported
+	*/
+	function list_databases($extra = array()) {
+		return true;
+	}
+
+	/**
+	* @not_supported
+	*/
+	function database_exists($db_name, $extra = array(), &$error = false) {
+		return true;
+	}
+
+	/**
+	* @not_supported
+	*/
+	function database_info($db_name = '', $extra = array(), &$error = OBfalse) {
+		return true;
+	}
+
+	/**
+	* @not_supported
+	*/
+	function create_database($db_name, $extra = array(), &$error = false) {
+		return true;
+	}
+
+	/**
+	* @not_supported
+	*/
+	function drop_database($db_name, $extra = array(), &$error = false) {
+		return true;
+	}
+
+	/**
+	* @not_supported
+	*/
+	function alter_database($db_name, $extra = array(), &$error = false) {
+		return true;
+	}
+
+	/**
+	* @not_supported
+	*/
+	function rename_database($db_name, $new_name, $extra = array(), &$error = false) {
+		return true;
+	}
+
+	/**
+	*/
+	function list_tables($db_name = '', $extra = array(), &$error = false) {
+		if (!$db_name) {
+			$db_name = $this->db->DB_NAME;
+		}
+		if (!strlen($db_name)) {
+			$error = 'db_name is empty';
+			return false;
+		}
+		$tables = $this->db->get_2d('SELECT name FROM sqlite_master WHERE type = "table" AND name <> "sqlite_sequence"');
+		return $tables ? array_combine($tables, $tables) : array();
+	}
 }
