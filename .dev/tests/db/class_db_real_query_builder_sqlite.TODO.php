720f523df38c913006bf12fef72a6aba35eb8de1
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-09-03 17:57:14 +0300

    db unit tests up

--- .dev/tests/db/class_db_real_query_builder_sqlite.TODO.php
+++ .dev/tests/db/class_db_real_query_builder_sqlite.TODO.php
@@ -12,6 +12,31 @@ class class_db_real_query_builder_sqlite_test extends class_db_real_query_builde
 		self::_connect();
 	}
 	public static function tearDownAfterClass() {
+		$db_file = self::$DB_NAME.'.db';
+		if (file_exists($db_file)) {
+			unlink($db_file);
+		}
 		self::$DB_DRIVER = self::$_bak['DB_DRIVER'];
+	}
+	public static function _connect() {
+		self::$DB_NAME = DB_NAME;
+		$db_class = load_db_class();
+		self::$db = new $db_class(self::$DB_DRIVER);
+		self::$db->ALLOW_AUTO_CREATE_DB = true;
+		self::$db->NO_AUTO_CONNECT = true;
+		self::$db->RECONNECT_NUM_TRIES = 1;
+		self::$db->CACHE_TABLE_NAMES = false;
+		self::$db->ERROR_AUTO_REPAIR = false;
+		self::$db->FIX_DATA_SAFE = true;
+		self::$db->_init();
+		$res = self::$db->connect(array(
+			'host'	=> 'localhost',
+			'name'	=> self::$DB_NAME.'.db',
+			'user'	=> DB_USER,
+			'pswd'	=> DB_PSWD,
+			'prefix'=> DB_PREFIX,
+			'force' => true,
+		));
+		return !empty($res) ? true : false;
 	}
 }
