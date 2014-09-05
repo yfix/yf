1dd9b73e0b191a5982cc6c6af9f35f7bdbe5c697
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-09-05 12:11:35 +0300

    db unit tests updated

--- .dev/tests/db/class_db_real_pgsql.TODO.php
+++ .dev/tests/db/class_db_real_pgsql.TODO.php
@@ -16,4 +16,50 @@ class class_db_real_pgsql_test extends db_real_abstract {
 		self::utils()->truncate_database(self::db_name());
 		self::$DB_DRIVER = self::$_bak['DB_DRIVER'];
 	}
+	public static function _connect($params = array()) {
+		self::$DB_NAME = $params['name'] ?: DB_NAME;
+		if ($params['driver']) {
+			self::$DB_DRIVER = $params['driver'];
+		}
+		$db_class = load_db_class();
+		self::$db = new $db_class(self::$DB_DRIVER);
+		self::$db->ALLOW_AUTO_CREATE_DB = true;
+		self::$db->NO_AUTO_CONNECT = true;
+		self::$db->RECONNECT_NUM_TRIES = 1;
+		self::$db->CACHE_TABLE_NAMES = false;
+		self::$db->ERROR_AUTO_REPAIR = false;
+		self::$db->FIX_DATA_SAFE = true;
+		self::$db->_init();
+		$res = self::$db->connect(array(
+			'host'	=> $params['host'] ?: '127.0.0.1',
+			'name'	=> self::$DB_NAME,
+			'user'	=> $params['user'] ?: DB_USER,
+			'pswd'	=> $params['pswd'] ?: DB_PSWD,
+			'prefix'=> $params['prefix'] ?: DB_PREFIX,
+			'force' => true,
+		));
+		return !empty($res) ? true : false;
+	}
+	public function db_name() {
+		return self::$DB_NAME;
+	}
+	public function table_name($name) {
+		return self::db_name().'.'.$name;
+	}
+	public function create_table_sql($table) {
+		return 'CREATE TABLE '.$this->table_name($table).'(id INT(10) AUTO_INCREMENT, id2 INT(10), id3 INT(10), PRIMARY KEY(id)) ENGINE=InnoDB DEFAULT CHARSET=utf8';
+	}
+
+	public function test_disconnect_connect() {
+		if ($this->_need_skip_test(__FUNCTION__)) { return ; }
+		$this->assertTrue( self::db()->close() );
+		$this->assertFalse( self::$db->_connected );
+		$this->assertFalse( self::$db->_tried_to_connect );
+		$this->assertNull( self::$db->db );
+		$this->assertTrue( self::_connect() );
+		$this->assertTrue( self::$db->_connected );
+		$this->assertTrue( self::$db->_tried_to_connect );
+		$this->assertTrue( is_object(self::$db->db) );
+		$this->assertTrue( !empty(self::$db->db->db_connect_id) );
+	}
 }
