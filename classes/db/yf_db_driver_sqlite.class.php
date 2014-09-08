27720086ea32cc4baa312150ad49306d6fa56481
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-09-08 16:37:49 +0300

    db driver code cleanup

--- classes/db/yf_db_driver_sqlite.class.php
+++ classes/db/yf_db_driver_sqlite.class.php
@@ -7,20 +7,12 @@ class yf_db_driver_sqlite extends yf_db_driver {
 	public $db_connect_id = null;
 
 	/**
-	* Catch missing method call
-	*/
-	function __call($name, $args) {
-		return main()->extend_call($this, $name, $args);
-	}
-
-	/**
 	*/
 	function __construct(array $params) {
 		if (!class_exists('SQLite3')) {
 			trigger_error('YF SQLite db driver require missing php extension sqlite3', E_USER_ERROR);
 			return false;
 		}
-		$params['charset'] = $params['charset'] ?: (defined('DB_CHARSET') ? DB_CHARSET : $this->DEF_CHARSET);
 		$this->params = $params;
 		$this->connect();
 		return $this->db_connect_id;
