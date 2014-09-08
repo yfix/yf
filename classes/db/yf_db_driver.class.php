cb76c15e3d123e282dab9463c2133fda9394c3a5
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-09-08 15:19:31 +0300

    db driver abstract cleanup

--- classes/db/yf_db_driver.class.php
+++ classes/db/yf_db_driver.class.php
@@ -5,22 +5,23 @@
 */
 abstract class yf_db_driver {
 	abstract protected function __construct(array $params);
-	abstract protected function affected_rows();
-	abstract protected function begin();
 	abstract protected function connect();
 	abstract protected function close();
+	abstract protected function query($query);
+	abstract protected function begin();
 	abstract protected function commit();
-	abstract protected function escape_key($data);
-	abstract protected function escape_val($data);
+	abstract protected function rollback();
 	abstract protected function error();
 	abstract protected function fetch_array($query_id);
 	abstract protected function fetch_assoc($query_id);
 	abstract protected function fetch_row($query_id);
+	abstract protected function fetch_object($query_id);
 	abstract protected function free_result($query_id);
-	abstract protected function insert_id();
-	abstract protected function limit($count, $offset);
+	abstract protected function affected_rows($query_id);
+	abstract protected function insert_id($query_id);
 	abstract protected function num_rows($query_id);
-	abstract protected function query($query);
 	abstract protected function real_escape_string($string);
-	abstract protected function rollback();
+	abstract protected function escape_key($data);
+	abstract protected function escape_val($data);
+	abstract protected function limit($count, $offset);
 }
