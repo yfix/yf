c2bb39d2e3b68e605ca11d86d9c2d615500ca53e
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-09-08 13:39:35 +0300

    db unit tests pgsql started

--- .dev/tests/db/class_db_real_pgsql.TODO.php
+++ .dev/tests/db/class_db_real_pgsql.TODO.php
@@ -10,14 +10,14 @@ class class_db_real_pgsql_test extends db_real_abstract {
 		self::$_bak['DB_DRIVER'] = self::$DB_DRIVER;
 		self::$DB_DRIVER = 'pgsql';
 		self::_connect();
-		self::utils()->truncate_database(self::db_name());
+#		self::utils()->truncate_database(self::db_name());
 	}
 	public static function tearDownAfterClass() {
-		self::utils()->truncate_database(self::db_name());
+#		self::utils()->truncate_database(self::db_name());
 		self::$DB_DRIVER = self::$_bak['DB_DRIVER'];
 	}
 	public static function _connect($params = array()) {
-		self::$DB_NAME = $params['name'] ?: DB_NAME;
+		self::$DB_NAME = $params['name'] ?: is_string(getenv('YF_DB_PG_NAME')) ? getenv('YF_DB_PG_NAME') : DB_NAME);
 		if ($params['driver']) {
 			self::$DB_DRIVER = $params['driver'];
 		}
@@ -31,11 +31,11 @@ class class_db_real_pgsql_test extends db_real_abstract {
 		self::$db->FIX_DATA_SAFE = true;
 		self::$db->_init();
 		$res = self::$db->connect(array(
-			'host'	=> $params['host'] ?: '127.0.0.1',
+			'host'	=> $params['host'] ?: (is_string(getenv('YF_DB_PG_HOST')) ? getenv('YF_DB_PG_HOST') : '127.0.0.1'),
 			'name'	=> self::$DB_NAME,
-			'user'	=> $params['user'] ?: DB_USER,
-			'pswd'	=> $params['pswd'] ?: DB_PSWD,
-			'prefix'=> $params['prefix'] ?: DB_PREFIX,
+			'user'	=> $params['user'] ?: (is_string(getenv('YF_DB_PG_USER')) ? getenv('YF_DB_PG_USER') : 'yf'),
+			'pswd'	=> is_string($params['pswd']) ? $params['pswd'] : (is_string(getenv('YF_DB_PG_PSWD')) ? getenv('YF_DB_PG_PSWD') : DB_PSWD),
+			'prefix'=> $params['prefix'] ?: (is_string(getenv('YF_DB_PG_PREFIX')) ? getenv('YF_DB_PG_PREFIX') : DB_PREFIX),
 			'force' => true,
 		));
 		return !empty($res) ? true : false;
@@ -61,5 +61,48 @@ class class_db_real_pgsql_test extends db_real_abstract {
 		$this->assertTrue( self::$db->_tried_to_connect );
 		$this->assertTrue( is_object(self::$db->db) );
 		$this->assertTrue( !empty(self::$db->db->db_connect_id) );
+	}
+	public function test_basic_queries_and_fetching() {
+		if ($this->_need_skip_test(__FUNCTION__)) { return ; }
+		$table = self::db()->DB_PREFIX. __FUNCTION__;
+		$this->assertNotEmpty( self::db()->query($this->create_table_sql($table)) );
+		$expected = array(
+			'Table' => $table,
+			'Create Table' => 'CREATE TABLE `'.$table.'` ('. PHP_EOL
+				. '  `id` int(10) NOT NULL AUTO_INCREMENT,'. PHP_EOL
+				. '  `id2` int(10) DEFAULT NULL,'. PHP_EOL
+				. '  `id3` int(10) DEFAULT NULL,'. PHP_EOL
+				. '  PRIMARY KEY (`id`)'. PHP_EOL
+				. ') ENGINE=InnoDB DEFAULT CHARSET=utf8',
+		);
+		$sql = 'SHOW CREATE TABLE '.$this->table_name($table);
+		$this->assertEquals( $expected, self::db()->fetch_assoc(self::db()->query($sql)) );
+		$this->assertEquals( $expected, self::db()->fetch_assoc(self::db()->unbuffered_query($sql)) );
+		$this->assertEquals( $expected, self::db()->query_fetch($sql) );
+		$this->assertEquals( $expected, self::db()->get($sql) );
+
+		$this->assertNotEmpty( self::db()->query('INSERT INTO '.$this->table_name($table).' VALUES (1,1,1),(2,2,2),(3,3,3)') );
+		$this->assertEquals( 3, self::db()->affected_rows() );
+		$this->assertEquals( 3, self::db()->insert_id() );
+		$this->assertEquals( array('id' => 1), self::db()->get('SELECT id FROM '.$this->table_name($table)) );
+		$this->assertEquals( array(1 => array('id' => 1), 2 => array('id' => 2), 3 => array('id' => 3)), self::db()->get_all('SELECT id FROM '.$this->table_name($table)) );
+		$this->assertEquals( array(3 => array('id' => 3), 2 => array('id' => 2), 1 => array('id' => 1)), self::db()->get_all('SELECT id FROM '.$this->table_name($table).' ORDER BY id DESC') );
+		$this->assertEmpty( self::db()->get('SELECT id FROM '.$this->table_name($table).' WHERE id > 9999') );
+		$this->assertEmpty( self::db()->get_all('SELECT id FROM '.$this->table_name($table).' WHERE id > 9999') );
+
+		$this->assertEquals( 3, self::db()->num_rows(self::db()->query('SELECT id FROM '.$this->table_name($table))) );
+		$this->assertEquals( 3, self::db()->query_num_rows('SELECT id FROM '.$this->table_name($table)) );
+
+		$q = self::db()->query('SELECT id FROM '.$this->table_name($table));
+		$this->assertEquals( 3, self::db()->num_rows($q) );
+		$this->assertEquals( array('id' => 1), self::db()->fetch_assoc($q) );
+		$this->assertTrue( self::db()->free_result($q) );
+
+		$this->assertEquals( array('message' => '', 'code' => 0), self::db()->error() );
+
+		$this->assertEquals( array(1), self::db()->fetch_row(self::db()->query('SELECT id FROM '.$this->table_name($table))) );
+		$obj = new stdClass();
+		$obj->id = 1;
+		$this->assertEquals( $obj, self::db()->fetch_object(self::db()->query('SELECT id FROM '.$this->table_name($table))) );
 	}
 }
