fabe866bde62ede12760be685191ed57bfe6fe5d
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-09-09 12:36:09 +0300

    codecept bootstrap changed to use sample_app script

--- .dev/tests/_bootstrap.php
+++ .dev/tests/_bootstrap.php
@@ -1,3 +1,3 @@
 <?php
 // This is global bootstrap for autoloading 
-exec('if [ -z "$(netstat -anlpt | grep 33380)" ]; then ((../sample_app/run_server.sh >/dev/null 2>&1) &) ; fi');
+exec('../sample_app/run_server_in_bg.sh');
