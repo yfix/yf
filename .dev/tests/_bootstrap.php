79c8a4f65aee6a95af01347ac1730982fd22621f
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-09-10 15:31:47 +0300

    tests up

--- .dev/tests/_bootstrap.php
+++ .dev/tests/_bootstrap.php
@@ -1,3 +1,5 @@
 <?php
 // This is global bootstrap for autoloading 
 exec('../sample_app/run_server_in_bg.sh');
+
+\Codeception\Util\Autoload::registerSuffix('Page', __DIR__.DIRECTORY_SEPARATOR.'_pages');
