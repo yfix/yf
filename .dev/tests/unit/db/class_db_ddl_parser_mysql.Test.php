c7584c4c74eaa7a81edc155989960812c0153a19
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-09-18 16:52:34 +0300

    db unit tests added

---
+++ .dev/tests/unit/db/class_db_ddl_parser_mysql.Test.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once __DIR__.'/db_offline_abstract.php';
+
+/**
+ * @requires extension mysql
+ */
+class class_db_ddl_parser_mysql_test extends db_offline_abstract {
+	public function test_1() {
+		if ($this->_need_skip_test(__FUNCTION__)) { return ; }
+/*
+		$expected = array();
+		$sql = <<<'EOD'
+CREATE TABLE `film` (
+  `film_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `description` text,
+  `release_year` year(4) DEFAULT NULL,
+  `language_id` tinyint(3) unsigned NOT NULL,
+  `original_language_id` tinyint(3) unsigned DEFAULT NULL,
+  `rental_duration` tinyint(3) unsigned NOT NULL DEFAULT '3',
+  `rental_rate` decimal(4,2) NOT NULL DEFAULT '4.99',
+  `length` smallint(5) unsigned DEFAULT NULL,
+  `replacement_cost` decimal(5,2) NOT NULL DEFAULT '19.99',
+  `rating` enum('G','PG','PG-13','R','NC-17') DEFAULT 'G',
+  `special_features` set('Trailers','Commentaries','Deleted Scenes','Behind the Scenes') DEFAULT NULL,
+  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`film_id`),
+  KEY `idx_title` (`title`),
+  KEY `idx_fk_language_id` (`language_id`),
+  KEY `idx_fk_original_language_id` (`original_language_id`),
+  CONSTRAINT `fk_film_language` FOREIGN KEY (`language_id`) REFERENCES `language` (`language_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_film_language_original` FOREIGN KEY (`original_language_id`) REFERENCES `language` (`language_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+EOD;
+		$response = _class('db_ddl_parser_mysql', 'classes/db/')->phpsqlparser($sql);
+		$this->assertEquals($expected, $response);
+*/
+	}
+}
