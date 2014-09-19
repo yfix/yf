df742021a09dce9d78d2ebbed920e78dd35a6acb
LArV <larv.job@gmail.com>
2014-09-19 11:30:48 +0300

    import2: add import rules info

--- plugins/shop/admin_modules/manage_shop/yf_manage_shop_import_products2.class.php
+++ plugins/shop/admin_modules/manage_shop/yf_manage_shop_import_products2.class.php
@@ -22,6 +22,61 @@ class yf_manage_shop_import_products2 {
 		'supplier_id'       => 'поставщик: идентификатор (supplier_id)',
 		'supplier_name'     => 'поставщик: название (supplier_name)',
 	);
+
+	/*
+		# addon select: supplier_id
+
+		# velika_kishenya
+		# olis
+		# start trade
+		# fortuna
+		# epicentr
+		# talisman
+		# zakaz_ua
+		# oblik
+		# dobra_hata
+		# promozp
+		# electrolux
+		# ekanevidal
+		# zoobonus
+		# svitcom
+
+		supplier_id, articul
+			new by articul
+				name, url( by name ),
+				articul,
+				price, price_raw,
+				description,
+				origin_url,
+				cat_id( by cat_name), supplier_id( by sup_name ), manufacturer_id( by man_name ),
+				status=1, active=1
+				add_date( by time )
+			update
+				price, price_raw, cat_id, update_date( by time )
+
+		format_data($name,$articul,$price,$supplier_id,$price_raw = 0)
+
+		# insert, if exists
+
+		# update, if exists id
+		id, name, price, etc
+			by id
+			by supplier_id, articul
+				update any fields
+
+		# rules
+			price > 0 or price_raw > 0
+			articul IS NOT NULL
+			cat_name IS NOT NULL
+			supplier_id MUST EXISTS IN DB
+			manufacturer_id MUST EXISTS IN DB
+				or add manufacturer( url by name )
+	*/
+
+	public $import_rulues = array(
+		array( 'id', ),
+		array( 'id', ),
+	);
 	public $upload_path            = null;
 	public $upload_list            = null;
 	public $upload_list__file_name = null;
