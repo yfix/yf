7b8f9aca00aca0803e528a93d25a2df83e69402f
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-09-19 17:02:38 +0300

    db unit tests fixtures up

--- .dev/tests/unit/db/fixtures/class_db_ddl_parser_mysql_test_tbl_actor.php
+++ .dev/tests/unit/db/fixtures/class_db_ddl_parser_mysql_test_tbl_actor.php
@@ -1,10 +1,8 @@
 <?php
-return array (
+return array(
 	'name' => 'actor',
-	'fields' => 
-	array (
-		'actor_id' => 
-		array (
+	'fields' => array(
+		'actor_id' => array(
 			'name' => 'actor_id',
 			'type' => 'smallint',
 			'length' => '5',
@@ -18,8 +16,7 @@ return array (
 			'values' => NULL,
 			'raw' => '`actor_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT',
 		),
-		'first_name' => 
-		array (
+		'first_name' => array(
 			'name' => 'first_name',
 			'type' => 'varchar',
 			'length' => '45',
@@ -33,8 +30,7 @@ return array (
 			'values' => NULL,
 			'raw' => '`first_name` varchar(45) NOT NULL',
 		),
-		'last_name' => 
-		array (
+		'last_name' => array(
 			'name' => 'last_name',
 			'type' => 'varchar',
 			'length' => '45',
@@ -48,8 +44,7 @@ return array (
 			'values' => NULL,
 			'raw' => '`last_name` varchar(45) NOT NULL',
 		),
-		'last_update' => 
-		array (
+		'last_update' => array(
 			'name' => 'last_update',
 			'type' => 'timestamp',
 			'length' => NULL,
@@ -64,34 +59,27 @@ return array (
 			'raw' => '`last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP',
 		),
 	),
-	'indexes' => 
-	array (
-		'PRIMARY' => 
-		array (
+	'indexes' => array(
+		'PRIMARY' => array(
 			'name' => 'PRIMARY',
 			'type' => 'primary',
-			'columns' => 
-			array (
+			'columns' => array(
 				'actor_id' => 'actor_id',
 			),
 			'raw' => 'PRIMARY KEY (`actor_id`)',
 		),
-		'idx_actor_last_name' => 
-		array (
+		'idx_actor_last_name' => array(
 			'name' => 'idx_actor_last_name',
 			'type' => 'index',
-			'columns' => 
-			array (
+			'columns' => array(
 				'last_name' => 'last_name',
 			),
 			'raw' => 'KEY `idx_actor_last_name` (`last_name`)',
 		),
 	),
-	'foreign_keys' => 
-	array (
+	'foreign_keys' => array(
 	),
-	'options' => 
-	array (
+	'options' => array(
 		'engine' => 'InnoDB',
 		'charset' => 'utf8',
 	),
