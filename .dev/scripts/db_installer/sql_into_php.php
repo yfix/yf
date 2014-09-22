bc2eb0ad359bac350de20732db304599c58b8238
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-09-22 16:56:17 +0300

    dev script sql into php upgraded

--- .dev/scripts/db_installer/sql_into_php.php
+++ .dev/scripts/db_installer/sql_into_php.php
@@ -11,19 +11,20 @@ foreach ($globs_sql as $glob) {
 	foreach (glob($glob) as $f) {
 		echo '== '.$f. PHP_EOL;
 		$t_name = substr(basename($f), 0, -strlen('.sql.php'));
+
 		$sql_php_file = dirname(dirname($f)).'/sql_php/'.$t_name.'.sql_php.php';
 		echo '++ sql_php: '.$sql_php_file. PHP_EOL;
 		if (file_exists($sql_php_file)) {
 #			echo 'exists, skipped'. PHP_EOL;
 #			continue;
 		}
+
 		$sql_php_dir = dirname($sql_php_file);
 		if (!file_exists($sql_php_dir)) {
 			mkdir($sql_php_dir, 0755, true);
 		}
 
-		$data = '';
-		include $f; // $data should be loaded from file
+		$data = include $f; // $data should be loaded from file
 
 		if (!$data) {
 			echo '-- ERROR: empty data'. PHP_EOL;
@@ -31,13 +32,22 @@ foreach ($globs_sql as $glob) {
 		}
 
 		$a = _class('db_installer_mysql', 'classes/db/')->_db_table_struct_into_array($data);
+		if (isset($a['name'])) {
+			unset($a['name']);
+		}
 
 		if (!$a) {
 			echo '-- ERROR: empty sql_php'. PHP_EOL;
 			continue;
 		}
 #		print_r($a);
-		$body = '<?'.'php'.PHP_EOL.'$data = '.str_replace('  ', "\t", var_export($a, 1)).';'.PHP_EOL;
+
+		$str = var_export($a, 1);
+		$str = str_replace('  ', "\t", $str);
+		$str = str_replace('array (', 'array(', $str);
+		$str = preg_replace('~=>[\s]+array\(~ims', '=> array(', $str);
+
+		$body = '<?'.'php'.PHP_EOL.'return '.$str.';'.PHP_EOL;
 		if (file_exists($sql_php_file) && md5($body) == md5(file_get_contents($sql_php_file))) {
 			continue;
 		}
