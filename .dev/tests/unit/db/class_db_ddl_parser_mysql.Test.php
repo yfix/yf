669c060e540320f35fcdd73cb0a2c11ab5d5e569
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-09-22 15:51:20 +0300

    db ddl parser unit tests up

--- .dev/tests/unit/db/class_db_ddl_parser_mysql.Test.php
+++ .dev/tests/unit/db/class_db_ddl_parser_mysql.Test.php
@@ -6,7 +6,8 @@ require_once __DIR__.'/db_offline_abstract.php';
  * @requires extension mysql
  */
 class class_db_ddl_parser_mysql_test extends db_offline_abstract {
-	public function test_1() {
+	/***/
+	public function test_sakila() {
 		if ($this->_need_skip_test(__FUNCTION__)) { return ; }
 
 		$fixtures_path = __DIR__.'/fixtures/';
@@ -27,5 +28,44 @@ class class_db_ddl_parser_mysql_test extends db_offline_abstract {
 			$response = _class('db_ddl_parser_mysql', 'classes/db/')->parse(str_replace(array("\r","\n"), ' ', $sql));
 			$this->assertEquals($expected, $response);
 		}
+	}
+	/***/
+	public function test_yf() {
+		if ($this->_need_skip_test(__FUNCTION__)) { return ; }
+
+		$db_installer = _class('db_installer_mysql', 'classes/db/');
+
+		$tables_sql = array();
+		$tables_php = array();
+
+		// Load install data from external files
+		$globs_sql = array(
+			'yf_main'			=> YF_PATH.'share/db_installer/sql/*.sql.php',
+			'yf_plugins'		=> YF_PATH.'plugins/*/share/db_installer/sql/*.sql.php',
+		);
+		foreach ($globs_sql as $glob) {
+			foreach (glob($glob) as $f) {
+				$t_name = substr(basename($f), 0, -strlen('.sql.php'));
+				require_once $f; // $data should be loaded from file
+				$tables_sql[$t_name] = $data;
+			}
+		}
+		$globs_php = array(
+			'yf_main'			=> YF_PATH.'share/db_installer/sql_php/*.sql_php.php',
+			'yf_plugins'		=> YF_PATH.'plugins/*/share/db_installer/sql_php/*.sql_php.php',
+		);
+		foreach ($globs_sql as $glob) {
+			foreach (glob($glob) as $f) {
+				$t_name = substr(basename($f), 0, -strlen('.sql.php'));
+				require_once $f; // $data should be loaded from file
+				$tables_php[$t_name] = $data;
+			}
+		}
+
+#		$this->assertEquals(array_keys($tables_sql), array_keys($tables_php));
+		
+#		foreach ($tables_sql) {
+#		}
+#var_dump($db_installer);
 	}
 }
