2859fd27c9b0e00219d202b2e25b3a7f77d05013
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-09-23 16:09:08 +0300

    db table bugfix

--- share/db_installer/sql/handshake.sql.php
+++ share/db_installer/sql/handshake.sql.php
@@ -3,7 +3,7 @@ return '
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `sender` int(11) NOT NULL DEFAULT \'0\',
   `receiver` int(11) NOT NULL DEFAULT \'0\',
-  `text` set(\'\') NOT NULL CHARACTER SET utf8,
+  `text` varchar(64) NOT NULL,
   `add_date` text NOT NULL,
   `action_date` text NOT NULL,
   `status` text NOT NULL,
