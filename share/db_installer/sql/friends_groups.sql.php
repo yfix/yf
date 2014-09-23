ce9582e1f9873d5ef10006a2fbc77bed1642896b
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-09-23 16:09:02 +0300

    db table bugfix

--- share/db_installer/sql/friends_groups.sql.php
+++ share/db_installer/sql/friends_groups.sql.php
@@ -3,7 +3,7 @@ return '
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id2` tinyint(4) NOT NULL DEFAULT \'0\',
   `user_id` int(11) NOT NULL DEFAULT \'0\',
-  `title` set(\'\') NOT NULL CHARACTER SET utf8,
+  `title` varchar(64) NOT NULL,
   `order` int(11) NOT NULL DEFAULT \'0\',
   PRIMARY KEY (`id`),
   KEY `id2` (`id2`)
