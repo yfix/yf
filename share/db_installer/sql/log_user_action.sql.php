cc9aa42c1a9ebc6b1d905a6a6a36ed1e9fe5bc03
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-09-23 16:18:13 +0300

    db table bugfix

--- share/db_installer/sql/log_user_action.sql.php
+++ share/db_installer/sql/log_user_action.sql.php
@@ -1,11 +1,11 @@
 <?php
 return '
-  `id` int() unsigned NOT NULL AUTO_INCREMENT,
-  `owner_id` int() unsigned NOT NULL,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `owner_id` int(11) unsigned NOT NULL,
   `action_name` varchar(255) NOT NULL,
-  `member_id` int() unsigned NOT NULL,
+  `member_id` int(11) unsigned NOT NULL,
   `object_name` varchar(255) NOT NULL,
-  `object_id` int() unsigned NOT NULL,
-  `add_date` int() unsigned NOT NULL,
+  `object_id` int(11) unsigned NOT NULL,
+  `add_date` int(11) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ';
