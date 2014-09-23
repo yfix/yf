59d3d34cb4a08498d5a50899c337f91bd82ab40a
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-09-23 16:18:45 +0300

    db table bugfix

--- share/db_installer/sql/tags_settings.sql.php
+++ share/db_installer/sql/tags_settings.sql.php
@@ -1,6 +1,6 @@
 <?php
 return '
-  `user_id` int() unsigned NOT NULL,
-  `allowed_group` tinyint() unsigned NOT NULL,
+  `user_id` int(11) unsigned NOT NULL,
+  `allowed_group` tinyint(3) unsigned NOT NULL,
   PRIMARY KEY (`user_id`)
 ';
