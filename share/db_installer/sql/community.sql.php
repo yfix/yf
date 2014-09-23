2569b8aedd76116678d982a42cf50702429af11d
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-09-23 16:08:56 +0300

    db table bugfix

--- share/db_installer/sql/community.sql.php
+++ share/db_installer/sql/community.sql.php
@@ -3,13 +3,13 @@ return '
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `owner_id` int(11) NOT NULL DEFAULT \'0\',
   `user_id` int(10) unsigned NOT NULL DEFAULT \'0\',
-  `title` set(\'\') NOT NULL CHARACTER SET utf8,
+  `title` varchar(255) NOT NULL,
   `membership` enum(\'open\',\'moderated\',\'closed\') NOT NULL DEFAULT \'open\',
   `nonmember_posting` enum(\'0\',\'1\') NOT NULL DEFAULT \'0\',
   `postlevel` enum(\'members\',\'select\') NOT NULL DEFAULT \'members\',
   `moderated` enum(\'0\',\'1\') NOT NULL DEFAULT \'0\',
   `adult` enum(\'none\',\'concepts\',\'explicit\') NOT NULL DEFAULT \'none\',
-  `about` set(\'\') NOT NULL CHARACTER SET utf8,
+  `about` varchar(255) NOT NULL,
   `active` enum(\'0\',\'1\') NOT NULL DEFAULT \'0\',
   PRIMARY KEY (`id`)
 ';
