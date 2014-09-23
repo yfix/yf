02a606f27efb0d12e45667b5962cb4f2670abd03
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-09-23 16:17:45 +0300

    db table fix

--- share/db_installer/sql/form_attributes.sql.php
+++ share/db_installer/sql/form_attributes.sql.php
@@ -1,9 +1,9 @@
 <?php
 return '
-	`form_id` varchar(64) NOT NULL DEFAULT \'\',
-	`field` varchar(64) NOT NULL DEFAULT \'\',
-	`attr` varchar(64) NOT NULL DEFAULT \'\',
-	`value` longtext NOT NULL DEFAULT \'\',
-	`active` enum(\'0\',\'1\') NOT NULL DEFAULT \'0\',
-	PRIMARY KEY (`form_id`,`field`,`attr`)
+  `form_id` varchar(64) NOT NULL DEFAULT \'\',
+  `field` varchar(64) NOT NULL DEFAULT \'\',
+  `attr` varchar(64) NOT NULL DEFAULT \'\',
+  `value` longtext NOT NULL DEFAULT \'\',
+  `active` enum(\'0\',\'1\') NOT NULL DEFAULT \'0\',
+  PRIMARY KEY (`form_id`,`field`,`attr`)
 ';
