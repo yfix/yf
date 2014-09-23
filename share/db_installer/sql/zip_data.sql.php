18233620a8ab16aa320c19899d371e242c748832
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-09-23 15:43:27 +0300

    zip_data db table fix

--- share/db_installer/sql/zip_data.sql.php
+++ share/db_installer/sql/zip_data.sql.php
@@ -2,7 +2,7 @@
 return '
 	`id` int(5) unsigned zerofill NOT NULL,
 	`lon` float NOT NULL default \'0\',
-	`lat` float NOT NULL default \'0\'
+	`lat` float NOT NULL default \'0\',
 	UNIQUE KEY `id` (`id`),
 	KEY `lon` (`lon`,`lat`)
 ';
