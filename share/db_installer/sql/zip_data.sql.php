c507c5b0a3cf4efa13af2bbdc889777aecfb4b3b
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-09-23 16:22:04 +0300

    db tables bugfix

--- share/db_installer/sql/zip_data.sql.php
+++ share/db_installer/sql/zip_data.sql.php
@@ -1,8 +1,8 @@
 <?php
 return '
-	`id` int(5) unsigned zerofill NOT NULL,
-	`lon` float NOT NULL default \'0\',
-	`lat` float NOT NULL default \'0\',
-	PRIMARY KEY (`id`),
-	KEY `lon` (`lon`,`lat`)
+  `id` int(5) unsigned NOT NULL,
+  `lon` float NOT NULL DEFAULT \'0\',
+  `lat` float NOT NULL DEFAULT \'0\',
+  PRIMARY KEY (`id`),
+  KEY `lon` (`lon`,`lat`)
 ';
