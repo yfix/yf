88a6b8584a725851e929dca8840745beae5d49d2
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-09-24 14:43:19 +0300

    db table bugfix

--- share/db_installer/sql/sys_locale_translate.sql.php
+++ share/db_installer/sql/sys_locale_translate.sql.php
@@ -3,5 +3,5 @@ return '
   `var_id` int(10) unsigned NOT NULL DEFAULT \'0\',
   `value` text NOT NULL,
   `locale` varchar(12) NOT NULL DEFAULT \'\',
-  PRIMARY KEY (``)
+  PRIMARY KEY (`var_id`, `locale`)
 ';
