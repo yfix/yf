40da0459fd940763bc5b457064beac2cc28382df
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-09-24 17:34:59 +0300

    db unit tests for db installer really checking for create missing yf table

--- .dev/tests/functional/db/class_db_real_installer_mysql.Test.php
+++ .dev/tests/functional/db/class_db_real_installer_mysql.Test.php
@@ -107,25 +107,10 @@ class class_db_real_installer_mysql_test extends db_real_abstract {
 		self::utils()->truncate_database(self::db_name());
 		$this->assertEquals( array(), self::utils()->list_tables(self::db_name()) );
 
-		$db_installer = _class('db_installer_mysql', 'classes/db/');
-
 		$parser = _class('db_ddl_parser_mysql', 'classes/db/');
 		$parser->RAW_IN_RESULTS = false;
 
-		$tables_sql = array();
 		$tables_php = array();
-
-		// Load install data from external files
-		$globs_sql = array(
-			'yf_main'		=> YF_PATH.'share/db_installer/sql/*.sql.php',
-			'yf_plugins'	=> YF_PATH.'plugins/*/share/db_installer/sql/*.sql.php',
-		);
-		foreach ($globs_sql as $glob) {
-			foreach (glob($glob) as $f) {
-				$t_name = substr(basename($f), 0, -strlen('.sql.php'));
-				$tables_sql[$t_name] = include $f; // $data should be loaded from file
-			}
-		}
 		$globs_php = array(
 			'yf_main'		=> YF_PATH.'share/db_installer/sql_php/*.sql_php.php',
 			'yf_plugins'	=> YF_PATH.'plugins/*/share/db_installer/sql_php/*.sql_php.php',
@@ -196,9 +181,86 @@ class class_db_real_installer_mysql_test extends db_real_abstract {
 		$this->assertEquals( $tables, self::utils()->list_tables(self::db_name()) );
 	}
 
-	/***/
+	/**
+	* check how db installer table creating working, when selecting missing column in db, but have it in structure
+	*/
 	public function test_yf_db_installer_create_missing_table() {
-// TODO: check how db installer table creating working when selecting missing column in db, but have it in structure
+		$bak = self::db()->ERROR_AUTO_REPAIR;
+		self::db()->ERROR_AUTO_REPAIR = true;
+
+		$db_prefix = self::db()->DB_PREFIX;
+
+		self::utils()->truncate_database(self::db_name());
+		$this->assertEquals( array(), self::utils()->list_tables(self::db_name()) );
+
+		$db_installer = _class('db_installer_mysql', 'classes/db/');
+
+		$parser = _class('db_ddl_parser_mysql', 'classes/db/');
+		$parser->RAW_IN_RESULTS = false;
+
+		$tables_php = array();
+		$globs_php = array(
+			'yf_main'		=> YF_PATH.'share/db_installer/sql_php/*.sql_php.php',
+			'yf_plugins'	=> YF_PATH.'plugins/*/share/db_installer/sql_php/*.sql_php.php',
+		);
+		foreach ($globs_php as $glob) {
+			foreach (glob($glob) as $f) {
+				$t_name = substr(basename($f), 0, -strlen('.sql_php.php'));
+				$tables_php[$t_name] = include $f; // $data should be loaded from file
+			}
+		}
+		$this->assertNotEmpty($tables_php);
+
+		$this->assertTrue( (bool)self::db()->query('SET foreign_key_checks = 0;') );
+
+		$tables = array();
+		foreach ((array)$tables_php as $name => $sql_php) {
+			$tables[$db_prefix.$name] = $db_prefix.$name;
+
+			$this->assertFalse( (bool)self::utils()->table_exists(self::table_name($db_prefix.$name)) );
+			$this->assertTrue( (bool)self::db()->query('SELECT * FROM '.self::table_name($db_prefix.$name).' LIMIT 1'), 'selecting from table: '.$db_prefix.$name );
+			$this->assertTrue( (bool)self::utils()->table_exists(self::table_name($db_prefix.$name)) );
+
+			foreach ((array)$sql_php['fields'] as $fname => $f) {
+				unset($sql_php['fields'][$fname]['raw']);
+				unset($sql_php['fields'][$fname]['collate']);
+				unset($sql_php['fields'][$fname]['charset']);
+				if ($f['default'] === 'NULL') {
+					$sql_php['fields'][$fname]['default'] = null;
+				}
+			}
+			foreach ((array)$sql_php['indexes'] as $fname => $f) {
+				unset($sql_php['indexes'][$fname]['raw']);
+			}
+			foreach ((array)$sql_php['foreign_keys'] as $fname => $fk) {
+				unset($sql_php['foreign_keys'][$fname]['raw']);
+				if (is_null($fk['on_update'])) {
+					$sql_php['foreign_keys'][$fname]['on_update'] = 'RESTRICT';
+				}
+				if (is_null($fk['on_delete'])) {
+					$sql_php['foreign_keys'][$fname]['on_delete'] = 'RESTRICT';
+				}
+			}
+
+			$columns = self::utils()->list_columns(self::table_name($db_prefix.$name));
+			foreach ((array)$columns as $fname => $f) {
+				unset($columns[$fname]['type_raw']);
+				unset($columns[$fname]['collate']);
+				unset($columns[$fname]['charset']);
+			}
+			$this->assertEquals( $sql_php['fields'], $columns, 'Compare columns with expected sql_php for table: '.$name );
+
+			$indexes = self::utils()->list_indexes(self::table_name($db_prefix.$name));
+			$this->assertEquals( $sql_php['indexes'], $indexes, 'Compare indexes with expected sql_php for table: '.$name );
+
+			$fks = self::utils()->list_foreign_keys(self::table_name($db_prefix.$name));
+			$this->assertEquals( $sql_php['foreign_keys'], $fks, 'Compare indexes with expected sql_php for table: '.$name );
+		}
+		$this->assertTrue( (bool)self::db()->query('SET foreign_key_checks = 1;') );
+
+		$this->assertEquals( $tables, self::utils()->list_tables(self::db_name()) );
+
+		self::db()->ERROR_AUTO_REPAIR = $bak;
 	}
 
 	/***/
