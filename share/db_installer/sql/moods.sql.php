b4f306e95b0eb992428e524aff72d5ad66cb735a
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-09-24 17:03:29 +0300

    auto-generated php_sql

--- share/db_installer/sql/moods.sql.php
+++ share/db_installer/sql/moods.sql.php
@@ -1,7 +1,7 @@
 <?php
 return '
   `id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL CHARACTER SET utf8 DEFAULT \'\',
+  `name` varchar(255) NOT NULL DEFAULT \'\',
   `active` enum(\'1\',\'0\') NOT NULL DEFAULT \'1\',
   `locale` char(7) NOT NULL DEFAULT \'en\',
   PRIMARY KEY (`id`)
--- share/db_installer/sql/tips.sql.php
+++ share/db_installer/sql/tips.sql.php
@@ -2,7 +2,7 @@
 return '
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL DEFAULT \'\',
-  `text` text NOT NULL CHARACTER SET utf8,
+  `text` text NOT NULL,
   `type` tinyint(1) NOT NULL DEFAULT \'1\',
   `active` enum(\'1\',\'0\') NOT NULL DEFAULT \'1\',
   `locale` char(7) NOT NULL DEFAULT \'en\',
