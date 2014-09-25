1f038b54d899eab79f3aae3311d0478805ab9d2a
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-09-25 11:06:53 +0300

    db driver pdo_sqlite unified methods

--- classes/db/yf_db_driver_pdo_sqlite.class.php
+++ classes/db/yf_db_driver_pdo_sqlite.class.php
@@ -45,27 +45,6 @@ class yf_db_driver_pdo_sqlite extends yf_db_driver_pdo {
 
 	/**
 	*/
-	function close() {
-		if ($this->db_connect_id) {
-			$this->db_connect_id = null;
-			return true;
-		}
-		return false;
-	}
-
-	/**
-	*/
-	function query($query) {
-		if (!$this->db_connect_id) {
-			return false;
-		}
-		$result = $this->db_connect_id->query($query);
-		$this->_last_query_id = $result;
-		return $result;
-	}
-
-	/**
-	*/
 	function num_rows($query_id) {
 		if ($query_id) {
 			$num_rows = 0;
@@ -81,90 +60,8 @@ class yf_db_driver_pdo_sqlite extends yf_db_driver_pdo {
 
 	/**
 	*/
-	function affected_rows($query_id = false) {
-		return $this->_last_query_id ? $this->_last_query_id->rowCount() : false;
-	}
-
-	/**
-	*/
-	function insert_id($query_id = false) {
-		return $this->db_connect_id->lastInsertId();
-	}
-
-	/**
-	*/
-	function fetch_row($query_id) {
-		return $query_id ? $query_id->fetch(PDO::FETCH_NUM) : false;
-	}
-
-	/**
-	*/
-	function fetch_assoc($query_id) {
-		return $query_id ? $query_id->fetch(PDO::FETCH_ASSOC) : false;
-	}
-
-	/**
-	*/
-	function fetch_array($query_id) {
-		return $query_id ? $query_id->fetch(PDO::FETCH_BOTH) : false;
-	}
-
-	/**
-	*/
-	function fetch_object($query_id) {
-		return $query_id ? $query_id->fetch(PDO::FETCH_OBJ) : false;
-	}
-
-	/**
-	*/
 	function real_escape_string($string) {
 		return $this->db_connect_id ? substr($this->db_connect_id->quote($string), 1, -1) : addslashes($string);
-	}
-
-	/**
-	*/
-	function free_result($query_id = false) {
-		if (!$query_id) {
-			return false;
-		}
-		$query_id = null;
-		return true;
-	}
-
-	/**
-	*/
-	function error() {
-		if ($this->db_connect_id) {
-			$info = $this->db_connect_id->errorInfo();
-			return array(
-				'message'	=> $info[2],
-				'code'		=> $info[1],
-			);
-		} elseif ($this->_connect_error) {
-			return array(
-				'message'	=> 'YF: Connect error: '.$this->_connect_error,
-				'code'		=> '9999',
-			);
-		}
-		return false;
-	}
-
-	/**
-	*/
-	function begin() {
-		return $this->db_connect_id->beginTransaction();
-	}
-
-	/**
-	*/
-	function commit() {
-		return $this->db_connect_id->commit();
-	}
-
-	/**
-	*/
-	function rollback() {
-		return $this->db_connect_id->rollBack();
 	}
 
 	/**
@@ -187,17 +84,5 @@ class yf_db_driver_pdo_sqlite extends yf_db_driver_pdo {
 	*/
 	function escape_val($data) {
 		return '\''.trim($data, '\'').'\'';
-	}
-
-	/**
-	*/
-	function get_server_version() {
-		return $this->db_connect_id ? $this->db_connect_id->getAttribute(PDO::ATTR_SERVER_VERSION) : false;
-	}
-
-	/**
-	*/
-	function get_host_info() {
-		return $this->db_connect_id ? $this->db_connect_id->getAttribute(PDO::ATTR_SERVER_INFO) : false;
 	}
 }
