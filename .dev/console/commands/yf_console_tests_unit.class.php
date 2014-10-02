bb4f7657d19af41e6f670c0be652243bc6f96cc1
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-10-02 15:47:41 +0300

    console command up

---
+++ .dev/console/commands/yf_console_tests_unit.class.php
@@ -0,0 +1,34 @@
+<?php
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class yf_console_tests_unit extends Command {
+	protected function configure() {
+		$this
+			->setName('tests:unit')
+			->setDescription('YF unit tests runner')
+#			->addArgument('method', InputArgument::OPTIONAL, 'API method to call')
+			->addArgument('params',	InputArgument::IS_ARRAY | InputArgument::OPTIONAL, 'Params for sub-command');
+		;
+	}
+	protected function execute(InputInterface $input, OutputInterface $output) {
+		init_yf();
+
+		$params = array();
+		// Parse arguments like that: k1=v1 k2=v2 into array('k1' => 'v1', 'k2' => 'v2')
+		foreach ((array)$input->getArgument('params') as $p) {
+			list($k, $v) = explode('=', trim($p));
+			$k = trim($k);
+			$v = trim($v);
+			if (strlen($k) && strlen($v)) {
+				$params[$k] = $v;
+			}
+		}
+// TODO
+		$output->writeln($text);
+	}
+}
