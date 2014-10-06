2dca14c9b90d478818afa7679b79b31ad9fbb413
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-10-06 15:10:26 +0300

    db tests migrator fixtures updated

---
+++ .dev/tests/functional/db/migrator_fixtures/test_get_real_table_sql_php_1.expected.php
@@ -0,0 +1,48 @@
+<?php
+return array(
+	'fields' => array(
+		'id' => array(
+			'name' => 'id',
+			'type' => 'int',
+			'length' => 10,
+			'decimals' => NULL,
+			'unsigned' => NULL,
+			'nullable' => false,
+			'default' => NULL,
+			'charset' => NULL,
+			'collate' => NULL,
+			'auto_inc' => false,
+			'primary' => true,
+			'unique' => false,
+			'values' => NULL,
+			'type_raw' => 'int(10)',
+		),
+	),
+	'indexes' => array(
+		'PRIMARY' => array(
+			'name' => 'PRIMARY',
+			'type' => 'primary',
+			'columns' => array(
+				'id' => 'id',
+			),
+		),
+	),
+	'foreign_keys' => array(
+		'fkey_prepare_sample_data' => array(
+			'name' => 'fkey_prepare_sample_data',
+			'columns' => array(
+				'id' => 'id',
+			),
+			'ref_table' => 't_test_get_real_table_sql_php_2',
+			'ref_columns' => array(
+				'id' => 'id',
+			),
+			'on_update' => 'RESTRICT',
+			'on_delete' => 'RESTRICT',
+		),
+	),
+	'options' => array(
+		'engine' => 'InnoDB',
+		'charset' => 'utf8',
+	),
+);
