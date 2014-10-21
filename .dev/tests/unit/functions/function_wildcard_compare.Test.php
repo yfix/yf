9ae4d3d2a7b1fdda3d679434c8456a19fd44ab04
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-10-21 13:50:22 +0300

    unit tests up

--- .dev/tests/unit/functions/function_wildcard_compare.Test.php
+++ .dev/tests/unit/functions/function_wildcard_compare.Test.php
@@ -19,5 +19,8 @@ class function_wildcard_compare_test extends PHPUnit_Framework_TestCase {
 		$this->assertTrue( wildcard_compare('regex are ??? useful', 'regex are 123 useful') );
 		$this->assertTrue( wildcard_compare('regex ?? are ???? useful', 'regex 12 are 1234 useful') );
 		$this->assertTrue( wildcard_compare('????? are ?????? useful', 'regex are always useful') );
+		$this->assertTrue( wildcard_compare('on[ce]* are ?????? useful', 'one are always useful') );
+		$this->assertTrue( wildcard_compare('on[ce]* are ?????? useful', 'once are always useful') );
+		$this->assertTrue( wildcard_compare('on[ce]* are ?????? useful', 'onccce are always useful') );
 	}
 }
