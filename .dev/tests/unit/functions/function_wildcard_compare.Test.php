8b17ed19fa714a27a25987f900573ce9b97c528f
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-10-21 13:38:46 +0300

    unti tests added for wildcard_compare()

---
+++ .dev/tests/unit/functions/function_wildcard_compare.Test.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once dirname(__DIR__).'/yf_unit_tests_setup.php';
+
+class function_wildcard_compare_test extends PHPUnit_Framework_TestCase {
+	public function test_basic() {
+		$this->assertFalse( wildcard_compare('regex are useful', '') );
+		$this->assertFalse( wildcard_compare('regex are * useful', '') );
+		$this->assertFalse( wildcard_compare('regex are * useful', 'regex') );
+		$this->assertFalse( wildcard_compare('regex are * useful', 'regex are') );
+		$this->assertFalse( wildcard_compare('regex are * useful', 'regex are useful') );
+		$this->assertFalse( wildcard_compare('regex are ? useful', 'regex are useful') );
+
+		$this->assertTrue( wildcard_compare('regex are * useful', 'regex are always useful') );
+		$this->assertTrue( wildcard_compare('* are * useful', 'regex are always useful') );
+		$this->assertTrue( wildcard_compare('* are * useful', ' are  useful') );
+		$this->assertTrue( wildcard_compare('regex are ? useful', 'regex are 1 useful') );
+		$this->assertTrue( wildcard_compare('regex are ?? useful', 'regex are 12 useful') );
+		$this->assertTrue( wildcard_compare('regex are ??? useful', 'regex are 123 useful') );
+		$this->assertTrue( wildcard_compare('regex ?? are ???? useful', 'regex 12 are 1234 useful') );
+		$this->assertTrue( wildcard_compare('????? are ?????? useful', 'regex are always useful') );
+	}
+}
