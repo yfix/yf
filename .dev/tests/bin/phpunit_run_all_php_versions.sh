d9943d80e85238b54fa0f7d999e0191f8c701ed5
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-10-29 15:33:51 +0200

    unit tests bin script updated

--- .dev/tests/bin/phpunit_run_all_php_versions.sh
+++ .dev/tests/bin/phpunit_run_all_php_versions.sh
@@ -10,6 +10,8 @@ echo "== test php version 5.5 =="
 ./phpunit_run_all_55.sh
 echo "== test php version 5.6 =="
 ./phpunit_run_all_56.sh
+echo "== test php version 5.7 =="
+./phpunit_run_all_57.sh
 echo "== test php version HHVM =="
 ./phpunit_run_all_hhvm.sh
 
