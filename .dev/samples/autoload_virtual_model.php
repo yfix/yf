2452f7650b61c58cdb58852f656a83c51018dffc
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-10-30 17:06:44 +0200

    added sample of autoloading virtual model class

---
+++ .dev/samples/autoload_virtual_model.php
@@ -0,0 +1,39 @@
+<?php
+
+class model_test1 {
+	public static function __callStatic($name, $args) {
+		echo 'Hello from '.__CLASS__.'::'.__FUNCTION__.PHP_EOL;
+	}
+	public function __call($name, $args) {
+		echo 'Hello from '.__CLASS__.'->'.__FUNCTION__.PHP_EOL;
+	}
+}
+
+spl_autoload_register(function ($class) {
+	if (class_exists($class)) {
+		return true;
+	}
+	$try_model = 'model_'.$class;
+	if (class_exists($try_model)) {
+		eval('class '.$class.' extends '.$try_model.'{};');
+		return true;
+	}
+});
+
+// Existing class call
+$ts = microtime();
+
+model_test1::hello();
+$model_test1 = new model_test1();
+$model_test1->hello();
+
+echo round(microtime() - $ts, 5).PHP_EOL;
+
+// Virtual class call
+$ts = microtime();
+
+test1::hello();
+$test1 = new test1();
+$test1->hello();
+
+echo round(microtime() - $ts, 5).PHP_EOL;
