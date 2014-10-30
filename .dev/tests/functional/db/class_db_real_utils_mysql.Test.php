5a6cb6d6936d67be5d4e1960fbaeb92345da1ccb
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-10-30 16:30:28 +0200

    db real utils tests up

--- .dev/tests/functional/db/class_db_real_utils_mysql.Test.php
+++ .dev/tests/functional/db/class_db_real_utils_mysql.Test.php
@@ -302,6 +302,26 @@ class class_db_real_utils_mysql_test extends db_real_abstract {
 		}) );
 		$this->assertTrue( (bool)self::utils()->table_exists($this->table_name($table)) );
 	}
+	public function test_create_table_complex() {
+		if ($this->_need_skip_test(__FUNCTION__)) { return ; }
+		$this->assertNotEmpty( self::utils()->create_database($this->db_name()) );
+		$table = self::utils()->db->DB_PREFIX. __FUNCTION__;
+		$this->assertTrue( (bool)self::utils()->drop_table($this->table_name($table)) );
+		$this->assertFalse( (bool)self::utils()->table_exists($this->table_name($table)) );
+
+		$this->assertTrue( (bool)self::utils()->create_table($this->table_name($table), function($t) {
+			$t->small_int('actor_id', array('length' => 5, 'unsigned' => true, 'nullable' => false, 'auto_inc' => true))
+			->string('first_name', array('length' => 45, 'nullable' => false))
+			->string('last_name', array('length' => 45, 'nullable' => false))
+			->primary('actor_id')
+			->index('last_name', 'idx_actor_last_name')
+			->option('engine', 'InnoDB')
+			->option('charset', 'utf8')
+			;
+		}) );
+
+		$this->assertTrue( (bool)self::utils()->table_exists($this->table_name($table)) );
+	}
 	public function test__parse_column_type() {
 		if ($this->_need_skip_test(__FUNCTION__)) { return ; }
 		$this->assertEquals( array('type' => 'int','length' => null,'unsigned' => false,'decimals' => null,'values' => null), self::utils()->_parse_column_type('int') );
