df442ffa9765a409975dd07d28150fa43abdd7e4
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-11-02 23:32:00 +0200

    model relation up

--- classes/model/yf_model_relation.class.php
+++ classes/model/yf_model_relation.class.php
@@ -6,6 +6,11 @@ class yf_model_relation {
 	public function __construct($model, $relation) {
 		$this->_model = $model;
 		$this->_relation = $relation;
+		if ($relation['type'] === 'has_one') {
+#			$model_name = get_class($model);
+#			$column = $model_name.'_id';
+			return model($relation['model'])->find($model->id);
+		}
 	}
 	public function _get_model() {
 		return $this->_model;
@@ -15,13 +20,13 @@ class yf_model_relation {
 	}
 	public function attach($id, $params = array()) {
 		$r = &$this->_relation;
+		$model = $this->_model;
+		$db = $model->_db;
+		$utils = $db->utils();
 		if ($r['type'] === 'belongs_to_many') {
-			$model = $this->_model;
-			$db = $model->_db;
-			$utils = $db->utils();
 			$pivot = $r['pivot_table'];
 /*
-			$utils->create_table($r['pivot_table'], function($t) use ($r) {
+			$utils->create_table($pivot, function($t) use ($r) {
 				$t->int($r['local_key'], array('unsigned' => true, 'nullable' => false));
 				$t->int($r['foreign_key'], array('unsigned' => true, 'nullable' => false));
 				$t->primary(array($r['local_key'] => $r['local_key'], $r['foreign_key'] => $r['foreign_key']));
