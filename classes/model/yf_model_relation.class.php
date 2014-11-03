60bab6418cbdf98236aa80ff218c306010a8532e
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-11-03 14:28:18 +0200

    model relation class updated

--- classes/model/yf_model_relation.class.php
+++ classes/model/yf_model_relation.class.php
@@ -3,14 +3,11 @@
 class yf_model_relation {
 	protected $_model = null;
 	protected $_relation = array();
+	protected $_parent = array(); // parent model
+	protected $_related = array(); // related model
 	public function __construct($model, $relation) {
 		$this->_model = $model;
 		$this->_relation = $relation;
-		if ($relation['type'] === 'has_one') {
-#			$model_name = get_class($model);
-#			$column = $model_name.'_id';
-			return model($relation['model'])->find($model->id);
-		}
 	}
 	public function _get_model() {
 		return $this->_model;
@@ -25,13 +22,6 @@ class yf_model_relation {
 		$utils = $db->utils();
 		if ($r['type'] === 'belongs_to_many') {
 			$pivot = $r['pivot_table'];
-/*
-			$utils->create_table($pivot, function($t) use ($r) {
-				$t->int($r['local_key'], array('unsigned' => true, 'nullable' => false));
-				$t->int($r['foreign_key'], array('unsigned' => true, 'nullable' => false));
-				$t->primary(array($r['local_key'] => $r['local_key'], $r['foreign_key'] => $r['foreign_key']));
-			});
-*/
 			return $db->replace($pivot, array(
 				$r['local_key']		=> $model->id,
 				$r['foreign_key']	=> $id,
@@ -39,4 +29,26 @@ class yf_model_relation {
 		}
 		return false;
 	}
+	public function get_data() {
+		$relation = $this->_relation;
+		$model = $this->_model;
+		if ($relation['type'] === 'has_one') {
+			return model($relation['related'])->find($model->id);
+		}
+	}
+/*
+	public function __call($name, $args) {
+		if (method_exists($this, $name)) {
+			return $this->$name;
+		}
+	}
+	public function _create_pivot_table($pivot, $relation) {
+		$r = $relation;
+		$utils->create_table($pivot, function($t) use ($r) {
+			$t->int($r['local_key'], array('unsigned' => true, 'nullable' => false));
+			$t->int($r['foreign_key'], array('unsigned' => true, 'nullable' => false));
+			$t->primary(array($r['local_key'] => $r['local_key'], $r['foreign_key'] => $r['foreign_key']));
+		});
+	}
+*/
 }
