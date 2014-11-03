588b0f9aec1fe6962d49bd1856970aaf07861fae
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-11-03 17:18:51 +0200

    unit tests for class_basename()

--- .dev/tests/unit/functions/function_class_basename.Test.php
+++ .dev/tests/unit/functions/function_class_basename.Test.php
@@ -16,6 +16,8 @@ class function_class_basename extends PHPUnit_Framework_TestCase {
 		$this->assertEquals('test', class_basename('test','',''));
 		$this->assertEquals('test', class_basename('test','','_suffix'));
 		$this->assertEquals('test', class_basename('test_suffix','','_suffix'));
+		$this->assertEquals('test', class_basename('prefix_test_suffix','prefix_','_suffix'));
 		$this->assertEquals('test', class_basename('yf_test_suffix','','_suffix'));
+		$this->assertEquals('test', class_basename('yf_prefix_test_suffix','prefix_','_suffix'));
 	}
 }
