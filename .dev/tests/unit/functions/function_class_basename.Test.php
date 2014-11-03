141d9a071806131ebdcd3262960089c3deb1bea2
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-11-03 17:17:57 +0200

    unit tests for class_basename()

---
+++ .dev/tests/unit/functions/function_class_basename.Test.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once dirname(__DIR__).'/yf_unit_tests_setup.php';
+
+class function_class_basename extends PHPUnit_Framework_TestCase {
+	public function test_main() {
+		$this->assertEquals('test', class_basename('test'));
+		$this->assertEquals('test', class_basename('yf_test'));
+		$this->assertEquals('test', class_basename('site__test'));
+		$this->assertEquals('test', class_basename('adm__test'));
+		$this->assertEquals('test', class_basename('custom__test', 'custom__'));
+		$this->assertEquals('test', class_basename('custom_test', 'custom_'));
+		$this->assertEquals('test', class_basename('yf_custom_test', 'custom_'));
+		$this->assertEquals('test', class_basename('test', 'custom_'));
+		$this->assertEquals('test', class_basename('test',''));
+		$this->assertEquals('test', class_basename('test','',''));
+		$this->assertEquals('test', class_basename('test','','_suffix'));
+		$this->assertEquals('test', class_basename('test_suffix','','_suffix'));
+		$this->assertEquals('test', class_basename('yf_test_suffix','','_suffix'));
+	}
+}
