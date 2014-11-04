15caf3ab397f5fb28fdb6212ce1900b29307eed8
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-11-04 14:48:06 +0200

    model relations cleanup

--- classes/model/yf_model_relation.class.php
+++ classes/model/yf_model_relation.class.php
@@ -1,61 +1,113 @@
 <?php
 
+/**
+* Relations handler for model
+*/
 class yf_model_relation {
+
 	protected $_model = null;
 	protected $_relation = array();
 	protected $_parent = array(); // parent model
 	protected $_related = array(); // related model
+
+	/**
+	*/
 	public function __construct($relation, $model) {
 		$this->_relation = $relation;
 		$this->_model = $model;
 	}
+
+	/**
+	*/
 	public function _get_model() {
 		return $this->_model;
 	}
+
+	/**
+	*/
 	public function _get_relation() {
 		return $this->_relation;
 	}
+
+	/**
+	*/
 	public function attach($id, $params = array()) {
-		$r = &$this->_relation;
+		$relation = $this->_relation;
 		$model = $this->_model;
 		$db = $model->_db;
 		$utils = $db->utils();
-		if ($r['type'] === 'belongs_to_many') {
-			$pivot = $r['pivot_table'];
-			return $db->replace($pivot, array(
-				$r['local_key']		=> $model->get_key(),
-				$r['foreign_key']	=> $id,
+		$type = $relation['type'];
+
+		if ($type === 'belongs_to_many') {
+
+			return $db->replace($relation['pivot_table'], array(
+				$relation['other_key']		=> $id,
+				$relation['foreign_key']	=> $model->get_key(),
 			));
+
 		}
 		return false;
 	}
+
+	/**
+	*/
 	public function get_data() {
 		$relation = $this->_relation;
 		$model = $this->_model;
+		$db = $model->_db;
 		$table = $model->get_table();
 		$table_alias = 't0';
 		$id = $model->get_key();
 		$query = $relation['query'];
 		$rel_model = $query->_model;
+		$rel_table = $rel_model->get_table();
 		$type = $relation['type'];
 
 		if ($type === 'has_one') {
 
-			return $query->whereid($id)->get();
+			return $query->whereid($id)
+				->get();
 
 		} elseif ($type === 'has_many') {
 
-			$rel_table = $rel_model->get_table();
 			$foreign_key = $relation['foreign_key'];
 			$local_key = $relation['local_key'];
 
-			return $query->whereid($id)->inner_join($rel_table, array(
-				$foreign_key => $table_alias.'.'.$local_key,
-			))->get_all();
+			return $query->whereid($id)
+// TODO: decide something with overlapping select keys
+				->inner_join($rel_table, array(
+					$foreign_key => $table_alias.'.'.$local_key,
+				))->get_all();
 
+		} elseif ($type === 'has_many_through') {
+// TODO
 		} elseif ($type === 'belongs_to') {
 // TODO
 		} elseif ($type === 'belongs_to_many') {
+
+			$pivot_table = $db->_fix_table_name($relation['pivot_table']);
+			$foreign_key = $relation['foreign_key'];
+			$other_key = $relation['other_key'];
+
+			return $query->whereid($id)
+// TODO: decide something with overlapping select keys
+				->inner_join($pivot_table, array(
+					$table_alias.'.'.$rel_model->get_key_name() => $pivot_table.'.'.$other_key,
+				))
+				->inner_join($table, array(
+					$table.'.'.$model->get_key_name() => $pivot_table.'.'.$foreign_key,
+				))
+				->get_all();
+
+		} elseif ($type === 'morph_one') {
+// TODO
+		} elseif ($type === 'morph_to') {
+// TODO
+		} elseif ($type === 'morph_many') {
+// TODO
+		} elseif ($type === 'morph_to_many') {
+// TODO
+		} elseif ($type === 'morphed_by_many') {
 // TODO
 		}
 	}
