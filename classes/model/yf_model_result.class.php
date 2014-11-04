a2abdf8e987e7b57f80d12623dde2f15692cf393
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-11-04 14:48:13 +0200

    model result cleanup

--- classes/model/yf_model_result.class.php
+++ classes/model/yf_model_result.class.php
@@ -1,21 +1,37 @@
 <?php
 
+/**
+* Resultset storage for model
+*/
 class yf_model_result {
+
 	protected $_model = null;
+
+	/**
+	*/
 	public function __construct($data, $model) {
 		$this->_set_data($data);
 		$this->_model = $model;
 	}
+
+	/**
+	*/
 	public function __get($name) {
 		if ($name !== 'model' && !isset($this->$name)) {
 			return $this->_model->$name;
 		}
 		return $this->$name;
 	}
+
+	/**
+	*/
 	public function __call($name, $args) {
 		$this->_sync_model_data();
 		return call_user_func_array(array($this->_model, $name), $args);
 	}
+
+	/**
+	*/
 	public function _set_data($data) {
 		foreach ((array)$data as $k => $v) {
 			$first = substr($k, 0, 1);
@@ -24,6 +40,9 @@ class yf_model_result {
 			}
 		}
 	}
+
+	/**
+	*/
 	public function _sync_model_data() {
 		foreach (get_object_vars($this) as $var => $value) {
 			if (substr($var, 0, 1) === '_') {
@@ -32,6 +51,9 @@ class yf_model_result {
 			$this->_model->$var = $value;
 		}
 	}
+
+	/**
+	*/
 	public function _get_model() {
 		return $this->_model;
 	}
