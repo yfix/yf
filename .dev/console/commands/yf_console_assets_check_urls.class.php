bd7a6a45c6b8bd43a2cb1303a460dc3fc9e46c8f
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-11-26 12:47:03 +0200

    yf console tool added assets url checking

---
+++ .dev/console/commands/yf_console_assets_check_urls.class.php
@@ -0,0 +1,25 @@
+<?php
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class yf_console_assets_check_urls extends Command {
+	protected function configure() {
+		$this
+			->setName('assets:check_urls')
+			->setDescription('YF unit tests runner')
+			->addArgument('params',	InputArgument::IS_ARRAY | InputArgument::OPTIONAL, 'Params for sub-command');
+		;
+	}
+	protected function execute(InputInterface $input, OutputInterface $output) {
+		init_yf();
+
+		$DIR_TO_CHECK = APP_PATH;
+		require YF_PATH. '.dev/scripts/assets/assets_urls_check.php';
+
+		$output->writeln($text);
+	}
+}
