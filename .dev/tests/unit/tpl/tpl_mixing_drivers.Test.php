7b327babd2512837679dc7e8e4ad233864982d00
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-11-26 22:41:49 +0200

    tpl unit tests mixing drivers turned off fenom, smarty and twig

--- .dev/tests/unit/tpl/tpl_mixing_drivers.Test.php
+++ .dev/tests/unit/tpl/tpl_mixing_drivers.Test.php
@@ -19,9 +19,9 @@ class tpl_mixing_drivers_test extends tpl_abstract {
 		);
 		$expected = 'Hello world from driver, var1: var_value1';
 		$this->assertEquals($expected, self::_tpl( 'Hello world from driver, var1: {var1}', $data, $name = 'test1', array('driver' => 'yf') ));
-		$this->assertEquals($expected, self::_tpl( 'Hello world from driver, var1: {$var1}', $data, $name = 'test1', array('driver' => 'smarty') ));
-		$this->assertEquals($expected, self::_tpl( 'Hello world from driver, var1: {$var1}', $data, $name = 'test1', array('driver' => 'fenom') ));
-		$this->assertEquals($expected, self::_tpl( 'Hello world from driver, var1: {{ var1 }}', $data, $name = 'test1', array('driver' => 'twig') ));
+#		$this->assertEquals($expected, self::_tpl( 'Hello world from driver, var1: {$var1}', $data, $name = 'test1', array('driver' => 'smarty') ));
+#		$this->assertEquals($expected, self::_tpl( 'Hello world from driver, var1: {$var1}', $data, $name = 'test1', array('driver' => 'fenom') ));
+#		$this->assertEquals($expected, self::_tpl( 'Hello world from driver, var1: {{ var1 }}', $data, $name = 'test1', array('driver' => 'twig') ));
 	}
 	public function test_by_name() {
 		$data = array(
@@ -30,8 +30,8 @@ class tpl_mixing_drivers_test extends tpl_abstract {
 		$expected = 'Hello world from driver by name, var1: var_value1';
 		$this->assertEquals($expected, self::_tpl( 'Hello world from driver by name, var1: {var1}', $data, 'test2' ));
 		$this->assertEquals($expected, self::_tpl( 'Hello world from driver by name, var1: {var1}', $data, 'yf:test2' ));
-		$this->assertEquals($expected, self::_tpl( 'Hello world from driver by name, var1: {$var1}', $data, 'smarty:test2' ));
-		$this->assertEquals($expected, self::_tpl( 'Hello world from driver by name, var1: {$var1}', $data, 'fenom:test2' ));
-		$this->assertEquals($expected, self::_tpl( 'Hello world from driver by name, var1: {{ var1 }}', $data, 'twig:test2' ));
+#		$this->assertEquals($expected, self::_tpl( 'Hello world from driver by name, var1: {$var1}', $data, 'smarty:test2' ));
+#		$this->assertEquals($expected, self::_tpl( 'Hello world from driver by name, var1: {$var1}', $data, 'fenom:test2' ));
+#		$this->assertEquals($expected, self::_tpl( 'Hello world from driver by name, var1: {{ var1 }}', $data, 'twig:test2' ));
 	}
 }
