124633b39dc88c93a75720b42a99284a8c28c99a
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-11-26 17:05:25 +0200

    html class tests up

--- .dev/tests/unit/class_html.Test.php
+++ .dev/tests/unit/class_html.Test.php
@@ -50,14 +50,57 @@ class class_html_test extends PHPUnit_Framework_TestCase {
 			'<select name="myselect3" id="select_box_3" class="form-control" data-unittest="testval"><option value="1">red</option><option value="2">green</option></select>'
 			), str_replace(PHP_EOL, '', trim($html))
 		);
-		$html = html()->select_box(array('name' => 'myselect3', 'data-unittest' => 'testval', 'values' => $data, 'disabled' => 1));
+		$html = html()->select_box(array(
+			'name' => 'myselect3',
+			'data-unittest' => 'testval',
+			'values' => $data,
+			'disabled' => 1
+		));
 		$this->assertEquals(str_replace(PHP_EOL, '', 
 			'<select name="myselect3" id="select_box_4" class="form-control" disabled="disabled" data-unittest="testval"><option value="1">red</option><option value="2">green</option></select>'
 			), str_replace(PHP_EOL, '', trim($html))
 		);
-		$html = html()->select_box(array('name' => 'myselect3', 'data-unittest' => 'testval', 'values' => $data, 'disabled' => 1, 'selected' => 2));
+		$html = html()->select_box(array(
+			'name' => 'myselect3',
+			'data-unittest' => 'testval',
+			'values' => $data,
+			'disabled' => 1,
+			'selected' => 2
+		));
 		$this->assertEquals(str_replace(PHP_EOL, '', 
 			'<select name="myselect3" id="select_box_5" class="form-control" disabled="disabled" data-unittest="testval"><option value="1">red</option><option value="2" selected="selected">green</option></select>'
+			), str_replace(PHP_EOL, '', trim($html))
+		);
+		$html = html()->select_box(array(
+			'name' => 'myselect3',
+			'data-unittest' => 'testval',
+			'values' => $data,
+			'disabled' => 1,
+			'selected' => 2,
+			'style' => 'color:red;',
+			'class' => 'myclass',
+			'add_str' => 'onclick="alert(\'Hello\')"',
+			'show_text' => 1,
+		));
+		$this->assertEquals(str_replace(PHP_EOL, '', 
+			'<select name="myselect3" id="select_box_6" class="myclass form-control" style="color:red;" disabled="disabled" data-unittest="testval" onclick="alert(\'Hello\')">'.
+			'<option value="">-select myselect3-</option><option value="1">red</option><option value="2" selected="selected">green</option></select>'
+			), str_replace(PHP_EOL, '', trim($html))
+		);
+		$html = html()->select_box(array(
+			'name' => 'myselect3',
+			'data-unittest' => 'testval',
+			'values' => array('sub1' => $data),
+			'disabled' => 1,
+			'selected' => 2,
+			'style' => 'color:red;',
+			'class' => 'myclass',
+			'add_str' => 'onclick="alert(\'Hello\')"',
+			'show_text' => 1,
+		));
+		$this->assertEquals(str_replace(PHP_EOL, '', 
+			'<select name="myselect3" id="select_box_7" class="myclass form-control" style="color:red;" disabled="disabled" data-unittest="testval" onclick="alert(\'Hello\')">'.
+			'<option value="">-select myselect3-</option><optgroup label="sub1" title="sub1"><option value="1">red</option><option value="2" selected="selected">green</option></optgroup></select>'
 			), str_replace(PHP_EOL, '', trim($html))
 		);
 	}
