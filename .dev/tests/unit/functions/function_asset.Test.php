67a7cf46249311c3dfe8983cef0d0dfb925295f0
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-11-27 13:19:25 +0200

    unit tests for asset up

--- .dev/tests/unit/functions/function_asset.Test.php
+++ .dev/tests/unit/functions/function_asset.Test.php
@@ -4,9 +4,9 @@ require_once dirname(__DIR__).'/yf_unit_tests_setup.php';
 
 class function_asset_test extends PHPUnit_Framework_TestCase {
 	public function test_basic() {
-		_class('assets')->clean_all();
+#		_class('assets')->clean_all();
 		$url = '//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.3.2/contents.css';
-#		asset($url);
+#		asset('css', $url);
 #		$this->assertEquals('<link href="'.$url.'" rel="stylesheet" />', _class('assets')->show());
 	}
 }
