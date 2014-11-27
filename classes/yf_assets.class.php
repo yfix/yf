ba4a999d7330d4543970dbab134c25cd5d1462d1
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-11-27 13:19:45 +0200

    assets class up

--- classes/yf_assets.class.php
+++ classes/yf_assets.class.php
@@ -2,9 +2,14 @@
 
 class yf_assets {
 
-	public $content = array();
+	/** @array Container for added content */
+	protected $content = array();
 	/** @array List of pre-defined assets. See share/assets.php */
-	public $assets = array();
+	protected $assets = array();
+	/** @array List of pre-defined bundles of assets. See share/assets.php */
+	protected $bundles = array();
+	/** @array All filters to apply stored here */
+	protected $filters = array();
 
 	/**
 	* Catch missing method call
@@ -29,98 +34,108 @@ class yf_assets {
 	/**
 	*/
 	public function clean_all() {
-		$this->content = array();
-		$this->assets = array();
+		$this->content	= array();
+		$this->assets	= array();
+		$this->bundles	= array();
+		$this->filters	= array();
 	}
 
 	/**
-	* $content: string/array
-	* $type: = auto|asset|url|file|inline|raw
 	*/
-	public function add($content, $force_type = 'auto', $params = array()) {
+	public function _init() {
+		$assets = array();
+		$bundles = array();
+		$supported_asset_types = array('js','css','less','sass','font');
+		foreach ((array)$this->load_predefined_assets() as $name => $data) {
+#			if (in_array($name, $supported_asset_types)) {
+				$assets[$name] = $data;
+#			} else {
+#				$bundles[$name] = $data;
+#			}
+		}
+		if ($assets) {
+			$this->assets += (array)$assets;
+		}
+		if ($bundles) {
+			$this->bundles += (array)$bundles;
+		}
+	}
+
+	/**
+	*/
+	public function load_predefined_assets() {
+		return require YF_PATH.'share/assets.php';
+	}
+
+	/**
+	* Add asset item into current workflow
+	*
+	* $content: string/array
+	* $asset_type: = bundle|js|css|img|less|sass|font
+	* $content_type_hint: = auto|asset|url|file|inline|raw
+	*/
+	public function add($content, $asset_type = '', $content_type_hint = 'auto', $params = array()) {
 		if (DEBUG_MODE) {
 			$trace = main()->trace_string();
+		}
+		if (!$asset_type) {
+			$asset_type = 'bundle';
+// TODO
 		}
 		if (!is_array($content)) {
 			$content = array($content);
 		}
-		if (is_array($force_type)) {
-			$params = (array)$params + $force_type;
-			$force_type = '';
+		if (is_array($content_type_hint)) {
+			$params = (array)$params + $content_type_hint;
+			$content_type_hint = '';
 		}
 		foreach ($content as $_content) {
 			$_content = trim($_content);
 			if (!strlen($_content)) {
 				continue;
 			}
-			$type = '';
-			if (in_array($force_type, array('url','file','inline','raw','asset'))) {
-				$type = $force_type;
+			$content_type = '';
+			if (in_array($content_type_hint, array('url','file','inline','raw','asset'))) {
+				$content_type = $content_type_hint;
 			} else {
-				$type = $this->_detect_content($_content);
+				$content_type = $this->detect_content_type($asset_type, $_content);
 			}
 			$md5 = md5($_content);
-			if ($type == 'url') {
-				$this->content[$md5] = array(
-					'type'	=> 'url',
-					'text'	=> $_content,
-					'params'=> $params,
-				);
-			} elseif ($type == 'file') {
+			if ($content_type == 'url') {
+				$this->set_content($asset_type, $md5, 'url', $_content, $params);
+			} elseif ($content_type == 'file') {
 				if (file_exists($_content)) {
 					$text = file_get_contents($_content);
 					if (strlen($text)) {
-						$this->content[$md5] = array(
-							'type'	=> 'file',
-							'text'	=> $_content,
-							'params'=> $params,
-						);
+						$this->set_content($asset_type, $md5, 'file', $_content, $params);
 					}
 				}
-			} elseif ($type == 'inline') {
-				$this->content[$md5] = array(
-					'type'	=> 'inline',
-					'text'	=> $_content,
-					'params'=> $params,
-				);
-			} elseif ($type == 'raw') {
-				$this->content[$md5] = array(
-					'type'	=> 'raw',
-					'text'	=> $_content,
-					'params'=> $params,
-				);
-			} elseif ($type == 'asset') {
-				$info = $this->assets[$_content];
+			} elseif ($content_type == 'inline') {
+				$this->set_content($asset_type, $md5, 'inline', $_content, $params);
+			} elseif ($content_type == 'raw') {
+				$this->set_content($asset_type, $md5, 'raw', $_content, $params);
+			} elseif ($content_type == 'asset') {
+				$info = $this->assets[$asset_type][$_content];
 				if (is_array($info)) {
 					$url = $info['url'];
 					if ($info['require']) {
-						$this->add($info['require'], 'asset');
+						$this->add($asset_type, $info['require'], 'asset');
 					}
 				} else {
 					$url = $info;
 				}
 				$md5 = md5($url);
-				$this->content[$md5] = array(
-					'type'	=> 'url',
-					'text'	=> $url,
-					'params'=> $params,
-				);
+				$this->set_content($asset_type, $md5, 'url', $url, $params);
 			}
 			if (DEBUG_MODE) {
-				if (false !== strpos(__CLASS__, '_js')) {
-					$debug_name = 'core_js';
-				} elseif (false !== strpos(__CLASS__, '_css')) {
-					$debug_name = 'core_css';
-				} else {
-					$debug_name = 'assets';
-				}
-				debug($debug_name.'[]', array(
-					'type'		=> $type,
-					'md5'		=> $md5,
-					'content'	=> $_content,
-					'is_added'	=> isset($this->content[$md5]),
-					'params'	=> $params,
-					'trace'		=> $trace,
+				debug('assets[]', array(
+					'asset_type'	=> $asset_type,
+					'content_type'	=> $content_type,
+					'md5'			=> $md5,
+					'content'		=> $_content,
+					'is_added'		=> !is_null($this->get_content($asset_type, $md5)),
+					'params'		=> $params,
+					'trace'			=> $trace,
 				));
 			}
 		}
@@ -128,86 +143,227 @@ class yf_assets {
 	}
 
 	/**
+	* Shortcut
 	*/
-	public function add_url($content, $params = array()) {
-		return $this->add($content, 'url', $params);
+	public function add_url($content, $asset_type, $params = array()) {
+		return $this->add($content, $asset_type, 'url', $params);
+	}
+
+	/**
+	* Shortcut
+	*/
+	public function add_file($content, $asset_type, $params = array()) {
+		return $this->add($content, $asset_type, 'file', $params);
+	}
+
+	/**
+	* Shortcut
+	*/
+	public function add_inline($content, $asset_type, $params = array()) {
+		return $this->add($content, $asset_type, 'inline', $params);
+	}
+
+	/**
+	* Shortcut
+	*/
+	public function add_raw($content, $asset_type, $params = array()) {
+		return $this->add($content, $asset_type, 'raw', $params);
+	}
+
+	/**
+	* Shortcut
+	*/
+	public function add_asset($content, $asset_type, $params = array()) {
+		return $this->add($content, $asset_type, 'asset', $params);
+	}
+
+	/**
+	* Return content for given asset type, optionally only for md5 of it
+	*/
+	public function get_content($asset_type, $params = array()) {
+		$md5 = (is_string($params) && strlen($params) === 32) ? $params : $params['md5'];
+		return $md5 ? $this->content[$asset_type][$md5] : $this->content[$asset_type];
+	}
+
+	/**
+	* Set unique content entry for given asset type
+	*/
+	public function set_content($asset_type, $md5, $content_type, $content, $params = array()) {
+		return $this->content[$asset_type][$md5] = array(
+			'content_type'	=> $content_type,
+			'content'		=> $content,
+			'params'		=> $params,
+		);
+	}
+	/**
+	* Clean content for given asset type
+	*/
+	public function clean_content($asset_type) {
+		return $this->content[$asset_type] = array();
 	}
 
 	/**
 	*/
-	public function add_file($content, $params = array()) {
-		return $this->add($content, 'file', $params);
+	public function get_asset($asset_type, $name, $params = array()) {
+		return $this->assets[$asset_type][$name];
 	}
 
 	/**
+	* Shortcut
 	*/
-	public function add_inline($content, $params = array()) {
-		return $this->add($content, 'inline', $params);
+	public function add_js($content, $content_type_hint = 'auto', $params = array()) {
+		return $this->add('js', $content, $content_type_hint, $params);
 	}
 
 	/**
+	* Shortcut
 	*/
-	public function add_raw($content, $params = array()) {
-		return $this->add($content, 'raw', $params);
+	public function add_css($content, $content_type_hint = 'auto', $params = array()) {
+		return $this->add('css', $content, $content_type_hint, $params);
 	}
 
 	/**
+	* Shortcut
 	*/
-	public function add_asset($content, $params = array()) {
-		return $this->add($content, 'asset', $params);
+	public function add_less($content, $content_type_hint = 'auto', $params = array()) {
+		return $this->add('less', $content, $content_type_hint, $params);
 	}
 
 	/**
+	* Shortcut
 	*/
-	public function add_js($content, $params = array()) {
+	public function add_sass($content, $content_type_hint = 'auto', $params = array()) {
+		return $this->add('sass', $content, $content_type_hint, $params);
+	}
+
+	/**
+	* Shortcut
+	*/
+	public function add_img($content, $content_type_hint = 'auto', $params = array()) {
+// TODO
+#		return $this->add('img', $content, $content_type_hint, $params);
+	}
+
+	/**
+	* Shortcut
+	*/
+	public function add_font($content, $content_type_hint = 'auto', $params = array()) {
+// TODO
+#		return $this->add('font', $content, $content_type_hint, $params);
+	}
+
+	/**
+	* General output/embed method
+	*/
+	public function show($out_type, $params = array()) {
 // TODO
 	}
 
 	/**
+	* Shortcut
 	*/
-	public function add_css($content, $params = array()) {
-// TODO
+	public function show_js($params = array()) {
+		return $this->show('js', $params);
+	}
+
+	/**
+	* Shortcut
+	*/
+	public function show_css() {
+		return $this->show('css', $params);
+	}
+
+	/**
+	* Shortcut
+	*/
+	public function show_images() {
+		return $this->show('images', $params);
+	}
+
+	/**
+	* Shortcut
+	*/
+	public function show_fonts() {
+		return $this->show('fonts', $params);
 	}
 
 	/**
 	*/
-	public function add_less($content, $params = array()) {
-// TODO
+	public function detect_content_type($asset_type, $content = '') {
+		$content = trim($content);
+		$type = false;
+		if (isset($this->assets[$asset_type][$content])) {
+			$type = 'asset';
+		} elseif ($asset_type === 'js') {
+			if (preg_match('~^(http://|https://|//)[a-z0-9]+~ims', $content)) {
+				$type = 'url';
+			} elseif (preg_match('~^/[a-z0-9\./_-]+\.js$~ims', $content) && file_exists($content)) {
+				$type = 'file';
+			} elseif (preg_match('~^(<script|[$;#\*/])~ims', $content) || strpos($content, PHP_EOL) !== false) {
+				$type = 'inline';
+			}
+		} elseif ($asset_type === 'css') {
+			if (preg_match('~^(http://|https://|//)[a-z0-9]+~ims', $content)) {
+				$type = 'url';
+			} elseif (preg_match('~^/[a-z0-9\./_-]+\.css$~ims', $content) && file_exists($content)) {
+				$type = 'file';
+			} elseif (preg_match('~^(<style|[$;#\.@/\*])~ims', $content) || strpos($content, PHP_EOL) !== false) {
+				$type = 'inline';
+			}
+		}
+		return $type;
+	}
+
+	/**
+	* Search for assets for current module in several places, where it can be stored.
+	*/
+	public function find_asset_type_for_module($asset_type, $module = '') {
+		if (!$module) {
+			$module = $_GET['object'];
+		}
+		$ext = '.'.$asset_type;
+		$path = $module. '/'. $module. $ext;
+		$paths = array(
+			MAIN_TYPE_ADMIN ? YF_PATH. 'templates/admin/'.$path : '',
+			YF_PATH. 'templates/user/'.$path,
+			MAIN_TYPE_ADMIN ? YF_PATH. 'plugins/'.$module.'/templates/admin/'.$path : '',
+			YF_PATH. 'plugins/'.$module.'/templates/user/'.$path,
+			MAIN_TYPE_ADMIN ? PROJECT_PATH. 'templates/admin/'.$path : '',
+			PROJECT_PATH. 'templates/user/'.$path,
+			SITE_PATH != PROJECT_PATH ? SITE_PATH. 'templates/user/'.$path : '',
+		);
+		$found = '';
+		foreach (array_reverse($paths, true) as $path) {
+			if (!strlen($path)) {
+				continue;
+			}
+			if (file_exists($path)) {
+				$found = $path;
+				break;
+			}
+		}
+		return $found;
+	}
+
+	/**
+	* Add custom filter callback
+	*/
+	public function filter_add($asset_type, $callback, $params = array()) {
+		$this->filters[$asset_type][] = array(
+			'callback'	=> $callback,
+			'params'	=> $params,
+		);
 	}
 
 	/**
 	*/
-	public function add_sass($content, $params = array()) {
-// TODO
-	}
-
-	/**
-	*/
-	public function add_img($content, $params = array()) {
-// TODO
-	}
-
-	/**
-	*/
-	public function add_font($content, $params = array()) {
-// TODO
-	}
-
-	/**
-	*/
-	public function filter_add() {
+	public function filters_apply($asset_type) {
 // TODO
 	}
 
 	/**
 	*/
 	public function filters_process_css_urls() {
-// TODO
-	}
-
-	/**
-	*/
-	public function filters_apply() {
 // TODO
 	}
 
@@ -220,30 +376,6 @@ class yf_assets {
 	/**
 	*/
 	public function upload_to() {
-// TODO
-	}
-
-	/**
-	*/
-	public function show_js() {
-// TODO
-	}
-
-	/**
-	*/
-	public function show_css() {
-// TODO
-	}
-
-	/**
-	*/
-	public function show_img() {
-// TODO
-	}
-
-	/**
-	*/
-	public function show_font() {
 // TODO
 	}
 
