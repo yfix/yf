9af23fe1102ebd6ec7850e163c6f2161e3159ec8
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-11-28 17:42:11 +0200

    unit tests up

--- .dev/tests/unit/functions/function_require_css.Test.php
+++ .dev/tests/unit/functions/function_require_css.Test.php
@@ -3,24 +3,6 @@
 require_once dirname(__DIR__).'/yf_unit_tests_setup.php';
 
 class function_require_css_test extends PHPUnit_Framework_TestCase {
-	public function test_detect_content_type() {
-		$this->assertEquals('asset', _class('core_css')->_detect_content('jquery-ui'));
-		$this->assertEquals('url', _class('core_css')->_detect_content('http://cdnjs.cloudflare.com/ajax/libs/ckeditor/4.3.2/contents.css'));
-		$this->assertEquals('url', _class('core_css')->_detect_content('https://cdnjs.cloudflare.com/ajax/libs/ckeditor/4.3.2/contents.css'));
-		$this->assertEquals('url', _class('core_css')->_detect_content('//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.3.2/contents.css'));
-		$this->assertEquals('inline', _class('core_css')->_detect_content('<style>$(function(){})</style>'));
-		$this->assertEquals('inline', _class('core_css')->_detect_content('<style type="text/css">.some_class { border: 1px solid black; } #some_id { display:none; }</style>'));
-		$this->assertEquals('inline', _class('core_css')->_detect_content('.some_class { border: 1px solid black; } #some_id { display:none; }'));
-		$this->assertEquals('inline', _class('core_css')->_detect_content('
-			.some_class { border: 1px solid black; }
-			#some_id { display:none; }
-		'));
-
-		$f = '/tmp/yf_unit_tests_empty_style.css';
-		file_put_contents($f, 'test');
-		$this->assertEquals('file', _class('core_css')->_detect_content($f));
-		unlink($f);
-	}
 	public function test_complex() {
 		css('//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.3.2/contents.css');
 		$this->assertEquals('<link href="//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.3.2/contents.css" rel="stylesheet" />', _class('core_css')->show());
--- .dev/tests/unit/functions/function_require_js.Test.php
+++ .dev/tests/unit/functions/function_require_js.Test.php
@@ -3,26 +3,6 @@
 require_once dirname(__DIR__).'/yf_unit_tests_setup.php';
 
 class function_require_js_test extends PHPUnit_Framework_TestCase {
-	public function test_detect_content_type() {
-		$this->assertEquals('asset', _class('core_js')->_detect_content('jquery'));
-		$this->assertEquals('url', _class('core_js')->_detect_content('http://cdnjs.cloudflare.com/ajax/libs/ckeditor/4.3.2/ckeditor.js'));
-		$this->assertEquals('url', _class('core_js')->_detect_content('https://cdnjs.cloudflare.com/ajax/libs/ckeditor/4.3.2/ckeditor.js'));
-		$this->assertEquals('url', _class('core_js')->_detect_content('//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.3.2/ckeditor.js'));
-		$this->assertEquals('inline', _class('core_js')->_detect_content('<script>$(function(){})</script>'));
-		$this->assertEquals('inline', _class('core_js')->_detect_content('<script type="text/javascript">$(function(){})</script>'));
-		$this->assertEquals('inline', _class('core_js')->_detect_content('$(function(){
-			$("#element").on("click", function(){})
-		})'));
-		$this->assertEquals('inline', _class('core_js')->_detect_content('$(function(){
-			var url="http://www.google.com/";
-		})'));
-		$this->assertEquals('inline', _class('core_js')->_detect_content('$(function(){})'));
-
-		$f = '/tmp/yf_unit_tests_empty_script.js';
-		file_put_contents($f, 'test');
-		$this->assertEquals('file', _class('core_js')->_detect_content($f));
-		unlink($f);
-	}
 	public function test_complex() {
 		js('//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.3.2/ckeditor.js');
 		$this->assertEquals('<script src="//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.3.2/ckeditor.js" type="text/javascript"></script>', _class('core_js')->show());
