8283ab691048bb6f32c754b370de58d0e02d31cf
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-11-28 13:45:13 +0200

    fixed tinymce version

--- share/assets/tinymce.php
+++ share/assets/tinymce.php
@@ -4,7 +4,7 @@ return array(
 	'versions' => array(
 		'4.1.7' => array(
 			'js' => array(
-				'//cdnjs.cloudflare.com/ajax/libs/tinymce/4.1.7/tiny_mce.js',
+				'//cdnjs.cloudflare.com/ajax/libs/tinymce/4.1.7/tinymce.min.js',
 // ???			'//cdnjs.cloudflare.com/ajax/libs/tinymce/4.1.7/jquery.tinymce.min.js',
 			),
 		),
