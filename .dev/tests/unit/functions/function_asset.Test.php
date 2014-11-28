8d6484b8a5c3013c55b7d8fff2d302f44c3075b8
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-11-28 17:33:34 +0200

    asset unit tests up

--- .dev/tests/unit/functions/function_asset.Test.php
+++ .dev/tests/unit/functions/function_asset.Test.php
@@ -3,14 +3,6 @@
 require_once dirname(__DIR__).'/yf_unit_tests_setup.php';
 
 class function_asset_test extends PHPUnit_Framework_TestCase {
-/*
-// TODO
-	function jquery($content, $params = array()) { return _class('assets')->jquery($content, $params); }
-	function angularjs($content, $params = array()) { return _class('assets')->angularjs($content, $params); }
-	function backbonejs($content, $params = array()) { return _class('assets')->backbonejs($content, $params); }
-	function reactjs($content, $params = array()) { return _class('assets')->reactjs($content, $params); }
-	function emberjs($content, $params = array()) { return _class('assets')->emberjs($content, $params); }
-*/
 	public function test_strip_script_tags() {
 		$this->assertEquals('$(function(){})', _class('assets')->_strip_script_tags('<script>$(function(){})</script>'));
 		$this->assertEquals('$(function(){})', _class('assets')->_strip_script_tags('<script>$(function(){})'));
@@ -68,15 +60,34 @@ class function_asset_test extends PHPUnit_Framework_TestCase {
 		$this->assertEquals( '<script src="'.$url.'" type="text/javascript"></script>'.PHP_EOL.'<script type="text/javascript">'.PHP_EOL.'alert("Hello");'.PHP_EOL.'</script>', _class('assets')->show_js() );
 	}
 	public function test_basic() {
-#		_class('assets')->clean_all();
-#		$url = '//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.3.2/contents.css';
-#		asset('css', $url);
-#		$this->assertEquals('<link href="'.$url.'" rel="stylesheet" />', _class('assets')->show_css());
+		_class('assets')->clean_all();
+		$url = '//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.3.2/contents.css';
+		$expected = '<link href="'.$url.'" rel="stylesheet" />';
+		asset($url, 'css');
+		$this->assertEquals($expected, _class('assets')->show_css());
+		css($url);
+		$this->assertEquals($expected, _class('assets')->show_css());
+		css($url);
+		asset($url, 'css');
+		css($url);
+		$this->assertEquals($expected, _class('assets')->show_css());
 	}
-	public function test_named_asset() {
-#		_class('assets')->clean_all();
-#		asset('blueimp-uploader');
-#		$this->assertEquals('', _class('assets')->show_js());
-#		$this->assertEquals('', _class('assets')->show_css());
+	public function test_bundle() {
+		_class('assets')->clean_all();
+
+		asset('blueimp-uploader');
+
+		$out_js = _class('assets')->show_js();
+		$this->assertNotEmpty($out_js);
+		$this->assertContains('<script', $out_js);
+		$this->assertContains('jquery.min.js', $out_js);
+		$this->assertContains('jquery-ui', $out_js);
+		$this->assertContains('jquery.fileupload', $out_js);
+
+		$out_css = _class('assets')->show_css();
+		$this->assertNotEmpty($out_css);
+		$this->assertContains('<link href="', $out_css);
+		$this->assertContains('jquery-ui.min.css', $out_css);
+		$this->assertContains('jquery.fileupload.css', $out_css);
 	}
 }
