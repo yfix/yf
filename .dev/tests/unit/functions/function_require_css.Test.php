f70be11d149cbaf4bf6ae67f299ce4757b46bfbb
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-11-29 13:32:32 +0200

    unit tests up

--- .dev/tests/unit/functions/function_require_css.Test.php
+++ .dev/tests/unit/functions/function_require_css.Test.php
@@ -4,10 +4,12 @@ require_once dirname(__DIR__).'/yf_unit_tests_setup.php';
 
 class function_require_css_test extends PHPUnit_Framework_TestCase {
 	public function test_complex() {
+		_class('assets')->clean_all();
 		css('//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.3.2/contents.css');
 		$this->assertEquals('<link href="//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.3.2/contents.css" rel="stylesheet" />', _class('core_css')->show());
 	}
 	public function test_params() {
+		_class('assets')->clean_all();
 		css('//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.3.2/contents.css', array('class' => 'yf_core'));
 		$this->assertEquals('<link href="//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.3.2/contents.css" rel="stylesheet" class="yf_core" />', _class('core_css')->show());
 	}
