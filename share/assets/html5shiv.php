afcf7e171255e7dc5ee26f3204d02852c300964b
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-04 12:58:24 +0200

    html5shiv asset updated to match config names before and after

--- share/assets/html5shiv.php
+++ share/assets/html5shiv.php
@@ -7,8 +7,7 @@ return array(
 		),
 	),
 	'config' => array(
-#		<!--[if lt IE 9]><script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js" class="yf_core"></script><![endif]-->
-		'before_tag' => '<!--[if lt IE 9]>',
-		'after_tag' => '<![endif]-->',
+		'before' => '<!--[if lt IE 9]>',
+		'after' => '<![endif]-->',
 	),
 );
