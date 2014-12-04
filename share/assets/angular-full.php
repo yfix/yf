574b839b73fb2243f284edc1616a26024af2064b
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-04 17:58:28 +0200

    assets config cleanup

--- share/assets/angular-full.php
+++ share/assets/angular-full.php
@@ -21,6 +21,8 @@ return array(
 				// '//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.10.0/ui-bootstrap-tpls.min.js',
 				// ng-lib ui-select2 is deprecated: use ui-select
 				'//cdn.rawgit.com/angular-ui/ui-select2/master/src/select2.js',
+				// ng-lib ui-select
+				'//cdn.rawgit.com/angular-ui/ui-select/v0.7.0/dist/select.min.js',
 				// jq-lib select2 (need for ng-lib select2/select)
 				'//cdnjs.cloudflare.com/ajax/libs/select2/3.5.0/select2.min.js',
 				'//cdnjs.cloudflare.com/ajax/libs/select2/3.5.0/select2_locale_ru.min.js',
@@ -29,7 +31,6 @@ return array(
 			    '//cdn.rawgit.com/mgcrea/angular-motion/v0.3.2/dist/angular-motion.min.css',
 				// '//cdn.rawgit.com/mgcrea/angular-motion/master/dist/angular-motion.min.css',
 				// ng-lib ui-select
-				'//cdn.rawgit.com/angular-ui/ui-select/v0.7.0/dist/select.min.js',
 				'//cdn.rawgit.com/angular-ui/ui-select/v0.7.0/dist/select.min.css',
 				// '//cdnjs.cloudflare.com/ajax/libs/selectize.js/0.8.5/css/selectize.default.css',
 				// '//cdnjs.cloudflare.com/ajax/libs/selectize.js/0.8.5/css/selectize.bootstrap2.css',
