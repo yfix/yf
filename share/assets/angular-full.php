9cb85cabd8b54dc35ceb75e8137a7284642c76e3
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-04 12:19:16 +0200

    assets config added bundles: font-awesome3, font-awesome4, angular-full

---
+++ share/assets/angular-full.php
@@ -0,0 +1,42 @@
+<?php
+
+return array(
+	'versions' => array(
+		'1.2.20' => array(
+			'js' => array(
+				'//cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.20/angular.min.js',
+				'//cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.20/angular-route.min.js',
+				'//cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.20/angular-animate.min.js',
+				'//cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.20/angular-sanitize.min.js',
+				'//cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.20/angular-resource.min.js',
+				'//cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.20/angular-touch.min.js',
+// TODO: split this out into separate asset bundles
+				'//cdnjs.cloudflare.com/ajax/libs/angular-i18n/1.2.15/angular-locale_ru.js',
+				// AngularStrap was initially written to provide AngularJS wrapping directives for Twitter Bootstrap
+				'//cdn.rawgit.com/mgcrea/angular-strap/v2.0.0-rc.4/dist/angular-strap.min.js',
+				'//cdn.rawgit.com/mgcrea/angular-strap/v2.0.0-rc.4/dist/angular-strap.tpl.min.js',
+				// '//cdn.rawgit.com/mgcrea/angular-strap/master/dist/angular-strap.min.js',
+				// '//cdn.rawgit.com/mgcrea/angular-strap/master/dist/angular-strap.tpl.min.js',
+				// '//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.10.0/ui-bootstrap.min.js',
+				// '//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.10.0/ui-bootstrap-tpls.min.js',
+				// ng-lib ui-select2 is deprecated: use ui-select
+				'//cdn.rawgit.com/angular-ui/ui-select2/master/src/select2.js',
+				// jq-lib select2 (need for ng-lib select2/select)
+				'//cdnjs.cloudflare.com/ajax/libs/select2/3.5.0/select2.min.js',
+				'//cdnjs.cloudflare.com/ajax/libs/select2/3.5.0/select2_locale_ru.min.js',
+			),
+			'css' => array(
+			    '//cdn.rawgit.com/mgcrea/angular-motion/v0.3.2/dist/angular-motion.min.css',
+				// '//cdn.rawgit.com/mgcrea/angular-motion/master/dist/angular-motion.min.css',
+				// ng-lib ui-select
+				'//cdn.rawgit.com/angular-ui/ui-select/v0.7.0/dist/select.min.js',
+				'//cdn.rawgit.com/angular-ui/ui-select/v0.7.0/dist/select.min.css',
+				// '//cdnjs.cloudflare.com/ajax/libs/selectize.js/0.8.5/css/selectize.default.css',
+				// '//cdnjs.cloudflare.com/ajax/libs/selectize.js/0.8.5/css/selectize.bootstrap2.css',
+				// '//cdnjs.cloudflare.com/ajax/libs/selectize.js/0.8.5/css/selectize.bootstrap3.css',
+				// jq-lib select2 (need for ng-lib select2/select)
+				'//cdnjs.cloudflare.com/ajax/libs/select2/3.5.0/select2.min.css',
+			),
+		),
+	),
+);
