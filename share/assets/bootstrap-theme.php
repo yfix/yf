86ce2d999ebaa17e687b0d3195dbd7ecb42d29e5
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-04 14:06:34 +0200

    assets added complex bundle bootstrap-theme

---
+++ share/assets/bootstrap-theme.php
@@ -0,0 +1,50 @@
+<?php
+
+$bs_theme = common()->bs_current_theme();
+$bs_version = $CONF['css_framework'] === 'bs3' ? '3' : '2';
+$inherit_name = 'bootstrap'. $bs_version;
+
+if ($bs_theme === 'bootstrap') {
+	return array(
+		'inherit' => array(
+			'js' => $inherit_name,
+			'css' => $inherit_name,
+		),
+	);
+} elseif ($bs_theme === 'flatui') {
+	return array(
+		'versions' => array(
+			'master' => array(
+				'css' => array(
+					'//cdn.rawgit.com/yfix/Flat-UI/master/dist/css/vendor/bootstrap.min.css',
+					'//cdn.rawgit.com/yfix/Flat-UI/master/dist/css/flat-ui.min.css',
+				),
+				'js' => array(
+					'//cdn.rawgit.com/yfix/Flat-UI/master/dist/js/flat-ui.min.js',
+				),
+			),
+		)
+	);
+} elseif ($bs_version == 2) {
+	return array(
+		'versions' => array(
+			'2.3.2' => array(
+				'css' => '//netdna.bootstrapcdn.com/bootswatch/2.3.2/'.$bs_theme.'/bootstrap.min.css',
+			),
+		),
+		'require' => array(
+			'js' => 'bootstrap2',
+		),
+	);
+} elseif ($bs_version == 3) {
+	return array(
+		'versions' => array(
+			'3.3.0' => array(
+				'css' => '//netdna.bootstrapcdn.com/bootswatch/3.3.0/'.$bs_theme.'/bootstrap.min.css',
+			),
+		),
+		'require' => array(
+			'js' => 'bootstrap3',
+		),
+	);
+}
