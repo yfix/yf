7de03b37f7089455322d1a58a9d8d7c669d22f44
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-04 17:35:09 +0200

    assets config bootstrap up

--- share/assets/bootstrap.php
+++ share/assets/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 // Virtual package, depends on globally selected bootstap version
-$inherit_name = 'bootstrap'. ($CONF['css_framework'] === 'bs3' ? '3' : '2');
+$inherit_name = 'bootstrap'. (conf('css_framework') === 'bs3' ? '3' : '2');
 return array(
 	'inherit' => array(
 		'js' => $inherit_name,
