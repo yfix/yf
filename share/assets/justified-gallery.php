ba5811d225d7de7b915054ca5b9c7503f0e30b4f
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-05 15:30:25 +0200

    assets bundles more added

---
+++ share/assets/justified-gallery.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+	'versions' => array(
+		'master' => array(
+			'css' => '//cdn.rawgit.com/yfix/Justified-Gallery/master/dist/css/justifiedGallery.min.css',
+			'js' => '//cdn.rawgit.com/yfix/Justified-Gallery/master/dist/js/jquery.justifiedGallery.min.js',
+		),
+	),
+	'require' => array(
+		'js' => 'jquery',
+	),
+);
