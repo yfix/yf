61a4afc0a4d32127be9f7ffd3572b8a3cb7eec96
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-05 15:10:22 +0200

    assets bundles several added

---
+++ share/assets/lightbox2.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+	'versions' => array(
+		'master' => array(
+			'css' => '//cdn.rawgit.com/lightbox2/master/css/lightbox.css',
+			'js' => '//cdn.rawgit.com/lightbox2/master/js/lightbox.min.js',
+		),
+	),
+	'require' => array(
+		'js' => 'jquery',
+	),
+);
