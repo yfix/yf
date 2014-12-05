f260fe19636f51b4755d4a9688165fd088752249
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-05 17:01:48 +0200

    asset bundle bootstrap fixed

--- share/assets/bootstrap.php
+++ share/assets/bootstrap.php
@@ -1,11 +1,7 @@
 <?php
 
-// Virtual package, depends on globally selected bootstap version
-#$inherit_name = 'bootstrap'. (conf('css_framework') === 'bs3' ? '3' : '2');
 return array(
-	'inherit' => array(
-#		'js' => $inherit_name,
-#		'css' => $inherit_name,
+	'require' => array(
 		'js' => 'bootstrap-theme',
 		'css' => 'bootstrap-theme',
 	),
