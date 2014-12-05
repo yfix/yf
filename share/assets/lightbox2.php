55de8027c67a2a1cc1e32ca2e6dfcf8d3096c8e1
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-05 15:14:05 +0200

    assets lightbox2 fixed urls

--- share/assets/lightbox2.php
+++ share/assets/lightbox2.php
@@ -3,8 +3,8 @@
 return array(
 	'versions' => array(
 		'master' => array(
-			'css' => '//cdn.rawgit.com/lightbox2/master/css/lightbox.css',
-			'js' => '//cdn.rawgit.com/lightbox2/master/js/lightbox.min.js',
+			'css' => '//cdn.rawgit.com/yfix/lightbox2/master/css/lightbox.css',
+			'js' => '//cdn.rawgit.com/yfix/lightbox2/master/js/lightbox.min.js',
 		),
 	),
 	'require' => array(
