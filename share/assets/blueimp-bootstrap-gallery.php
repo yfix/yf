239f278dd492de9213c30b2b3cb3f3c92626cd8e
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-05 14:55:50 +0200

    assets bundles setup up

---
+++ share/assets/blueimp-bootstrap-gallery.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+	'versions' => array(
+		'master' => array(
+			'css' => '//cdn.rawgit.com/yfix/Bootstrap-Image-Gallery/master/css/bootstrap-image-gallery.min.css',
+			'js' => '//cdn.rawgit.com/yfix/Bootstrap-Image-Gallery/master/js/bootstrap-image-gallery.min.js',
+		),
+	),
+	'require' => array(
+		'css' => array(
+			'bootstrap',
+			'blueimp-gallery',
+		),
+		'js' => array(
+			'bootstrap',
+			'blueimp-gallery',
+		),
+	),
+);
---
+++ share/assets/blueimp-load-image.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+	'versions' => array(
+		'master' => array(
+			'js' => '//cdn.rawgit.com/yfix/JavaScript-Load-Image/master/js/load-image.all.min.js',
+		),
+	),
+	'require' => array(
+		'js' => 'jquery',
+	),
+);
