3b03eb1e20d66233c7625a9b0401f1d0028f8374
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-05 17:39:26 +0200

    up

--- share/assets/angular-locale.php
+++ share/assets/angular-locale.php
@@ -3,7 +3,7 @@
 $locale = conf('language');
 return array(
 	'versions' => array(
-		'1.2.20' => array(
+		'1.2.15' => array(
 			'js' => '//cdnjs.cloudflare.com/ajax/libs/angular-i18n/1.2.15/angular-locale_'.$locale.'.js',
 		),
 	),
