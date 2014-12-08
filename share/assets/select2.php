cedf624c557fcffec560022882d958658c58b188
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-08 12:04:43 +0200

    asset select2 fix for missing en locale

--- share/assets/select2.php
+++ share/assets/select2.php
@@ -6,7 +6,7 @@ return array(
 		'3.4.6' => array(
 			'js' => array(
 				'//cdnjs.cloudflare.com/ajax/libs/select2/3.4.6/select2.min.js',
-				'//cdnjs.cloudflare.com/ajax/libs/select2/3.4.6/select2_locale_'.$locale.'.min.js',
+				$locale !== 'en' ? '//cdnjs.cloudflare.com/ajax/libs/select2/3.4.6/select2_locale_'.$locale.'.min.js' : '',
 			),
 			'css' => array(
 				'//cdnjs.cloudflare.com/ajax/libs/select2/3.4.6/select2.min.css',
@@ -15,7 +15,7 @@ return array(
 		'3.5.2' => array(
 			'js' => array(
 				'//cdnjs.cloudflare.com/ajax/libs/select2/3.5.2/select2.min.js',
-				'//cdnjs.cloudflare.com/ajax/libs/select2/3.5.2/select2_locale_'.$locale.'.min.js',
+				$locale !== 'en' ? '//cdnjs.cloudflare.com/ajax/libs/select2/3.5.2/select2_locale_'.$locale.'.min.js' : '',
 			),
 			'css' => array(
 				'//cdnjs.cloudflare.com/ajax/libs/select2/3.5.2/select2.min.css',
