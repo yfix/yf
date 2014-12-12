665efdbba46aea93553175d0870fa073b09c7d93
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-12 14:04:25 +0200

    assets added typeaheadjs

---
+++ share/assets/typeaheadjs.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+	'versions' => array(
+		'master' => array(
+			'js' => '//cdn.rawgit.com/yfix/typeahead.js/dist/typeahead.jquery.js',
+		),
+	),
+	'require' => array(
+		'js' => 'jquery',
+	),
+);
