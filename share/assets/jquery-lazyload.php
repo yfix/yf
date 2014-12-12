6bdef766f6f717a6c1573c1962b513510d74f613
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-12 14:03:24 +0200

    assets added jquery lazyload

---
+++ share/assets/jquery-lazyload.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+	'versions' => array(
+		'master' => array(
+			'js' => '//cdn.rawgit.com/yfix/jquery_lazyload/jquery.lazyload.js',
+		),
+	),
+	'require' => array(
+		'js' => 'jquery',
+	),
+);
