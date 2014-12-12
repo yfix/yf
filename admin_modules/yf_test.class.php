cb153c14f0e0a4d1dea2ca7e672ff10d61cac7b2
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-12 16:22:08 +0200

    admin test class cleanup

--- admin_modules/yf_test.class.php
+++ admin_modules/yf_test.class.php
@@ -2,11 +2,5 @@
 
 class yf_test {
 	function show() {
-		return form2()->file_uploader('test');
-	}
-	
-	function ajax_file_uploader() {
-		_class('form2_file_handler','classes/form2/')->process();
-		exit;
 	}
 }
