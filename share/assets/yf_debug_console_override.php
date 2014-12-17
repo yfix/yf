0a3bff450574f7f8b1aeeecc0c5636a1044d20f1
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-17 14:05:19 +0200

    assets for debug updated

--- share/assets/yf_debug_console_override.php
+++ share/assets/yf_debug_console_override.php
@@ -1,19 +1,23 @@
 <?php
 
 return DEBUG_MODE ? function() {
-// TODO: replace with current links got from assets bootstrap2, font-awesome3 and jquery
-	$js = '
-	    var debug_console_override_head = [
-	        \'<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>\',
-    	    \'<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">\',
-	        \'<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>\',
-    	    \'<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css" rel="stylesheet">\',
-			\'<link href="//bootswatch.com/2/slate/bootstrap.css" rel="stylesheet">\'
-    	];
-	';
+	$assets = _class('assets');
+	$bs2_slate_css = '//netdna.bootstrapcdn.com/bootswatch/2.3.2/slate/bootstrap.min.css';
 	return array(
 		'versions' => array('master' => array(
-#			'js' => $js,
+			'js' => array(
+				'content' => '
+					var debug_console_override_head = [
+						\'<l\' + \'ink href="'.$bs2_slate_css.'" rel="stylesheet">\',
+						\'<l\' + \'ink href="'.$assets->get_asset('font-awesome3', 'css').'" rel="stylesheet">\',
+						\'<sc\' + \'ript src="'.$assets->get_asset('jquery', 'js').'"></sc\' + \'ript>\',
+						\'<sc\' + \'ript src="'.$assets->get_asset('bootstrap2', 'js').'"></sc\' + \'ript>\'
+					];
+				',
+				'params' => array(
+					'class' => 'yf_debug_console_asset',
+				),
+			),
 		)),
 	);
 } : null;
