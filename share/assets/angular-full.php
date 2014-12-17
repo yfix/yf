992f25e7c6c4f0c35c4e813fe268174343279361
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-17 17:36:47 +0200

    assets cleanup

--- share/assets/angular-full.php
+++ share/assets/angular-full.php
@@ -15,9 +15,22 @@ return array(
 			'angular-select',
 		),
 		'css' => array(
-			'angular-motion',
 			'angular-select2',
 			'angular-select',
 		),
+/*
+		'assets' => array(
+			'angularjs',
+			'angular-locale',
+			'angular-route',
+			'angular-animate',
+			'angular-sanitize',
+			'angular-resource',
+			'angular-touch',
+			'angular-strap',
+			'angular-select2',
+			'angular-select',
+		),
+*/
 	),
 );
--- share/assets/angular-strap.php
+++ share/assets/angular-strap.php
@@ -8,6 +8,7 @@ return array(
 				'//cdn.rawgit.com/mgcrea/angular-strap/v2.0.0-rc.4/dist/angular-strap.min.js',
 				'//cdn.rawgit.com/mgcrea/angular-strap/v2.0.0-rc.4/dist/angular-strap.tpl.min.js',
 			),
+			'css' => '//cdn.rawgit.com/mgcrea/angular-motion/v0.3.2/dist/angular-motion.min.css',
 		),
 	),
 	'require' => array(
