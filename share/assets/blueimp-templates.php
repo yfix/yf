087890cb12397dc670e55338124b4226ea802651
ser.korzhikov <ser.korzhikov@gmail.com>
2014-12-18 16:51:04 +0200

    fix blueimp assets

--- share/assets/blueimp-templates.php
+++ share/assets/blueimp-templates.php
@@ -3,7 +3,12 @@
 return array(
 	'versions' => array(
 		'master' => array(
-			'js' => '//cdn.rawgit.com/yfix/JavaScript-Templates/master/js/tmpl.min.js',
+			'js' => array(
+				'content' => '//cdn.rawgit.com/yfix/JavaScript-Templates/master/js/tmpl.min.js',
+				'params' => array(
+					'out_before' => 'blueimp-gallery',
+				),
+			),
 		),
 	),
 	'require' => array(
