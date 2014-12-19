8d4e7568c1ac42c20879961f45937052fb3e6cee
ser.korzhikov <ser.korzhikov@gmail.com>
2014-12-19 15:33:10 +0200

    change blueimp gallery js url in assets

--- share/assets/blueimp-gallery.php
+++ share/assets/blueimp-gallery.php
@@ -4,7 +4,7 @@ return array(
 	'versions' => array(
 		'master' => array(
 			'css' => '//cdn.rawgit.com/yfix/blueimp-gallery/master/css/blueimp-gallery.min.css',
-			'js' => '//cdn.rawgit.com/yfix/blueimp-gallery/master/js/blueimp-gallery.min.js',
+			'js' => '//cdn.rawgit.com/yfix/blueimp-gallery/master/js/jquery.blueimp-gallery.min.js',
 		),
 	),
 	'require' => array(
