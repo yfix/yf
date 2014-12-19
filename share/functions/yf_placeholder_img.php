942d27592fb6e3be0d9456df3205478341764258
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-19 14:36:28 +0200

    placeholder img function added support for colors and font min, max sizes

--- share/functions/yf_placeholder_img.php
+++ share/functions/yf_placeholder_img.php
@@ -3,10 +3,10 @@
 /**
 * Simple replacement for http://placehold.it/
 */
-function yf_placeholder_img($w = 100, $h = 100, $text = '', $params = array()) {
+function yf_placeholder_img($w = 100, $h = 100, $params = array()) {
 	$w = (int)$w ?: 100;
 	$h = (int)$h ?: 100;
-	$text = $text ?: $w.' x '.$h;
+	$text = $params['text'] ?: $w.' x '.$h;
 
 	$font_try_paths = $params['font_paths'] ?: array(
 		'/usr/share/fonts/',
@@ -25,18 +25,32 @@ function yf_placeholder_img($w = 100, $h = 100, $text = '', $params = array()) {
 	}
 	$im = imagecreatetruecolor($w, $h);
 
-	$color_text = imagecolorallocate($im, 80, 80, 80);
-	$color_bg = imagecolorallocate($im, 160, 160, 160);
+	$ctext = $params['color_text'] ?: '777';
+	$cbg = $params['color_bg'] ?: 'bbb';
 
-	$font_size = $params['font_size'] ?: 12;
+	strlen($ctext) === 3 && $ctext = str_repeat(substr($ctext, 0, 1), 2). str_repeat(substr($ctext, 1, 1), 2). str_repeat(substr($ctext, 2, 1), 2);
+	strlen($cbg) === 3 && $cbg = str_repeat(substr($cbg, 0, 1), 2). str_repeat(substr($cbg, 1, 1), 2). str_repeat(substr($cbg, 2, 1), 2);
+	$color_text = imagecolorallocate($im, hexdec(substr($ctext, 0, 2)), hexdec(substr($ctext, 2, 2)), hexdec(substr($ctext, 4, 2)));
+	$color_bg = imagecolorallocate($im, hexdec(substr($cbg, 0, 2)), hexdec(substr($cbg, 2, 2)), hexdec(substr($cbg, 4, 2)));
+
+	$min_size = $params['min_size'] ?: 5;
+	$max_size = $params['max_size'] ?: 14;
+	$def_size = ceil($w / 10) + 1;
+	if ($def_size < $min_size) {
+		$def_size = $min_size;
+	}
+	if ($def_size > $max_size) {
+		$def_size = $max_size;
+	}
+	$font_size = $params['font_size'] ?: $def_size;
 	$font_angle = $params['font_angle'] ?: 0;
 
 	// Set the background
 	imagefilledrectangle($im, 0, 0, $w, $h, $color_bg);
 
 	$bbox = imagettfbbox($font_size, $font_angle, $font, $text);
-	$x = $bbox[0] + (imagesx($im) / 2) - ($bbox[4] / 2) - 5;
-	$y = $bbox[1] + (imagesy($im) / 2) - ($bbox[5] / 2) - 5;
+	$x = $bbox[0] + (imagesx($im) / 2) - ($bbox[4] / 2) - $font_size / 2 + 3;
+	$y = $bbox[1] + (imagesy($im) / 2) - ($bbox[5] / 2) - $font_size / 2 + 1;
 	imagettftext($im, $font_size, $font_angle, $x, $y, $color_text, $font, $text);
 
 	ob_start();
