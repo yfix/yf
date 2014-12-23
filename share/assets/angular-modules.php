85e98d508bb28cac57c3d64eef60f571b5f05dc8
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-23 16:10:41 +0200

    assets added

---
+++ share/assets/angular-modules.php
@@ -0,0 +1,15 @@
+<?php
+
+$content = <<<EOS
+var __angular_modules__ = [];
+EOS;
+
+return array(
+	'versions' => array(
+		'master' => array(
+			'js' => array(
+				'content' => $content,
+			),
+		),
+	),
+);
