cac6875bd1dca6b68a5fe18c85b7a06d9441eff7
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-28 19:29:56 +0200

    samples up

--- .dev/samples/apns_php.php
+++ .dev/samples/apns_php.php
@@ -1,9 +1,22 @@
+#!/usr/bin/php
 <?php
 
-# git clone git@github.com:yfix/ApnsPHP.git /home/www/yf/libs/apns_php/
-
 define('YF_PATH', dirname(dirname(__DIR__)).'/');
+$libs_root = YF_PATH.'libs/';
 
-require_once YF_PATH . 'libs/apns_php/ApnsPHP/Autoload.php';
-$server = new ApnsPHP_Push_Server(ApnsPHP_Abstract::ENVIRONMENT_SANDBOX, 'server_certificates_bundle_sandbox.pem');
-var_dump($server);
+$git_urls = array(
+	'https://github.com/yfix/ApnsPHP.git' => $libs_root. 'apns_php/',
+);
+foreach ($git_urls as $git_url => $lib_dir) {
+	if (!file_exists($lib_dir.'.git')) {
+		passthru('git clone --depth 1 '.$git_url.' '.$lib_dir);
+	}
+}
+
+require_once $lib_dir. 'ApnsPHP/Autoload.php';
+
+// Test mode when direct call
+if (realpath($argv[0]) === realpath(__FILE__)) {
+	$server = new ApnsPHP_Push_Server(ApnsPHP_Abstract::ENVIRONMENT_SANDBOX, 'server_certificates_bundle_sandbox.pem');
+	var_dump($server);
+}
