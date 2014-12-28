acb9d1ffbc1ebb050e092279b018c54e4e766399
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-28 19:30:57 +0200

    samples up

--- .dev/samples/sf_dom_crawler.php
+++ .dev/samples/sf_dom_crawler.php
@@ -1,19 +1,40 @@
+#!/usr/bin/php
 <?php
 
-# git clone git@github.com:yfix/DomCrawler.git /home/www/yf/libs/sf_dom_crawler/
 define('YF_PATH', dirname(dirname(__DIR__)).'/');
+$libs_root = YF_PATH.'libs/';
 
-spl_autoload_register(function($class){
-	$lib_root = YF_PATH.'libs/sf_dom_crawler/';
-	$prefix = 'Symfony\Component\DomCrawler';
-	if (strpos($class, $prefix) === 0) {
+$git_urls = array(
+	'https://github.com/yfix/DomCrawler.git' => $libs_root. 'sf_dom_crawler/',
+);
+foreach ($git_urls as $git_url => $lib_dir) {
+	if (!file_exists($lib_dir.'.git')) {
+		passthru('git clone --depth 1 '.$git_url.' '.$lib_dir);
+	}
+}
+$config = array(
+	$libs_root. 'sf_dom_crawler/' => 'Symfony\Component\DomCrawler',
+);
+spl_autoload_register(function($class) use ($config) {
+#	echo '=='.$class .PHP_EOL;
+	foreach ($config as $lib_root => $prefix) {
+		if (strpos($class, $prefix) !== 0) {
+			continue;
+		}
 		$path = $lib_root. str_replace("\\", '/', substr($class, strlen($prefix) + 1)).'.php';
+		if (!file_exists($path)) {
+			continue;
+		}
 #		echo $path.PHP_EOL;
 		include $path;
+		return true;
 	}
 });
 
-$crawler = new \Symfony\Component\DomCrawler\Crawler();
-$crawler->addContent('<html><body><p>Hello World!</p></body></html>');
-echo $crawler->filterXPath('descendant-or-self::body/p')->text() . PHP_EOL;
-#print $crawler->filter('body > p')->text(); // require css selector
+// Test mode when direct call
+if (realpath($argv[0]) === realpath(__FILE__)) {
+	$crawler = new \Symfony\Component\DomCrawler\Crawler();
+	$crawler->addContent('<html><body><p>Hello World!</p></body></html>');
+	echo $crawler->filterXPath('descendant-or-self::body/p')->text() . PHP_EOL;
+//	print $crawler->filter('body > p')->text(); // require css selector
+}
