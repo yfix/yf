c4ea8580dee952c6d6936d6028d51124f8904b3d
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-28 18:05:21 +0200

    sample for sf_dom_crawler added

---
+++ .dev/samples/sf_dom_crawler.php
@@ -0,0 +1,19 @@
+<?php
+
+# git clone git@github.com:yfix/DomCrawler.git /home/www/yf/libs/sf_dom_crawler/
+define('YF_PATH', dirname(dirname(__DIR__)).'/');
+
+spl_autoload_register(function($class){
+	$lib_root = YF_PATH.'libs/sf_dom_crawler/';
+	$prefix = 'Symfony\Component\DomCrawler';
+	if (strpos($class, $prefix) === 0) {
+		$path = $lib_root. str_replace("\\", '/', substr($class, strlen($prefix) + 1)).'.php';
+		echo $path.PHP_EOL;
+		include $path;
+	}
+});
+
+$crawler = new \Symfony\Component\DomCrawler\Crawler();
+$crawler->addContent('<html><body><p>Hello World!</p></body></html>');
+echo $crawler->filterXPath('descendant-or-self::body/p')->text() . PHP_EOL;
+#print $crawler->filter('body > p')->text(); // require css selector
