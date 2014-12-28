5fa7ddc8a8f52084b424614a96535d586f46b4a9
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-28 16:21:53 +0200

    sf yaml sample now working

--- .dev/samples/sf_yaml.php
+++ .dev/samples/sf_yaml.php
@@ -2,14 +2,28 @@
 
 define('YF_PATH', dirname(dirname(__DIR__)).'/');
 $libs_root = YF_PATH.'libs';
+/*
 require_once $libs_root.'/sf_class_loader/UniversalClassLoader.php';
 $loader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
 $loader->registerNamespaces(array(
 	'Symfony\Component\Yaml'	=> $libs_root.'/sf_yaml',
 ));
 $loader->register();
-
-require_once $libs_root.'/sf_yaml/Yaml.php';
+*/
+$files = array(
+	'Exception/ExceptionInterface.php',
+	'Exception/RuntimeException.php',
+	'Exception/ParseException.php',
+	'Inline.php',
+	'Parser.php',
+	'Dumper.php',
+	'Escaper.php',
+	'Unescaper.php',
+	'Yaml.php',
+);
+foreach ($files as $file) {
+	require_once $libs_root. '/sf_yaml/'. $file;
+}
 
 function yaml_parse($input) {
 	return \Symfony\Component\Yaml\Yaml::parse($input);
@@ -18,14 +32,22 @@ function yaml_dump($yaml) {
 	return \Symfony\Component\Yaml\Yaml::dump($yaml);
 }
 
-var_dump(yaml_parse('
-    - apple
-    - banana
-    - carrot
-'));
+$yaml_str = '
+receipt:     Oz-Ware Purchase Invoice
+date:        2012-08-06
+customer:
+    given:   Dorothy
+    family:  Gale
+';
 
-var_dump(yaml_dump(array(
-	'apple',
-	'banana',
-	'carrot',
-)));
+$php_array = array(
+	'receipt' => 'Oz-Ware Purchase Invoice',
+	'date' => 1344200400,
+	'customer' => array(
+		'given' => 'Dorothy',
+		'family' => 'Gale',
+	),
+);
+
+var_export(yaml_parse($yaml_str));
+var_dump(yaml_dump($php_array));
