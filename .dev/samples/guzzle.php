52e43308910f9f3f76dad80fd8f76fd727dec1d0
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-28 19:30:14 +0200

    samples guzzle up

--- .dev/samples/guzzle.php
+++ .dev/samples/guzzle.php
@@ -1,20 +1,27 @@
+#!/usr/bin/php
 <?php
 
-# git clone git@github.com:yfix/guzzle.git /home/www/yf/libs/guzzle/
-# git clone git@github.com:yfix/guzzle-ring.git /home/www/yf/libs/guzzle-ring/
-# git clone git@github.com:yfix/guzzle-streams.git /home/www/yf/libs/guzzle-streams/
-# git clone git@github.com:yfix/promise.git /home/www/yf/libs/promise/
-define('YF_PATH', dirname(dirname(__DIR__)).'/');
+ob_start();
+require_once __DIR__.'/promise.php';
+ob_end_clean();
 
+define('YF_PATH', dirname(dirname(__DIR__)).'/');
 $libs_root = YF_PATH.'libs/';
+$git_urls = array(
+	'https://github.com/yfix/guzzle.git' => $libs_root. 'guzzle/',
+	'https://github.com/yfix/guzzle-ring.git' => $libs_root. 'guzzle-ring/',
+	'https://github.com/yfix/guzzle-streams.git' => $libs_root. 'guzzle-streams/',
+);
+foreach ($git_urls as $git_url => $lib_dir) {
+	if (!file_exists($lib_dir.'.git')) {
+		passthru('git clone --depth 1 '.$git_url.' '.$lib_dir);
+	}
+}
 $config = array(
 	$libs_root. 'guzzle/src/' => 'GuzzleHttp',
 	$libs_root. 'guzzle-ring/src/' => 'GuzzleHttp\Ring',
 	$libs_root. 'guzzle-streams/src/' => 'GuzzleHttp\Stream',
-	$libs_root. 'promise/src/' => 'React\Promise',
 );
-require_once $libs_root. 'promise/src/functions.php';
-
 spl_autoload_register(function($class) use ($config) {
 #	echo '=='.$class .PHP_EOL;
 	foreach ($config as $lib_root => $prefix) {
@@ -31,8 +38,11 @@ spl_autoload_register(function($class) use ($config) {
 	}
 });
 
-$client = new GuzzleHttp\Client();
-$res = $client->get('http://google.com');
-echo $res->getStatusCode(). PHP_EOL;
-echo $res->getHeader('content-type'). PHP_EOL;
-echo $res->getBody(). PHP_EOL;
+// Test mode when direct call
+if (realpath($argv[0]) === realpath(__FILE__)) {
+	$client = new GuzzleHttp\Client();
+	$res = $client->get('http://google.com');
+	echo $res->getStatusCode(). PHP_EOL;
+	echo $res->getHeader('content-type'). PHP_EOL;
+	echo $res->getBody(). PHP_EOL;
+}
