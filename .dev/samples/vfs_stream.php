6c173443d9a43acdc5a51e12bfd709c0f1b751a7
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-28 19:31:28 +0200

    samples up

--- .dev/samples/vfs_stream.php
+++ .dev/samples/vfs_stream.php
@@ -1,18 +1,39 @@
+#!/usr/bin/php
 <?php
 
-# git clone git@github.com:yfix/vfsStream.git /home/www/yf/libs/vfs_stream/
-
 define('YF_PATH', dirname(dirname(__DIR__)).'/');
+$libs_root = YF_PATH.'libs/';
 
-spl_autoload_register(function($class){
-	$lib_root = YF_PATH.'libs/vfs_stream/src/main/php/';
-	$prefix = 'org\bovigo\vfs';
-	if (strpos($class, $prefix) === 0) {
+$git_urls = array(
+	'https://github.com/yfix/vfsStream.git' => $libs_root. 'vfs_stream/',
+);
+foreach ($git_urls as $git_url => $lib_dir) {
+	if (!file_exists($lib_dir.'.git')) {
+		passthru('git clone --depth 1 '.$git_url.' '.$lib_dir);
+	}
+}
+$config = array(
+	$libs_root. 'vfs_stream/src/main/php/' => 'org\bovigo\vfs',
+);
+spl_autoload_register(function($class) use ($config) {
+#	echo '=='.$class .PHP_EOL;
+	foreach ($config as $lib_root => $prefix) {
+		if (strpos($class, $prefix) !== 0) {
+			continue;
+		}
+#		$path = $lib_root. str_replace("\\", '/', substr($class, strlen($prefix) + 1)).'.php';
 		$path = $lib_root. str_replace("\\", '/', $class).'.php';
-#		echo $path. PHP_EOL;
+		if (!file_exists($path)) {
+			continue;
+		}
+#		echo $path.PHP_EOL;
 		include $path;
+		return true;
 	}
 });
 
-$vdir = org\bovigo\vfs\vfsStream::setup('example_dir');
-var_dump($vdir);
+// Test mode when direct call
+if (realpath($argv[0]) === realpath(__FILE__)) {
+	$vdir = org\bovigo\vfs\vfsStream::setup('example_dir');
+	var_dump($vdir);
+}
