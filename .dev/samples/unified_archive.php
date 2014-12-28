a0120227b81589cc72fa0c65e2ef4076f59e1172
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-28 19:31:20 +0200

    samples up

--- .dev/samples/unified_archive.php
+++ .dev/samples/unified_archive.php
@@ -1,16 +1,38 @@
+#!/usr/bin/php
 <?php
 
 define('YF_PATH', dirname(dirname(__DIR__)).'/');
+$libs_root = YF_PATH.'libs/';
 
-spl_autoload_register(function($class){
-	$lib_root = YF_PATH.'libs/php-unified-archive/src/';
-	$prefix = 'wapmorgan\UnifiedArchive';
-	if (strpos($class, $prefix) === 0) {
+$git_urls = array(
+	'https://github.com/yfix/php-unified-archive.git' => $libs_root. 'php-unified-archive/',
+);
+foreach ($git_urls as $git_url => $lib_dir) {
+	if (!file_exists($lib_dir.'.git')) {
+		passthru('git clone --depth 1 '.$git_url.' '.$lib_dir);
+	}
+}
+$config = array(
+	$libs_root. 'php-unified-archive/src/' => 'wapmorgan\UnifiedArchive',
+);
+spl_autoload_register(function($class) use ($config) {
+#	echo '=='.$class .PHP_EOL;
+	foreach ($config as $lib_root => $prefix) {
+		if (strpos($class, $prefix) !== 0) {
+			continue;
+		}
 		$path = $lib_root. str_replace("\\", '/', substr($class, strlen($prefix) + 1)).'.php';
+		if (!file_exists($path)) {
+			continue;
+		}
 #		echo $path.PHP_EOL;
 		include $path;
+		return true;
 	}
 });
 
-$out = \wapmorgan\UnifiedArchive\UnifiedArchive::archiveNodes('./form2', 'samples_archive.zip', $fake = true);
-var_export($out);
+// Test mode when direct call
+if (realpath($argv[0]) === realpath(__FILE__)) {
+	$out = \wapmorgan\UnifiedArchive\UnifiedArchive::archiveNodes('./form2', 'samples_archive.zip', $fake = true);
+	var_export($out);
+}
