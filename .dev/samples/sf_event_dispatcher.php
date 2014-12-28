afb09855de8719b3e70f8ff08f2e55533d3a494f
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-28 19:59:06 +0200

    samples added sf_event_dispatcher

---
+++ .dev/samples/sf_event_dispatcher.php
@@ -0,0 +1,38 @@
+#!/usr/bin/php
+<?php
+
+define('YF_PATH', dirname(dirname(__DIR__)).'/');
+$libs_root = YF_PATH.'libs/';
+
+$git_urls = array(
+	'https://github.com/symfony/EventDispatcher.git' => $libs_root. 'sf_event_dispatcher/',
+);
+foreach ($git_urls as $git_url => $lib_dir) {
+	if (!file_exists($lib_dir.'.git')) {
+		passthru('git clone --depth 1 '.$git_url.' '.$lib_dir);
+	}
+}
+$config = array(
+	$libs_root. 'sf_event_dispatcher/' => 'Symfony\Component\EventDispatcher',
+);
+spl_autoload_register(function($class) use ($config) {
+#	echo '=='.$class .PHP_EOL;
+	foreach ($config as $lib_root => $prefix) {
+		if (strpos($class, $prefix) !== 0) {
+			continue;
+		}
+		$path = $lib_root. str_replace("\\", '/', substr($class, strlen($prefix) + 1)).'.php';
+		if (!file_exists($path)) {
+			continue;
+		}
+#		echo $path.PHP_EOL;
+		include $path;
+		return true;
+	}
+});
+
+// Test mode when direct call
+if (realpath($argv[0]) === realpath(__FILE__)) {
+	$dispatcher = new \Symfony\Component\EventDispatcher\EventDispatcher();
+	var_dump($dispatcher);
+}
