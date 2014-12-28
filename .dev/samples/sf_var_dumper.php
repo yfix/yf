322d2d59b805e4d837aaa03564393465155e1a1d
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-28 19:31:07 +0200

    samples up

--- .dev/samples/sf_var_dumper.php
+++ .dev/samples/sf_var_dumper.php
@@ -1,15 +1,33 @@
+#!/usr/bin/php
 <?php
 
-# git clone git@github.com:yfix/var-dumper.git /home/www/yf/libs/sf_var_dumper/
 define('YF_PATH', dirname(dirname(__DIR__)).'/');
+$libs_root = YF_PATH.'libs/';
 
-spl_autoload_register(function($class){
-	$lib_root = YF_PATH.'libs/sf_var_dumper/';
-	$prefix = 'Symfony\Component\VarDumper';
-	if (strpos($class, $prefix) === 0) {
+$git_urls = array(
+	'https://github.com/yfix/var-dumper.git' => $libs_root. 'sf_var_dumper/',
+);
+foreach ($git_urls as $git_url => $lib_dir) {
+	if (!file_exists($lib_dir.'.git')) {
+		passthru('git clone --depth 1 '.$git_url.' '.$lib_dir);
+	}
+}
+$config = array(
+	$libs_root. 'sf_var_dumper/' => 'Symfony\Component\VarDumper',
+);
+spl_autoload_register(function($class) use ($config) {
+#	echo '=='.$class .PHP_EOL;
+	foreach ($config as $lib_root => $prefix) {
+		if (strpos($class, $prefix) !== 0) {
+			continue;
+		}
 		$path = $lib_root. str_replace("\\", '/', substr($class, strlen($prefix) + 1)).'.php';
+		if (!file_exists($path)) {
+			continue;
+		}
 #		echo $path.PHP_EOL;
 		include $path;
+		return true;
 	}
 });
 
@@ -19,4 +37,7 @@ function dump($var) {
 	}
 }
 
-dump($_SERVER);
+// Test mode when direct call
+if (realpath($argv[0]) === realpath(__FILE__)) {
+	dump($_SERVER);
+}
