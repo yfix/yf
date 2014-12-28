e9a0fcbd45c693c6c97f0268f7e3ca37ab4b43c6
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-28 19:30:52 +0200

    samples up

--- .dev/samples/sf_css_selector.php
+++ .dev/samples/sf_css_selector.php
@@ -1,17 +1,38 @@
+#!/usr/bin/php
 <?php
 
-# git clone git@github.com:yfix/CssSelector.git /home/www/yf/libs/sf_css_selector/
 define('YF_PATH', dirname(dirname(__DIR__)).'/');
+$libs_root = YF_PATH.'libs/';
 
-spl_autoload_register(function($class){
-	$lib_root = YF_PATH.'libs/sf_css_selector/';
-	$prefix = 'Symfony\Component\CssSelector';
-	if (strpos($class, $prefix) === 0) {
+$git_urls = array(
+	'https://github.com/yfix/CssSelector.git' => $libs_root. 'sf_css_selector/',
+);
+foreach ($git_urls as $git_url => $lib_dir) {
+	if (!file_exists($lib_dir.'.git')) {
+		passthru('git clone --depth 1 '.$git_url.' '.$lib_dir);
+	}
+}
+$config = array(
+	$libs_root. 'sf_css_selector/' => 'Symfony\Component\CssSelector',
+);
+spl_autoload_register(function($class) use ($config) {
+#	echo '=='.$class .PHP_EOL;
+	foreach ($config as $lib_root => $prefix) {
+		if (strpos($class, $prefix) !== 0) {
+			continue;
+		}
 		$path = $lib_root. str_replace("\\", '/', substr($class, strlen($prefix) + 1)).'.php';
+		if (!file_exists($path)) {
+			continue;
+		}
 #		echo $path.PHP_EOL;
 		include $path;
+		return true;
 	}
 });
 
-print \Symfony\Component\CssSelector\CssSelector::toXPath('div.item > h4 > a');
-print PHP_EOL;
+// Test mode when direct call
+if (realpath($argv[0]) === realpath(__FILE__)) {
+	print \Symfony\Component\CssSelector\CssSelector::toXPath('div.item > h4 > a');
+	print PHP_EOL;
+}
