bc97056ff18e0b6dd530f925cc5c0679ecb4ae38
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-28 19:30:35 +0200

    samples up

--- .dev/samples/promise.php
+++ .dev/samples/promise.php
@@ -1,18 +1,41 @@
+#!/usr/bin/php
 <?php
 
-# git clone git@github.com:yfix/promise.git /home/www/yf/libs/promise/
 define('YF_PATH', dirname(dirname(__DIR__)).'/');
+$libs_root = YF_PATH.'libs/';
 
-spl_autoload_register(function($class){
-	$lib_root = YF_PATH.'libs/promise/src/';
-	$prefix = 'React\Promise';
-	if (strpos($class, $prefix) === 0) {
+$git_urls = array(
+	'https://github.com/yfix/promise.git' => $libs_root. 'promise/',
+);
+foreach ($git_urls as $git_url => $lib_dir) {
+	if (!file_exists($lib_dir.'.git')) {
+		passthru('git clone --depth 1 '.$git_url.' '.$lib_dir);
+	}
+}
+$config = array(
+	$libs_root. 'promise/src/' => 'React\Promise',
+);
+spl_autoload_register(function($class) use ($config) {
+#	echo '=='.$class .PHP_EOL;
+	foreach ($config as $lib_root => $prefix) {
+		if (strpos($class, $prefix) !== 0) {
+			continue;
+		}
 		$path = $lib_root. str_replace("\\", '/', substr($class, strlen($prefix) + 1)).'.php';
-		echo $path.PHP_EOL;
+		if (!file_exists($path)) {
+			continue;
+		}
+#		echo $path.PHP_EOL;
 		include $path;
+		return true;
 	}
 });
 
-$deferred = new React\Promise\Deferred();
-$promise = $deferred->promise();
-var_dump($promise);
+require_once $libs_root. 'promise/src/functions.php';
+
+// Test mode when direct call
+if (realpath($argv[0]) === realpath(__FILE__)) {
+	$deferred = new \React\Promise\Deferred();
+	$promise = $deferred->promise();
+	var_dump($promise);
+}
