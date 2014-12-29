7a60520d8d2e856fa15b413a929753aebd5c330e
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-29 21:37:54 +0200

    service providers added php-opencloud

---
+++ share/service_providers/php_opencloud.php
@@ -0,0 +1,16 @@
+#!/usr/bin/php
+<?php
+
+$requires = array('guzzle3');
+$git_urls = array('https://github.com/rackspace/php-opencloud.git' => 'php-opencloud/');
+$autoload_config = array('php-opencloud/lib/OpenCloud/' => 'OpenCloud');
+require __DIR__.'/_config.php';
+
+// Test mode when direct call
+if (!$_SERVER['REQUEST_METHOD'] && realpath($argv[0]) === realpath(__FILE__)) {
+	$client = new \OpenCloud\Rackspace(\OpenCloud\Rackspace::US_IDENTITY_ENDPOINT, array(
+	    'username' => 'foo',
+	    'apiKey'   => 'bar'
+	));
+	var_dump($client);
+}
