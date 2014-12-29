e6b51cffd6a025859a902b6ab93e0e0063065303
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2014-12-29 18:25:47 +0200

    service providers added

---
+++ share/service_providers/psr_log.php
@@ -0,0 +1,14 @@
+#!/usr/bin/php
+<?php
+
+$requires = array();
+$git_urls = array('https://github.com/php-fig/log.git' => 'psr_log/');
+$autoload_config = array('psr_log/Psr/Log/' => 'Psr\Log');
+require __DIR__.'/_config.php';
+
+// Test mode when direct call
+if (!$_SERVER['REQUEST_METHOD'] && realpath($argv[0]) === realpath(__FILE__)) {
+	$logger = new Psr\Log\NullLogger();
+	$logger->info('Doing work');
+	var_dump($logger);
+}
