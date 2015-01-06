5d43bcccaaafd6dd1671f6ce02ec22e85c68b60f
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-01-06 18:02:11 +0200

    assets added

---
+++ share/assets/kendoui.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+	'versions' => array(
+		'2014.2.903' => array(
+			'js' => '//cdn.kendostatic.com/2014.2.903/js/kendo.all.min.js',
+			'css' => array(
+				'//cdn.kendostatic.com/2014.2.903/styles/kendo.common.min.css',
+				'//cdn.kendostatic.com/2014.2.903/styles/kendo.default.min.css',
+				'//cdn.kendostatic.com/2014.2.903/styles/kendo.bootstrap.min.css',
+			),
+		),
+	),
+	'require' => array(
+		'asset' => 'jquery',
+	),
+);
