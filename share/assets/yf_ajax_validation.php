70cd86c1c69ed8fa60c6732be91e689ffc3936d2
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-01-07 15:36:06 +0200

    asset ajax validation up

--- share/assets/yf_ajax_validation.php
+++ share/assets/yf_ajax_validation.php
@@ -117,6 +117,62 @@ return array(
 		}
 	})
 ',
+/*
+	$("form").each(function() {
+		var form = this;
+
+		// Suppress the default bubbles
+		form.addEventListener("invalid", function(event) {
+			event.preventDefault();
+		}, true);
+
+		// Support Safari, iOS Safari, and the Android browser—each of which do not prevent form submissions by default
+		$(form).on("submit", function(event) {
+			if (!this.checkValidity()) {
+				event.preventDefault();
+			}
+		});
+
+		$("input, select, textarea", form)
+			// Destroy the tooltip on blur if the field contains valid data
+			.on("change keyup", function() {
+//			.on("change keyup invalid valid", function() {
+				yf_on_validate_change($(this));
+			})
+			.on("blur", function() {
+				var field = $(this);
+				if (this.validity.valid) {
+					field.popover("destroy");
+				} else {
+					field.popover("hide");
+				}
+			})
+			.on("focus", function() {
+				$(this).popover("show");
+			})
+		;
+
+		$("button:not([type=button]), input[type=submit]", form).on("click", function(event) {
+			// Destroy any tooltips from previous runs
+			$("input, select, textarea", form).each(function() {
+				$(this).popover("destroy");
+			});
+			// Add a tooltip to each invalid field
+			var invalidFields = $(":invalid", form).each(function() {
+				var field = $(this).popover({
+					trigger: "hover",
+					placement: "bottom",
+					html: true,
+					content: function() {
+						return "<i class=\"" + yf_css_icon_error + "\"></i>&nbsp;" + field[ 0 ].validationMessage;
+					}
+				});
+			});
+			// If there are errors, give focus to the first invalid field
+			invalidFields.first().trigger("focus").eq(0).focus();
+		});
+	});
+*/
 		),
 	),
 );
