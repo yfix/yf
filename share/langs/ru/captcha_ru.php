e89ea9458488fad603a6e931a2d02dcd0c448b30
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-01-07 15:12:06 +0200

    lang ru added captcha translations

---
+++ share/langs/ru/captcha_ru.php
@@ -0,0 +1,6 @@
+<?php
+
+return array(
+	'get_new_image'	=> 'Перезагрузить',
+	'Please, confirm your action by entering the digital code you see on the image' => 'Пожалуйста введите число на картинке.',
+);
