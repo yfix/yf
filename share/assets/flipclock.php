9f1f013abaf55a0a02fe68ca8bc516061a7eb6b6
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-01-07 14:17:14 +0200

    assets added flipclock

---
+++ share/assets/flipclock.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+	'versions' => array(
+		'master' => array(
+			'js' => '//cdn.rawgit.com/yfix/FlipClock/master/compiled/flipclock.min.js',
+			'css' => '//cdn.rawgit.com/yfix/FlipClock/master/compiled/flipclock.css',
+		),
+	),
+	'require' => array(
+		'asset' => 'jquery',
+	),
+);
