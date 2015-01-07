36017ea89b7b291309f3b37620b5de3697adcf6c
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-01-07 13:30:29 +0200

    asset ckeditor changed urls, added jquery adapter

--- share/assets/ckeditor.php
+++ share/assets/ckeditor.php
@@ -3,7 +3,15 @@
 return array(
 	'versions' => array(
 		'4.4.5' => array(
-			'js' => '//cdnjs.cloudflare.com/ajax/libs/ckeditor/4.4.5/ckeditor.js',
+			'js' => array(
+				'//cdn.ckeditor.com/4.4.5/standard/ckeditor.js',
+				'//cdn.ckeditor.com/4.4.5/standard/adapters/jquery.js',
+//				'//cdn.ckeditor.com/4.4.5/basic/ckeditor.js',
+//				'//cdn.ckeditor.com/4.4.5/basic/adapters/jquery.js',
+			),
 		),
+	),
+	'require' => array(
+		'assets' => 'jquery',
 	),
 );
