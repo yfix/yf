e772111317c1c29bc51894bd04b816cdb5cd2cfe
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-01-07 13:52:24 +0200

    dev added chrome_validation_messages

---
+++ .dev/scripts/chrome_validation_messages/get_validation_messages.php
@@ -0,0 +1,14 @@
+#!/usr/bin/php
+<?php
+
+$files = array(
+	'en_content_strings.grd' => 'https://chromium.googlesource.com/chromium/src/+/master/content/app/strings/content_strings.grd?format=TEXT',
+	'en_gb_content_strings.xtb' => 'https://chromium.googlesource.com/chromium/src/+/master/content/app/strings/translations/content_strings_en-GB.xtb?format=TEXT',
+	'ru_content_strings.xtb' => 'https://chromium.googlesource.com/chromium/src/+/master/content/app/strings/translations/content_strings_ru.xtb?format=TEXT',
+	'uk_content_strings.xtb' => 'https://chromium.googlesource.com/chromium/src/+/master/content/app/strings/translations/content_strings_uk.xtb?format=TEXT',
+);
+foreach ($files as $file => $url) {
+	if (!file_exists($file)) {
+		file_put_contents($file, base64_decode(file_get_contents($url)));
+	}
+}
