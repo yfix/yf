6e51992582d6a8bcb7290bbf4379c48b30b37596
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-01-10 13:57:54 +0200

    service provider jade_php added

---
+++ share/service_providers/coffeescript_php.php
@@ -0,0 +1,16 @@
+#!/usr/bin/php
+<?php
+
+$requires = array();
+$git_urls = array('https://github.com/yfix/coffeescript-php.git' => 'coffeescript_php/');
+$autoload_config = array();
+require __DIR__.'/_config.php';
+
+require $libs_root. 'coffeescript_php/src/CoffeeScript/Init.php';
+\CoffeeScript\Init::load();
+
+// Test mode when direct call
+if (!$_SERVER['REQUEST_METHOD'] && realpath($argv[0]) === realpath(__FILE__)) {
+	$coffee = 'alert "I knew it!" if elvis?';
+	echo \CoffeeScript\Compiler::compile($coffee, array('header' => false));
+}
