d3acf3228efe2f8bd65ace904bbabda19e111a0f
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-01-10 13:58:33 +0200

    aliases functions added jade()

--- share/functions/yf_aliases.php
+++ share/functions/yf_aliases.php
@@ -177,6 +177,9 @@ if (!function_exists('sass')) {
 if (!function_exists('less')) {
 	function less($content, $content_type = 'auto', $params = array()) { return _class('assets')->add($content, 'less', $content_type, $params); }
 }
+if (!function_exists('jade')) {
+	function jade($content, $content_type = 'auto', $params = array()) { return _class('assets')->add($content, 'jade', $content_type, $params); }
+}
 if (!function_exists('coffee')) {
 	function coffee($content, $content_type = 'auto', $params = array()) { return _class('assets')->add($content, 'coffee', $content_type, $params); }
 }
