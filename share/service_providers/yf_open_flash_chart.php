d3fce8a7f1b27dfab919f803392e9e6d41bcc92e
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-01-12 13:43:43 +0200

    service providers added yf_open_flash_chart

---
+++ share/service_providers/yf_open_flash_chart.php
@@ -0,0 +1,22 @@
+#!/usr/bin/php
+<?php
+
+$requires = array();
+$git_urls = array('https://github.com/yfix/yf_open_flash_chart.git' => 'yf_open_flash_chart/');
+$autoload_config = array();
+require __DIR__.'/_config.php';
+
+// Test mode when direct call
+if (!$_SERVER['REQUEST_METHOD'] && realpath($argv[0]) === realpath(__FILE__)) {
+	include_once $libs_root.'yf_open_flash_chart/open-flash-chart.php';
+
+	$bar = new bar_outline( 70, '#A2C2FC', '#0750D9' );
+	$bar->data = array();
+	$g = new graph();
+	$g->js_path = isset($params['js_path']) ?: '/js/';
+	$g->swf_path = isset($params['swf_path_path']) ?: '/js/';
+	$g->title( ' ', '{font-size: 20px;}' );
+	$g->set_tool_tip(  '#val# EUR on #x_label#' );
+	$g->set_output_type('js');
+	echo $g->render();
+}
