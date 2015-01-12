02992c57ffcad16ac257e46bf78df696af12b6c8
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-01-12 15:39:19 +0200

    services added json_schema

---
+++ share/service_providers/json_schema.php
@@ -0,0 +1,13 @@
+#!/usr/bin/php
+<?php
+
+$requires = array();
+$git_urls = array('https://github.com/justinrainbow/json-schema.git' => 'json_schema/');
+$autoload_config = array('json_schema/src/JsonSchema/' => 'JsonSchema');
+require __DIR__.'/_config.php';
+
+// Test mode when direct call
+if (!$_SERVER['REQUEST_METHOD'] && realpath($argv[0]) === realpath(__FILE__)) {
+	$retriever = new JsonSchema\Uri\UriRetriever;
+	var_dump($retriever);
+}
