b303d2b6c310c85032cf564691df222f7c20cdb0
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-01-13 14:34:11 +0200

    dev sample app update

--- .dev/sample_app/run_server.sh
+++ .dev/sample_app/run_server.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
+# http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+DIR=$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd )
  
-DOCROOT="$(pwd)/public"
-ROUTER="$(pwd)/router.php"
+DOCROOT="$DIR/public"
+ROUTER="$DIR/router.php"
 HOST=0.0.0.0
 PORT=33380
  
