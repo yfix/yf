58ff954f4a6bf98a4ded618f97ac01dee5fa8dc4
LArV <larv.job@gmail.com>
2015-01-15 11:10:01 +0200

    add: payment provider test

---
+++ plugins/payment/classes/yf_payment_api__provider_test.class.php
@@ -0,0 +1,15 @@
+<?php
+
+_class( 'payment_api__provider' );
+
+class yf_payment_api__provider_test extends yf_payment_api__provider {
+
+	public $ENABLE = true;
+
+	public function _init() {
+		defined( 'TEST_MODE' ) && $allow = $this->allow( TEST_MODE );
+		if( !$allow ) { return( false ); }
+		parent::_init();
+	}
+
+}
