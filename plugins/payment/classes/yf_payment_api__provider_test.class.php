999c30d0b0f36e3d92b98487079a261482bc2ba6
LArV <larv.job@gmail.com>
2015-01-15 16:40:06 +0200

    up: payment_api, provider test

--- plugins/payment/classes/yf_payment_api__provider_test.class.php
+++ plugins/payment/classes/yf_payment_api__provider_test.class.php
@@ -4,7 +4,7 @@ _class( 'payment_api__provider' );
 
 class yf_payment_api__provider_test extends yf_payment_api__provider {
 
-	public $ENABLE = true;
+	public $ENABLE = false;
 
 	public function _init() {
 		defined( 'TEST_MODE' ) && $allow = $this->allow( TEST_MODE );
