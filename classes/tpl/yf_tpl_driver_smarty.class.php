f2a6f7e9b2d87e75eab4226d26ffc92c502ced95
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-01-19 23:20:09 +0200

    tpl drivers bugfixes

--- classes/tpl/yf_tpl_driver_smarty.class.php
+++ classes/tpl/yf_tpl_driver_smarty.class.php
@@ -7,24 +7,23 @@
 class yf_tpl_driver_smarty {
 
 	/**
-	* Constructor
+	* Catch missing method call
+	*/
+	function __call($name, $arguments) {
+		trigger_error(__CLASS__.': No method '.$name, E_USER_WARNING);
+		return false;
+	}
+
+	/**
 	*/
 	function _init () {
-		require_once YF_PATH. 'libs/smarty/libs/Smarty.class.php';
+		require_php_lib('smarty');
 		$smarty = new Smarty();
 		$smarty->setTemplateDir(YF_PATH. tpl()->TPL_PATH);
 		$smarty->setCompileDir(STORAGE_PATH.'templates_c/');
 		$smarty->setCacheDir(STORAGE_PATH.'smarty_cache/');
 #		$smarty->setConfigDir(STORAGE_PATH.'smarty_configs/');
 		$this->smarty = $smarty;
-	}
-
-	/**
-	* Catch missing method call
-	*/
-	function __call($name, $arguments) {
-		trigger_error(__CLASS__.': No method '.$name, E_USER_WARNING);
-		return false;
 	}
 
 	/**
--- classes/tpl/yf_tpl_driver_twig.class.php
+++ classes/tpl/yf_tpl_driver_twig.class.php
@@ -7,23 +7,19 @@
 class yf_tpl_driver_twig {
 
 	/**
-	* Constructor
-	*/
-	function _init () {
-		require_once YF_PATH.'libs/twig/lib/Twig/Autoloader.php';
-		Twig_Autoloader::register();
-		$loader = new Twig_Loader_String();
-		$twig = new Twig_Environment($loader);
-		$this->twig = $twig;
-// TODO: configure twig
-	}
-
-	/**
 	* Catch missing method call
 	*/
 	function __call($name, $arguments) {
 		trigger_error(__CLASS__.': No method '.$name, E_USER_WARNING);
 		return false;
+	}
+
+	/**
+	*/
+	function _init () {
+		require_php_lib('twig');
+		$this->twig = new Twig_Environment( new Twig_Loader_String() );
+// TODO: configure twig
 	}
 
 	/**
