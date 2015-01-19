75034f30686bba42f49dbb6c27ec1f82f720efc9
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-01-19 10:36:39 +0200

    ckeditor asset up to 4.4.6

--- share/assets/ckeditor.php
+++ share/assets/ckeditor.php
@@ -2,12 +2,12 @@
 
 return array(
 	'versions' => array(
-		'4.4.5' => array(
+		'4.4.6' => array(
 			'js' => array(
-				'//cdn.ckeditor.com/4.4.5/standard/ckeditor.js',
-				'//cdn.ckeditor.com/4.4.5/standard/adapters/jquery.js',
-//				'//cdn.ckeditor.com/4.4.5/basic/ckeditor.js',
-//				'//cdn.ckeditor.com/4.4.5/basic/adapters/jquery.js',
+				'//cdn.ckeditor.com/4.4.6/standard/ckeditor.js',
+				'//cdn.ckeditor.com/4.4.6/standard/adapters/jquery.js',
+//				'//cdn.ckeditor.com/4.4.6/basic/ckeditor.js',
+//				'//cdn.ckeditor.com/4.4.6/basic/adapters/jquery.js',
 			),
 		),
 	),
