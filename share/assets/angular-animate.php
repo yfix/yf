8c9a541904c945990ed688f520bcc5075324028b
LArV <larv.job@gmail.com>
2015-01-20 12:38:47 +0200

    up: asset angular 1.3.8

--- share/assets/angular-animate.php
+++ share/assets/angular-animate.php
@@ -5,6 +5,9 @@ return array(
 		'1.2.26' => array(
 			'js' => '//cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.26/angular-animate.min.js',
 		),
+		'1.3.8' => array(
+			'js' => '//cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.8/angular-animate.min.js',
+		),
 	),
 	'require' => array(
 		'js' => 'angularjs',
--- share/assets/angular-sanitize.php
+++ share/assets/angular-sanitize.php
@@ -5,6 +5,9 @@ return array(
 		'1.2.26' => array(
 			'js' => '//cdnjs.cloudflare.com/ajax/libs/angular.js/1.2.26/angular-sanitize.min.js',
 		),
+		'1.3.8' => array(
+			'js' => '//cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.8/angular-sanitize.min.js',
+		),
 	),
 	'require' => array(
 		'js' => 'angularjs',
