8c77b5cca13809f562b3e01d6915a6263f6eb4e1
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-01-23 13:05:21 +0200

    asset jq-select2 removed not working old version

--- share/assets/jq-select2.php
+++ share/assets/jq-select2.php
@@ -5,17 +5,6 @@ return function() {
 $locale = conf('language') ?: 'ru';
 return array(
 	'versions' => array(
-		'3.4.6' => array(
-			'js' => array(
-				'//cdnjs.cloudflare.com/ajax/libs/select2/3.4.6/select2.min.js',
-				$locale !== 'en' ? '//cdnjs.cloudflare.com/ajax/libs/select2/3.4.6/select2_locale_'.$locale.'.min.js' : '',
-			),
-			'css' => array(
-				'//cdnjs.cloudflare.com/ajax/libs/select2/3.4.6/select2.min.css',
-				// CSS to make Select2 fit in with Bootstrap 3.x
-				'//cdnjs.cloudflare.com/ajax/libs/select2/3.4.6/select2-bootstrap.min.css',
-			),
-		),
 		'3.5.2' => array(
 			'js' => array(
 				'//cdnjs.cloudflare.com/ajax/libs/select2/3.5.2/select2.min.js',
