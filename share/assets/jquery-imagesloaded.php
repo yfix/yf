dc7712d88be3eeca7fb77fbb6c5983f5bedd5cf8
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-01-27 16:06:32 +0200

    assets added jquery-imagesloaded

---
+++ share/assets/jquery-imagesloaded.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+	'versions' => array(
+		'master' => array(
+			'js' => '//cdn.rawgit.com/desandro/imagesloaded/master/imagesloaded.pkgd.min.js',
+		),
+	),
+	'require' => array(
+		'js' => 'jquery',
+	),
+);
