3c645dc8cca5e04e0362e1ebd18a68fdd5c974b3
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-01-27 15:37:52 +0200

    assets added masonry

---
+++ share/assets/masonry.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+	'versions' => array(
+		'master' => array(
+			'js' => '//cdn.rawgit.com/desandro/masonry/master/dist/masonry.pkgd.min.js',
+		),
+	),
+	'require' => array(
+		'asset' => 'jquery',
+	),
+);
