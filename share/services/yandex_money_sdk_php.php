7909fc5f400eb7461c308fc3bfed7578656b0599
LArV <larv.job@gmail.com>
2015-01-29 13:37:30 +0200

    up

--- share/services/yandex_money_sdk_php.php
+++ share/services/yandex_money_sdk_php.php
@@ -1,14 +1,16 @@
 #!/usr/bin/php
 <?php
 
+$path = 'yandex-money-sdk-php/';
+$lib  = $path.'lib/';
 $requires = array();
-$git_urls = array('https://github.com/yandex-money/yandex-money-sdk-php.git' => 'yandex-money-sdk-php/');
-$autoload_config = array('yandex-money-sdk-php/lib/' => 'YandexMoney');
+$git_urls = array('https://github.com/yandex-money/yandex-money-sdk-php.git' => $path);
+$autoload_config = array($lib => 'YandexMoney');
+
 require __DIR__.'/_config.php';
 
-foreach(explode(' ', 'api.php base.php exceptions.php external_payment.php') as $f) {
-	include_once $libs_root. 'yandex-money-sdk-php/lib/'. $f;
-}
+require_once $libs_root.$lib.'api.php';
+require_once $libs_root.$lib.'external_payment.php';
 
 // Test mode when direct call
 if (!isset($_SERVER['REQUEST_METHOD']) && realpath($argv[0]) === realpath(__FILE__)) {
