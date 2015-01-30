d776460fe9d8ec95245f950e8bac75b5d5a6cb2e
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-01-30 16:19:46 +0200

    assets added bootstrap-tabdrop

---
+++ share/assets/bootstrap-tabdrop.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+	'versions' => array(
+		'master' => array(
+			'js' => '//cdn.rawgit.com/jschab/bootstrap-tabdrop/master/js/bootstrap-tabdrop.js',
+			'css' => '//cdn.rawgit.com/jschab/bootstrap-tabdrop/master/css/tabdrop.css',
+		),
+	),
+	'require' => array(
+		'asset' => array(
+			'jquery',
+			'bootstrap-theme',
+		),
+	),
+);
