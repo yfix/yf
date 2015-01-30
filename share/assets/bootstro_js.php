ea4fd2e97f706587c660f2786fbbd4ea53de2cca
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-01-30 13:26:14 +0200

    asset added bootstro_js

---
+++ share/assets/bootstro_js.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+	'versions' => array(
+		'master' => array(
+			'js' => '//cdn.rawgit.com/clu3/bootstro.js/master/bootstro.js',
+			'css' => '//cdn.rawgit.com/clu3/bootstro.js/master/bootstro.css',
+		),
+	),
+	'require' => array(
+		'asset' => array(
+			'jquery',
+			'bootstrap-theme',
+		),
+	),
+);
