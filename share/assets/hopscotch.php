fdbf008f5be9c629c471593c8124fe712908ef27
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-01-30 13:28:23 +0200

    asset added hopscotch

---
+++ share/assets/hopscotch.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+	'versions' => array(
+		'master' => array(
+			'js' => '//cdn.rawgit.com/linkedin/hopscotch/master/dist/js/hopscotch.min.js',
+			'css' => '//cdn.rawgit.com/linkedin/hopscotch/master/dist/css/hopscotch.min.css',
+		),
+	),
+	'require' => array(
+		'asset' => array(
+			'jquery',
+#			'bootstrap-theme',
+		),
+	),
+);
