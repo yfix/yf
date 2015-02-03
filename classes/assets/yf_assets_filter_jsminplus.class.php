16357e772d960e07a10fda537ba164609193454c
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-02-03 17:33:29 +0200

    assets filter jsminplus up

--- classes/assets/yf_assets_filter_jsminplus.class.php
+++ classes/assets/yf_assets_filter_jsminplus.class.php
@@ -5,6 +5,7 @@ class yf_assets_filter_jsminplus {
 	/**
 	*/
 	public function apply($in, $params = array()) {
+		require_php_lib('jsminplus');
 		if (!class_exists('\JSMinPlus')) {
 			throw new Exception('Assets: class \JSMinPlus not found');
 			return $in;
