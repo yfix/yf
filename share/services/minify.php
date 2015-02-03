c4b11479ae3b5c0379139b5eaa211890862b7361
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-02-03 16:31:05 +0200

    services added todo minify

---
+++ share/services/minify.php
@@ -0,0 +1,16 @@
+#!/usr/bin/php
+<?php
+
+$requires = array();
+$git_urls = array('https://github.com/mrclay/minify.git' => 'minify/');
+#$autoload_config = array('minify/' => '');
+require __DIR__.'/_config.php';
+
+// Test mode when direct call
+if (!isset($_SERVER['REQUEST_METHOD']) && realpath($argv[0]) === realpath(__FILE__)) {
+
+	$js = ' function  hello_world ( i , v ) { return " " ; } ';
+#	var_dump($js);
+#	var_dump($min);
+
+}
