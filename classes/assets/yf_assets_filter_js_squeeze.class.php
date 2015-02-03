9979aa9a98088f5693885da2e198de298b7ed1bb
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-02-03 17:12:29 +0200

    assets filter up

--- classes/assets/yf_assets_filter_js_squeeze.class.php
+++ classes/assets/yf_assets_filter_js_squeeze.class.php
@@ -5,7 +5,8 @@ class yf_assets_filter_js_squeeze {
 	/**
 	*/
 	public function apply($in, $params = array()) {
-// TODO
+		require_php_lib('jsqueeze');
+
 		!isset($params['single_line']) && $params['single_line'] = true;
 		!isset($params['keep_important_comments']) && $params['keep_important_comments'] = true;
 		!isset($params['special_var_rx']) && $params['special_var_rx'] = \JSqueeze::SPECIAL_VAR_RX;
