fbd84512a9e255b844eef34fff03b52c3b294338
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-02-04 14:50:54 +0200

    assets added owlcarousel

---
+++ share/assets/owlcarousel.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+	'versions' => array(
+		'master' => array(
+			'js' => '//cdn.rawgit.com/yfix/OwlCarousel/master/owl-carousel/owl.carousel.min.js',
+			'css' => array(
+				'//cdn.rawgit.com/yfix/OwlCarousel/master/owl-carousel/owl.carousel.css',
+				'//cdn.rawgit.com/yfix/OwlCarousel/master/owl-carousel/owl.theme.css',
+			),
+		),
+	),
+	'require' => array(
+		'asset' => 'jquery',
+	),
+);
