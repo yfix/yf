159726a465fb6b12d9d1c9eb8a6864de8b65bc89
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-02-04 15:28:23 +0200

    asset added owlcarousel2

---
+++ share/assets/owlcarousel2.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+	'versions' => array(
+		'master' => array(
+			'js' => 'http://owlcarousel.owlgraphic.com/assets/owlcarousel/owl.carousel.js',
+			'css' => array(
+				'http://owlcarousel.owlgraphic.com/assets/owlcarousel/assets/owl.carousel.min.css',
+				'http://owlcarousel.owlgraphic.com/assets/owlcarousel/assets/owl.theme.default.min.css',
+			),
+		),
+	),
+	'require' => array(
+		'asset' => 'jquery',
+	),
+);
