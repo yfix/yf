dce2106cdc5af7addf4407c480703a835077823e
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-02-04 12:42:35 +0200

    added asset jquery-prettyphoto

---
+++ share/assets/jquery-prettyphoto.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+	'versions' => array(
+		'3.1.5' => array(
+			'js' => '//cdn.rawgit.com/scaron/prettyphoto/master/js/jquery.prettyPhoto.js',
+			'css' => '//cdn.rawgit.com/scaron/prettyphoto/master/css/prettyPhoto.css',
+		),
+	),
+	'require' => array(
+		'asset' => 'jquery',
+	),
+);
