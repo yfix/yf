80e76fc67282cc6c425e5277fa8568a62ac8217b
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-02-13 17:13:12 +0200

    asset jquery-jcrop fixed

--- share/assets/jquery-jcrop.php
+++ share/assets/jquery-jcrop.php
@@ -3,7 +3,7 @@
 return array(
 	'versions' => array(
 		'master' => array(
-			'js' => '//cdnjs.cloudflare.com/ajax/libs/jquery-jcrop/0.9.12/css/jquery.Jcrop.min.js',
+			'js' => '//cdnjs.cloudflare.com/ajax/libs/jquery-jcrop/0.9.12/js/jquery.Jcrop.min.js',
 			'css' => '//cdnjs.cloudflare.com/ajax/libs/jquery-jcrop/0.9.12/css/jquery.Jcrop.min.css',
 		),
 	),
