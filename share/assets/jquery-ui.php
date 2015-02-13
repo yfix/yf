3e7b489c96a37ec044aa458b1f5b90b9aa1c1bfc
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-02-13 12:55:50 +0200

    asset jqueryui updated

--- share/assets/jquery-ui.php
+++ share/assets/jquery-ui.php
@@ -2,9 +2,9 @@
 
 return array(
 	'versions' => array(
-		'1.11.1' => array(
-			'css' => '//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.css',
-			'js' => '//ajax.googleapis.com/ajax/libs/jqueryui/1.11.1/jquery-ui.min.js',
+		'1.11.2' => array(
+			'css' => '//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.css',
+			'js' => '//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js',
 		),
 	),
 	'require' => array(
