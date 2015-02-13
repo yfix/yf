7c4f614ee4f220466d8b896e022c18aa7a025ae7
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-02-13 12:56:23 +0200

    asset added jquery-pjax

---
+++ share/assets/jquery-pjax.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+	'versions' => array(
+		'master' => array(
+			'js' => '//cdn.rawgit.com/yfix/jquery-pjax/master/jquery.pjax.js',
+		),
+	),
+	'require' => array(
+		'asset' => 'jquery',
+	),
+);
