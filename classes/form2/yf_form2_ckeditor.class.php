d4d68f4f5b2a719431b7c1546f6ce91ca12700e2
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-02-18 15:24:12 +0200

    form2 ckeditor up

--- classes/form2/yf_form2_ckeditor.class.php
+++ classes/form2/yf_form2_ckeditor.class.php
@@ -20,33 +20,22 @@ class yf_form2_ckeditor {
 		if (!is_array($params)) {
 			$params = array();
 		}
-		if ($form->_ckeditor_scripts_included) {
-			return '';
-		}
-		$web_ck_path = '';
-		if (conf('ckeditor::use_submodule')) {
-			$ck_path = $params['ck_path'] ? $params['ck_path'] : 'ckeditor/ckeditor.js';
-			$fs_ck_path = PROJECT_PATH. $ck_path;
-			$web_ck_path = WEB_PATH. $ck_path;
-			if (!file_exists($fs_ck_path)) {
-				return '';
-			}
-			js($fs_ck_path);
-		} else {
-			js('ckeditor');
-		}
-
+		js('ckeditor');
 		// Theme-wide ckeditor config inside stpl (so any engine vars can be processed or included there)
 		$stpl_name = 'ckeditor_config'; // Example filesystem location: PROJECT_PATH.'templates/admin/ckeditor_config.stpl'
 		if (!isset($replace['content_id'])) {
 			$replace['content_id'] = $content_id;
 		}
-		$config_js .= tpl()->_stpl_exists($stpl_name) ? tpl()->parse($stpl_name, (array)$extra + (array)$replace) : '';
-		js($config_js);
-
+		if (isset($params['config'])) {
+			$config_js = $params['config'];
+		} else {
+			$config_js = tpl()->_stpl_exists($stpl_name) ? tpl()->parse($stpl_name, (array)$extra + (array)$replace) : '';
+		}
+		if (strlen($config_js)) {
+			js($config_js);
+		}
 		$content_id = $extra['id'] ? $extra['id'] : 'content_editable';
 		$hidden_id = $params['hidden_id'] ? $params['hidden_id'] : '';
-
 		jquery('
 			var _content_id = "#'.$content_id.'";
 			var _hidden_id = "#'.$hidden_id.'";
@@ -54,10 +43,6 @@ class yf_form2_ckeditor {
 				$("input[type=hidden]" + _hidden_id).val( $(_content_id).html() );
 			})
 		');
-
-		// Avoid including ckeditor scripts several times on same page
-		$form->_ckeditor_scripts_included = true;
-
 		return $body;
 	}
 }
