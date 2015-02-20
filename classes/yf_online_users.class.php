a5038b3f4e68ca2909275185d138deddbdea544a
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-02-20 11:15:38 +0200

    cleanup

--- classes/yf_online_users.class.php
+++ classes/yf_online_users.class.php
@@ -1,13 +1,14 @@
 <?php
 
 class yf_online_users {
-	var $_CACHE_UPDATE_TTL = 30;
-	var $_CACHE_CLEANUP_TTL = 90;
-	var $_ONLINE_TTL = 60;
-	var $_COOKIE_TTL = 3600;
+
+	public $_CACHE_UPDATE_TTL = 30;
+	public $_CACHE_CLEANUP_TTL = 90;
+	public $_ONLINE_TTL = 60;
+	public $_COOKIE_TTL = 3600;
 	
 	function process() {
-        list($this->online_user_id,$this->online_user_type) = $this->_set_user_id();
+        list($this->online_user_id, $this->online_user_type) = $this->_set_user_id();
         if (main()->TRACK_ONLINE_STATUS) {        
             $this->_update();
             $this->_cleanup();
@@ -15,38 +16,43 @@ class yf_online_users {
 	}
 	
 	function _set_user_id() {
-		if (intval($_SESSION['admin_id']) != 0) return array($_SESSION['admin_id'], 'admin_id');
-		if (intval(main()->USER_ID) != 0) return array(main()->USER_ID, 'user_id');
-		if (intval($_COOKIE['user_id_tmp']) != 0) return array($_COOKIE['user_id_tmp'], 'user_id_tmp');
+		if (intval($_SESSION['admin_id']) != 0) {
+			return array($_SESSION['admin_id'], 'admin_id');
+		}
+		if (intval(main()->USER_ID) != 0) {
+			return array(main()->USER_ID, 'user_id');
+		}
+		if (intval($_COOKIE['user_id_tmp']) != 0) {
+			return array($_COOKIE['user_id_tmp'], 'user_id_tmp');
+		}
 		
 		// todo: more 'smart' algorythm for user id generation
-		setcookie('user_id_tmp', rand(), $_SERVER['REQUEST_TIME']+$this->_COOKIE_TTL);
+		setcookie('user_id_tmp', rand(), $_SERVER['REQUEST_TIME'] + $this->_COOKIE_TTL);
 		
 		return array($_COOKIE['user_id_tmp'], 'user_id_tmp');
 	}
 	
 	function _update() {
-		$cache_name = __CLASS__.'|'.__FUNCTION__."|".$this->online_user_id."|".$this->online_user_type;
-		if (cache()->get($cache_name) != 'OK' && intval($this->online_user_id)!=0) {
-			db()->replace(db('users_online'),array(
-				'user_id' => $this->online_user_id,
-				'user_type' => $this->online_user_type,
-				'time' => $_SERVER['REQUEST_TIME'],
+		$cache_name = __CLASS__.'|'.__FUNCTION__.'|'.$this->online_user_id.'|'.$this->online_user_type;
+		if (cache()->get($cache_name) != 'OK' && intval($this->online_user_id) != 0) {
+			db()->replace_safe('users_online', array(
+				'user_id'	=> $this->online_user_id,
+				'user_type'	=> $this->online_user_type,
+				'time'		=> $_SERVER['REQUEST_TIME'],
 			));
-			cache()->set($cache_name,'OK',$this->_CACHE_UPDATE_TTL);            
+			cache()->set($cache_name, 'OK', $this->_CACHE_UPDATE_TTL);            
         }
         // details not cached for current url to be shown
-        if (main()->TRACK_ONLINE_DETAILS && !(strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest' || !empty($_GET['ajax_mode'])) && intval($this->online_user_id)!=0) {
-            db()->replace(db('users_online_details'),array(
-                'user_id' => $this->online_user_id,
-                'user_type' => $this->online_user_type,
-                'time' => $_SERVER['REQUEST_TIME'],
-                'session_id' => session_id(),
-                'url' => "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'],
-                'user_agent' => $_SERVER['HTTP_USER_AGENT'],
-                'ip' => $_SERVER['REMOTE_ADDR'],
+        if (main()->TRACK_ONLINE_DETAILS && !(strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest' || !empty($_GET['ajax_mode'])) && intval($this->online_user_id) != 0) {
+            db()->replace_safe('users_online_details', array(
+                'user_id'		=> $this->online_user_id,
+                'user_type'		=> $this->online_user_type,
+                'time'			=> $_SERVER['REQUEST_TIME'],
+                'session_id'	=> session_id(),
+                'url'			=> 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'],
+                'user_agent'	=> $_SERVER['HTTP_USER_AGENT'],
+                'ip'			=> $_SERVER['REMOTE_ADDR'],
             ));
-
         }
 	}
 
@@ -55,8 +61,10 @@ class yf_online_users {
 		$cache_name = __CLASS__.'|'.__FUNCTION__;
 		if (cache()->get($cache_name) != 'OK') {
 			$time = $_SERVER['REQUEST_TIME'] - $this->_ONLINE_TTL;
-			db()->query("DELETE FROM `".db('users_online')."` WHERE `time`<".$time);
-            if (main()->TRACK_ONLINE_DETAILS) db()->query("DELETE FROM `".db('users_online_details')."` WHERE `time`<".$time);
+			db()->query('DELETE FROM '.db('users_online').' WHERE `time`<'.$time);
+            if (main()->TRACK_ONLINE_DETAILS) {
+				db()->query('DELETE FROM '.db('users_online_details').' WHERE `time`<'.$time);
+			}
 			cache()->set($cache_name,'OK',$this->_CACHE_CLEANUP_TTL);
 		}
 	}
