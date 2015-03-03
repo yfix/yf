e500a185097b6f7ed33d8b224aefddf64531fa12
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-03-03 14:57:50 +0200

    unit tests added init scripts for user and admin

---
+++ .dev/tests/yf_unit_tests_setup_shared.php
@@ -0,0 +1,15 @@
+<?php
+
+define('APP_PATH', __DIR__.'/_tmp/');
+define('STORAGE_PATH', __DIR__.'/_tmp/');
+if (!defined('YF_PATH')) {
+	$CONF['cache']['DRIVER'] = 'tmp';
+	$CONF['cache']['NO_CACHE'] = true;
+	define('YF_IN_UNIT_TESTS', true);
+	define('YF_PATH', dirname(dirname(__DIR__)).'/');
+	require YF_PATH.'classes/yf_main.class.php';
+	new yf_main($MAIN_TYPE ?: 'user', $no_db_connect = 1, $auto_init_all = 0, $CONF);
+	date_default_timezone_set('Europe/Kiev');
+	ini_set('display_errors', 'on');
+	error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_STRICT);
+}
---
+++ .dev/tests/yf_unit_tests_setup_user.php
@@ -0,0 +1,4 @@
+<?php
+
+$MAIN_TYPE = 'user';
+require_once __DIR__.'/yf_unit_tests_setup_shared.php';
