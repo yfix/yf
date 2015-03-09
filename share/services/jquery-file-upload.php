dd36be5a224479d7756172250433b31456d45d98
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-03-09 21:43:29 +0200

    services added jquery file upload

---
+++ share/services/jquery-file-upload.php
@@ -0,0 +1,16 @@
+#!/usr/bin/php
+<?php
+
+$requires = array();
+$git_urls = array('https://github.com/yfix/jQuery-File-Upload.git' => 'jquery-file-upload/');
+$autoload_config = array();
+require __DIR__.'/_config.php';
+
+require_once $libs_root. 'jquery-file-upload/server/php/UploadHandler.php';
+
+// Test mode when direct call
+if (!isset($_SERVER['REQUEST_METHOD']) && realpath($argv[0]) === realpath(__FILE__)) {
+
+	echo (int)class_exists('UploadHandler');
+
+}
