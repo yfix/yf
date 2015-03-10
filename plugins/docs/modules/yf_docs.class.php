c3c85bfd9c32198564157f2dfc035a03db537a55
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-03-10 14:52:42 +0200

    docs up

--- plugins/docs/modules/yf_docs.class.php
+++ plugins/docs/modules/yf_docs.class.php
@@ -3,6 +3,13 @@
 class yf_docs {
 
 	/**
+	* Catch missing method call
+	*/
+	function __call($name, $args) {
+		return main()->extend_call($this, $name, $args);
+	}
+
+	/**
 	* Catch all methods calls
 	*/
 	public function _module_action_handler($name) {
@@ -19,7 +26,7 @@ class yf_docs {
 	public function _init() {
 		_class('core_api')->add_syntax_highlighter();
 
-		$this->docs_dir = YF_PATH.'.dev/docs/en/';
+		$this->docs_dir = YF_PATH.'.dev/docs/';
 
 		tpl()->add_function_callback('github', function($m, $r, $name, $_this) {
 			return _class('core_api')->get_github_link($m[1]);
@@ -32,49 +39,23 @@ class yf_docs {
 			return $this->view();
 		}
 		return implode(PHP_EOL, array(
-			'<h1><a href="'.url('/@object/show_docs').'">Docs</a></h1>',
-			$this->show_docs(),
-			'<h1><a href="'.url('/@object/assets').'">Assets</a></h1>',
-			$this->assets(),
+			'<h2><a href="'.url('/@object/assets').'">Assets</a></h2>',
+			'<h2><a href="'.url('/@object/services').'">Services</a></h2>',
+			'<h2><a href="'.url('/@object/form').'">Form</a></h2>',
+			'<h2><a href="'.url('/@object/table').'">Table</a></h2>',
+			'<h2><a href="'.url('/@object/html').'">Html</a></h2>',
+#			'<h2><a href="'.url('/@object/db').'">Database</a></h2>',
+			'<h2><a href="'.url('/@object/misc').'">Miscellaneous</a></h2>',
 		));
-		;
-	}
-
-	/***/
-	public function view() {
-		$name = preg_replace('~[^a-z0-9_-]+~ims', '', $_GET['id']);
-		if ($name) {
-			$f = $this->docs_dir. $name. '.stpl';
-			if (file_exists($f)) {
-				return '<section class="page-contents">'.tpl()->parse_string(file_get_contents($f), $replace, 'doc_'.$name).'</section>';
-			}
-		}
-		return _e('Not found');
-	}
-
-	/***/
-	public function show_docs() {
-		foreach (glob($this->docs_dir.'*.stpl') as $path) {
-			$f = basename($path);
-			$name = substr($f, 0, -strlen('.stpl'));
-			$data[++$i] = array(
-				'name'	=> $name,
-				'link'	=> url('/@object/show/'.$name),
-			);
-		}
-		return _class('html')->li($data);
 	}
 
 	/***/
 	public function assets() {
 		asset('font-awesome4');
-
-		$yf_len = strlen(realpath(YF_PATH));
-		foreach ($this->_load_predefined_assets() as $asset) {
+		foreach ($this->_get_assets() as $asset) {
 			$name = $asset['name'];
 			$sub = array();
-			$asset_github_link = 'https://github.com/yfix/yf/tree/master/'.ltrim(substr(realpath($asset['path']), $yf_len), '/');
-			$sub[] = '<i class="fa fa-github fa-lg"></i> <a href="'.$asset_github_link.'">'.$asset_github_link.'</a>';
+			$sub[] = $this->_github_link($asset['path']);
 			$content = $asset['content'];
 			$info = is_array($content) ? $content['info'] : array();
 			if ($info['name']) {
@@ -94,25 +75,144 @@ class yf_docs {
 				'link'	=> url('/@object/@action/#'.$name),
 				'sub'	=> $sub,
 				'id'	=> $name,
-				'class'	=> 'btn btn-default btn-small btn-sm',
+#				'class'	=> 'btn btn-default btn-small btn-sm',
 			);
 		}
 		return _class('html')->li($data);
 	}
 
 	/***/
-	public function _load_predefined_assets() {
+	public function services() {
+		asset('font-awesome4');
+		foreach ($this->_get_services() as $service) {
+			$name = $service['name'];
+			$sub = array();
+			$sub[] = $this->_github_link($service['path']);
+			$content = $service['content'];
+			$info = is_array($content) ? $content['info'] : array();
+			if ($info['name']) {
+				$sub[] = '<b>'.t('name').'</b>: '.$info['name'];
+			}
+			if ($info['desc']) {
+				$sub[] = '<b>'.t('desc').'</b>: '.$info['desc'];
+			}
+			if ($info['url']) {
+				$sub[] = '<b>'.t('url').'</b>: <a href="'._prepare_html($info['url']).'">'._prepare_html($info['url']).'</a>';
+			}
+			if ($info['git']) {
+				$sub[] = '<b>'.t('git').'</b>: <a href="'.$info['git'].'">'.$info['git'].'</a>';
+			}
+			$data[++$i] = array(
+				'name'	=> $name,
+				'link'	=> url('/@object/@action/#'.$name),
+				'sub'	=> $sub,
+				'id'	=> $name,
+#				'class'	=> 'btn btn-default btn-small btn-sm',
+			);
+		}
+		return _class('html')->li($data);
+	}
+
+	/***/
+	public function form() {
+// TODO
+	}
+
+	/***/
+	public function table() {
+// TODO
+	}
+
+	/***/
+	public function html() {
+// TODO
+	}
+
+	/***/
+	public function db() {
+// TODO
+	}
+
+	/***/
+	public function orm() {
+// TODO
+	}
+
+	/***/
+	public function common() {
+// TODO
+	}
+
+	/***/
+	public function main() {
+// TODO
+	}
+
+	/***/
+	public function aliases() {
+// TODO
+	}
+
+	/***/
+	public function fast_init() {
+// TODO
+	}
+
+	/***/
+	public function yf() {
+// TODO: console tool docs
+	}
+
+	/***/
+	public function dir() {
+// TODO
+	}
+
+	/***/
+	public function auth() {
+// TODO
+	}
+
+	/***/
+	public function misc() {
+		$dir = $this->docs_dir;
+		$dir_len = strlen($dir);
+		$ext = '.stpl';
+		$ext_len = strlen($ext);
+
+		$name = preg_replace('~[^a-z0-9/_-]+~ims', '', $_GET['id']);
+		if (strlen($name)) {
+			$f = $dir. $name. '.stpl';
+			if (!file_exists($f)) {
+				return _404('Not found');
+			}
+			return '<section class="page-contents">'.tpl()->parse_string(file_get_contents($f), $replace, 'doc_'.$name).'</section>';
+		}
+		$url = rtrim(url('/@object/@action/')).'/';
+		$data = array();
+		foreach ((array)_class('dir')->scan($dir) as $path) {
+			if (substr($path, -$ext_len) !== $ext) {
+				continue;
+			}
+			$name = substr($path, $dir_len, -$ext_len);
+			$data[$name] = array(
+				'name'	=> $name,
+				'link'	=> $url. urlencode($name),
+			);
+		}
+		ksort($data);
+		return _class('html')->li($data);
+	}
+
+	/***/
+	public function _get_assets() {
 		$assets = array();
 		$suffix = '.php';
 		$dir = 'share/assets/';
 		$pattern = $dir. '*'. $suffix;
 		$globs = array(
-			'yf_main'				=> YF_PATH. $pattern,
-			'yf_plugins'			=> YF_PATH. 'plugins/*/'. $pattern,
-#			'project_main'			=> PROJECT_PATH. $pattern,
-#			'project_app'			=> APP_PATH. $pattern,
-#			'project_plugins'		=> PROJECT_PATH. 'plugins/*/'. $pattern,
-#			'project_app_plugins'	=> APP_PATH. 'plugins/*/'. $pattern,
+			'yf_main'		=> YF_PATH. $pattern,
+			'yf_plugins'	=> YF_PATH. 'plugins/*/'. $pattern,
 		);
 		$slen = strlen($suffix);
 		$names = array();
@@ -122,15 +222,61 @@ class yf_docs {
 				$names[$name] = $path;
 			}
 		}
-		// This double iterating code ensures we can inherit/replace assets with same name inside project
 		foreach($names as $name => $path) {
 			$assets[$name] = array(
 				'name'		=> $name,
 				'path'		=> $path,
 				'content'	=> include $path,
+				'raw'		=> file_get_contents($path),
 			);
 		}
 		return $assets;
+	}
+
+	/***/
+	public function _get_services() {
+		$services = array();
+		$suffix = '.php';
+		$dir = 'share/services/';
+		$pattern = $dir. '*'. $suffix;
+		$globs = array(
+			'yf_main'		=> YF_PATH. $pattern,
+			'yf_plugins'	=> YF_PATH. 'plugins/*/'. $pattern,
+		);
+		$slen = strlen($suffix);
+		$names = array();
+		foreach($globs as $gname => $glob) {
+			foreach(glob($glob) as $path) {
+				$name = substr(basename($path), 0, -$slen);
+				$names[$name] = $path;
+			}
+		}
+		foreach($names as $name => $path) {
+			if (substr($name, 0, 1) === '_') {
+				continue;
+			}
+			$services[$name] = array(
+				'name'	=> $name,
+				'path'	=> $path,
+				'raw'	=> file_get_contents($path),
+			);
+		}
+		return $services;
+	}
+
+	/***/
+	public function _github_link($path = '') {
+		if (!strlen($path)) {
+			return '';
+		}
+		$path = realpath($path);
+		$yf_path = realpath(YF_PATH);
+		$yf_len = strlen($yf_path);
+		if (substr($path, 0, $yf_len) === $yf_path) {
+			$path = substr($path, $yf_len);
+		}
+		$link = 'https://github.com/yfix/yf/tree/master/'. trim($path, '/');
+		return '<i class="fa fa-github fa-lg"></i> <a href="'.$link.'">'.$link.'</a>';
 	}
 
 	/***/
