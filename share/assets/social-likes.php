becf9464aa00c5dfb70ee52c1be83414430046ef
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-03-16 12:06:36 +0200

    asset added social likes

---
+++ share/assets/social-likes.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+	'versions' => array(
+		'master' => array(
+			'js' => array(
+				'//cdn.rawgit.com/yfix/social-likes/master/social-likes.min.js',
+			),
+			'css' => array(
+				'//cdn.rawgit.com/yfix/social-likes/master/social-likes_birman.css',
+			),
+		),
+	),
+	'require' => array(
+		'asset' => 'jquery',
+	),
+);
