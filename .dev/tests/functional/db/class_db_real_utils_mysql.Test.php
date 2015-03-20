7a8a3d19e77673a73178727beda6fc0643941875
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-03-20 21:04:46 +0200

    unit tests up for hhvm

--- .dev/tests/functional/db/class_db_real_utils_mysql.Test.php
+++ .dev/tests/functional/db/class_db_real_utils_mysql.Test.php
@@ -18,8 +18,13 @@ class class_db_real_utils_mysql_test extends db_real_abstract {
 		self::$DB_DRIVER = self::$_bak['DB_DRIVER'];
 	}
 	public static function _need_skip_test($name) {
+		if (main()->is_hhvm()) {
+			if (in_array($name, array('test_drop_database','test_create_database','test_rename_database'))) {
+				return true;
+			}
+		}
 		if ($_ENV['TRAVIS']) {	
-			if (in_array($name, array('test_drop_database','test_create_database')) || false !== strpos($name, '_foreign_')) {
+			if (in_array($name, array('test_drop_database','test_create_database','test_rename_database')) || false !== strpos($name, '_foreign_')) {
 				return true;
 			}
 		}
