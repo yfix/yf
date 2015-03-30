66ff1529f7d89dbf017eeade073b1aa4e181f3cb
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-03-30 14:08:09 +0300

    logs exec user up

--- plugins/logs/classes/logs/yf_logs_exec_user.class.php
+++ plugins/logs/classes/logs/yf_logs_exec_user.class.php
@@ -37,9 +37,9 @@ class yf_logs_exec_user {
 	/** @var bool */
 	public $LOG_IS_AJAX			= false;
 	/** @var bool */
-	public $LOG_IS_SPIDER		= false;
+	public $LOG_IS_SPIDER		= true;
 	/** @var bool */
-	public $LOG_IS_REDIRECT		= false;
+	public $LOG_IS_REDIRECT		= true;
 	/** @var bool */
 	public $LOG_IS_UNIT_TEST	= false;
 	/** @var bool */
@@ -47,15 +47,15 @@ class yf_logs_exec_user {
 	/** @var bool */
 	public $LOG_IS_DEV			= false;
 	/** @var bool */
-	public $LOG_IS_DEBUG		= false;
+	public $LOG_IS_DEBUG		= true;
 	/** @var bool */
 	public $LOG_IS_BANNED		= true;
 	/** @var bool */
-	public $LOG_IS_404			= false;
+	public $LOG_IS_404			= true;
 	/** @var bool */
-	public $LOG_IS_403			= false;
+	public $LOG_IS_403			= true;
 	/** @var bool */
-	public $LOG_IS_503			= false;
+	public $LOG_IS_503			= true;
 
 	/**
 	*/
@@ -70,28 +70,39 @@ class yf_logs_exec_user {
 	function allow () {
 		if (!$this->LOGGING || MAIN_TYPE_ADMIN) { return false; }
 		$main = main();
-		if (!$main->is_logged_in() && !$this->LOG_IS_USER_GUEST) { return false; }
-		if ($main->is_logged_in() && !$this->LOG_IS_USER_MEMBER) { return false; }
-		if (!$main->is_common_page() && $this->LOG_IS_COMMON_PAGE) { return false; }
-		if ($main->is_https() && !$this->LOG_IS_HTTPS) { return false; }
-		if ($main->is_post() && !$this->LOG_IS_POST) { return false; }
-		if ($main->no_graphics() && !$this->LOG_IS_NO_GRAPHICS) { return false; }
-		if ($main->is_ajax() && !$this->LOG_IS_AJAX) { return false; }
-		if ($main->is_spider() && !$this->LOG_IS_SPIDER) { return false; }
-		if ($main->is_redirect() && !$this->LOG_IS_REDIRECT) { return false; }
-		if ($main->is_console() && !$this->LOG_IS_CONSOLE) { return false; }
-		if ($main->is_unit_test() && !$this->LOG_IS_UNIT_TEST) { return false; }
-		if ($main->is_dev() && !$this->LOG_IS_DEV) { return false; }
-		if ($main->is_debug() && !$this->LOG_IS_DEBUG) { return false; }
-		if ($main->is_banned() && !$this->LOG_IS_BANNED) { return false; }
-		if ($main->is_403() && !$this->LOG_IS_403) { return false; }
-		if ($main->is_404() && !$this->LOG_IS_404) { return false; }
-		if ($main->is_503() && !$this->LOG_IS_503) { return false; }
+		$checks = array(
+			'is_user_guest'	=> !$main->is_logged_in() && !$this->LOG_IS_USER_GUEST,
+			'is_user_member'=> $main->is_logged_in() && !$this->LOG_IS_USER_MEMBER,
+			'is_common_page'=> !$main->is_common_page() && $this->LOG_IS_COMMON_PAGE,
+			'is_https'		=> $main->is_https() && !$this->LOG_IS_HTTPS,
+			'is_post'		=> $main->is_post() && !$this->LOG_IS_POST,
+			'is_no_graphics'=> $main->no_graphics() && !$this->LOG_IS_NO_GRAPHICS,
+			'is_ajax'		=> $main->is_ajax() && !$this->LOG_IS_AJAX,
+			'is_spider'		=> $main->is_spider() && !$this->LOG_IS_SPIDER,
+			'is_redirect'	=> $main->is_redirect() && !$this->LOG_IS_REDIRECT,
+			'is_console'	=> $main->is_console() && !$this->LOG_IS_CONSOLE,
+			'is_unit_test'	=> $main->is_unit_test() && !$this->LOG_IS_UNIT_TEST,
+			'is_dev'		=> $main->is_dev() && !$this->LOG_IS_DEV,
+			'is_debug'		=> $main->is_debug() && !$this->LOG_IS_DEBUG,
+			'is_banned'		=> $main->is_banned() && !$this->LOG_IS_BANNED,
+			'is_403'		=> $main->is_403() && !$this->LOG_IS_403,
+			'is_404'		=> $main->is_404() && !$this->LOG_IS_404,
+			'is_503'		=> $main->is_503() && !$this->LOG_IS_503,
+			'is_stop_list'	=> false,
+		);
 		if ($this->USE_STOP_LIST) {
 			foreach ((array)$this->STOP_LIST as $_cur_pattern) {
 				if (preg_match('/'.$_cur_pattern.'/i', $_SERVER['QUERY_STRING'])) {
-					return false;
+					$checks['stop_list'] = true;
+					break;
 				}
+			}
+		}
+		$this->checks = $checks;
+		foreach ((array)$checks as $name => $is_denied) {
+			if ($is_denied) {
+				$this->log_denied_because = $name;
+				return false;
 			}
 		}
 		return true;
