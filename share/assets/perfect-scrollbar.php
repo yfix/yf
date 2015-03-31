13e1167efeb9e9456d8d0bce03b5f0772e7b225a
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-03-31 11:12:16 +0300

    asset added perfect-scrollbar

---
+++ share/assets/perfect-scrollbar.php
@@ -0,0 +1,23 @@
+<?php
+
+return array(
+	'versions' => array(
+		'0.5.9' => array(
+			'js' => '//cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/0.5.9/perfect-scrollbar.min.js',
+			'css' => '//cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/0.5.9/perfect-scrollbar.min.css',
+		),
+	),
+	'require' => array(
+		'asset' => array(
+			'jquery',
+		),
+	),
+	'info' => array(
+		'url' => 'http://noraesae.github.io/perfect-scrollbar',
+		'name' => 'Minimalistic but perfect custom scrollbar plugin',
+		'desc' => 'Why perfect-scrollbar?
+			I was once working on a personal project, and trying to find the jQuery scrollbar plugin that\'s perfect. But there was no perfect one. That\'s why I decided to make one.
+			perfect-scrollbar is minimalistic but perfect (for me, and maybe for most developers) scrollbar plugin working with jQuery or vanilla JavaScript as well.',
+		'git' => 'https://github.com/noraesae/perfect-scrollbar.git',
+	),
+);
