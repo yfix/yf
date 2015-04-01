baef99f3320bf636f8d41dfc10c77f7c21d4a7d1
LArV <larv.job@gmail.com>
2015-04-01 17:22:54 +0300

    up

--- plugins/payment/classes/yf_payment_api__provider_webmoney.class.php
+++ plugins/payment/classes/yf_payment_api__provider_webmoney.class.php
@@ -290,6 +290,7 @@ class yf_payment_api__provider_webmoney extends yf_payment_api__provider_remote 
 			'status'         => true,
 			'status_message' => 'Поплнение через сервис: WebMoney',
 		);
+		return( true );
 	}
 
 	public function __api_response__success( $operation_id, $response ) {
@@ -371,17 +372,26 @@ class yf_payment_api__provider_webmoney extends yf_payment_api__provider_remote 
 			case 'result':
 				$state = 'wait';
 				$result = $this->__api_response__result( $operation_id, $response );
+$dump = $this->dump( array( 'var' => array(
+	'result' => $result,
+)));
 				return( $result );
 				break;
 			case 'success':
 				$state = 'success';
 				$result = $this->__api_response__success( $operation_id, $response );
+$dump = $this->dump( array( 'var' => array(
+	'success' => $result,
+)));
 				if( is_array( $result ) ) { $state = 'fail'; }
 				break;
 			case 'fail':
 			default:
 				$state = 'fail';
 				$result = $this->__api_response__fail( $operation_id, $response );
+$dump = $this->dump( array( 'var' => array(
+	'fail' => $result,
+)));
 				if( is_array( $result ) ) { $state = 'fail'; }
 				break;
 		}
