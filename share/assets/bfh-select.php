2587042272c87c6ba07d19e23d7c95377fd6d6c2
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-04-07 14:34:25 +0300

    asset bfh select up

--- share/assets/bfh-select.php
+++ share/assets/bfh-select.php
@@ -1,37 +1,33 @@
 <?php
 
+return function() {
+
+$bfh_css_fixes = '
+	[class^="bfh-flag-"], [class*="bfh-flag-"] { display: inline-block; margin-right: 5px; }
+	[class^="bfh-flag-"]:empty, [class*="bfh-flag-"]:empty { width: 16px; }
+	.bfh-selectbox { max-width: 300px; }
+	.bfh-selectbox-options a { padding-left: 10px; }
+';
 return array(
 	'versions' => array(
 		'master' => array(
-			'js' => array(
-				'//cdn.rawgit.com/yfix/bootstrap-form-helpers/master/dist/js/bootstrap-formhelpers.min.js',
-			),
+			'js' => '//cdn.rawgit.com/yfix/bootstrap-form-helpers/master/dist/js/bootstrap-formhelpers.min.js',
 			'css' => array(
 				'//cdn.rawgit.com/yfix/bootstrap-form-helpers/master/dist/css/bootstrap-formhelpers.min.css',
-				'
-					[class^="bfh-flag-"], [class*="bfh-flag-"] { display: inline-block; margin-right: 5px; }
-					[class^="bfh-flag-"]:empty, [class*="bfh-flag-"]:empty { width: 16px; }
-					.bfh-selectbox-options a { padding-left: 10px; }
-				',
+				$bfh_css_fixes,
 			),
 		),
 		'2.3.0' => array(
-			'js' => array(
-				'//cdn.rawgit.com/yfix/bootstrap-form-helpers/2.3.0/dist/js/bootstrap-formhelpers.min.js',
-			),
+			'js' => '//cdn.rawgit.com/yfix/bootstrap-form-helpers/2.3.0/dist/js/bootstrap-formhelpers.min.js',
 			'css' => array(
 				'//cdn.rawgit.com/yfix/bootstrap-form-helpers/2.3.0/dist/css/bootstrap-formhelpers.min.css',
-				'
-					[class^="bfh-flag-"], [class*="bfh-flag-"] { display: inline-block; margin-right: 5px; }
-					[class^="bfh-flag-"]:empty, [class*="bfh-flag-"]:empty { width: 16px; }
-					.bfh-selectbox-options a { padding-left: 10px; }
-				',
+				$bfh_css_fixes,
 			),
 		),
 	),
 	'require' => array(
 		'asset' => array(
-#			'jquery',
+			'jquery',
 #			'bootstrap-theme',
 		),
 	),
@@ -42,3 +38,5 @@ return array(
 		'git' => 'https://github.com/yfix/bootstrap-form-helpers.git',
 	),
 );
+
+};
