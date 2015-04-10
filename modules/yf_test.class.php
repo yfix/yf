eb5e6d76ed8bf20b09152fd5edd0cc446d40d036
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-04-10 17:36:34 +0300

    cleanup

--- modules/yf_test.class.php
+++ modules/yf_test.class.php
@@ -37,9 +37,9 @@ class yf_test {
 			return;
 		}
 		if (is_post()) {
-#			$_SESSION['stpls_inline_edit']		= intval((bool)$_POST['stpl_edit']);
-			$_SESSION['locale_vars_edit']		= intval((bool)$_POST['locale_edit']);
 			$_SESSION['debug_console_light']	= intval((bool)$_POST['debug_console_light']);
+			$_SESSION['locale_vars_edit']		= intval((bool)$_POST['locale_edit']);
+#			$_SESSION['stpls_inline_edit']		= intval((bool)$_POST['stpl_edit']);
 			return js_redirect($_SERVER['HTTP_REFERER'], 0);
 		}
 		$a = $_POST + $_SESSION;
