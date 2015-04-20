c4abc3d0af334317c7ae7c9a8a8a5c6a3409d8b6
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-04-20 16:53:43 +0300

    asset added yf popover fix

---
+++ share/assets/yf_js_popover_fix.php
@@ -0,0 +1,27 @@
+<?php
+
+return array('versions' => array('master' => array(
+	// Ideas from here: http://stackoverflow.com/a/28731847/2521354
+	'jquery' => '
+			var originalLeave = $.fn.popover.Constructor.prototype.leave;
+			$.fn.popover.Constructor.prototype.leave = function(obj) {
+				var self = obj instanceof this.constructor ? obj : $(obj.currentTarget)[this.type](this.getDelegateOptions()).data("bs." + this.type);
+				originalLeave.call(this, obj);
+				if (obj.currentTarget) {
+					self.$tip.one("mouseenter", function() {
+						clearTimeout(self.timeout);
+						self.$tip.one("mouseleave", function() {
+							$.fn.popover.Constructor.prototype.leave.call(self, self);
+						});
+					})
+				}
+			};
+		'
+	)),
+	'require' => array(
+		'asset' => array(
+			'jquery',
+			'bootstrap-theme',
+		),
+	),
+);
