5fd3acb9310f31c7a63f35c7f8f69c210e11d13a
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-04-21 12:09:51 +0300

    asset added jquery sparklines

---
+++ share/assets/jquery-sparklines.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+	'versions' => array(
+		'master' => array(
+			'js' => '//cdnjs.cloudflare.com/ajax/libs/jquery-sparklines/2.1.2/jquery.sparkline.min.js',
+		),
+	),
+	'require' => array(
+		'asset' => 'jquery',
+	),
+);
