21c44ef7074d0628d0d1cafdcee02616a0372e50
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-04-27 17:58:52 +0300

    form ckeditor up

--- classes/form2/yf_form2_ckeditor.class.php
+++ classes/form2/yf_form2_ckeditor.class.php
@@ -44,37 +44,7 @@ class yf_form2_ckeditor {
 				$config_js = $params['config'];
 			}
 		} else {
-#			CKEDITOR.inline( _ck_content_id, ck_config)
-/*
-var ck_config = {
-	toolbarGroups: [
-	    { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-//		{ name: 'document', items: [ 'Source', '-', 'NewPage', 'Preview', '-', 'Templates' ] },
-//	    { name: 'source', items: ['Source'] },
-{ name: 'insert' },
-    	{ name: 'paragraph',   groups: [ 'list', 'indent', 'blocks', 'align' ] },
-		{ name: 'styles' },
-	],
-	filebrowserBrowseUrl: '/kcfinder/browse.php?type=files',
-	filebrowserImageBrowseUrl: '/kcfinder/browse.php?type=images',
-	filebrowserFlashBrowseUrl: '/kcfinder/browse.php?type=flash',
-	filebrowserUploadUrl: '/kcfinder/upload.php?type=files',
-	filebrowserImageUploadUrl: '/kcfinder/upload.php?type=images',
-	filebrowserFlashUploadUrl: '/kcfinder/upload.php?type=flash',
-}
-var _ck_content_id = '{content_id}';
-if (!_ck_content_id) {
-	_ck_content_id = 'content_editable';
-}
-try { 
-{if_ok(ckeditor_inline)}
-	CKEDITOR.inline( _ck_content_id, ck_config);
-{else}
-	CKEDITOR.replace( 'text', ck_config);
-{/if}
-} catch(e) { console.error("CKeditor init failed:", e) }
-*/
-#			$config_js = tpl()->_stpl_exists($stpl_name) ? tpl()->parse($stpl_name, (array)$extra + (array)$replace) : '';
+			$config_js = tpl()->_stpl_exists($stpl_name) ? tpl()->parse($stpl_name, (array)$extra + (array)$replace) : '';
 		}
 		if (strlen($config_js)) {
 			js($config_js);
