0f72a16f03baae60f17811f46358b1b8d78a56e2
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-04-29 18:38:24 +0300

    dynamic added method preview for static content from admin

--- modules/yf_dynamic.class.php
+++ modules/yf_dynamic.class.php
@@ -544,4 +544,45 @@ class yf_dynamic {
 		}
 		return '<img'._attrs($extra, array('src', 'type', 'class', 'id')).' />';
 	}
+
+	/**
+	*/
+	function preview($extra = array()) {
+		conf('ROBOTS_NO_INDEX', true);
+		no_graphics(true);
+		if (main()->USER_ID != 1) {
+			return print _403('You should be logged as user 1');
+		}
+		// Example of url: /dynamic/preview/static_pages/29/
+		$object = preg_replace('~[^a-z0-9_]+~ims', '', $_GET['id']);
+		$id = preg_replace('~[^a-z0-9_]+~ims', '', $_GET['page']);
+		if (!strlen($object)) {
+			return print _403('Object is required');
+		}
+		$ref = $_SERVER['HTTP_REFERER'];
+		$body = '';
+		if (main()->is_post() && isset($_POST['text'])) {
+			$u_ref	= parse_url($ref);
+			$u_self	= parse_url(WEB_PATH);
+			$u_adm	= parse_url(ADMIN_WEB_PATH);
+			if ($u_ref['host'] && $u_ref['host'] == $u_self['host'] && $u_ref['host'] == $u_adm['host'] && $u_ref['path'] == $u_adm['path']) {
+				$body = $_POST['text'];
+			} else {
+				return print _403('Preview security check not passed');
+			}
+		}
+		if (!$body) {
+			if ($object == 'static_pages') {
+				$body = db()->from($object)->whereid($id)->get_one('text');
+			} elseif ($object == 'tips') {
+				$body = db()->from($object)->whereid($id)->get_one('text');
+			} elseif ($object == 'faq') {
+				$body = db()->from($object)->whereid($id)->get_one('text');
+			} elseif ($object == 'news') {
+				$body = db()->from($object)->whereid($id)->get_one('full_text');
+			}
+		}
+		$body = '<div class="container">'.$body.'</div>';
+		return print common()->show_empty_page($body);
+	}
 }
