3b2cc4f164236fa79ec12e25614bc50adc14bf12
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-05-11 17:19:46 +0300

    sql php fix

--- plugins/news/share/db/sql/news.sql.php
+++ plugins/news/share/db/sql/news.sql.php
@@ -1,14 +1,14 @@
 <?php
 return '
-	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-	`title` varchar(255) NOT NULL,
-	`url` varchar(255) NOT NULL,
-	`head_text` text NOT NULL,
-	`full_text` text NOT NULL,
-	`meta_keywords` text NOT NULL,
-	`meta_desc` text NOT NULL,
-	`add_date` int(10) unsigned NOT NULL DEFAULT \'0\',
-	`active` enum(\'1\',\'0\') NOT NULL DEFAULT \'1\',
-	`locale` char(2) NOT NULL DEFAULT \'ru\',
-	PRIMARY KEY (`id`)
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `url` varchar(255) NOT NULL,
+  `head_text` text NOT NULL,
+  `full_text` text NOT NULL,
+  `meta_keywords` text NOT NULL,
+  `meta_desc` text NOT NULL,
+  `add_date` int(10) unsigned NOT NULL DEFAULT \'0\',
+  `active` enum(\'1\',\'0\') NOT NULL DEFAULT \'1\',
+  `locale` char(2) NOT NULL DEFAULT \'ru\',
+  PRIMARY KEY (`id`)
 ';
