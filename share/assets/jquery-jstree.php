5c8de249e9541bd656b186c172a1f76b01ce0cd7
Sergey Mushta <futukrishna@gmail.com>
2015-05-15 11:05:26 +0300

    jstree asset

---
+++ share/assets/jquery-jstree.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+	'versions' => array(
+		'master' => array(
+			'js' => '//cdn.rawgit.com/yfix/jstree/master/dist/jstree.min.js',
+			'css' => '//cdn.rawgit.com/yfix/jstree/master/dist/themes/default/style.min.css',
+		),
+	),
+	'require' => array(
+		'asset' => 'jquery',
+	),
+);
