463afbd64003631e0089327ba852625e919ddea3
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-06-09 13:42:40 +0300

    dir recursive bench up

--- .dev/scripts/bench/dir_recursive_bench.php
+++ .dev/scripts/bench/dir_recursive_bench.php
@@ -5,28 +5,26 @@ $argv[1] = '/home/www/test2/';
 require dirname(__DIR__).'/scripts_init.php';
 
 function rsearch($folder, $pattern) {
-    $dir = new RecursiveDirectoryIterator($folder);
-    $ite = new RecursiveIteratorIterator($dir);
-    $files = new RegexIterator($ite, $pattern, RegexIterator::GET_MATCH);
-    $fileList = array();
-    foreach($files as $file) {
-        $fileList[] = $file[0];
-    }
-    return $fileList;
+	$out = array();
+	$flags = FilesystemIterator::KEY_AS_PATHNAME | FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::FOLLOW_SYMLINKS;
+	foreach(new RegexIterator(new RecursiveIteratorIterator(new RecursiveDirectoryIterator($folder, $flags)), $pattern, RegexIterator::GET_MATCH) as $path => $f) {
+		$out[] = $path;
+	}
+	return $out;
 }
 function rglob($pattern, $flags = 0) {
-    $files = glob($pattern, $flags); 
-    foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
-        $files = array_merge($files, rglob($dir.'/'.basename($pattern), $flags));
-    }
-    return $files;
+	$files = glob($pattern, $flags); 
+	foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
+		$files = array_merge($files, rglob($dir.'/'.basename($pattern), $flags));
+	}
+	return $files;
 }
 function rglob2($pattern, $flags = 0) {
-    $files = iterator_to_array(new GlobIterator($pattern, $flags));
-    foreach (new GlobIterator(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
-        $files = array_merge($files, rglob($dir.'/'.basename($pattern), $flags));
-    }
-    return $files;
+	$files = iterator_to_array(new GlobIterator($pattern, $flags));
+	foreach (new GlobIterator(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
+		$files = array_merge($files, rglob($dir.'/'.basename($pattern), $flags));
+	}
+	return $files;
 }
 
 class bench {
@@ -40,7 +38,7 @@ class bench {
 			$start_time = microtime(true);
 			list($desc, $_files) = $this->$name();
 			$files[$name] = $_files;
-			$out[$name] = ++$i.') '.$desc.' | time: '.round(microtime(true) - $start_time, 3).' | mem: '.(memory_get_usage() - $start_mem).' | peakmem: '.memory_get_peak_usage();
+			$out[$name] = ++$i.') '.$desc.' | time: '.round(microtime(true) - $start_time, 3).' | mem: '.(memory_get_usage() - $start_mem).' | peakmem: '.memory_get_peak_usage().' | found: '.count($_files);
 		}
 		return print_r($files, 1). PHP_EOL. implode(PHP_EOL, $out). PHP_EOL;
 	}
@@ -49,8 +47,12 @@ class bench {
 		return array('_class("dir")->scan()', $files);
 	}
 	function directory_iterator() {
-		$files = rsearch(YF_PATH, '~gallery.*.php$~ims');
+		$files = rsearch(YF_PATH, '~gallery.*\.php$~ims');
 		return array('DirectoryIterator', $files);
+	}
+	function exec_find() {
+		$files = explode("\n", trim(shell_exec('find -L '.YF_PATH.' -iname '.'*gallery*.php')));
+		return array('exec(find ...)', $files);
 	}
 	function rglob() {
 		$files = rglob(YF_PATH.'*gallery*.php');
@@ -59,10 +61,6 @@ class bench {
 	function rglob2() {
 		$files = rglob2(YF_PATH.'*gallery*.php');
 		return array('rglob2()', $files);
-	}
-	function exec_find() {
-		$files = explode("\n", trim(shell_exec('find '.YF_PATH.' -name '.'*gallery*.php')));
-		return array('exec(find ...)', $files);
 	}
 }
 
