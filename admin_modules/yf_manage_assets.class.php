9e9868bc91f555a930bf24f28ac25b2d746e0fcb
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-06-11 11:57:50 +0300

    manage assets up

--- admin_modules/yf_manage_assets.class.php
+++ admin_modules/yf_manage_assets.class.php
@@ -3,12 +3,34 @@
 class yf_manage_assets {
 
 	/**
+	* Catch missing method call
 	*/
-	function show() {
-		return $this->search_used();
-#		return a('/@object/purge_cache', 'Purge cache');
+	function __call($name, $args) {
+		return main()->extend_call($this, $name, $args);
 	}
 
+	/**
+	*/
+	function _module_action_handler($method) {
+		$prepend = $this->_menu();
+		if (!$method || substr($method, 0, 1) === '_' || !method_exists($this, $method)) {
+			$method = 'show';
+		}
+		return $prepend. $this->$method(). $append;
+	}
+
+	/**
+	*/
+	function _menu() {
+		return html()->module_menu($this, array(
+			array('/@object/search_used/', 'Search used', 'fa fa-search'),
+			array('/@object/cache_purge/', 'Cache purge', 'fa fa-recycle'),
+			array('/@object/cache_fill/', 'Cache fill', 'fa fa-refresh'),
+			array('/@object/combine/', 'Combine', 'fa fa-rocket'),
+			array('/@object/upload/', 'Upload', 'fa fa-upload'),
+			array('/@object/settings/', 'Settings', 'fa fa-wrench'),
+		));
+	}
 	/**
 	*/
 	function search_used() {
@@ -69,4 +91,41 @@ class yf_manage_assets {
 		return '<pre style="color:white;background:black;line-height:1em;font-weight:bold;"><small>'._prepare_html(var_export(array_keys($assets), 1)).'</small></pre>'
 			.'<h3>Used assets</h3>'.html()->simple_table($table);
 	}
+
+	/**
+	*/
+	function show() {
+		return redirect('/@object/search_used');
+	}
+
+	/**
+	*/
+	function cache_purge() {
+// TODO
+	}
+
+	/**
+	*/
+	function cache_fill() {
+// TODO
+	}
+
+	/**
+	*/
+	function combine() {
+// TODO
+	}
+
+	/**
+	*/
+	function upload() {
+// TODO
+	}
+
+	/**
+	*/
+	function settings() {
+// TODO
+	}
+
 }
