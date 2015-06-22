d75d4558bd5d392904540625449f38f4d1ab36fd
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-06-22 23:21:36 +0300

    dev cron runner up

--- .dev/scripts/cron_runner.php
+++ .dev/scripts/cron_runner.php
@@ -1,7 +1,7 @@
 <?php
+
 $PROJECT_PATH = dirname(dirname(dirname(__DIR__))).'/scripts/scripts_init.php';
 require_once $PROJECT_PATH;
-
 
 if (!empty($argv[1])) {
     $time_start = microtime(true);
@@ -11,17 +11,17 @@ if (!empty($argv[1])) {
     ));
 	$id = db()->insert_id();
 
-	if($argv[2] == "include_php"){
+	if ($argv[2] == "include_php") {
 	    //run cron script body
 		$cmd = "php -r 'require(\"".$PROJECT_PATH."\"); \$func=include(\"".$argv[1]."\"); echo \$func();'";
 		exec($cmd, $log);
 		$log = implode("\n", $log);
 	}
-	if($argv[2] == "custom_php_script"){
+	if ($argv[2] == "custom_php_script") {
 		exec("php ".$argv[1], $log);
 		$log = implode("\n", $log);
 	}
-	if($argv[2] == "sh"){
+	if ($argv[2] == "sh") {
 		exec($argv[1], $log);
 		$log = implode("\n", $log);
 	}
@@ -33,23 +33,22 @@ if (!empty($argv[1])) {
         'time_spent'=> round($time_diff, 2), 
     ), 'id ='.intval($id));
     exit();
-
-}else{
+} else {
 	$crons = db()->get_all("SELECT * FROM ".db('cron_tasks')." WHERE active='1'");
-	foreach($crons as $cron_info){
+	foreach ($crons as $cron_info) \{
 		$file = $cron_info['dir'].$cron_info['name'];
 		$last_start_time = db()->get_one("SELECT time_start FROM ".db('cron_logs'). " WHERE cron_id = ".$cron_info['id']." ORDER BY time_start DESC");
 		exec('pgrep -a php | grep '.$file, $is_already_run);
-		if(empty($last_start_time) || (time() > strtotime("+".$cron_info['frequency'], $last_start_time))){
+		if (empty($last_start_time) || (time() > strtotime("+".$cron_info['frequency'], $last_start_time))) {
 			$timer = true;
-		}else{ 
+		} else { 
 			$timer = false;
 		}
 		// kill if exectime is over
-		if($is_already_run && (time() > strtotime("+".$cron_info['exec_time']." seconds", $last_start_time))){
+		if ($is_already_run && (time() > strtotime("+".$cron_info['exec_time']." seconds", $last_start_time))) {
 			exec('pkill -f '.$file);
 		}	
-		if(file_exists($file) && $timer && empty($is_already_run)) {
+		if (file_exists($file) && $timer && empty($is_already_run)) {
   			$cmd = 'php '.__FILE__.' '.$file.' '.$cron_info['exec_type'].' '.$cron_info['id'].' &';
 		    echo $cmd.PHP_EOL;
    			exec($cmd);
