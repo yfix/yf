6db438e5e8c7fec2721c9cd7856eb769077a7600
LArV <larv.job@gmail.com>
2015-07-14 13:36:57 +0300

    add: font opensans

--- share/assets/font-roboto.php
+++ share/assets/font-roboto.php
@@ -3,7 +3,7 @@
 return array(
 	'versions' => array(
 		'master' => array(
-			'css' => '//fonts.googleapis.com/css?family=Roboto+Condensed:300italic,400italic,700italic,400,700,300&subset=cyrillic-ext,latin-ext',
+			'css' => '//fonts.googleapis.com/css?family=Roboto+Condensed:300italic,400italic,700italic,300,400,700&subset=cyrillic-ext,latin-ext',
 		),
 	),
 );
