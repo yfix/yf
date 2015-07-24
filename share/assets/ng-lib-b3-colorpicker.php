57bd2692e8320b84a6319af35d2cdbcd97631263
LArV <larv.job@gmail.com>
2015-07-24 15:23:54 +0300

    add: angular bootstrap3 colorpicker

---
+++ share/assets/ng-lib-b3-colorpicker.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+	'versions' => array(
+		'v3.0.18' => array(
+			'css' => '//cdn.rawgit.com/buberdds/angular-bootstrap-colorpicker/v3.0.18/css/colorpicker.css',
+			'js'  => '//cdn.rawgit.com/buberdds/angular-bootstrap-colorpicker/v3.0.18/js/bootstrap-colorpicker-module.js',
+		),
+	),
+	'require' => array(
+		'asset' => array(
+			'jquery',
+			'angularjs',
+			'bootstrap3',
+		),
+	),
+);
