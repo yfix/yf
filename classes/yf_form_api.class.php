db1ad12da01de5ed290b1d274b8ca604fb6e5cda
LArV <larv.job@gmail.com>
2015-09-08 16:07:53 +0300

    up: manage payment transfer create, user select box

---
+++ classes/yf_form_api.class.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+* Form api
+*
+* @package		YF
+* @author		YFix Team <yfix.dev@gmail.com>
+* @version		1.0
+*/
+class yf_form_api {
+
+	public $URL = '/api/form_api/';
+
+	function user_select_box(&$name, &$values, &$extra, &$replace) {
+		if( !@$extra[ 'ajax' ][ 'url' ] ) {
+			$extra[ 'ajax' ][ 'url' ] = url( $this->URL . __FUNCTION__ );
+		}
+	}
+
+	function _api_user_select_box( $options = null ) {
+		$result = array();
+		// prepare query
+		$db = db()->table( 'user' );
+		// limit
+		$page_per = 10;
+		$page = (int)$_GET[ 'page' ]; $page = $page < 1 ? 1: $page;
+		$offset = ( $page - 1 ) * $page_per;
+		$db->limit( $page_per, $offset );
+		// q
+		$q = @$_GET[ 'q' ];
+		$q_int = (int)$q;
+		$q_is_int = $q_int > 0;
+		// order
+		if( $q_is_int ) {
+			$db->order_by( 'id' );
+		} else {
+			$db->order_by( 'name' );
+		}
+		// prepare text fields
+		$fields_text = array( 'name', 'login', 'email', 'first_name', 'last_name', 'nick', 'phone' );
+		foreach( $fields_text as $item ) {
+			$db->where_or( $item, 'like', _es( $q ). '%' );
+		}
+		// prepare int fields
+		$fields_int = array( 'id' );
+		if( $q_is_int ) {
+			foreach( $fields_int as $item ) {
+				$db->where_or( $item, '=', $q_int );
+			}
+		}
+		// prepare select fields
+		$fields = array_merge( $fields_int, $fields_text );
+		$db->select( implode( ',', $fields ) );
+		// DEBUG
+		// $data = $db->sql(); var_dump( $data ); exit;
+		// get db data
+		$data = $db->get_all();
+		// more?
+		$more = count( $data ) == $page_per;
+		// DEBUG
+		// var_dump( $data ); exit;
+		// empty
+		if( !$data ) { return( $result ); }
+		// prepare data
+		$fields_json = array( 'login', 'name', 'email' );
+		foreach( $data as $idx => $item ) {
+			$fields = array();
+			foreach( $fields_json as $field ) {
+				$f = &$item[ $field ];
+				if( !empty( $f ) ) {
+					$fields[ $f ] = $f;
+				}
+			}
+			$fields = empty( $fields ) ? '': implode( '; ', $fields );
+			$result[] = array(
+				'id' => $item[ 'id' ],
+				'text' => sprintf( '%u: %s', $item[ 'id' ], $fields ),
+			);
+		}
+		$result = array(
+			'more'  => $more,
+			'items' => $result
+		);
+		// etc
+		$api = _class( 'api' );
+		$api->JSON_VULNERABILITY_PROTECTION = false;
+		return( $result );
+	}
+
+}
--- plugins/payment/admin_modules/yf_manage_transfer.class.php
+++ plugins/payment/admin_modules/yf_manage_transfer.class.php
@@ -352,7 +352,6 @@ class yf_manage_transfer {
 	function create() {
 		@$replace = array() + $_POST;
 		$_this = $this;
-		$user_search_url = $this->_url( 'api_user_search' );
 		$result = form( $replace, array( 'autocomplete' => 'off' ) )
 			->validate(array(
 				'from[user_id]' => 'trim|required|numeric|greater_than[0]|exists[user.id]',
@@ -400,76 +399,6 @@ class yf_manage_transfer {
 				->submit( 'operation', 'transfer_and_continue', array( 'desc' => 'Перечислить и продолжить' ) )
 			->row_end()
 		;
-		return( $result );
-	}
-
-	function _api_user_search( $options = null ) {
-		$result = array();
-		// prepare query
-		$db = db()->table( 'user' );
-		// limit
-		$page_per = 10;
-		$page = (int)$_GET[ 'page' ]; $page = $page < 1 ? 1: $page;
-		$offset = ( $page - 1 ) * $page_per;
-		$db->limit( $page_per, $offset );
-		// q
-		$q = @$_GET[ 'q' ];
-		$q_int = (int)$q;
-		$q_is_int = $q_int > 0;
-		// order
-		if( $q_is_int ) {
-			$db->order_by( 'id' );
-		} else {
-			$db->order_by( 'name' );
-		}
-		// prepare text fields
-		$fields_text = array( 'name', 'login', 'email', 'first_name', 'last_name', 'nick', 'phone' );
-		foreach( $fields_text as $item ) {
-			$db->where_or( $item, 'like', _es( $q ). '%' );
-		}
-		// prepare int fields
-		$fields_int = array( 'id' );
-		if( $q_is_int ) {
-			foreach( $fields_int as $item ) {
-				$db->where_or( $item, '=', $q_int );
-			}
-		}
-		// prepare select fields
-		$fields = array_merge( $fields_int, $fields_text );
-		$db->select( implode( ',', $fields ) );
-		// DEBUG
-		// $data = $db->sql(); var_dump( $data ); exit;
-		// get db data
-		$data = $db->get_all();
-		// more?
-		$more = count( $data ) == $page_per;
-		// DEBUG
-		// var_dump( $data ); exit;
-		// empty
-		if( !$data ) { return( $result ); }
-		// prepare data
-		$fields_json = array( 'login', 'name', 'email' );
-		foreach( $data as $idx => $item ) {
-			$fields = array();
-			foreach( $fields_json as $field ) {
-				$f = &$item[ $field ];
-				if( !empty( $f ) ) {
-					$fields[ $f ] = $f;
-				}
-			}
-			$fields = empty( $fields ) ? '': implode( '; ', $fields );
-			$result[] = array(
-				'id' => $item[ 'id' ],
-				'text' => sprintf( '%u: %s', $item[ 'id' ], $fields ),
-			);
-		}
-		$result = array(
-			'more'  => $more,
-			'items' => $result
-		);
-		// etc
-		$api = _class( 'api' );
-		$api->JSON_VULNERABILITY_PROTECTION = false;
 		return( $result );
 	}
 
