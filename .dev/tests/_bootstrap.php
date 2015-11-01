4df68963da8000fa1c3c243b1c8c707897fd9305
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-11-01 17:27:28 +0200

    unit tests reduced error reporting level

--- .dev/tests/_bootstrap.php
+++ .dev/tests/_bootstrap.php
@@ -1,4 +1,7 @@
 <?php
+
+error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_STRICT);
+
 // This is global bootstrap for autoloading 
 exec('../sample_app/run_server_in_bg.sh');
 
--- .dev/tests/unit/_bootstrap.php
+++ .dev/tests/unit/_bootstrap.php
@@ -1,2 +1,3 @@
 <?php
 // Here you can initialize variables that will be available to your tests
+error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_STRICT);
