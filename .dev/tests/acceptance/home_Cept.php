1bc8e7028da4216a3a9de73f95da2efeb3a085e8
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-11-02 16:39:23 +0200

    tests acceptance improved

--- .dev/tests/acceptance/home_Cept.php
+++ .dev/tests/acceptance/home_Cept.php
@@ -4,11 +4,3 @@ $I = new AcceptanceTester($scenario);
 $I->wantTo('ensure that frontpage works');
 $I->amOnPage('/');
 $I->see('Home');
-
-$I->wantTo('ensure that html5 bootstrap2 works');
-$I->amOnPage('/test/html5fw_bs2');
-$I->see('text');
-
-$I->wantTo('ensure that html5 bootstrap3 works');
-$I->amOnPage('/test/html5fw_bs3');
-$I->see('text');
