404595d401c1896dda76bb2a14fb3c14bd3d6052
LArV <larv.job@gmail.com>
2015-12-01 16:09:30 +0200

    update

--- plugins/payment/modules/yf_payment_test.class.php
+++ plugins/payment/modules/yf_payment_test.class.php
@@ -1036,7 +1036,7 @@ EOS;
 
 	public function _api_js_cors() {
 		header('Access-Control-Allow-Origin: *');
-		header('Access-Control-Allow-Credentials: false');
+		header('Access-Control-Allow-Credentials: true');
 		header('Access-Control-Allow-Methods: OPTIONS, HEAD, GET, POST, PUT, PATCH, DELETE');
 		header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Content-Range, Content-Disposition');
 		return( array(
