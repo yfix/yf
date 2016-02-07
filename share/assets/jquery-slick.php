87768ab6b6b3235c29717cfe38aecf3b2906a68e
Sergey Mushta <futukrishna@gmail.com>
2016-02-07 15:20:01 +0200

    jquery-slick asset added (external cdn)

---
+++ share/assets/jquery-slick.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+	'versions' => array(
+		'master' => array(
+			'js' => '//cdn.jsdelivr.net/jquery.slick/1.5.9/slick.min.js',
+			'css' => '//cdn.jsdelivr.net/jquery.slick/1.5.9/slick.css',
+		),
+	),
+	'require' => array(
+		'asset' => 'jquery',
+	),
+);
