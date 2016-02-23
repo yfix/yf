f30dff2c03aec0378c97e50505db78d1bee36dbc
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2016-02-23 14:24:42 +0200

    service predis up

--- share/services/predis.php
+++ share/services/predis.php
@@ -7,11 +7,12 @@ $config = array(
 	'example' => function() {
 		$client = new Predis\Client(array(
 		    'scheme' => 'tcp',
-		    'host'   => '127.0.0.1',
-    		'port'   => 6379,
+		    'host'   => getenv('REDIS_HOST') ?: '127.0.0.1',
+    		'port'   => getenv('REDIS_PORT') ?: 6379,
 		));
-		$client->set('foo', 'bar');
-		$value = $client->get('foo');
+		$client->set('testme_key', 'testme_val');
+		$value = $client->get('testme_key');
+		print($value == 'testme_val' ? 'OK' : 'ERROR'). PHP_EOL;
 	}
 );
 if ($return_config) { return $config; } require_once __DIR__.'/_yf_autoloader.php'; new yf_autoloader($config);
