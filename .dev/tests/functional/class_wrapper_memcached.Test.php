a8b51ad4e5328ff0c8204b0be071e003d92d8a0f
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2016-03-10 15:23:44 +0200

    tests functional added for wrapper memcached

---
+++ .dev/tests/functional/class_wrapper_memcached.Test.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once __DIR__.'/db_real_abstract.php';
+
+class class_wrapper_redis_test extends yf_unit_tests {
+	protected function setUp() {
+		if (!extension_loaded('memcache') && !extension_loaded('memcached')) {
+			$this->markTestSkipped('PHP extension required: memcache or memcached.');
+			return ;
+    	}
+		parent::setUp();
+	}
+	public function test_redis() {
+		$mc = memcached();
+		$this->assertInternalType('object', $mc);
+		$this->assertSame($mc, _class('wrapper_memcached'));
+		$mc->connect();
+		$this->assertTrue($mc->is_ready());
+		$key = 'mytestkey';
+		$val = 'mytestval';
+		if (!empty($mc->get($key))) {
+			$this->assertEquals($mc->del($key), 1);
+		}
+		$this->assertEmpty($mc->get($key));
+		$this->assertTrue($mc->set($key, $val));
+		$this->assertEquals($mc->get($key), $val);
+		$this->assertEquals($mc->del($key), 1);
+		$this->assertEmpty($mc->get($key));
+	}
+}
