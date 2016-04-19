ad7bafc79c8d6d8db65c836c5261ca94025df45c
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2016-04-19 16:53:54 +0300

    asset added jquery-checkboxes

---
+++ share/assets/jquery-checkboxes.php
@@ -0,0 +1,25 @@
+<?php
+
+return array(
+	'versions' => array(
+		'master' => array(
+			'js' => '//cdn.rawgit.com/rmariuzzo/checkboxes.js/master/src/jquery.checkboxes.js',
+		),
+	),
+	'require' => array(
+		'asset' => 'jquery',
+	),
+	'info' => array(
+		'url' => 'http://rmariuzzo.github.io/checkboxes.js',
+		'name' => 'A jQuery plugin that gives you nice powers over your checkboxes.',
+		'desc' => 'Features
+			Check all checkboxes in context.
+			Uncheck all checkboxes in context.
+			Toggle states of all checkboxes in context.
+			Enable range selection.
+			Limit the number of checked checkbox per context.
+			Data API like Twitter Bootstrap.
+		',
+		'git' => 'https://github.com/rmariuzzo/checkboxes.js',
+	),
+);
