d5261c100879ae698c5b698512f35c921581c456
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2016-04-21 14:35:43 +0300

    asset bootstrap3 updated to version 3.3.6

--- share/assets/bootstrap3.php
+++ share/assets/bootstrap3.php
@@ -4,12 +4,12 @@ return function() {
 
 return array(
 	'versions' => array(
-		'3.3.2' => array(
+		'3.3.6' => array(
 			'css' => array(
-				'//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css',
-				! conf('bs3_no_default_theme') ? '//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css' : '',
+				'//netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css',
+				! conf('bs3_no_default_theme') ? '//netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css' : '',
 			),
-			'js' => '//netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js',
+			'js' => '//netdna.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js',
 		),
 	),
 	'require' => array(
