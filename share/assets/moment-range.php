c45e0e4ed94d0c8b75250be05305a3d59f90b424
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2016-05-12 17:51:22 +0300

    assets momentjs and its plugins up

---
+++ share/assets/moment-range.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+	'versions' => array(
+		'2.2.0' => array(
+			'js' => '//cdnjs.cloudflare.com/ajax/libs/moment-range/2.2.0/moment-range.min.js',
+		),
+	),
+	'require' => array(
+		'asset' => 'momentjs',
+	),
+	'info' => array(
+		'url' => 'https://github.com/gf3/moment-range',
+		'name' => 'Fancy date ranges for Moment.js',
+		'desc' => 'MomentJS plugin for fancy date ranges. http://gf3.github.io/moment-range/DateRange.html',
+		'git' => 'https://github.com/gf3/moment-range.git',
+	),
+);
