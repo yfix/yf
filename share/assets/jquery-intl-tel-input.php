8f8a6f640088cd75ffdc43c3113e105ab8cbccf8
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2016-05-13 17:40:37 +0300

    asset added jquery intl tel input

---
+++ share/assets/jquery-intl-tel-input.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+	'versions' => [
+		'8.5.2' => [
+			'js' => [
+				'//cdnjs.cloudflare.com/ajax/libs/intl-tel-input/8.5.2/js/intlTelInput.min.js',
+				'//cdnjs.cloudflare.com/ajax/libs/intl-tel-input/8.5.2/js/utils.js',
+			],
+			'css' => [
+				'//cdnjs.cloudflare.com/ajax/libs/intl-tel-input/8.5.2/css/intlTelInput.css',
+				'//cdnjs.cloudflare.com/ajax/libs/intl-tel-input/8.5.2/img/flags.png',
+			],
+		],
+	],
+	'require' => [
+		'asset' => ['jquery'],
+	],
+];
