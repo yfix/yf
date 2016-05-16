aed4c79ce5f45faa9468ed468ddb748a26fdde6b
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2016-05-16 17:21:06 +0300

    asset yf validation simple compatibility with formvalidation

--- share/assets/yf_validation_simple.php
+++ share/assets/yf_validation_simple.php
@@ -20,7 +20,7 @@ return array(
 ',
 <<<END
 $(function(){
-	$("input[data-ajax-validate]", "form").on("blur", function(i){
+	$("input[data-ajax-validate]", "form").not("[data-fv-field]").on("blur", function(i){
 		var _this = $(this);
 		var _val = _this.val();
 		if (!_val.length) {
@@ -115,8 +115,11 @@ $(function(){
 	}
 
 	// HTML5 custom validation messages
-	$("input, textarea", "form, select").not("[type=submit]").not("[type=image]")/*.not("[data-ajax-validate]")*/.on("change keyup invalid valid", function() {
+	$("input, textarea", "form, select").not("[type=submit]").not("[type=image]").on("change keyup invalid valid", function() {
 		var _this = $(this)
+		if (_this.attr('data-fv-field')) {
+			return ;
+		}
 		var textfield = _this.get(0);
 		var control_group = _this.closest(".control-group");
 		var controls = _this.closest(".controls");
