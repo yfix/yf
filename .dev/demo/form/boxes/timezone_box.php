497ca7886339845c77a02bbee8a111fe681683a1
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2016-05-17 14:02:14 +0300

    timezone demo up

--- .dev/demo/form/boxes/timezone_box.php
+++ .dev/demo/form/boxes/timezone_box.php
@@ -3,6 +3,6 @@
 return function() {
 	return form()
 		->timezone_box(['selected' => 'UTC'])
-		->timezone_box(['selected' => 'UTC', 'row_tpl' => '%name %code %offset', 'renderer' => 'div_box'])
+		->timezone_box(['selected' => 'UTC', 'row_tpl' => '%name %offset', 'renderer' => 'div_box'])
 	;
 };
