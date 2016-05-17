2484ec2fb9f3ea4e7fc75c14b600ee77ff0a849e
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2016-05-17 15:40:16 +0300

    demo form stpl up

--- .dev/demo/form/stpl.php
+++ .dev/demo/form/stpl.php
@@ -4,12 +4,21 @@ return function() {
 	$stpl = '
 		{form.begin}
 
-		{form.timezone}	<br> {form.country}
-		<br>
-		<br>
-		{form.currency} <br> {form.language}
-
 		{form._token}
+
+		<div class="pull-left col-md-3">
+			{form.currency}
+			{form.language}
+		</div>
+		<div class="pull-center col-md-3">
+			{form.country}
+		</div>
+		<div class="pull-center col-md-3">
+			<h1>Some header</h1>
+		</div>
+		<div class="pull-right col-md-3">
+			{form.timezone}
+		</div>
 
 		{form.end}
 	';
