d8ccd4777ad4199dd37fd06ee57d9a57562f21cc
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2016-05-18 16:52:45 +0300

    assets aded bootstrap3-dialog

---
+++ share/assets/bootstrap3-dialog.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+	'versions' => array(
+		'master' => array(
+			'js' => '//cdn.rawgit.com/yfix/bootstrap3-dialog/master/dist/js/bootstrap-dialog.min.js',
+			'css' => '//cdn.rawgit.com/yfix/bootstrap3-dialog/master/dist/css/bootstrap-dialog.min.css',
+		),
+	),
+	'require' => array(
+		'asset' => array(
+			'jquery',
+		),
+	),
+);
