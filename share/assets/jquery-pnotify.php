c92cf06a707ff3e6b22abb18f68043167aea6c2e
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2016-05-23 18:09:39 +0300

    asset jquery pnotify up

--- share/assets/jquery-pnotify.php
+++ share/assets/jquery-pnotify.php
@@ -9,6 +9,10 @@ return array(
 				'//cdn.rawgit.com/yfix/jquery-pnotify/1.2.0/jquery.pnotify.default.icons.css',
 			),
 		),
+		'3.0.0' => array(
+			'js' => '//cdnjs.cloudflare.com/ajax/libs/pnotify/3.0.0/pnotify.min.js',
+			'css' => '//cdnjs.cloudflare.com/ajax/libs/pnotify/3.0.0/pnotify.min.css',
+		),
 	),
 	'require' => array(
 		'asset' => 'jquery',
