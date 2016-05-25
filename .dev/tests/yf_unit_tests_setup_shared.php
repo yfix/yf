3927ba41550b461826edb24688adf22bfd479581
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2016-05-25 10:40:34 +0300

    unit tests enabled db connection by default

--- .dev/tests/yf_unit_tests_setup_shared.php
+++ .dev/tests/yf_unit_tests_setup_shared.php
@@ -11,7 +11,7 @@ if (!function_exists('main')) {
 	define('YF_IN_UNIT_TESTS', true);
 	define('YF_PATH', dirname(dirname(__DIR__)).'/');
 	require YF_PATH.'classes/yf_main.class.php';
-	new yf_main($MAIN_TYPE ?: 'user', $no_db_connect = 1, $auto_init_all = 0, $CONF);
+	new yf_main($MAIN_TYPE ?: 'user', $no_db_connect = 0, $auto_init_all = 0, $CONF);
 	date_default_timezone_set('Europe/Kiev');
 	ini_set('display_errors', 'on');
 	error_reporting(E_ALL ^ E_NOTICE ^ E_DEPRECATED ^ E_STRICT);
