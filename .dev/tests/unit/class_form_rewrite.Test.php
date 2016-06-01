aa6e53017a4f515a350131879a598b37d211f7bd
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2016-06-01 15:43:10 +0300

    unit tests form rewrite

--- .dev/tests/unit/class_form_rewrite.Test.php
+++ .dev/tests/unit/class_form_rewrite.Test.php
@@ -13,6 +13,7 @@ class class_form_rewrite_test extends yf_unit_tests {
 		$_GET = array(
 			'object' => 'dynamic',
 			'action' => 'unit_test_form',
+			'id' => 'id/with/slashes',
 		);
 		self::$_bak_settings['REWRITE_MODE'] = $GLOBALS['PROJECT_CONF']['tpl']['REWRITE_MODE'];
 		$GLOBALS['PROJECT_CONF']['tpl']['REWRITE_MODE'] = true;
@@ -26,12 +27,12 @@ class class_form_rewrite_test extends yf_unit_tests {
 
 	public function test_rewrite_form_url() {
 		$this->assertEquals(
-			'<form method="post" action="http://'.$_SERVER['HTTP_HOST'].'/dynamic/unit_test_form" class="form-horizontal" name="form_action" autocomplete="1"><fieldset></fieldset></form>'
+			'<form method="post" action="http://'.$_SERVER['HTTP_HOST'].'/dynamic/unit_test_form/id%2Fwith%2Fslashes" class="form-horizontal" name="form_action" autocomplete="1"><fieldset></fieldset></form>'
 			, str_replace(PHP_EOL, '', trim(form()))
 		);
 		$GLOBALS['PROJECT_CONF']['tpl']['REWRITE_MODE'] = self::$_bak_settings['REWRITE_MODE'];
 		$this->assertEquals(
-			'<form method="post" action="http://'.$_SERVER['HTTP_HOST'].'/?object=dynamic&action=unit_test_form" class="form-horizontal" name="form_action" autocomplete="1"><fieldset></fieldset></form>'
+			'<form method="post" action="http://'.$_SERVER['HTTP_HOST'].'/?object=dynamic&action=unit_test_form&id=id%2Fwith%2Fslashes" class="form-horizontal" name="form_action" autocomplete="1"><fieldset></fieldset></form>'
 			, str_replace(PHP_EOL, '', trim(form()))
 		);
 
