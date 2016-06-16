c6f1f62a02112156f7b0dd7f6e3d42541c4e69ab
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2016-06-16 13:30:51 +0300

    service added paragonie_constant_time_encoding

---
+++ share/services/paragonie_constant_time_encoding.php
@@ -0,0 +1,12 @@
+#!/usr/bin/php
+<?php
+
+$config = array(
+	'git_urls' => ['https://github.com/paragonie/constant_time_encoding.git' => 'paragonie_constant_time_encoding/'],
+	'autoload_config' => ['paragonie_constant_time_encoding/src/' => 'ParagonIE\ConstantTime'],
+	'example' => function() {
+		$out = \ParagonIE\ConstantTime\Encoding::base32Encode(random_bytes(32));
+		var_dump($out);
+	}
+);
+if ($return_config) { return $config; } require_once __DIR__.'/_yf_autoloader.php'; new yf_autoloader($config);
