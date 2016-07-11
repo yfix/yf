23a23dc2c07d992e3a337d8ce8550cff4a14f128
LArV <larv.job@gmail.com>
2016-07-11 15:51:34 +0300

    up: interkassa payout api

--- plugins/payment/classes/yf_payment_api__provider_interkassa.class.php
+++ plugins/payment/classes/yf_payment_api__provider_interkassa.class.php
@@ -167,13 +167,13 @@ class yf_payment_api__provider_interkassa extends yf_payment_api__provider_remot
 		],
 		'payout' => [
 			// changed: 2016-07-01 16:07:07
-			'visa_p2p_privat_uah' => [
-			// 'visa_p2p_tasprivat_uah' => [
+			// 'visa_p2p_privat_uah' => [
+			'visa_p2p_tasprivat_uah' => [
 				'title' => 'Visa (Privat24, UAH)',
 				'icon'  => 'visa',
 				'request_option' => [
-					'paywayId' => '52e7f883e4ae1a2406000000', // visa_p2p_privat_uah
-					// 'paywayId' => '5763bad33d1eaf04218b456a', // visa_p2p_tasprivat_uah
+					// 'paywayId' => '52e7f883e4ae1a2406000000', // visa_p2p_privat_uah
+					'paywayId' => '5763bad33d1eaf04218b456a', // visa_p2p_tasprivat_uah
 					'calcKey'  => 'psPayeeAmount',
 				],
 				'amount' => [
@@ -287,13 +287,13 @@ class yf_payment_api__provider_interkassa extends yf_payment_api__provider_remot
 				],
 			],
 			// changed: 2016-07-01 16:07:07
-			'mastercard_p2p_privat_uah' => [
-			// 'mastercard_p2p_tasprivat_uah' => [
+			// 'mastercard_p2p_privat_uah' => [
+			'mastercard_p2p_tasprivat_uah' => [
 				'title'      => 'MasterCard (Privat24, UAH)',
 				'icon'       => 'mastercard',
 				'request_option'     => [
-					'paywayId' => '52efa902e4ae1a780e000001', // mastercard_p2p_privat_uah
-					// 'paywayId' => '5763bb6e3d1eaf4b218b4567', // mastercard_p2p_tasprivat_uah
+					// 'paywayId' => '52efa902e4ae1a780e000001', // mastercard_p2p_privat_uah
+					'paywayId' => '5763bb6e3d1eaf4b218b4567', // mastercard_p2p_tasprivat_uah
 					'calcKey'  => 'psPayeeAmount',
 				],
 				'amount' => [
