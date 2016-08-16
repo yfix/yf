a146de0066745f732df12644136d033f835a8531
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2016-08-16 17:51:28 +0300

    added service disque-php

---
+++ share/services/disque-php.php
@@ -0,0 +1,11 @@
+#!/usr/bin/php
+<?php
+
+$config = [
+	'git_urls' => ['https://github.com/yfix/disque-php.git' => 'disque-php/'],
+	'autoload_config' => ['disque-php/src/' => 'Disque'],
+	'example' => function() {
+		var_dump(class_exists('Disque\Connection\Credentials'));
+	}
+];
+if ($return_config) { return $config; } require_once __DIR__.'/_yf_autoloader.php'; new yf_autoloader($config);
