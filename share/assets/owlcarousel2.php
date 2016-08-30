06574729be4608e4e05f2bf2f91604b8684627cf
Zakhovalko Pavel <zakhovalko.pavel@gmail.com>
2016-08-30 17:49:07 +0300

    update owcarousel2 asset

--- share/assets/owlcarousel2.php
+++ share/assets/owlcarousel2.php
@@ -2,11 +2,11 @@
 
 return [
 	'versions' => [
-		'2.1.6' => [
-			'js' => '//rawgit.yfix.net/yfix/OwlCarousel2/2.1.6/dist/owl.carousel.min.js',
+		'2.0.0' => [
+			'js' => '//rawgit.yfix.net/yfix/OwlCarousel2/2.0.0/dist/owl.carousel.min.js',
 			'css' => [
-				'//rawgit.yfix.net/yfix/OwlCarousel2/2.1.6/dist/assets/owl.carousel.min.css',
-				'//rawgit.yfix.net/yfix/OwlCarousel2/2.1.6/dist/assets/owl.theme.default.min.css',
+				'//rawgit.yfix.net/yfix/OwlCarousel2/2.0.0/dist/assets/owl.carousel.min.css',
+				'//rawgit.yfix.net/yfix/OwlCarousel2/2.0.0/dist/assets/owl.theme.default.min.css',
 			],
 		],
 	],
