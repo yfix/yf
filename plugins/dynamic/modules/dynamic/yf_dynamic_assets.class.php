656bdebb951eb777d4a6e4f9591bbb96886b4ac1
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2016-09-06 15:46:38 +0300

    dynamic assets done

--- plugins/dynamic/modules/dynamic/yf_dynamic_assets.class.php
+++ plugins/dynamic/modules/dynamic/yf_dynamic_assets.class.php
@@ -22,13 +22,56 @@ class yf_dynamic_assets {
 	* Display dynamic|on-the-fly asset content (CSS|JS)
 	*/
 	function asset ($type = '') {
+		session_write_close();
 		no_graphics(true);
-		$name = preg_replace('~[^a-z0-9_-]+~ims', '', trim($_GET['id']));
-		$type = preg_replace('~[^a-z0-9_-]+~ims', '', trim($type ?: $_GET['page']));
+		$name = strtolower(preg_replace('~[^a-z0-9_-]+~ims', '', trim($_GET['id'])));
+		$type = strtolower(preg_replace('~[^a-z0-9_-]+~ims', '', trim($type ?: $_GET['page'])));
 		if (!strlen($name) || !strlen($type) || !in_array($type, ['css','js','jquery','ng'])) {
 			_404();
 			exit();
 		}
+		$class_assets = _class('assets');
+
+		$content_types = [
+			'js' => 'text/javascript',
+			'css' => 'text/css',
+		];
+		$content_types['jquery'] = $content_types['js'];
+		$content_types['ng'] = $content_types['js'];
+
+		if (in_array($type, ['css','js'])) {
+			$content = $class_assets->get_asset($name, $type);
+			foreach ($content as $v) {
+				$ctype = $class_assets->detect_content_type($type, $v);
+				$_out = '';
+				if ($ctype === 'inline') {
+					$_out = $v;
+				} elseif ($ctype === 'file') {
+					$_out = file_get_contents($v);
+				} elseif ($ctype === 'url') {
+					$_out = file_get_contents($v);
+				}
+				if (DEBUG_MODE) {
+					$out[] = '/* DEBUG: asset: '.$name.', type: '.$type.', ctype: '.$ctype.', length: '.strlen($_out).', src: '.$v.' */';
+				}
+				$out[] = $_out;
+			}
+		}
+		$out = implode(PHP_EOL.PHP_EOL, $out);
+
+		$now = time();
+		$max_age = 3600;
+		header('Content-Type: '.$content_types[$type]);
+		header('Content-Length: '.strlen($out));
+		header('Cache-Control: max-age=3600, must-revalidate');
+		header('Expires: '.gmdate('D, d M Y H:i:s \G\M\T', $now + $max_age));
+		header('Last-Modified: '.gmdate('D, d M Y H:i:s \G\M\T', $now));
+		header('X-Robots-Tag: noindex,nofollow,noarchive,nosnippet');
+		header_remove('Pragma');
+		header_remove('Set-Cookie');
+
+		print $out;
+		exit;
 	}
 
 	/**
