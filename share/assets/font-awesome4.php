993aa9181e518e3f2ab5d2d8c2415fb2752ce6d4
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2016-09-30 14:18:22 +0300

    assets font awesome latest

--- share/assets/font-awesome4.php
+++ share/assets/font-awesome4.php
@@ -5,5 +5,8 @@ return [
 		'4.3.0' => [
 			'css' => '//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css',
 		],
+		'4.6.3' => [
+			'css' => '//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css',
+		],
 	],
 ];
