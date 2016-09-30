17e16e57d8bf1431cf33ecf6c2c25c6e141e4191
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2016-09-30 13:24:16 +0300

    sample test added

---
+++ .dev/samples/url_arrays_rewrite.test.php
@@ -0,0 +1,5 @@
+<?php
+
+#return js_redirect('./?object=vendor&action=vendors');
+#return js_redirect('./?object=vendor&action=vendors&f[search_text]=dfgdfg&f[rating]=4.5-5.0&f[cat_price]=0.5-1.0&utf8=тест');
+#return js_redirect('/vendor/vendors?f[search_text]=dfgdfg&f[rating]=4.5-5.0&f[cat_price]=0.5-1.0&utf8=тест');
