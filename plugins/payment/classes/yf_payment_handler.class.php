dfd62a0152332eb828da252f3384013f0f2ea81d
LArV <larv.job@gmail.com>
2016-10-21 13:22:04 +0300

    add: api payment handler, balance refresh

---
+++ plugins/payment/classes/yf_payment_handler.class.php
@@ -0,0 +1,27 @@
+
+<?php
+
+class yf_payment_handler {
+
+	public function balance_refresh( $options = null ) {
+		// import options
+		is_array( $options ) && extract( $options, EXTR_PREFIX_ALL | EXTR_REFS, '' );
+		if( @$_user_id < 1 ) { return( null ); }
+		$user_id = (int)$_user_id;
+		$api         = _class( 'api'         );
+		$payment     = _class( 'payment'     );
+		$payment_api = _class( 'payment_api' );
+		// get account by user_id
+		list( $account_id,  $account  ) = $payment_api->get_account([ 'user_id' => $user_id ]);
+		if( empty( $account_id ) ) { return( null ); }
+		list( $currency_id, $currency ) = $payment_api->get_currency__by_id( $account );
+		$payment->t( $currency, 'currency' );
+		$result = [
+			'account'  => $account,
+			'currency' => $currency,
+		];
+		return( $result );
+	}
+
+}
+
