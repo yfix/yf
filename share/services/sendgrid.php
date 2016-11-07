eae7f90ee4fd1602e77c74b659d8ff1a122d6663
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2016-11-07 18:49:44 +0200

    mail sendgrid driver ready

---
+++ share/services/sendgrid.php
@@ -0,0 +1,12 @@
+#!/usr/bin/php
+<?php
+
+$config = [
+	'require_services' => ['sendgrid_phphttp_client'],
+	'git_urls' => ['https://github.com/sendgrid/sendgrid-php.git' => 'sendgrid/'],
+	'require_once' => ['sendgrid/lib/SendGrid.php', 'sendgrid/lib/helpers/mail/Mail.php'],
+	'example' => function() {
+		var_dump(class_exists('SendGrid\Email'));
+	}
+];
+if ($return_config) { return $config; } require_once __DIR__.'/_yf_autoloader.php'; new yf_autoloader($config);
