9fb6209697e434f1047d56769ad4d16b19cd65db
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2016-12-29 17:54:02 +0200

    admin manager improved

--- admin_modules/yf_admin.class.php
+++ admin_modules/yf_admin.class.php
@@ -13,6 +13,16 @@ class yf_admin {
 
 	/**
 	*/
+	function _init () {
+		$admin_id = main()->ADMIN_ID;
+		$this->display_func = function($a = [], $a2 = []) use ($admin_id) {
+			isset($a2['id']) && $a = $a2;
+			return !($a['id'] == $admin_id || $a['id'] == 1);
+		};
+	}
+
+	/**
+	*/
 	function _show_quick_filter () {
 		$filter = _class('admin_methods')->_get_filter();
 		foreach ((array)$filter as $k => $v) {
@@ -25,10 +35,6 @@ class yf_admin {
 	/**
 	*/
 	function show() {
-		$admin_id = main()->ADMIN_ID;
-		$func = function($row) use ($admin_id) {
-			return !($row['id'] == $admin_id);
-		};
 		$quick_filter = $this->_show_quick_filter();
 		return 
 			'<div class="col-md-12">' .
@@ -80,9 +86,9 @@ class yf_admin {
 				return $out ? '<small>'.implode('<br />'.PHP_EOL, $out).'</small>' : null;
 			}, ['desc' => 'Dates'])
 			->btn_edit(['no_ajax' => 1, 'btn_no_text' => 1])
-			->btn_delete(['display_func' => $func, 'btn_no_text' => 1])
-			->btn_active(['display_func' => $func, 'btn_no_text' => 1, 'short' => 1])
-			->btn('login as', url('/@object/login_as/%d'), ['display_func' => $func, 'no_ajax' => 1, 'icon' => 'fa fa-sign-in', 'btn_no_text' => 1, 'class_add' => 'btn-info'])
+			->btn_delete(['display_func' => $this->display_func, 'btn_no_text' => 1])
+			->btn_active(['display_func' => $this->display_func, 'btn_no_text' => 1, 'short' => 1])
+			->btn('login as', url('/@object/login_as/%d'), ['display_func' => $this->display_func, 'no_ajax' => 1, 'icon' => 'fa fa-sign-in', 'btn_no_text' => 1, 'class_add' => 'btn-info'])
 			->footer_add(['no_ajax' => 1, 'class_add' => 'btn-primary'])
 			->footer_link('Auth log', url('/log_admin_auth'))
 			->footer_link('Auth fails log', url('/log_admin_auth_fails'))
@@ -96,16 +102,15 @@ class yf_admin {
 		if (!$id) {
 			return _e('Wrong id');
 		}
-		$admin_id = main()->ADMIN_ID;
-		$func = function($row) use ($admin_id) {
-			return !($row['id'] == $admin_id);
-		};
 		$a = from(self::table)->whereid($id)->get();
 		$a['back_link'] = url('/@object');
 		$a['redirect_link'] = url('/@object');
 		$a['password'] = '';
+		$display = ($this->display_func)($a);
 		$a = (array)$_POST + $a;
-		return form($a, ['autocomplete' => 'off'])
+		return 
+			'<div class="col-md-6">'
+			. form($a, ['autocomplete' => 'off', 'show_alerts' => 1])
 			->validate([
 				'__before__'	=> 'trim',
 				'login'			=> 'required|alpha_dash_dots|is_unique_without[admin.login.'.$id.']',
@@ -113,10 +118,10 @@ class yf_admin {
 				'first_name'	=> 'required',
 				'last_name'		=> 'required',
 				'password'		=> 'password_update',
-				'group'			=> 'required|exists[admin_groups.id]',
+				'group'			=> $display ? 'required|exists[admin_groups.id]' : '',
 			])
-			->db_update_if_ok(self::table, ['login','email','first_name','last_name','go_after_login','password','group'], 'id='.$id)
-			->on_after_update(function() {
+			->db_update_if_ok(self::table, ['login','email','first_name','last_name','go_after_login','password'] + ($display ? ['group','active'] : []))
+			->on_after_update(function() use ($a) {
 				common()->admin_wall_add([t('admin account edited: %login', ['%login' => $_POST['login']]), $id]);
 			})
 			->login()
@@ -124,15 +129,12 @@ class yf_admin {
 			->password()
 			->text('first_name')
 			->text('last_name')
-			->select_box('group', main()->get_data('admin_groups'), ['selected' => $a['group']])
+			->select_box('group', main()->get_data('admin_groups'), ['selected' => $a['group'], 'display_func' => $this->display_func])
 			->text('go_after_login', 'Url after login')
-			->active_box()
-			->info_date('add_date','Added')
-			->row_start()
-				->save_and_back()
-				->link('log auth', url('/log_admin_auth/show_for_admin/'.$a['id']))
-				->link('login as', url('/@object/login_as/'.$a['id']), ['display_func' => $func])
-			->row_end()
+			->active_box('', ['display_func' => $this->display_func])
+			->save_and_back()
+			. '</div>'
+			. '<div class="col-md-6">'.$this->ajax($id).'</div>'
 		;
 	}
 
@@ -141,7 +143,7 @@ class yf_admin {
 	function add() {
 		$a = $_POST;
 		$a['redirect_link'] = url('/@object');
-		return form($a, ['autocomplete' => 'off'])
+		return form($a, ['autocomplete' => 'off', 'show_alerts' => 1])
 			->validate([
 				'__before__'	=> 'trim',
 				'login'			=> 'required|alpha_dash_dots|is_unique[admin.login]',
@@ -169,9 +171,180 @@ class yf_admin {
 
 	/**
 	*/
+	function ajax ($force_id = false) {
+		$id = (int)($force_id ?: $_GET['id']);
+		$admin = from(self::table)->whereid($id)->get();
+		if (!$id || !$admin) {
+			return _404();
+		}
+		$a = [
+			'auths'		=> (int)select('COUNT(*)')->from('sys_log_admin_auth')->group_by('admin_id')->where('admin_id', $id)->one(),
+			'visits'	=> (int)select('COUNT(*)')->from('sys_log_admin_exec')->group_by('admin_id')->where('admin_id', $id)->one(),
+			'last_visit'=> (int)select('MAX(`date`)')->from('sys_log_admin_exec')->group_by('admin_id')->where('admin_id', $id)->one(),
+			'revisions'	=> (int)select('COUNT(*)')->from('sys_revisions')->group_by('user_id')->where('user_id', $id)->one(),
+			'wall'		=> (int)select('COUNT(*)')->from('admin_walls')->group_by('user_id')->where('user_id', $id)->one(),
+		];
+		$info = [];
+		$a['auths'] ? $info[] = a('/log_admin_auth/show_for_admin/'.$id, t('Auths'), 'fa fa-sign-in', (int)$a['auths']) : '';
+		$a['visits'] ? $info[] = a('/log_admin_exec/show_for_admin/'.$id, t('Visits'), 'fa fa-eye', (int)$a['visits']) : '';
+		$a['revisions'] ? $info[] = a('/log_admin_revisions/show_for_admin/'.$id, t('Content revisions'), 'fa fa-file-text', (int)$a['revisions']) : '';
+		$a['wall'] ? $info[] = a('/log_admin_wall/show_for_admin/'.$id, t('Wall records'), 'fa fa-asterisk', (int)$a['wall']) : '';
+
+		$format = 'long';
+		$dates = [];
+		$admin['add_date']		&& $dates[] = html()->icon('fa fa-plus', t('Account add date'), _format_date($admin['add_date'], $format));
+		$admin['last_login']	&& $dates[] = html()->icon('fa fa-sign-in', t('Last sign in'), _format_date($admin['last_login'], $format));
+		$a['last_visit']		&& $dates[] = html()->icon('fa fa-eye', t('Last visit'), _format_date($a['last_visit'], $format));
+
+		$pair_active = str_replace('class="', 'disabled class="', main()->get_data('pair_active'));
+		$pair_yes_no = str_replace('class="', 'disabled class="', main()->get_data('pair_yes_no'));
+
+		$allowed = ($this->display_func)($admin);
+
+		$actions = [];
+		$actions[] = $pair_active[$admin['active']];
+		$actions[] = a('/@object/edit/'.$id, t('Edit'), 'fa fa-edit', '', 'no-popover');
+		$allowed && $actions[] = a('/@object/login_as/'.$id, t('Login as'), 'fa fa-sign-in', '', 'no-popover');
+
+		$visits_chart = '';
+		if (!is_ajax()) {
+			$daily_visits = $this->_get_admin_daily_visits($id);
+			if ($daily_visits) {
+				$visits_chart = _class('charts')->jquery_sparklines($daily_visits[$id]);
+			}
+		}
+		$auths_chart = '';
+		if (!is_ajax()) {
+			$daily_auths = $this->_get_admin_daily_auths($id);
+			if ($daily_auths) {
+				$auths_chart = _class('charts')->jquery_sparklines($daily_auths[$id]);
+			}
+		}
+
+		$online_admin_ids = (array)redis()->smembers('online_by_admin');
+		$online_last_time = (int)(redis()->hget('online_by_admin_last', $id) / 100);
+		$online_minutes_ago = floor((time() - $online_last_time) / 60);
+		$online_sockets = array_map('stripslashes', (array)redis()->smembers('socket_by_admin:'.$id));
+		$is_online = in_array($id, $online_admin_ids) && $online_minutes_ago <= 30 && $online_sockets;
+		if ($is_online) {
+			$online_info = '<small class="text-success"><b>'
+				.a('#', t('Last visit time').': '. date('Y-m-d H:i:s', $online_last_time), 'fa fa-clock-o', $online_minutes_ago.' '.t('minutes ago'), 'btn-success').' '
+				.a('#', t('Websockets'), 'fa fa-link', count($online_sockets), '')
+			.'</b></small>';
+		}
+
+		$gid = $admin['group'];
+		$admin_groups = main()->get_data('admin_groups');
+		$group_html = a('/admin_groups/edit/'.$gid, $admin_groups[$gid], 'fa fa-users').
+			a('/@object/filter_save/clear/?filter=group:'.$gid, '', 'fa fa-filter', '');
+
+		$data = [
+			'is_online'	=> $is_online ? $online_info : '',
+			'id'		=> $admin['id']. '&nbsp;'. implode(PHP_EOL, $actions),
+			'login'		=> _prepare_html($admin['login']),
+			'name'		=> _prepare_html($admin['first_name'].'&nbsp;'.$admin['last_name']),
+			'email'		=> _prepare_html($admin['email']),
+			'group'		=> $group_html,
+			'info'		=> implode(PHP_EOL, $info),
+			'dates'		=> $dates ? '<small>'.implode(PHP_EOL.'<br />', $dates).'</small>' : '',
+			'visits'	=> $visits_chart,
+			'auths'		=> $auths_chart,
+		];
+		foreach ((array)$data as $k => $v) {
+			if (!strlen($v)) {
+				unset($data[$k]);
+			}
+		}
+		$body = html()->simple_table($data, [
+			'condensed' => 1,
+			'val' => [
+				'func' => function($in) { return $in ?: false; },
+			],
+		]);
+		if ($force_id) {
+			return $body;
+		}
+		print is_ajax() ? $body : common()->show_empty_page($body);
+	}
+
+	/**
+	*/
+	function _get_admin_daily_visits($admin_ids = []) {
+		$days = 60;
+		$min_time = time() - $days * 86400;
+		$sql = select('admin_id', 'FROM_UNIXTIME(`date`, "%Y-%m-%d") AS d', 'COUNT(*) AS c')
+			->from('sys_log_admin_exec')
+			->whereid($admin_ids, 'admin_id')
+			->where('`date`', '>', $min_time)
+			->group_by('admin_id', 'FROM_UNIXTIME(`date`, "%Y-%m-%d")');
+		return $this->_get_admin_daily_info($sql, $days, $admin_ids);
+	}
+
+	/**
+	*/
+	function _get_admin_daily_auths($admin_ids = []) {
+		$days = 60;
+		$min_time = time() - $days * 86400;
+		$sql = select('admin_id', 'FROM_UNIXTIME(`date`, "%Y-%m-%d") AS d', 'COUNT(*) AS c')
+			->from('sys_log_admin_auth')
+			->whereid($admin_ids, 'admin_id')->where('`date`', '>', $min_time)
+			->group_by('admin_id', 'FROM_UNIXTIME(`date`, "%Y-%m-%d")');
+		return $this->_get_admin_daily_info($sql, $days, $admin_ids);
+	}
+
+	/**
+	*/
+	function _get_admin_daily_info($sql, $days = 60, $admin_ids = []) {
+		if (!$admin_ids) {
+			return false;
+		}
+		if (!is_array($admin_ids)) {
+			$admin_ids = [$admin_ids];
+		}
+		$time = time();
+		$days = $days ?: 60;
+		$data = [];
+		foreach ((array)$sql->all() as $a) {
+			$data[$a['admin_id']][$a['d']] = $a['c'];
+		}
+		if (!$data) {
+			return false;
+		}
+		$dates = [];
+		foreach (range($days, 0) as $days_ago) {
+			$date = date('Y-m-d', $time - $days_ago * 86400);
+			$dates[$date] = $days_ago;
+		}
+		$out = [];
+		foreach ((array)$data as $admin_id => $admin_dates) {
+			$out[$admin_id] = [];
+			$_data = null;
+			foreach ($dates as $date => $days_ago) {
+				$_data = $admin_dates[$date];
+				// Trim empty values from left side
+				if (!$out[$admin_id] && !$_data) {
+					continue;
+				}
+				$out[$admin_id][$date] = $_data;
+			}
+			// Trim values from the right side too
+			foreach (array_reverse($out[$admin_id], $preserve_keys = true) as $k => $v) {
+				if ($v) {
+					break;
+				}
+				unset($out[$admin_id][$k]);
+			}
+		}
+		return $out;
+	}
+
+	/**
+	*/
 	function delete() {
 		$id = (int)$_GET['id'];
-		if ($id && $id != 1 && $id != main()->ADMIN_ID) {
+		$id && $a = from(self::table)->whereid($id)->get();
+		$allowed = ($this->display_func)($a);
+		if ($a['id'] && $allowed) {
 			db()->delete(self::table, $id);
 			common()->admin_wall_add(['admin account deleted', $id]);
 		}
@@ -186,11 +359,10 @@ class yf_admin {
 	/**
 	*/
 	function active () {
-		$id = intval($_GET['id']);
-		if (!empty($id)) {
-			$a = from(self::table)->whereid($id)->get();
-		}
-		if (!empty($a['id']) && $id != 1 && $id != main()->ADMIN_ID) {
+		$id = (int)$_GET['id'];
+		$id && $a = from(self::table)->whereid($id)->get();
+		$allowed = ($this->display_func)($a);
+		if ($a['id'] && $allowed) {
 			db()->update_safe(self::table, ['active' => (int)!$a['active']], $id);
 			common()->admin_wall_add(['admin account '.($a['active'] ? 'inactivated' : 'activated'), $id]);
 		}
@@ -234,11 +406,13 @@ class yf_admin {
 		return form($r, [
 				'filter' => true,
 			])
-			->login('login', ['class' => 'input-medium'])
-			->email('email', ['class' => 'input-medium'])
-			->select_box('group', main()->get_data('admin_groups'))
-			->select_box('order_by', $order_fields, ['show_text' => 1])
-			->order_box()
+			->login('login', ['class' => 'input-medium', 'no_label' => 1])
+			->email('email', ['class' => 'input-medium', 'no_label' => 1])
+			->select_box('group', main()->get_data('admin_groups'), ['no_label' => 1, 'show_text' => 1])
+			->row_start()
+				->select_box('order_by', $order_fields, ['show_text' => '= '.t('Order by').' =', 'desc' => t('Order by')])
+				->select_box('order_direction', ['asc' => '⇑', 'desc' => '⇓'])
+			->row_end()
 			->save_and_clear();
 		;
 	}
