bb090083750d4746d2fb6fba60e886453260a68e
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2017-02-09 12:31:44 +0200

    demo docs added simple jsonp

--- plugins/docs/modules/yf_docs.class.php
+++ plugins/docs/modules/yf_docs.class.php
@@ -42,6 +42,23 @@ class yf_docs {
 	}
 
 	/***/
+	public function jsonp() {
+		$callback = (string)$_GET['callback'];
+		if (!$callback) {
+			$callback = 'callback';
+		}
+		$base_dir = YF_PATH. '.dev/demo/data/';
+		$filename = basename($_GET['filename']);
+		if (preg_match('/^[a-zA-Z\-\.]+\.json$/', $filename)) {
+			$json = file_get_contents($base_dir. $filename);
+		}
+		no_graphics(true);
+		header('Content-Type: text/javascript');
+		echo "$callback($json);";
+		exit;
+	}
+
+	/***/
 	public function show() {
 		$methods = [];
 		foreach (get_class_methods($this) as $m) {
