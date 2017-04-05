d5911ba739af95d3b24b6512a16bd64cf926d7aa
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2017-04-05 17:37:36 +0300

    asset jquery-ajax-queue up

--- share/assets/jquery-ajax-queue.php
+++ share/assets/jquery-ajax-queue.php
@@ -1,57 +1,58 @@
 <?php
 
+// from: http://stackoverflow.com/questions/3034874/sequencing-ajax-requests/3035268#3035268
+// and fixes from: https://github.com/adrian-fernandez/jquery-ajaxQueue/blob/master/src/jQuery.ajaxQueue.js
 return ['versions' => ['master' => ['js' => '
-/*
-* jQuery.ajaxQueue - A queue for ajax requests
-* 
-* (c) 2011 Corey Frang
-* Dual licensed under the MIT and GPL licenses.
-*
-* Requires jQuery 1.5+
-*/ 
 (function($) {
 
 // jQuery on an empty object, we are going to use this as our Queue
 var ajaxQueue = $({ });
 
 $.ajaxQueue = function( ajaxOpts ) {
-    var jqXHR,
-        dfd = $.Deferred(),
-        promise = dfd.promise();
+	var jqXHR,
+		dfd = $.Deferred(),
+		promise = dfd.promise();
 
-    // queue our ajax request
-    ajaxQueue.queue( doRequest );
+	//  If there is no ajax request return an empty 200 code
+	if (typeof ajaxOpts == "undefined"){
+		return $.Deferred(function() {
+				this.resolve(["", "200", jqXHR]); 
+			}).promise();
+	}
 
-    // add the abort method
-    promise.abort = function( statusText ) {
+	// run the actual query
+	function doRequest( next ) {
+		jqXHR = $.ajax( ajaxOpts );
+		jqXHR.done( dfd.resolve )
+			.fail( dfd.reject )
+			.then( next, next );
+	}
 
-        // proxy abort to the jqXHR if it is active
-        if ( jqXHR ) {
-            return jqXHR.abort( statusText );
-        }
+	// queue our ajax request
+	ajaxQueue.queue( doRequest );
 
-        // if there wasn"t already a jqXHR we need to remove from queue
-        var queue = ajaxQueue.queue(),
-            index = $.inArray( doRequest, queue );
+	// add the abort method
+	promise.abort = function( statusText ) {
 
-        if ( index > -1 ) {
-            queue.splice( index, 1 );
-        }
+		// proxy abort to the jqXHR if it is active
+		if ( jqXHR ) {
+			return jqXHR.abort( statusText );
+		}
 
-        // and then reject the deferred
-        dfd.rejectWith( ajaxOpts.context || ajaxOpts, [ promise, statusText, "" ] );
-        return promise;
-    };
+		// if there wasn"t already a jqXHR we need to remove from queue
+		var queue = ajaxQueue.queue(),
+			index = $.inArray( doRequest, queue );
 
-    // run the actual query
-    function doRequest( next ) {
-        jqXHR = $.ajax( ajaxOpts )
-            .done( dfd.resolve )
-            .fail( dfd.reject )
-            .then( next, next );
-    }
+		if ( index > -1 ) {
+			queue.splice( index, 1 );
+		}
 
-    return promise;
+		// and then reject the deferred
+		dfd.rejectWith( ajaxOpts.context || ajaxOpts, [ promise, statusText, "" ] );
+		return promise;
+	};
+
+	return promise;
 };
 
 })(jQuery);
