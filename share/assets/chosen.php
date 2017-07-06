bd57e60515e1d8fde94310ffce8cdef041311cfa
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2017-07-06 13:39:33 +0300

    asset chosen up, added chosen-order

--- share/assets/chosen.php
+++ share/assets/chosen.php
@@ -2,9 +2,9 @@
 
 return [
 	'versions' => [
-		'1.5.1' => [
-			'js' => '//cdnjs.cloudflare.com/ajax/libs/chosen/1.5.1/chosen.jquery.min.js',
-			'css' => '//cdnjs.cloudflare.com/ajax/libs/chosen/1.5.1/chosen.min.css',
+		'1.7.0' => [
+			'js' => '//cdnjs.cloudflare.com/ajax/libs/chosen/1.7.0/chosen.jquery.min.js',
+			'css' => '//cdnjs.cloudflare.com/ajax/libs/chosen/1.7.0/chosen.min.css',
 		],
 	],
 	'require' => [
