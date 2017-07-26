5b32bcc511f7040509941c7b5fe086469223daec
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2017-07-26 14:09:07 +0300

    chosen added cdn cache

--- share/assets/chosen.php
+++ share/assets/chosen.php
@@ -1,6 +1,12 @@
 <?php
 
 return [
+	'cdn' => [
+		'url' => '//cdnjs.cloudflare.com/ajax/libs/{name}/{version}/',
+		'version' => '1.7.0',
+		'js' => 'chosen.jquery.min.js',
+		'css' => 'chosen.min.css',
+	],
 	'versions' => [
 		'1.7.0' => [
 			'js' => '//cdnjs.cloudflare.com/ajax/libs/chosen/1.7.0/chosen.jquery.min.js',
