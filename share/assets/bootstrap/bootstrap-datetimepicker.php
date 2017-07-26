fcdad10a094e20b0af10a8e350050a3491dc9b5d
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2017-07-26 13:55:45 +0300

    assets added caches

--- share/assets/bootstrap/bootstrap-datetimepicker.php
+++ share/assets/bootstrap/bootstrap-datetimepicker.php
@@ -1,24 +1,22 @@
 <?php
 
 return [
+	'bower' => [
+		'name' => 'yfix/bootstrap-datetimepicker',
+		'version' => '4.17.37',
+		'js' => 'build/js/bootstrap-datetimepicker.min.js',
+		'css' => 'build/css/bootstrap-datetimepicker.min.css',
+	],
+	'github' => [
+		'name' => 'yfix/bootstrap-datetimepicker',
+		'version' => '4.17.37',
+		'js' => 'build/js/bootstrap-datetimepicker.min.js',
+		'css' => 'build/css/bootstrap-datetimepicker.min.css',
+	],
 	'versions' => [
-		'v4.0.0' => [
-			'js' => [
-				'//rawgit.yfix.net/yfix/bootstrap-datetimepicker/v4.0.0/build/js/bootstrap-datetimepicker.min.js',
-			],
-			'css' => [
-				'//rawgit.yfix.net/yfix/bootstrap-datetimepicker/v4.0.0/build/css/bootstrap-datetimepicker.min.css',
-				'.bootstrap-datetimepicker-widget .picker-switch { width: 90%; }',
-			],
-		],
 		'4.17.37' => [
-			'js' => [
-				'//rawgit.yfix.net/yfix/bootstrap-datetimepicker/4.17.37/build/js/bootstrap-datetimepicker.min.js',
-			],
-			'css' => [
-				'//rawgit.yfix.net/yfix/bootstrap-datetimepicker/4.17.37/build/css/bootstrap-datetimepicker.min.css',
-				'.bootstrap-datetimepicker-widget .picker-switch { width: 90%; }',
-			],
+			'js' => '//rawgit.yfix.net/yfix/bootstrap-datetimepicker/4.17.37/build/js/bootstrap-datetimepicker.min.js',
+			'css' => '//rawgit.yfix.net/yfix/bootstrap-datetimepicker/4.17.37/build/css/bootstrap-datetimepicker.min.css',
 		],
 	],
 	'require' => [
@@ -26,5 +24,8 @@ return [
 			'jquery',
 			'momentjs',
 		],
+	],
+	'add' => [
+		'css' => '.bootstrap-datetimepicker-widget .picker-switch { width: 90%; }',
 	],
 ];
--- share/assets/parallax.php
+++ share/assets/parallax.php
@@ -1,9 +1,23 @@
 <?php
 
 return [
+	'bower' => [
+		'name' => 'yfix/parallax',
+		'version' => 'v2.1.3',
+		'js' => [
+			'deploy/parallax.min.js',
+		],
+	],
+	'github' => [
+		'name' => 'yfix/parallax',
+		'version' => 'v2.1.3',
+		'js' => [
+			'deploy/parallax.min.js',
+		],
+	],
 	'versions' => [
-		'master' => [
-			'js' => '//rawgit.yfix.net/yfix/parallax/master/deploy/parallax.min.js',
+		'v2.1.3' => [
+			'js' => '//rawgit.yfix.net/yfix/parallax/v2.1.3/deploy/parallax.min.js',
 		],
 	],
 ];
