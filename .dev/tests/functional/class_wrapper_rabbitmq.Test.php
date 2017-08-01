90387b06f389e751f9cb222846ed9f5617865265
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2017-08-01 17:10:08 +0300

    rabbitmq tests up

--- .dev/tests/functional/class_wrapper_rabbitmq.Test.php
+++ .dev/tests/functional/class_wrapper_rabbitmq.Test.php
@@ -6,23 +6,24 @@ require_once __DIR__.'/db_real_abstract.php';
  * @requires extension amqp
  */
 class class_wrapper_rabbitmq_test extends yf_unit_tests {
-	public function test_1() {
-		$r = rabbitmq();
+	public function test_ready_driver_pecl() {
+		$r = clone rabbitmq();
+		$r->driver = 'pecl';
+		$cnn = $r->connect();
 		$this->assertInternalType('object', $r);
-		$this->assertSame($r, _class('wrapper_rabbitmq'));
-		$r->connect();
+		$this->assertSame(get_class($r), get_class(_class('wrapper_rabbitmq')));
 		$this->assertTrue($r->is_ready());
-/*
-		$key = 'mytestkey';
-		$val = 'mytestval';
-		if ($redis->get($key)) {
-			$this->assertEquals($redis->del($key), 1);
-		}
-		$this->assertEmpty($redis->get($key));
-		$this->assertTrue($redis->set($key, $val));
-		$this->assertEquals($redis->get($key), $val);
-		$this->assertEquals($redis->del($key), 1);
-		$this->assertEmpty($redis->get($key));
-*/
+		$this->assertEquals($r->driver, 'pecl');
+		$this->assertSame(get_class($cnn), 'AMQPConnection');
+	}
+	public function test_ready_driver_amqplib() {
+		$r = clone rabbitmq();
+		$r->driver = 'amqplib';
+		$cnn = $r->connect();
+		$this->assertInternalType('object', $r);
+		$this->assertSame(get_class($r), get_class(_class('wrapper_rabbitmq')));
+		$this->assertTrue($r->is_ready());
+		$this->assertEquals($r->driver, 'amqplib');
+		$this->assertSame(get_class($cnn), 'PhpAmqpLib\Connection\AMQPConnection');
 	}
 }
