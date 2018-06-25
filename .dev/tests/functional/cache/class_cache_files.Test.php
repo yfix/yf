2026de063f8f3a2282f53cad11ca3c2ee963e21e
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2018-06-25 17:43:44 +0300

    tests for cache cleanup

--- .dev/tests/functional/cache/class_cache_files.Test.php
+++ .dev/tests/functional/cache/class_cache_files.Test.php
@@ -2,4 +2,5 @@
 
 require_once __DIR__.'/class_cache.Test.php';
 
-class class_cache_files_test extends yf\tests\wrapper { }
+class class_cache_files_test extends class_cache_test {
+}
--- .dev/tests/functional/cache/class_cache_tmp.Test.php
+++ .dev/tests/functional/cache/class_cache_tmp.Test.php
@@ -2,4 +2,5 @@
 
 require_once __DIR__.'/class_cache.Test.php';
 
-class class_cache_tmp_test extends yf\tests\wrapper { }
+class class_cache_tmp_test extends class_cache_test {
+}
