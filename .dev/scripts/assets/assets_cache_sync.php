992129e9b7b19cc40cc4db0d24d9ef166514d2ea
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2019-07-15 17:45:39 +0300

    assets cache up

--- .dev/scripts/assets/assets_cache_sync.php
+++ .dev/scripts/assets/assets_cache_sync.php
@@ -4,21 +4,22 @@
 $cache_dir = dirname(dirname(__DIR__)) . '/assets_cache/';
 
 // https://api.github.com/repos/thomaspark/bootswatch/tags
-$twbs_v4 = '4.0.0';
-$twbs_v3 = '3.3.7';
 $twbs_v2 = '2.3.2';
+$twbs_v3 = '3.4.1';
+$twbs_v4 = '4.3.1';
 $fa3 = '3.2.1';
 $fa4 = '4.7.0';
-$jquery_v = '3.3.1';
-$jquery_url = 'http://ajax.googleapis.com/ajax/libs/jquery/' . $jquery_v . '/jquery.min.js';
+$fa5 = '5.9.0';
+$jquery_v = '3.4.1';
+$jquery_ui_v = '1.12.1';
 
-$dir_twbs4 = $cache_dir . 'bootswatch/' . $twbs_v4 . '/';
-$dir_twbs3 = $cache_dir . 'bootswatch/' . $twbs_v3 . '/';
 $dir_twbs2 = $cache_dir . 'bootswatch/' . $twbs_v2 . '/';
+$dir_twbs3 = $cache_dir . 'bootswatch/' . $twbs_v3 . '/';
+$dir_twbs4 = $cache_dir . 'bootswatch/' . $twbs_v4 . '/';
 
-$themes_twbs4_file = $cache_dir . 'bootswatch/themes_twbs4.txt';
-$themes_twbs3_file = $cache_dir . 'bootswatch/themes_twbs3.txt';
 $themes_twbs2_file = $cache_dir . 'bootswatch/themes_twbs2.txt';
+$themes_twbs3_file = $cache_dir . 'bootswatch/themes_twbs3.txt';
+$themes_twbs4_file = $cache_dir . 'bootswatch/themes_twbs4.txt';
 
 function save_url_to_file($url, $file)
 {
@@ -38,7 +39,7 @@ function save_url_to_file($url, $file)
 
 function get_urls_from_css($css)
 {
-    preg_match_all('~url\(\'(?P<url>.*?)\'\)~ims', $css, $m);
+    preg_match_all('~url\([\']?(?P<url>.*?)[\']?\)~ims', $css, $m);
     $urls = [];
     foreach ((array) $m['url'] as $url) {
         if (substr($url, 0, strlen('../')) === '../') {
@@ -57,60 +58,33 @@ function get_urls_from_css($css)
 
 function url_get($url)
 {
+	echo $url. PHP_EOL;
     // github requires http user agent string
-    $opts = ['http' => ['method' => 'GET', 'header' => ['User-Agent: PHP']]];
+    $opts = ['http' => ['method' => 'GET', 'header' => ['User-Agent: PHP'], 'timeout' => 10]];
     return file_get_contents($url, false, stream_context_create($opts));
 }
 
-function get_themes_twbs4()
-{
-    global $themes_twbs4_file, $twbs_v4;
-    $gh_api_url = 'https://api.github.com/repos/thomaspark/bootswatch/contents/dist?ref=v' . $twbs_v4;
-    $themes = [];
-    foreach (json_decode(url_get($gh_api_url), $arr = true) as $v) {
-        $name = $v['name'];
-        if ($v['type'] !== 'dir') {
-            continue;
-        }
-        if ( ! $name || $name === 'default') {
-            continue;
-        }
-        $themes[$name] = $name;
-    }
-    if ($themes) {
-        file_put_contents($themes_twbs4_file, trim(implode(PHP_EOL, $themes)));
-    }
-    if ( ! file_exists($themes_twbs4_file) || ! filesize($themes_twbs4_file)) {
-        exit('ERROR: TWBS4 Themes not found');
-    }
-    return explode(PHP_EOL, trim(file_get_contents($themes_twbs4_file)));
+// Jquery
+save_url_to_file('https://ajax.googleapis.com/ajax/libs/jquery/' . $jquery_v . '/jquery.min.js', $cache_dir . 'jquery/' . $jquery_v . '/jquery.min.js');
+
+// Jquery UI
+save_url_to_file('https://ajax.googleapis.com/ajax/libs/jqueryui/' . $jquery_ui_v . '/jquery-ui.min.js', $cache_dir . 'jquery-ui/' . $jquery_ui_v . '/jquery-ui.min.js');
+
+// Font Awesome
+save_url_to_file('https://netdna.bootstrapcdn.com/font-awesome/' . $fa3 . '/css/font-awesome.min.css', $cache_dir . 'fontawesome/' . $fa3 . '/css/font-awesome.min.css');
+foreach (get_urls_from_css(file_get_contents($cache_dir . 'fontawesome/' . $fa3 . '/css/font-awesome.min.css')) as $url) {
+    save_url_to_file('https://netdna.bootstrapcdn.com/font-awesome/' . $fa3 . '/' . $url, $cache_dir . 'fontawesome/' . $fa3 . '/' . $url);
+}
+save_url_to_file('https://netdna.bootstrapcdn.com/font-awesome/' . $fa4 . '/css/font-awesome.min.css', $cache_dir . 'fontawesome/' . $fa4 . '/css/font-awesome.min.css');
+foreach (get_urls_from_css(file_get_contents($cache_dir . 'fontawesome/' . $fa4 . '/css/font-awesome.min.css')) as $url) {
+    save_url_to_file('https://netdna.bootstrapcdn.com/font-awesome/' . $fa4 . '/' . $url, $cache_dir . 'fontawesome/' . $fa4 . '/' . $url);
+}
+save_url_to_file('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/' . $fa5 . '/css/all.min.css', $cache_dir . 'fontawesome/' . $fa5 . '/css/all.min.css');
+foreach (get_urls_from_css(file_get_contents($cache_dir . 'fontawesome/' . $fa5 . '/css/all.min.css')) as $url) {
+    save_url_to_file('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/' . $fa5 . '/' . $url, $cache_dir . 'fontawesome/' . $fa5 . '/' . $url);
 }
 
-function get_themes_twbs3()
-{
-    global $themes_twbs3_file, $twbs_v3;
-
-    $gh_api_url = 'https://api.github.com/repos/thomaspark/bootswatch/contents/?ref=v' . $twbs_v3;
-    $themes = [];
-    foreach (json_decode(url_get($gh_api_url), $arr = true) as $v) {
-        $name = $v['name'];
-        if ($v['type'] !== 'dir') {
-            continue;
-        }
-        if (in_array($name, ['tests', 'help', 'global', 'fonts', 'default', 'custom', 'bower_components', 'assets', 'api', '2'])) {
-            continue;
-        }
-        $themes[$name] = $name;
-    }
-    if ($themes) {
-        file_put_contents($themes_twbs3_file, trim(implode(PHP_EOL, $themes)));
-    }
-    if ( ! file_exists($themes_twbs3_file) || ! filesize($themes_twbs3_file)) {
-        exit('ERROR: TWBS3 Themes not found');
-    }
-    return explode(PHP_EOL, trim(file_get_contents($themes_twbs3_file)));
-}
-
+// Bootstrap 2
 function get_themes_twbs2()
 {
     global $themes_twbs2_file, $twbs_v2;
@@ -119,7 +93,7 @@ function get_themes_twbs2()
     $themes = [];
     foreach (json_decode(url_get($gh_api_url), $arr = true) as $v) {
         $name = $v['name'];
-        if ($v['type'] !== 'dir') {
+        if ($v['type'] !== 'dir' || substr($name, 0, 1) === '.') {
             continue;
         }
         if (in_array($name, ['swatchmaker', 'js', 'img', 'global', 'font', 'default', 'css', 'api'])) {
@@ -135,49 +109,79 @@ function get_themes_twbs2()
     }
     return explode(PHP_EOL, trim(file_get_contents($themes_twbs2_file)));
 }
-
-// Bootstrap 4
-foreach ((array) get_themes_twbs4() as $theme) {
-    save_url_to_file(
-        'http://netdna.bootstrapcdn.com/bootswatch/' . $twbs_v4 . '/' . $theme . '/bootstrap.min.css',
-        $dir_twbs4 . '/' . $theme . '/bootstrap.min.css'
-    );
-}
-
-save_url_to_file('http://netdna.bootstrapcdn.com/bootstrap/' . $twbs_v4 . '/css/bootstrap.min.css', $dir_twbs4 . 'default/bootstrap.min.css');
-save_url_to_file('http://netdna.bootstrapcdn.com/bootstrap/' . $twbs_v4 . '/js/bootstrap.min.js', $dir_twbs4 . 'bootstrap.min.js');
-
-// Bootstrap 3
-foreach (get_themes_twbs3() as $theme) {
-    save_url_to_file(
-        'http://netdna.bootstrapcdn.com/bootswatch/' . $twbs_v3 . '/' . $theme . '/bootstrap.min.css',
-        $dir_twbs3 . '/' . $theme . '/bootstrap.min.css'
-    );
-}
-save_url_to_file('http://netdna.bootstrapcdn.com/bootstrap/' . $twbs_v3 . '/css/bootstrap.min.css', $dir_twbs3 . 'default/bootstrap.min.css');
-save_url_to_file('http://netdna.bootstrapcdn.com/bootstrap/' . $twbs_v3 . '/css/bootstrap-theme.min.css', $dir_twbs3 . 'default/bootstrap-theme.min.css');
-save_url_to_file('http://netdna.bootstrapcdn.com/bootstrap/' . $twbs_v3 . '/js/bootstrap.min.js', $dir_twbs3 . 'bootstrap.min.js');
-
-// Bootstrap 2
 foreach (get_themes_twbs2() as $theme) {
     save_url_to_file(
-        'http://netdna.bootstrapcdn.com/bootswatch/' . $twbs_v2 . '/' . $theme . '/bootstrap.min.css',
+        'https://netdna.bootstrapcdn.com/bootswatch/' . $twbs_v2 . '/' . $theme . '/bootstrap.min.css',
         $dir_twbs2 . '/' . $theme . '/bootstrap.min.css'
     );
 }
-save_url_to_file('http://netdna.bootstrapcdn.com/twitter-bootstrap/' . $twbs_v2 . '/css/bootstrap-combined.min.css', $dir_twbs2 . 'default/bootstrap-combined.min.css');
-save_url_to_file('http://netdna.bootstrapcdn.com/twitter-bootstrap/' . $twbs_v2 . '/js/bootstrap.min.js', $dir_twbs2 . 'bootstrap.min.js');
+save_url_to_file('https://netdna.bootstrapcdn.com/twitter-bootstrap/' . $twbs_v2 . '/css/bootstrap-combined.min.css', $dir_twbs2 . 'default/bootstrap-combined.min.css');
+save_url_to_file('https://netdna.bootstrapcdn.com/twitter-bootstrap/' . $twbs_v2 . '/js/bootstrap.min.js', $dir_twbs2 . 'bootstrap.min.js');
 
-// Jquery
-save_url_to_file($jquery_url, $cache_dir . 'jquery/' . $jquery_v . '/jquery.min.js');
-save_url_to_file('http://ajax.googleapis.com/ajax/libs/jqueryui/' . $jquery_v . '/jquery-ui.min.js', $cache_dir . 'jquery-ui/' . $jquery_v . '/jquery-ui.min.js');
+// Bootstrap 3
+function get_themes_twbs3()
+{
+    global $themes_twbs3_file, $twbs_v3;
 
-// Font Awesome
-save_url_to_file('http://netdna.bootstrapcdn.com/font-awesome/' . $fa3 . '/css/font-awesome.min.css', $cache_dir . 'fontawesome/' . $fa3 . '/css/font-awesome.min.css');
-foreach (get_urls_from_css(file_get_contents($cache_dir . 'fontawesome/' . $fa3 . '/css/font-awesome.min.css')) as $url) {
-    save_url_to_file('http://netdna.bootstrapcdn.com/font-awesome/' . $fa3 . '/' . $url, $cache_dir . 'fontawesome/' . $fa3 . '/' . $url);
+    $gh_api_url = 'https://api.github.com/repos/thomaspark/bootswatch/contents/?ref=v' . $twbs_v3;
+    $themes = [];
+    foreach (json_decode(url_get($gh_api_url), $arr = true) as $v) {
+        $name = $v['name'];
+        if ($v['type'] !== 'dir' || substr($name, 0, 1) === '.') {
+            continue;
+        }
+        if (in_array($name, ['tests', 'help', 'global', 'fonts', 'default', 'custom', 'bower_components', 'assets', 'api', '2'])) {
+            continue;
+        }
+        $themes[$name] = $name;
+    }
+    if ($themes) {
+        file_put_contents($themes_twbs3_file, trim(implode(PHP_EOL, $themes)));
+    }
+    if ( ! file_exists($themes_twbs3_file) || ! filesize($themes_twbs3_file)) {
+        exit('ERROR: TWBS3 Themes not found');
+    }
+    return explode(PHP_EOL, trim(file_get_contents($themes_twbs3_file)));
 }
-save_url_to_file('http://netdna.bootstrapcdn.com/font-awesome/' . $fa4 . '/css/font-awesome.min.css', $cache_dir . 'fontawesome/' . $fa4 . '/css/font-awesome.min.css');
-foreach (get_urls_from_css(file_get_contents($cache_dir . 'fontawesome/' . $fa4 . '/css/font-awesome.min.css')) as $url) {
-    save_url_to_file('http://netdna.bootstrapcdn.com/font-awesome/' . $fa4 . '/' . $url, $cache_dir . 'fontawesome/' . $fa4 . '/' . $url);
+foreach (get_themes_twbs3() as $theme) {
+    save_url_to_file(
+        'https://netdna.bootstrapcdn.com/bootswatch/' . $twbs_v3 . '/' . $theme . '/bootstrap.min.css',
+        $dir_twbs3 . '/' . $theme . '/bootstrap.min.css'
+    );
 }
+save_url_to_file('https://netdna.bootstrapcdn.com/bootstrap/' . $twbs_v3 . '/css/bootstrap.min.css', $dir_twbs3 . 'default/bootstrap.min.css');
+save_url_to_file('https://netdna.bootstrapcdn.com/bootstrap/' . $twbs_v3 . '/css/bootstrap-theme.min.css', $dir_twbs3 . 'default/bootstrap-theme.min.css');
+save_url_to_file('https://netdna.bootstrapcdn.com/bootstrap/' . $twbs_v3 . '/js/bootstrap.min.js', $dir_twbs3 . 'bootstrap.min.js');
+
+// Bootstrap 4
+function get_themes_twbs4()
+{
+    global $themes_twbs4_file, $twbs_v4;
+    $gh_api_url = 'https://api.github.com/repos/thomaspark/bootswatch/contents/dist?ref=v' . $twbs_v4;
+    $themes = [];
+    foreach (json_decode(url_get($gh_api_url), $arr = true) as $v) {
+        $name = $v['name'];
+        if ($v['type'] !== 'dir' || substr($name, 0, 1) === '.') {
+            continue;
+        }
+        if ( ! $name || $name === 'default') {
+            continue;
+        }
+        $themes[$name] = $name;
+    }
+    if ($themes) {
+        file_put_contents($themes_twbs4_file, trim(implode(PHP_EOL, $themes)));
+    }
+    if ( ! file_exists($themes_twbs4_file) || ! filesize($themes_twbs4_file)) {
+        exit('ERROR: TWBS4 Themes not found');
+    }
+    return explode(PHP_EOL, trim(file_get_contents($themes_twbs4_file)));
+}
+foreach ((array) get_themes_twbs4() as $theme) {
+    save_url_to_file(
+        'https://netdna.bootstrapcdn.com/bootswatch/' . $twbs_v4 . '/' . $theme . '/bootstrap.min.css',
+        $dir_twbs4 . '/' . $theme . '/bootstrap.min.css'
+    );
+}
+save_url_to_file('https://netdna.bootstrapcdn.com/bootstrap/' . $twbs_v4 . '/css/bootstrap.min.css', $dir_twbs4 . 'default/bootstrap.min.css');
+save_url_to_file('https://netdna.bootstrapcdn.com/bootstrap/' . $twbs_v4 . '/js/bootstrap.min.js', $dir_twbs4 . 'bootstrap.min.js');
