6afd6f00f4c6aa872d323fff9c8d23cdc8217140
LArV <larv.job@gmail.com>
2022-01-04 17:05:35 +0200

    add: htmx api

---
+++ plugins/htmx/classes/htmx.class.php
@@ -0,0 +1,82 @@
+<?php
+
+class htmx {
+
+  public $is_hx   = null;
+
+  public $target  = null;
+  public $boosted = null;
+
+  public $_api;
+
+  function _init() {
+    $this->_api = _class( 'api' );
+    $this->_api->JSON_VULNERABILITY_PROTECTION = false;
+    $this->_is_hx();
+    $this->target  = @$_SERVER[ 'HTTP_HX_TARGET'  ];
+    $this->boosted = @$_SERVER[ 'HTTP_HX_BOOSTED' ];
+  }
+
+  function _is_hx() {
+    $r = (bool) @$_SERVER[ 'HTTP_HX_REQUEST' ];
+    $this->is_hx = $r;
+    return( $r );
+  }
+
+  function _firewall() {
+    if( !$this->is_hx ) { $this->_reject(); }
+    return( $this->is_hx );
+  }
+
+  function _reject() {
+    no_graphics( true );
+    $this->_api->_reject();
+  }
+
+  function _forbidden() {
+    no_graphics( true );
+    $this->_api->_forbidden();
+  }
+
+  function _error() {
+    no_graphics( true );
+    $this->_api->_error();
+  }
+
+  function _redirect( $url ) {
+    no_graphics( true );
+    header( 'HX-Redirect: '. $url );
+    exit;
+  }
+
+  function _redirect_login() {
+    $this->_redirect( '/login/' );
+  }
+
+  function _refresh() {
+    no_graphics( true );
+    header( 'HX-Refresh: true' );
+    exit;
+  }
+
+  function _robot_none() {
+    return( $this->_api->_robot_none() );
+  }
+
+  function _cache( $options = null ) {
+    return( $this->_api->_cache( $options ) );
+  }
+
+  function _cache_none() {
+    return( $this->_api->_cache([ 'no-store' => true ]) );
+  }
+
+  function _result( $r ) {
+    no_graphics( true );
+    $this->_robot_none();
+    echo $r;
+    exit;
+  }
+
+}
+
