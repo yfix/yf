daf701818a1a06871bacba05f1e7cec4397e7c88
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2024-11-11 20:53:48 +0100

    php-cs-fixer

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,66 +1,43 @@
 <?php
-# Based on https://github.com/yiisoft/yii2/blob/master/cs/src/YiiConfig.php
-return PhpCsFixer\Config::create()
+
+return (new PhpCsFixer\Config())
+    ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
     ->setUsingCache(true)
     ->setRiskyAllowed(true)
     ->setRules([
         '@PSR2' => true,
-        'array_syntax' => [
-            'syntax' => 'short',
-        ],
-        'binary_operator_spaces' => [
-            'align_double_arrow' => false,
-            'align_equals' => false,
-        ],
+        'array_syntax' => ['syntax' => 'short'],
         'blank_line_after_opening_tag' => true,
+        'blank_lines_before_namespace' => true,
         'cast_spaces' => true,
-        'concat_space' => [
-            'spacing' => 'one',
-        ],
+        'concat_space' => ['spacing' => 'one'],
         'dir_constant' => true,
         'ereg_to_preg' => true,
-        'function_typehint_space' => true,
-        'hash_to_slash_comment' => true,
-        'include' => true,
         'heredoc_to_nowdoc' => true,
-        'is_null' => [
-            'use_yoda_style' => false,
-        ],
+        'include' => true,
         'linebreak_after_opening_tag' => true,
         'lowercase_cast' => true,
         'magic_constant_casing' => true,
         'modernize_types_casting' => true,
+        'multiline_whitespace_before_semicolons' => true,
         'native_function_casing' => true,
-        'new_with_braces' => true,
+        'new_with_parentheses' => true,
         'no_alias_functions' => true,
         'no_blank_lines_after_class_opening' => true,
         'no_blank_lines_after_phpdoc' => true,
         'no_empty_comment' => true,
         'no_empty_phpdoc' => true,
         'no_empty_statement' => true,
-        'no_extra_consecutive_blank_lines' => [
-            'tokens' => [
-                'break',
-                'continue',
-                'return',
-                'throw',
-                'use',
-                'use_trait',
-                'parenthesis_brace_block',
-                'square_brace_block',
-            ],
-        ],
+        'no_extra_blank_lines' => ['tokens' => ['break', 'continue', 'return', 'throw', 'use', 'parenthesis_brace_block', 'square_brace_block']],
         'no_leading_import_slash' => true,
         'no_leading_namespace_whitespace' => true,
         'no_mixed_echo_print' => true,
         'no_multiline_whitespace_around_double_arrow' => true,
-        'no_multiline_whitespace_before_semicolons' => true,
         'no_php4_constructor' => true,
         'no_short_bool_cast' => true,
         'no_singleline_whitespace_before_semicolons' => true,
         'no_spaces_around_offset' => true,
-        'no_trailing_comma_in_list_call' => true,
-        'no_trailing_comma_in_singleline_array' => true,
+        'no_trailing_comma_in_singleline' => true,
         'no_unneeded_control_parentheses' => true,
         'no_unused_imports' => true,
         'no_useless_else' => true,
@@ -71,38 +48,8 @@ return PhpCsFixer\Config::create()
         'normalize_index_brace' => true,
         'not_operator_with_space' => true,
         'object_operator_without_whitespace' => true,
-        'ordered_class_elements' => [
-            'order' => [
-                'use_trait',
-                'constant_public',
-                'constant_protected',
-                'constant_private',
-                'property_public_static',
-                'property_protected_static',
-                'property_private_static',
-                'property_public',
-                'property_protected',
-                'property_private',
-                'method_public_static',
-                'construct',
-                'destruct',
-                'magic',
-                'phpunit',
-                'method_public',
-                'method_protected',
-                'method_private',
-                'method_protected_static',
-                'method_private_static',
-            ],
-        ],
-        'ordered_imports' => [
-            'sortAlgorithm' => 'alpha',
-            'importsOrder' => [
-                'const',
-                'function',
-                'class',
-            ],
-        ],
+        'ordered_class_elements' => ['order' => ['use_trait', 'constant_public', 'constant_protected', 'constant_private', 'property_public_static', 'property_protected_static', 'property_private_static', 'property_public', 'property_protected', 'property_private', 'method_public_static', 'construct', 'destruct', 'magic', 'phpunit', 'method_public', 'method_protected', 'method_private', 'method_protected_static', 'method_private_static']],
+        'ordered_imports' => ['sort_algorithm' => 'alpha', 'imports_order' => ['const', 'function', 'class']],
         'php_unit_construct' => true,
         'php_unit_dedicate_assert' => true,
         'php_unit_fqcn_annotation' => true,
@@ -120,15 +67,16 @@ return PhpCsFixer\Config::create()
         'phpdoc_types' => true,
         'phpdoc_var_without_name' => true,
         'protected_to_private' => true,
-        'psr4' => true,
+        'psr_autoloading' => true,
         'self_accessor' => true,
         'short_scalar_cast' => true,
-        'single_blank_line_before_namespace' => true,
+        'single_line_comment_style' => true,
         'single_quote' => true,
         'standardize_not_equals' => true,
         'ternary_operator_spaces' => true,
-        'trailing_comma_in_multiline_array' => true,
+        'trailing_comma_in_multiline' => true,
         'trim_array_spaces' => true,
+        'type_declaration_spaces' => true,
         'unary_operator_spaces' => true,
         'whitespace_after_comma_in_array' => true,
     ])
@@ -139,5 +87,4 @@ return PhpCsFixer\Config::create()
             ->notPath('www')
             ->notPath('vendor')
             ->notPath('libs')
-        )
-;
+    );
--- Makefile
+++ Makefile
@@ -1,25 +1,38 @@
+SHELL  := /bin/bash
+PHP_CONTAINER := yfixnet-php80-1
+DIR_YF := /var/www/vendor/yf31
+DIR_YF_TESTS := $(DIR_YF)/.dev/tests/
+
 composer-install:
 	composer install -vvv
-
 composer-update:
 	composer update -vvv
-
 composer-install-prod:
 	composer install --no-dev -vvv
-
 composer-update-prod:
 	composer update --no-dev -vvv
-
 parallel-lint:
 	./vendor/bin/parallel-lint -e php --exclude vendor --exclude .dev .
-
 phpunit-tests:
 	(cd ./.dev/tests/ && ../../vendor/bin/phpunit ./)
-
 paratests:
 	(cd ./.dev/tests/ && ../../vendor/bin/paratest -p4 --colors --stop-on-failure --configuration ./phpunit.xml --log-junit ./reports/logfile.xml ./)
-
 php-cs-fixer:
-	./vendor/bin/php-cs-fixer --verbose --show-progress=estimating fix --verbose --config=.php_cs ./
-
+	./vendor/bin/php-cs-fixer --verbose --show-progress=dots check
 test: parallel-lint php-cs-fixer phpunit-tests
+
+php-bash: docker-bash
+docker-bash:
+	docker exec -it $(PHP_CONTAINER) bash
+docker-phpunit:
+	docker exec $(PHP_CONTAINER) bash -c 'cd $(DIR_YF_TESTS) && make phpunit-all'
+docker-phpunit-unit:
+	docker exec $(PHP_CONTAINER) bash -c 'cd $(DIR_YF_TESTS) && make phpunit-unit'
+docker-phpunit-functional:
+	docker exec $(PHP_CONTAINER) bash -c 'cd $(DIR_YF_TESTS) && make phpunit-functional'
+docker-parallel-lint:
+	docker exec $(PHP_CONTAINER) bash -c 'cd $(DIR_YF) && make parallel-lint'
+docker-php-cs-fixer:
+	docker exec $(PHP_CONTAINER) bash -c 'cd $(DIR_YF) && make php-cs-fixer'
+docker-test:
+	docker exec $(PHP_CONTAINER) bash -c 'cd $(DIR_YF) && make test'
