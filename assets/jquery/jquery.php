13d4e2bf3c2ca9c841a7bb4c4a0d4eeccc69477d
LArV <larv.job@gmail.com>
2025-03-10 14:58:01 +0200

    admin: user info up for bs 3.4.1

--- assets/jquery/jquery.php
+++ assets/jquery/jquery.php
@@ -3,28 +3,28 @@
 return [
     'bower' => [
         'name' => 'jquery',
-        'version' => '1.11.2',
+        'version' => '1.11.3',
         'js' => [
             'dist/jquery.min.js',
         ],
     ],
     'github' => [
         'name' => 'jquery/jquery',
-        'version' => '1.11.2',
+        'version' => '1.11.3',
         'js' => [
             'dist/jquery.min.js',
         ],
     ],
     'cdn' => [
         'url' => '//ajax.googleapis.com/ajax/libs/{name}/{version}/',
-        'version' => '1.11.2',
+        'version' => '1.11.3',
         'js' => [
             'jquery.min.js',
         ],
     ],
     'versions' => [
-        '1.11.2' => [
-            'js' => '//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js',
+        '1.11.3' => [
+            'js' => '//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js',
         ],
     ],
 ];
