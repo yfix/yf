1351b605724858be50a5457a632220682a4fcf62
ser.korzhikov <ser.korzhikov@gmail.com>
2025-03-19 11:41:39 +0200

    upd phpmailer to version 6.9.3

--- services/mail/phpmailer.php
+++ services/mail/phpmailer.php
@@ -2,7 +2,8 @@
 <?php
 
 $config = [
-    'git_urls' => ['https://github.com/yfix/phpmailer.git~v5.2.26' => 'phpmailer/'],
+//    'git_urls' => ['https://github.com/yfix/phpmailer.git~v5.2.26' => 'phpmailer/'],
+    'git_urls' => ['https://github.com/yfix/phpmailer.git~v6.9.3' => 'phpmailer/'],
     'require_once' => ['phpmailer/PHPMailerAutoload.php'],
     'example' => function () {
         $mail = new PHPMailer(true);
