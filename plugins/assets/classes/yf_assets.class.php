ff65220abac34e8cd4b77213e16948c5a7f741ed
LArV <larv.job@gmail.com>
2025-04-03 10:12:15 +0300

    up

--- plugins/assets/classes/yf_assets.class.php
+++ plugins/assets/classes/yf_assets.class.php
@@ -2557,6 +2557,7 @@ requirejs( [ "module1", "module2" ], function( angular ) {
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+        curl_setopt($ch, CURLOPT_USERAGENT, '');
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
         curl_setopt($ch, CURLOPT_TIMEOUT, $this->URL_TIMEOUT);
         $data = curl_exec($ch);
